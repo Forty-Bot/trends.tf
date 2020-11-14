643c5dc86a12214b13a826ad53932ae827521274
Sean Anderson <seanga2@gmail.com>
2020-11-14 10:12:10 -0500

    Set syncronous = NORMAL
    This should speed up commits without affecting integrity.
    
    Signed-off-by: Sean Anderson <seanga2@gmail.com>

--- sql.py
+++ sql.py
@@ -24,6 +24,7 @@ def db_connect(url):
 def db_init(c):
     with open("{}/schema.sql".format(os.path.dirname(__file__))) as schema:
         c.execute("PRAGMA journal_mode = WAL;")
+        c.execute("PRAGMA synchronous = NORMAL;")
         c.executescript(schema.read())
 
 def get_db():
