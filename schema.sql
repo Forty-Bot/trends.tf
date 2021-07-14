Forty-Bot/trends.tf

commit 1f0493354a7f1c121c4a161e187dd7a5797dee96

Split off some columns of player_stats
Most of the columns in player_stats were not referenced outside of the
totals page. To reduce the amount of data we have to load for each query,
move them into another table.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

schema.sql
@@ -110,23 +110,8 @@ CREATE TABLE IF NOT EXISTS player_stats (
 	kills INT NOT NULL,
 	assists INT NOT NULL,
 	deaths INT NOT NULL,
-	suicides INT,
 	dmg INT NOT NULL,
-	dmg_real INT, -- Damage dealt just before/after a kill, cap, or uber
 	dt INT,
-	dt_real INT,
-	hr INT, -- Heals Received
-	lks INT NOT NULL, -- Longest KillStreak
-	airshots INT, -- "as" in the json
-	medkits INT, -- Medkits taken (small: 1, medium: 2, large: 4)
-	medkits_hp INT, -- HP from medkits
-	backstabs INT,
-	headshots INT, -- headshot kills
-	headshots_hit INT, -- headshot non-kills
-	sentries INT, -- sentries built
-	healing INT NOT NULL,
-	cpc INT, -- Capture Point Captures
-	ic INT, -- Intel Captures
 	PRIMARY KEY (steamid64, logid),
 	CHECK ((dmg_real NOTNULL AND dt_real NOTNULL) OR (dmg_real ISNULL AND dt_real ISNULL))
 );
@@ -174,6 +159,29 @@ JOIN (SELECT
 	JOIN player_stats AS ps USING (logid)
 	GROUP BY ps.logid, steamid64
 ) AS round USING (logid);
+
+CREATE TABLE IF NOT EXISTS player_stats_extra (
+	logid INT NOT NULL,
+	steamid64 BIGINT NOT NULL,
+	suicides INT,
+	dmg_real INT, -- Damage dealt just before/after a kill, cap, or uber
+	dt_real INT,
+	hr INT, -- Heals Received
+	lks INT NOT NULL, -- Longest KillStreak
+	airshots INT, -- "as" in the json
+	medkits INT, -- Medkits taken (small: 1, medium: 2, large: 4)
+	medkits_hp INT, -- HP from medkits
+	backstabs INT,
+	headshots INT, -- headshot kills
+	headshots_hit INT, -- headshot non-kills
+	sentries INT, -- sentries built
+	healing INT NOT NULL,
+	cpc INT, -- Capture Point Captures
+	ic INT, -- Intel Captures
+	PRIMARY KEY (steamid64, logid),
+	FOREIGN KEY (logid, steamid64) REFERENCES player_stats (logid, steamid64),
+	CHECK ((dmg_real NOTNULL AND dt_real NOTNULL) OR (dmg_real ISNULL AND dt_real ISNULL))
+);
 
 CREATE TABLE IF NOT EXISTS medic_stats (
 	logid INT NOT NULL,

import.py
@@ -151,15 +151,19 @@ def import_log(c, logid, log):
 
         c.execute("INSERT INTO name (name) VALUES (%(name)s) ON CONFLICT DO NOTHING;", player)
         c.execute("""INSERT INTO player_stats (
-                         logid, steamid64, teamid, nameid, kills, assists, deaths, suicides, dmg,
-                         dmg_real, dt, dt_real, hr, lks, airshots, medkits, medkits_hp, backstabs,
-                         headshots, headshots_hit, sentries, healing, cpc, ic
+                         logid, steamid64, teamid, nameid, kills, assists, deaths, dmg, dt
                      ) VALUES (
                          %(logid)s, %(steamid)s, (SELECT teamid FROM team WHERE team = %(team)s),
                          (SELECT nameid FROM name WHERE name = %(name)s), %(kills)s, %(assists)s,
-                         %(deaths)s, %(suicides)s, %(dmg)s, %(dmg_real)s, %(dt)s, %(dt_real)s,
-                         %(hr)s, %(lks)s, %(as)s, %(medkits)s, %(medkits_hp)s, %(backstabs)s,
-                         %(headshots)s, %(headshots_hit)s, %(sentries)s, %(heal)s, %(cpc)s, %(ic)s
+                         %(deaths)s, %(dmg)s, %(dt)s
+                     );""", player)
+        c.execute("""INSERT INTO player_stats_extra (
+                         logid, steamid64, suicides, dmg_real, dt_real, hr, lks, airshots, medkits,
+                         medkits_hp, backstabs, headshots, headshots_hit, sentries, healing, cpc, ic
+                     ) VALUES (
+                         %(logid)s, %(steamid)s, %(suicides)s, %(dmg_real)s, %(dt_real)s, %(hr)s,
+                         %(lks)s, %(as)s, %(medkits)s, %(medkits_hp)s, %(backstabs)s, %(headshots)s,
+                         %(headshots_hit)s, %(sentries)s, %(heal)s, %(cpc)s, %(ic)s
                      );""", player)
 
         for (prop, event) in (('classkills', 'kill'), ('classdeaths', 'death'),
@@ -305,9 +309,9 @@ def import_log(c, logid, log):
 
                 c.execute("INSERT INTO name (name) VALUES (%(name)s) ON CONFLICT DO NOTHING;", msg)
                 c.execute("""INSERT INTO player_stats (
-                               logid, steamid64, nameid, kills, assists, deaths, dmg, lks, healing
+                               logid, steamid64, nameid, kills, assists, deaths, dmg
                            ) VALUES (
-                               %s, %s, (SELECT nameid FROM name WHERE name = %s), 0, 0, 0, 0, 0, 0
+                               %s, %s, (SELECT nameid FROM name WHERE name = %s), 0, 0, 0, 0
                            );""",
                           (logid, SteamID(msg['steamid']), msg['name']))
                 continue
@@ -383,7 +387,7 @@ def delete_dup_logs(c):
 
     # Done in reverse order as import_log
     for table in ('chat', 'event_stats', 'weapon_stats', 'class_stats', 'heal_stats', 'medic_stats',
-                  'player_stats', 'round'):
+                  'player_stats_extra', 'player_stats', 'round'):
         cur.execute("DELETE FROM {} WHERE logid IN (SELECT logid FROM dupes);".format(table))
 
     cur.execute("""UPDATE log

player.py
@@ -329,7 +329,10 @@ def totals(steamid):
                total(deaths_after_uber) AS deaths_after_uber,
                total(deaths_before_uber) AS deaths_before_uber
            FROM player_stats AS ps
-           JOIN log ON (ps.logid=log.logid)
+           JOIN player_stats_extra AS pse ON (
+               pse.logid=ps.logid
+               AND pse.steamid64=ps.steamid64
+           ) JOIN log ON (ps.logid=log.logid)
            JOIN format USING (formatid)
            JOIN map USING (mapid)
            LEFT JOIN medic_stats AS ms ON (

commit bda7a2feb8693cb153d9431fe258163e9a02d981

Fix left over check constraint
These tables don't exist in player_stats any more, so remove the check.

Fixes: 1f04933 ("Split off some columns of player_stats")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

schema.sql
@@ -115,8 +115,7 @@ CREATE TABLE IF NOT EXISTS player_stats (
 	deaths INT NOT NULL,
 	dmg INT NOT NULL,
 	dt INT,
-	PRIMARY KEY (steamid64, logid),
-	CHECK ((dmg_real NOTNULL AND dt_real NOTNULL) OR (dmg_real ISNULL AND dt_real ISNULL))
+	PRIMARY KEY (steamid64, logid)
 );
 
 -- This index includes steamid64 and team so that it can be used as a covering index for the peers