7a5812deaec0a5ef086d2ecfd1c6cbd86de64b29
Sean Anderson <seanga2@gmail.com>
2021-07-18 23:28:58 -0400

    Add autocompletion for map names
    One alternative considered was to query the server on every input event,
    but as it turns out browsers can do the autocompletion as long as you give
    them an appropriate list. Since I anticipate that most users won't ever
    filter by map (and that the map list could double or triple some pages), we
    lazily load it using javascript. Since this is an enhancement, I don't
    think it matters too much.
    
    Closes: #24
    Signed-off-by: Sean Anderson <seanga2@gmail.com>

---
+++ api.py
@@ -0,0 +1,30 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+# Copyright (C) 2021 Sean Anderson <seanga2@gmail.com>
+
+import flask
+
+from sql import get_db
+
+api = flask.Blueprint('api', __name__)
+
+@api.after_request
+def do_cache(resp):
+    resp.add_etag()
+    resp.cache_control.max_age = 300
+    return resp
+
+@api.route('/maps')
+def maps():
+    maps = get_db().cursor()
+    # We use statistics here to avoid a sequential scan on log just to order results
+    maps.execute("""SELECT
+                     map
+                 FROM map
+                 JOIN (SELECT
+                         mapid,
+                         count(*) AS popularity
+                     FROM log
+                     GROUP BY mapid
+                 ) AS log USING (mapid)
+                 ORDER BY popularity DESC, mapid ASC;""")
+    return flask.jsonify(maps=[row[0] for row in maps])
