Forty-Bot/trends.tf

commit 07028a714eef9a2df7b413bb90c8adc3f2a5a71b

Add stochastic player importer
Instead of going sequentially thorugh every player, just pick 100 at
random. This is less efficient than going in order, but it removes state
from the importer. This matches what yasp does, and seems moderately
reasonable.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

import_players.py
@@ -25,12 +25,20 @@ def get_steamids_full(c):
         last_steamid = steamids[-1][0]
         yield steamids
 
+def get_steamids_random(c):
+    cur = c.cursor()
+    while True:
+        cur.execute("SELECT steamid64 FROM player TABLESAMPLE SYSTEM_ROWS(100);")
+        yield cur.fetchall()
+
 def create_players_parser(sub):
     players = sub.add_parser("players", help="Import players")
     players.set_defaults(importer=import_players)
     player_sub = players.add_subparsers()
     full = player_sub.add_parser("full", help="Import all players in order")
     full.set_defaults(get_steamids=get_steamids_full, wait=0)
+    random = player_sub.add_parser("random", help="Import 100 random players each request")
+    random.set_defaults(get_steamids=get_steamids_random, wait=1)
     players.add_argument("-k", "--key", type=str, metavar="KEY", help="Steam API key")
     players.add_argument("-w", "--wait", type=int, metavar="DELAY",
                          help="Seconds to wait between API requests")

schema.sql
@@ -2,6 +2,7 @@
 -- Copyright (C) 2020 Sean Anderson <seanga2@gmail.com>
 
 CREATE EXTENSION IF NOT EXISTS pg_trgm;
+CREATE EXTENSION IF NOT EXISTS tsm_system_rows;
 
 -- Compatibility functions for migration from SQLite
 

commit f7bf4db9c03db39ff030ff5835c4e7817136e79a

Fix crashing on HTTPErrors when importing from steam
The response an HTTPError was raised from is stored as response, not resp.

Fixes: 07028a7 ("Add stochastic player importer")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

import_players.py
@@ -81,8 +81,8 @@ def import_players(args, c):
         except OSError as e:
             # Bail on client errors, except for rate-limiting
             if isinstance(e, requests.exceptions.HTTPError) \
-               and e.resp.status_code < 500 \
-               and e.resp.status_code != requests.codes.too_many:
+               and e.response.status_code < 500 \
+               and e.response.status_code != requests.codes.too_many:
                 raise
             # Otherwise just log and try again later
             logging.exception("Could not fetch player info")