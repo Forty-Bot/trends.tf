Forty-Bot/trends.tf

commit 0cb77626e525a423b155be131a72192727a8f84d

Add filters to overview page
This results in joining on class_stats_pkey ever time for the events query,
but given the prior queries it should already be in cache. We don't filter
aliases, since I don't want to bother adding a bunch of joins. Instead, we
just add a note.

Closes: #46
Signed-off-by: Sean Anderson <seanga2@gmail.com>

player.py
@@ -119,6 +119,7 @@ def get_logs(c, steamid, filters, order_clause="logid DESC", limit=100, offset=0
 @player.route('/')
 def overview(steamid):
     c = get_db()
+    filters = get_filters(flask.request.args)
     classes = c.cursor()
     classes.execute(
         """WITH classes AS MATERIALIZED (
@@ -132,8 +133,11 @@ def overview(steamid):
                    hits,
                    shots
                FROM log
+               LEFT JOIN format USING (formatid)
+               LEFT JOIN map USING (mapid)
                JOIN player_stats USING (logid)
                JOIN class_stats cs USING (logid, steamid64)
+               LEFT JOIN class USING (classid)
                JOIN (SELECT
                        logid,
                        steamid64,
@@ -143,7 +147,8 @@ def overview(steamid):
                    FROM weapon_stats
                    GROUP BY logid, steamid64, classid
                ) AS ws USING (logid, steamid64, classid)
-               WHERE steamid64 = %s
+               WHERE steamid64 = %(steamid)s
+                   {}
            ) SELECT
                *,
                (wins + 0.5 * ties) / (wins + losses + ties) AS winrate
@@ -158,28 +163,36 @@ def overview(steamid):
                    total(hits) / nullif(sum(shots), 0.0) AS acc
                FROM class
                LEFT JOIN classes USING (classid)
+               WHERE class = %(class)s OR %(class)s ISNULL
                GROUP BY classid
                ORDER BY classid
-           ) AS classes;""", (steamid,))
+           ) AS classes;""".format(filter_clauses),
+        { 'steamid': steamid, **filters})
     event_stats = c.cursor()
     event_stats.execute(
             """SELECT
                    event,
-                   total(demoman) * 30 * 60 / nullif(sum(duration), 0) AS demoman,
-                   total(engineer) * 30 * 60 / nullif(sum(duration), 0) AS engineer,
-                   total(heavyweapons) * 30 * 60 / nullif(sum(duration), 0) AS heavyweapons,
-                   total(medic) * 30 * 60 / nullif(sum(duration), 0) AS medic,
-                   total(pyro) * 30 * 60 / nullif(sum(duration), 0) AS pyro,
-                   total(scout) * 30 * 60 / nullif(sum(duration), 0) AS scout,
-                   total(sniper) * 30 * 60 / nullif(sum(duration), 0) AS sniper,
-                   total(soldier) * 30 * 60 / nullif(sum(duration), 0) AS soldier,
-                   total(spy) * 30 * 60 / nullif(sum(duration), 0) AS spy
+                   total(demoman) * 30 * 60 / nullif(sum(log.duration), 0) AS demoman,
+                   total(engineer) * 30 * 60 / nullif(sum(log.duration), 0) AS engineer,
+                   total(heavyweapons) * 30 * 60 / nullif(sum(log.duration), 0) AS heavyweapons,
+                   total(medic) * 30 * 60 / nullif(sum(log.duration), 0) AS medic,
+                   total(pyro) * 30 * 60 / nullif(sum(log.duration), 0) AS pyro,
+                   total(scout) * 30 * 60 / nullif(sum(log.duration), 0) AS scout,
+                   total(sniper) * 30 * 60 / nullif(sum(log.duration), 0) AS sniper,
+                   total(soldier) * 30 * 60 / nullif(sum(log.duration), 0) AS soldier,
+                   total(spy) * 30 * 60 / nullif(sum(log.duration), 0) AS spy
                FROM event
                LEFT JOIN event_stats USING (eventid)
                LEFT JOIN log USING (logid)
-               WHERE steamid64=%s
+               LEFT JOIN format USING (formatid)
+               LEFT JOIN map USING (mapid)
+               LEFT JOIN class_stats USING (logid, steamid64)
+               LEFT JOIN class USING (classid)
+               WHERE steamid64 = %(steamid)s
+                   {}
                GROUP BY event
-               ORDER BY event DESC;""", (steamid,))
+               ORDER BY event DESC;""".format(filter_clauses),
+               { 'steamid': steamid, **filters })
     aliases = c.cursor()
     aliases.execute(
             """SELECT
@@ -195,9 +208,9 @@ def overview(steamid):
                    LIMIT 10
                ) AS names
                JOIN name USING (nameid)""", (steamid,))
-    logs = get_logs(c, steamid, get_filters(flask.request.args), limit=25)
+    logs = get_logs(c, steamid, filters, limit=25)
     return flask.render_template("player/overview.html", logs=logs, classes=classes,
-                                 event_stats=event_stats, aliases=aliases)
+                                 event_stats=event_stats, aliases=aliases, filters=filters)
 
 @player.route('/logs')
 def logs(steamid):

templates/player/overview.html
@@ -1,5 +1,6 @@
 {# SPDX-License-Identifier: AGPL-3.0-only #}
 {# Copyright (C) 2020 Sean Anderson <seanga2@gmail.com> #}
+{% from "macros/filter.html" import filter %}
 {% from "macros/class.html" import class_map %}
 {% from "macros/format.html" import optformat, optint %}
 {% from "macros/logs.html" import log_table %}
@@ -10,6 +11,7 @@
 	{{ super() }}
 
 	<h2>Overview</h2>
+	{{ filter(filters) }}
 	<div class="box">
 	<div class="float">
 	<h3>Classes</h3>
@@ -61,6 +63,9 @@
 			{% endfor %}
 		</tbody>
 	</table>
+	{% if request.args.values() | any %}
+		<p>Filtering aliases is not supported.</p>
+	{% endif %}
 	</div>
 
 	<div class="float">

trends.py
@@ -26,6 +26,8 @@ def create_app():
     app.config.from_envvar('CONFIG', silent=True)
 
     app.teardown_appcontext(put_db)
+    app.add_template_filter(any)
+    app.add_template_filter(all)
 
     app.jinja_options['trim_blocks'] = True
     app.jinja_options['lstrip_blocks'] = True

commit 2f8d2cd970cd38c793393f91430fb6c540760645

Add primary_classid column to player_stats
Postgres has no way of knowing that sum(class_stats.duration) < log.duration,
so it must join to class stats every time we want to filter on it (since
there could be multiple "primary" classes).  Help it out by doing the
calculation up front. In some cases, this also means we don't have to join
with class_stats at all. Finally, this should help prevent bugs like
JOINing to class_stats without an appropriate filter or GROUP BY.

Fixes: 0cb7762 ("Add filters to overview page")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

import_logs.py
@@ -573,6 +573,21 @@ def update_wlt(c):
                  WHERE ps.logid = new.logid
                      AND ps.steamid64 = new.steamid64""")
 
+def update_primary_class(c):
+    c.execute("""UPDATE player_stats AS ps
+                 SET primary_classid = classid
+                 FROM (SELECT
+                         logid,
+                         steamid64,
+                         classid
+                     FROM class_stats
+                     JOIN log USING (logid)
+                     WHERE class_stats.duration * 1.5 > log.duration
+                     ORDER BY steamid64, logid
+                 ) AS cs
+                 WHERE cs.logid = ps.logid
+                 AND cs.steamid64 = ps.steamid64;""")
+
 def create_logs_parser(sub):
     class LogAction(argparse.Action):
         def __init__(self, option_strings, dest, **kwargs):
@@ -695,6 +710,7 @@ def import_logs(args, c):
         update_stalemates(cur)
         update_formats(cur)
         update_wlt(cur)
+        update_primary_class(cur)
         for table in temp_tables:
             set_clause = ", ".join("{}=EXCLUDED.{}".format(col, col)
                                    for col in table_columns(c, table[0]))

player.py
@@ -104,11 +104,7 @@ def get_logs(c, steamid, filters, order_clause="logid DESC", limit=100, offset=0
                FROM heal_stats
                GROUP BY logid, healee
            ) AS hsr USING (logid, steamid64)
-           LEFT JOIN class_stats AS cs ON (
-               cs.logid=log.logid
-               AND cs.steamid64=ps.steamid64
-               AND cs.duration * 1.5 >= log.duration
-           ) LEFT JOIN class ON (class.classid=cs.classid)
+           LEFT JOIN class ON (primary_classid=classid)
            WHERE ps.steamid64 = %(steamid)s
                {}
            ORDER BY {} NULLS LAST
@@ -125,19 +121,19 @@ def overview(steamid):
         """WITH classes AS MATERIALIZED (
                SELECT
                    classid,
-                   cs.duration * 1.5 > log.duration AS mostly,
+                   classid = primary_classid AS mostly,
                    wins AS round_wins,
                    losses AS round_losses,
                    cs.duration,
                    cs.dmg,
                    hits,
                    shots
-               FROM log
-               LEFT JOIN format USING (formatid)
-               LEFT JOIN map USING (mapid)
-               JOIN player_stats USING (logid)
+               FROM player_stats
                JOIN class_stats cs USING (logid, steamid64)
                LEFT JOIN class USING (classid)
+               JOIN log USING (logid)
+               JOIN format USING (formatid)
+               JOIN map USING (mapid)
                JOIN (SELECT
                        logid,
                        steamid64,
@@ -186,8 +182,8 @@ def overview(steamid):
                LEFT JOIN log USING (logid)
                LEFT JOIN format USING (formatid)
                LEFT JOIN map USING (mapid)
-               LEFT JOIN class_stats USING (logid, steamid64)
-               LEFT JOIN class USING (classid)
+               LEFT JOIN player_stats USING (logid, steamid64)
+               LEFT JOIN class ON (primary_classid=classid)
                WHERE steamid64 = %(steamid)s
                    {}
                GROUP BY event
@@ -399,11 +395,7 @@ def totals(steamid):
                FROM heal_stats
                GROUP BY logid, steamid64
            ) AS hs USING (logid, steamid64)
-           LEFT JOIN class_stats AS cs ON (
-               cs.logid=ps.logid
-               AND cs.steamid64=ps.steamid64
-               AND cs.duration * 1.5 >= log.duration
-           ) LEFT JOIN class USING (classid)
+           LEFT JOIN class ON (primary_classid=classid)
            WHERE ps.steamid64 = %(steamid)s
                {};""".format(filter_clauses),
         {'steamid': steamid, **filters})
@@ -477,11 +469,7 @@ def trends(steamid):
            JOIN player_stats AS ps USING (logid)
            JOIN format USING (formatid)
            JOIN map USING (mapid)
-           LEFT JOIN class_stats AS cs ON (
-               cs.logid=log.logid
-               AND cs.steamid64=ps.steamid64
-               AND cs.duration * 1.5 >= log.duration
-           ) LEFT JOIN class USING (classid)
+           LEFT JOIN class ON (primary_classid=classid)
            LEFT JOIN (SELECT
                    logid,
                    healer,

schema.sql
@@ -109,6 +109,23 @@ CREATE TABLE IF NOT EXISTS player (
 	last_active BIGINT NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS class (
+	classid SERIAL PRIMARY KEY,
+	class TEXT NOT NULL UNIQUE
+);
+
+INSERT INTO class (class) VALUES
+	('scout'),
+	('soldier'),
+	('pyro'),
+	('demoman'),
+	('engineer'),
+	('heavyweapons'),
+	('medic'),
+	('sniper'),
+	('spy')
+ON CONFLICT DO NOTHING;
+
 CREATE TABLE IF NOT EXISTS player_stats (
 	logid INT REFERENCES log (logid) NOT NULL,
 	steamid64 BIGINT REFERENCES player (steamid64) NOT NULL,
@@ -122,6 +139,7 @@ CREATE TABLE IF NOT EXISTS player_stats (
 	wins INT NOT NULL DEFAULT 0, -- rounds won
 	losses INT NOT NULL DEFAULT 0, -- rounds lost
 	ties INT NOT NULL DEFAULT 0, -- rounds tied
+	primary_classid INT REFERENCES class (classid), -- Class played more than 2/3 of the time
 	PRIMARY KEY (steamid64, logid)
 );
 
@@ -200,23 +218,6 @@ CREATE INDEX IF NOT EXISTS heal_stats_healer ON heal_stats (healer);
 CREATE STATISTICS IF NOT EXISTS heal_stats (ndistinct)
 ON logid, healer
 FROM heal_stats;
-
-CREATE TABLE IF NOT EXISTS class (
-	classid SERIAL PRIMARY KEY,
-	class TEXT NOT NULL UNIQUE
-);
-
-INSERT INTO class (class) VALUES
-	('scout'),
-	('soldier'),
-	('pyro'),
-	('demoman'),
-	('engineer'),
-	('heavyweapons'),
-	('medic'),
-	('sniper'),
-	('spy')
-ON CONFLICT DO NOTHING;
 
 CREATE TABLE IF NOT EXISTS class_stats (
 	logid INT NOT NULL,