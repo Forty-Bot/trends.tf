db0bca17b2a238e9da892a5100c755c2508570dc
s0127943 <bv939@web.de>
2021-09-22 02:55:13 +0200

    Replace the c3 graphing library with chart.js
    This tries to emulate the previous experience as closely as possible.
    This also removes the js dependency on the d3 library.
    
    Closes: #23
    
    Signed-off-by: Patrick Schrottenbacher <thebv939@gmail.com>

--- static/js/trends.js
+++ static/js/trends.js
@@ -2,106 +2,163 @@
 // Copyright (C) 2020 Sean Anderson <seanga2@gmail.com>
 
 document.addEventListener('DOMContentLoaded', () => {
-	var data = JSON.parse(d3.select("#trend-data").text());
+	const data = JSON.parse(document.getElementById("trend-data").textContent);
 
-	function onclick(d) {
+	const color_orange = '#CF7336';
+	const color_purple = '#7D4071';
+	const color_green = '#729E42';
+	const color_blue = '#2F4F4F';
+
+	function open_log(logid) {
 		Object.assign(document.createElement('a'), {
 			target: '_blank',
-			href: `/log?id=${data[d.index].logid}`,
+			href: `/log?id=${logid}#${steamid64}`,
 		}).click();
 	}
 
-	var pm_format = d3.format(".0f");
-	var kda_format = d3.format(".1f");
-	var wr_format = d3.format(".2%");
-	var tooltip = {
-		format: {
-			title: function (x, index) {
-				return time_formatter.format(new Date(data[index].time * 1000));
-			},
-			value: function (value, ratio, id, index) {
-				if (['winrate', 'round_winrate'].includes(id))
-					return wr_format(value);
-				else if (['kills', 'deaths', 'assists'].includes(id))
-					return kda_format(value);
-				else
-					return pm_format(value);
-			},
-		},
-	};
+	function create_title(context) {
+		return time_formatter.format(new Date(data[context[0].dataIndex].time * 1000));
+	}
 
-	var base_config = {
-		color: {
-			pattern: ['#CF7336', '#7D4071', '#729E42', '#2F4F4F'],
-		},
-		axis: {
-			x: {
-				tick: {
-					fit: false,
-					count: 50,
-					format: d3.format(".0f"),
+	function create_label(context, format) {
+		return context.dataset.label + ": " + format(context.raw);
+	}
+
+	function create_footer(context) {
+		context[0].chart.selectLogid = data[context[0].dataIndex].logid;
+		return "Logid: " + data[context[0].dataIndex].logid;
+	}
+
+	const pm_format = (value) => parseFloat(value).toFixed(0);
+	const kda_format = (value) => parseFloat(value).toFixed(1);
+	const wr_format = (new Intl.NumberFormat('en-us', {
+		style: 'percent',
+		minimumFractionDigits: 2,
+		maximumFractionDigits: 2
+	})).format;
+
+
+	const base_config = (format) => ({
+		type: 'line',
+		// Keep track of the last hovered over logid so the onClick works
+		selectLogid: -1,
+		options: {
+			// Don't keep the aspect ratio since we're limiting the amount of space
+			// on the y axis quite a bit
+			maintainAspectRatio: false,
+			animation: false,
+			plugins: {
+				tooltip: {
+					callbacks: {
+						title: create_title,
+						label: (context) =>
+							create_label(context, format),
+						footer: create_footer
+					}
+				}
+			},
+			elements: {
+				line: {
+					borderWidth: 1,
+				},
+				point: {
+					pointRadius: 0.5
+				}
+			},
+			scales: {
+				y: {
+					ticks: {
+						// Make sure we format the values for the y-axis as well
+						callback : function (value) {
+							return format(value);
+						}
+					}
 				},
 			},
-			y: {
-				min: 0,
-				padding: 0,
+			// When hovering over values we want to select all points over all datasets
+			interaction: {
+				mode: 'index',
+				intersect: false
 			},
-		},
-		tooltip: tooltip,
-		point: {
-			show: false,
-		},
-	};
+			// Open a new tab with the logid on logs.tf
+			onClick: function (test) {
+				if (test.chart.selectLogid > 0)
+					open_log(test.chart.selectLogid)
+			},
+			// Display a pointer cursor when hovering over the graph
+			// so people know they can click on it
+			onHover: (event, chartElement) => {
+				if (chartElement[0])
+					event.native.target.style.cursor = 'pointer';
+				else
+					event.native.target.style.cursor = 'default';
+			}
+		}
+	});
 
-	pm_config = {...base_config};
-	pm_config.bindto = '#pm-chart';
+	const pm_config = {...base_config(pm_format)};
 	pm_config.data = {
-		json: data,
-		keys: {
-			value: ['dpm', 'dtm', 'hpm_given', 'hpm_recieved'],
-		},
-		names: {
-			dpm: "D/M",
-			dtm: "DT/M",
-			hpm_given: "HG/M",
-			hpm_recieved: "HR/M",
-		},
-		onclick: onclick,
+		labels: [...Array(data.length).keys()],
+		datasets: [{
+			data: data.map((value) => value.dpm),
+			label: "D/M",
+			borderColor: color_orange,
+			backgroundColor: color_orange
+		}, {
+			data: data.map((value) => value.dtm),
+			label: "DT/M",
+			borderColor: color_purple,
+			backgroundColor: color_purple
+		}, {
+			data: data.map((value) => value.hpm_given),
+			label: "HG/M",
+			borderColor: color_green,
+			backgroundColor: color_green
+		}, {
+			data: data.map((value) => value.hpm_recieved),
+			label: "HR/M",
+			borderColor: color_blue,
+			backgroundColor: color_blue
+		}],
 	};
-	c3.generate(pm_config);
+	new Chart(document.getElementById("pm-chart"), pm_config);
 
-	kda_config = {...base_config};
-	kda_config.bindto = '#kda-chart';
+	const kda_config = {...base_config(kda_format)};
 	kda_config.data = {
-		json: data,
-		keys: {
-			value: ['kills', 'deaths', 'assists'],
-		},
-		names: {
-			kills: "K/30",
-			deaths: "D/30",
-			assists: "A/30",
-		},
-		onclick: onclick,
+		labels: [...Array(data.length).keys()],
+		datasets: [{
+			data: data.map((value) => value.kills),
+			label: "K/30",
+			borderColor: color_orange,
+			backgroundColor: color_orange
+		}, {
+			data: data.map((value) => value.deaths),
+			label: "D/30",
+			borderColor: color_purple,
+			backgroundColor: color_purple
+		}, {
+			data: data.map((value) => value.assists),
+			label: "A/30",
+			borderColor: color_green,
+			backgroundColor: color_green
+		}],
 	};
-	c3.generate(kda_config);
+	new Chart(document.getElementById("kda-chart"), kda_config);
 
-	wr_config = {...base_config};
-	wr_config.bindto = '#wr-chart';
+	const wr_config = {...base_config(wr_format)};
 	wr_config.data = {
-		json: data,
-		keys: {
-			value: ['winrate', 'round_winrate'],
-		},
-		names: {
-			winrate: "WR",
-			round_winrate: "Round WR",
-		},
-		onclick: onclick,
+		labels: [...Array(data.length).keys()],
+		datasets: [{
+			data: data.map((value) => value.winrate),
+			label: "WR",
+			borderColor: color_orange,
+			backgroundColor: color_orange
+		}, {
+			data: data.map((value) => value.round_winrate),
+			label: "Round WR",
+			borderColor: color_purple,
+			backgroundColor: color_purple
+		}],
 	};
-	wr_config.axis.y.max = 1;
-	wr_config.axis.y.tick = {
-		format: d3.format(".0%"),
-	};
-	c3.generate(wr_config);
-})
+	new Chart(document.getElementById("wr-chart"), wr_config);
+});
