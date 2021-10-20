34e027c828b3e625c44783cf143253c400cec4b6
Sean Anderson <seanga2@gmail.com>
2021-10-20 16:45:14 -0400

    Move filters and url_defaults to be initialized in create_app
    This ensures that the whole app is created by create_app. While we're add
    it, convert the url defaults to be its own class (with a better name to
    boot).
    
    Signed-off-by: Sean Anderson <seanga2@gmail.com>

--- trends.py
+++ trends.py
@@ -27,14 +27,69 @@ def json_default(obj):
     raise TypeError("Object of type '{}' is not JSON serializable" \
                     .format(type(obj).__name__))
 
+def duration_filter(timestamp):
+    mm, ss = divmod(timestamp, 60)
+    hh, mm = divmod(mm, 60)
+    if hh:
+        return "{:.0f}:{:02.0f}:{:02.0f}".format(hh, mm, ss)
+    else:
+        return "{:.0f}:{:02.0f}".format(mm, ss)
+
+def avatar_filter(hash, size='full'):
+    if not hash:
+        return ''
+    url = "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/{}/{}{}.jpg"
+    return url.format(hash[:2], hash, {
+            'small': '',
+            'medium': '_medium',
+            'full': '_full',
+        }[size])
+
+def anynone(iterable):
+    for item in iterable:
+        if item is None:
+            return False
+    return True
+
+class StaticHashDefaults:
+    def __init__(self, app):
+        self.app = app
+        self.cache = {}
+
+    def __call__(self, endpoint, values):
+        if endpoint != 'static' or 'filename' not in values:
+            return
+
+        filename = werkzeug.utils.safe_join(self.app.static_folder, values['filename'])
+        if not os.path.isfile(filename):
+            return
+
+        mtime, hash = self.cache.get(filename, (None, None))
+        if mtime == os.path.getmtime(filename):
+            values['h'] = hash
+            return
+
+        hash = hashlib.md5()
+        with open(filename, 'rb') as file:
+            hash.update(file.read())
+        hash = base64.urlsafe_b64encode(hash.digest())[:10]
+
+        self.cache[filename] = (mtime, hash)
+        values['h'] = hash
+
 def create_app():
     app = flask.Flask(__name__)
     app.config.from_object(DefaultConfig)
     app.config.from_envvar('CONFIG', silent=True)
 
     app.teardown_appcontext(put_db)
+    app.url_defaults(StaticHashDefaults(app))
+
     app.add_template_filter(any)
     app.add_template_filter(all)
+    app.add_template_filter(anynone)
+    app.add_template_filter(duration_filter, 'duration')
+    app.add_template_filter(avatar_filter, 'avatar')
 
     app.jinja_options['trim_blocks'] = True
     app.jinja_options['lstrip_blocks'] = True
@@ -51,56 +106,6 @@ def create_app():
     return app
 
 application = create_app()
-
-@application.template_filter('duration')
-def duration_filter(timestamp):
-    mm, ss = divmod(timestamp, 60)
-    hh, mm = divmod(mm, 60)
-    if hh:
-        return "{:.0f}:{:02.0f}:{:02.0f}".format(hh, mm, ss)
-    else:
-        return "{:.0f}:{:02.0f}".format(mm, ss)
-
-@application.template_filter('avatar')
-def avatar_filter(hash, size='full'):
-    if not hash:
-        return ''
-    url = "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/{}/{}{}.jpg"
-    return url.format(hash[:2], hash, {
-            'small': '',
-            'medium': '_medium',
-            'full': '_full',
-        }[size])
-
-@application.template_filter()
-def anynone(iterable):
-    for item in iterable:
-        if item is None:
-            return False
-    return True
-
-application.static_cache = {}
-@application.url_defaults
-def foo(endpoint, values):
-    if endpoint != 'static' or 'filename' not in values:
-        return
-
-    filename = werkzeug.utils.safe_join(application.static_folder, values['filename'])
-    if not os.path.isfile(filename):
-        return
-
-    mtime, hash = application.static_cache.get(filename, (None, None))
-    if mtime == os.path.getmtime(filename):
-        values['h'] = hash
-        return
-
-    hash = hashlib.md5()
-    with open(filename, 'rb') as file:
-        hash.update(file.read())
-    hash = base64.urlsafe_b64encode(hash.digest())[:10]
-
-    application.static_cache[filename] = (mtime, hash)
-    values['h'] = hash
 
 if __name__ == '__main__':
     application.run()
