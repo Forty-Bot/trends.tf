Forty-Bot/trends.tf

commit 07097d72f76c7520c4a1ebea5dd6649e0056b8b6

Rename common.py to util.py
This better reflects its status as holding utility functions which do not
depend on any external framework (e.g. flask or postgresql).

Signed-off-by: Sean Anderson <seanga2@gmail.com>

import_logs.py
@@ -10,10 +10,10 @@ import psycopg2
 import zstandard
 
 import common
-from common import classes
 from fetch import ListFetcher, BulkFetcher, FileFetcher, ReverseFetcher, CloneLogsFetcher
 from steamid import SteamID
 from sql import table_columns
+from util import classes
 
 def filter_logids(c, logids, update_only=False):
     """Filter log ids to exclude those already present in the database.

fetch.py
@@ -9,7 +9,7 @@ import requests
 import sqlite3
 import time
 
-from common import classes, events
+from util import classes, events
 
 class APIError(OSError):
     """The logs.tf API returned a failure"""

player.py
@@ -3,8 +3,8 @@
 
 import flask
 
-from common import clamp, get_filters, filter_clauses, get_order
 from sql import get_db
+from util import clamp, get_filters, filter_clauses, get_order
 
 player = flask.Blueprint('player', __name__)
 

root.py
@@ -4,9 +4,9 @@
 import flask
 import sqlite3
 
-from common import get_filters, get_order
 from sql import get_db
 from steamid import SteamID
+from util import get_filters, get_order
 
 root = flask.Blueprint('root', __name__)
 

commit 83390b6317f2e2b1a3aad3f668ee54908b61c2f7

Fix importing logs
the events object lives in util now.

Fixes: 07097d7 ("Rename common.py to util.py")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

import_logs.py
@@ -9,7 +9,6 @@ import logging
 import psycopg2
 import zstandard
 
-import common
 from fetch import ListFetcher, BulkFetcher, FileFetcher, ReverseFetcher, CloneLogsFetcher
 from steamid import SteamID
 from sql import table_columns
@@ -211,7 +210,7 @@ def import_log(cctx, c, logid, log):
                              %(ic)s
                          );""", player)
 
-        for prop, event in common.events.items():
+        for prop, event in util.events.items():
             if not log.get(prop):
                 continue
 