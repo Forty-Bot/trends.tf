Forty-Bot/trends.tf

commit 25b8c41e169668feaa80e48021ce6ba69d055620

importer: Add 'json' parser to download json for already-imported logs
New logs have their json already in the database, but logs imported before
6b4e31d ("Store original json for logs") do not. Add a script to download
the json for those logs from logs.tf.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/importer/json.py
@@ -1,0 +1,37 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+# Copyright (C) 2021 Sean Anderson <seanga2@gmail.com>
+
+import json
+import logging
+
+import psycopg2.extras
+import zstandard
+
+from .fetch import ListFetcher
+from ..util import chunk
+
+def create_json_parser(sub):
+    json = sub.add_parser("json", help="Download JSON for logs already in the database")
+    json.set_defaults(importer=import_json)
+
+def pack_log(c):
+    fetcher = ListFetcher()
+    cctx = zstandard.ZstdCompressor()
+    cur = c.cursor()
+
+    cur.execute("""SELECT logid
+                   FROM log
+                   LEFT JOIN log_json USING (logid)
+                   WHERE data ISNULL;""")
+    for log in cur:
+        fetcher.get_log(log[0])
+        yield log[0], cctx.compress(json.dumps(log).encode())
+
+def import_json(args, c):
+    cur = c.cursor()
+
+    for logs in chunk(pack_log(c), 50):
+        cur.execute("BEGIN;")
+        psycopg2.extras.execute_values(cur, "INSERT INTO log_json (logid, data) VALUES %s;", logs)
+        cur.execute("COMMIT;")
+        logging.info("Committed json")

trends/importer/cli.py
@@ -9,6 +9,7 @@ import psycopg2
 
 from ..sql import db_connect, db_init
 from .ad import create_ad_parser
+from .json import create_json_parser
 from .logs import create_logs_parser
 from .players import create_players_parser
 
@@ -18,6 +19,7 @@ def create_parser():
     create_logs_parser(sub)
     create_players_parser(sub)
     create_ad_parser(sub)
+    create_json_parser(sub)
     parser.add_argument("database", default="postgresql:///trends", metavar="DATABASE",
                         help="Database URL to connect to")
     parser.add_argument("-v", "--verbose", action='count', default=0, dest='verbosity',

commit 8646e0673a9703c3f69c4893f9bf99fd071d5d94

importer: json: Fix not actually importing json
We didn't actually store the downloaded log json in the database. We just
stored the logid... oops

Fixes: 25b8c41 ("importer: Add 'json' parser to download json for already-imported logs")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/importer/json.py
@@ -23,9 +23,9 @@ def pack_log(c):
                    FROM log
                    LEFT JOIN log_json USING (logid)
                    WHERE data ISNULL;""")
-    for log in cur:
-        fetcher.get_log(log[0])
-        yield log[0], cctx.compress(json.dumps(log).encode())
+    for logid in cur:
+        log = fetcher.get_log(logid[0])
+        yield logid[0], cctx.compress(json.dumps(log).encode())
 
 def import_json(args, c):
     cur = c.cursor()