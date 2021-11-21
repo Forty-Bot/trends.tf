Forty-Bot/trends.tf

commit 335acfbb8a6633bf07296800a0c3650f0ace81f5

importer: Add importer for A/D scoring
This importer imports the A/D scoring flag from existing logs in the
database. This should help me make a decision about how to handle logs
where the score and rounds won/lost don't match up.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/importer/ad.py
@@ -1,0 +1,43 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+# Copyright (C) 2021 Sean Anderson <seanga2@gmail.com>
+
+import json
+import logging
+
+import psycopg2.extras
+import zstandard
+
+from ..util import chunk
+
+def create_ad_parser(sub):
+    ad = sub.add_parser("ad", help="Import A/D scoring data from existing logs")
+    ad.set_defaults(importer=import_ad)
+
+def extract_ad(c):
+    dctx = zstandard.ZstdDecompressor()
+    cur = c.cursor(name='ad', withhold=True)
+
+    cur.execute("""SELECT
+                       logid,
+                       data
+                   FROM log_json
+                   JOIN log USING (logid)
+                   WHERE ad_scoring ISNULL""")
+    for log in cur:
+        try:
+            yield log[0], json.loads(dctx.decompress(log[1]))['info']['AD_scoring']
+        except (IndexError, KeyError):
+            logging.exception("Could not parse log %s", logid)
+
+def import_ad(args, c):
+    cur = c.cursor()
+
+    for logs in chunk(extract_ad(c), 1000):
+        cur.execute("BEGIN;")
+        psycopg2.extras.execute_values(cur,
+            """UPDATE log
+               SET ad_scoring = data.ad
+               FROM (VALUES %s) AS data (logid, ad)
+               WHERE log.logid = data.logid;""", logs)
+        cur.execute("COMMIT;")
+        logging.info("Imported chunk")

trends/importer/cli.py
@@ -8,6 +8,7 @@ import sys
 import psycopg2
 
 from ..sql import db_connect, db_init
+from .ad import create_ad_parser
 from .logs import create_logs_parser
 from .players import create_players_parser
 
@@ -16,6 +17,7 @@ def create_parser():
     sub = parser.add_subparsers()
     create_logs_parser(sub)
     create_players_parser(sub)
+    create_ad_parser(sub)
     parser.add_argument("database", default="postgresql:///trends", metavar="DATABASE",
                         help="Database URL to connect to")
     parser.add_argument("-v", "--verbose", action='count', default=0, dest='verbosity',

trends/schema.sql
@@ -49,6 +49,7 @@ CREATE TABLE IF NOT EXISTS log (
 	red_score INT NOT NULL,
 	blue_score INT NOT NULL,
 	formatid INT REFERENCES format (formatid),
+	ad_scoring BOOLEAN, -- Whether attack/defense scoring is enabled
 	-- Some logs may be duplicates or subsets of another log
 	duplicate_of INT REFERENCES log (logid),
 	-- All duplicates must be earlier (and have smaller logids) than what they are duplicates of

trends/util.py
@@ -3,6 +3,7 @@
 
 from datetime import datetime, timedelta
 from dateutil import tz
+import itertools
 
 classes = (
     'demoman',
@@ -24,6 +25,17 @@ events = {
 
 def clamp(n, lower, upper):
     return max(lower, min(n, upper))
+
+# Adapted from https://stackoverflow.com/a/8998040/5086505
+def chunk(iterable, n):
+    it = iter(iterable)
+    while True:
+        slice = itertools.islice(it, n)
+        try:
+            first = next(slice)
+        except StopIteration:
+            return
+        yield itertools.chain((first,), slice)
 
 def get_filters(args):
     ret = {}

commit 4590e04ddb4e8d28cf932448fb1cd2f63b08a3cb

importer: ad: Catch more exceptions
If there was an issue parsing a log, not all exceptions would be caught. In
addition, the logid would not be printed.

Fixes: 335acfb ("importer: Add importer for A/D scoring")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/importer/ad.py
@@ -25,8 +25,8 @@ def extract_ad(c):
         for log in cur:
             try:
                 yield log[0], json.loads(dctx.decompress(log[1]))['info']['AD_scoring']
-            except (IndexError, KeyError):
-                logging.exception("Could not parse log %s", logid)
+            except (IndexError, KeyError, TypeError):
+                logging.exception("Could not parse log %s", log[0])
 
 def import_ad(args, c):
     cur = c.cursor()