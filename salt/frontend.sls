Forty-Bot/trends.tf

commit cd2cf0accd72e3bd2e55ed295ae4629e2ff4ed80

salt: Track metrics with netdata
This seems to be the easiest option for now. Other options not taken
include munin, influxdb+telegraf, and prometheus.

Unfortunately, netdata doesn't allow for easy aggregation over time, only
line graphs (effectively). And it doesn't have great options for breaking
down requests. So #65 is still open.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

salt/frontend.sls
@@ -107,6 +107,77 @@ uwsgi_service:
       - uwsgi_installed
       - uwsgi_config
 
+# Metrics
+netdata:
+  pkg.installed:
+    - refresh: False
+  postgres_user.present:
+    - runas: postgres
+    - require:
+      - pkg: netdata
+
+python-psycopg2:
+  pkg.installed:
+    - refresh: False
+
+/etc/netdata/.opt-out-from-anonymous-statistics:
+  file.managed:
+    - require:
+      - netdata
+
+/etc/netdata/netdata.conf:
+  file.managed:
+    - contents: |
+        [global]
+          run as user = netdata
+          access log = none
+
+          memory mode = dbengine
+          page cache size = 32
+          dbengine multihost disk space = 4096
+
+          process scheduling policy = idle
+          OOM score = 1000
+
+        [web]
+          web files owner = root
+          web files group = netdata
+          bind to = unix:/run/netdata/netdata.sock
+    - require:
+      - netdata
+
+/etc/netdata/python.d/web_log.conf:
+  file.managed:
+    - contents: |
+        nginx_log:
+          name: 'nginx'
+          path: '/var/log/nginx/access.log'
+          histogram: [1,10,100,1000]
+    - require:
+      - netdata
+
+/etc/netdata/python.d/postgres.conf:
+  file.managed:
+    - contents: |
+        socket:
+          name: 'local'
+          user: 'netdata'
+          database: 'trends'
+    - require:
+      - netdata
+
+netdata.service:
+  service.running:
+    - enable: True
+    - reload: True
+    - requires:
+      - netdata
+      - python-psycopg2
+      - /etc/netdata/.opt-out-from-anonymous-statistics
+      - /etc/netdata/netdata.conf
+      - /etc/netdata/python.d/web_log.conf
+      - /etc/netdata/python.d/postgres.conf
+
 certbot:
   pkg.installed:
     - refresh: False
@@ -195,3 +266,4 @@ nginx.service:
       - /srv/uwsgi/trends
       - nginx_service
       - uwsgi_service
+      - netdata.service

salt/etc/nginx/nginx.conf
@@ -43,7 +43,11 @@ http {
 	resolver_timeout 2s;
 
 	# Logging Settings
-	access_log /var/log/nginx/access.log;
+	log_format netdata '$remote_addr - $remote_user [$time_local] '
+	                   '"$request" $status $body_bytes_sent '
+			   '$request_length $request_time $upstream_response_time '
+			   '"$http_referer" "$http_user_agent"';
+	access_log /var/log/nginx/access.log netdata;
 	error_log /var/log/nginx/error.log;
 
 	# Gzip Settings

salt/etc/nginx/sites-available/trends.tf
@@ -1,6 +1,16 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # Copyright (C) 2021 Sean Anderson <seanga2@gmail.com>
 
+upstream netdata {
+	server unix:/run/netdata/netdata.sock;
+	keepalive 64;
+}
+
+map $http_upgrade $connection_upgrade {
+	default upgrade;
+	""      close;
+}
+
 {% set certdir = "/etc/letsencrypt/live/trends.tf" %}
 server {
 	listen                  443 ssl http2;
@@ -53,13 +63,32 @@ server {
 	# favicon.ico
 	location = /favicon.ico {
 		access_log off;
-		return 301 https://$host/static/img/favicon.ico;
+		return 301 /static/img/favicon.ico;
 	}
 
 	# robots.txt
 	location = /robots.txt {
 		access_log off;
-		return 301 https://$host/static/robots.txt;
+		return 301 /static/robots.txt;
+	}
+
+	# netdata metrics
+	location /netdata {
+		return 301 /netdata/;
+	}
+
+	location /netdata/ {
+		allow 127.0.0.0/8;
+		deny all;
+
+		proxy_pass                        http://netdata/;
+		proxy_set_header Connection       $connection_upgrade;
+		proxy_set_header Host             $host;
+		proxy_set_header X-Forwarded-Host $host;
+		proxy_set_header X-Forwarded-Port $server_port;
+
+		proxy_http_version 1.1;
+		proxy_cache_bypass $http_upgrade;
 	}
 }
 

commit 76f1a4ae46db1a478d1ba59b5c2fede2898d6aa4

salt: Fix netdata user on Debian
On Debian the web files are owned by root:root.

Fixes: cd2cf0a ("salt: Track metrics with netdata")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

salt/frontend.sls
@@ -146,7 +146,11 @@ psycopg2:
 
         [web]
           web files owner = root
+{% if grains.os_family == 'Debian' %}
+          web files group = root
+{% else %}
           web files group = netdata
+{% endif %}
           bind to = unix:/run/netdata/netdata.sock
     - require:
       - netdata