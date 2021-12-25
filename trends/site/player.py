Forty-Bot/trends.tf

commit b0a42a180ecc6d7991c93dfa53a446d0b2bb3206

site: Dynamically generate filter clauses
Postgres has consistently been unable to optimize optional filter clauses
properly. For example, before 9a27f5e ("Filter using OR %s ISNULL instead
of ifnull") we wrote filters like

    WHERE format = coalesce(%(format)s, format)

but this was optimized poorly by postgres, so I switched to the current
format. However, this format still has flaws. For example, poor query
performance may result for a clause like

    WHERE format = %(format)s OR %(format)s ISNULL

because the `JOIN format USING (formatid)` may not be optimized properly if
postgres suspects we will get more than one format. We could assure
postgres that we only want one format by doing

    WHERE formatid = (SELECT formatid
                       FROM format
                       WHERE format = %(format)s
        ) OR %(format)s ISNULL

but postgres will still do the lookup on format even if we have a NULL
formatid. This why we use multiple queries for the leaderboard page.
Additionally, clauses like

	WHERE foo IN %(foos)s OR %(foos) ISNULL

are not optimized properly when foos is non-null.

We can avoid all of the above by just omiting clauses which we aren't
using. We no longer have to rely on postgres's (terrible) constant
optimization. Further, this allows us to dynamically create clauses as
necessary. This makes implementing player filtering (with AND and not OR)
possible without a recursive CTE.

get_filters is split into two functions to allow one request to use
different clauses. This is most evident in the player overview, where some
queries use primary_classid/formatid/mapid and others use class/format/map.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/player.py
@@ -3,7 +3,7 @@
 
 import flask
 
-from .util import get_filters, common_clauses, get_order
+from .util import get_filter_params, get_filter_clauses, get_order
 from ..sql import get_db
 from ..util import clamp
 
@@ -43,9 +43,14 @@ def get_player(endpoint, values):
     else:
         flask.abort(404)
 
-player_clauses = common_clauses + "\nAND (class = %(class)s OR %(class)s ISNULL)"
+# The base set of column filters for most queries in this file
+base_filter_columns = frozenset({'formatid', 'title', 'mapid', 'time', 'logid'})
+# These columns filters should be used when pretty names for class, format, and map are not used
+surrogate_filter_columns = base_filter_columns.union({'primary_classid'})
 
 def get_logs(c, steamid, filters, order_clause="logid DESC", limit=100, offset=0):
+    filter_clauses = get_filter_clauses(filters, 'primary_classid', 'format', 'title', 'map',
+                                        'time', 'logid')
     logs = c.cursor()
     logs.execute(
         """SELECT
@@ -74,7 +79,7 @@ def get_logs(c, steamid, filters, order_clause="logid DESC", limit=100, offset=0
            FROM log
            JOIN player_stats AS ps USING (logid)
            JOIN map USING (mapid)
-           JOIN format USING (formatid)
+           LEFT JOIN format USING (formatid)
            JOIN (SELECT
                      logid,
                      steamid64,
@@ -99,14 +104,15 @@ def get_logs(c, steamid, filters, order_clause="logid DESC", limit=100, offset=0
            WHERE ps.steamid64 = %(steamid)s
                {}
            ORDER BY {} NULLS LAST
-           LIMIT %(limit)s OFFSET %(offset)s;""".format(player_clauses, order_clause),
-           { 'steamid': steamid, **filters, 'limit': limit, 'offset': offset })
+           LIMIT %(limit)s OFFSET %(offset)s;""".format(filter_clauses, order_clause),
+        { 'steamid': steamid, **filters, 'limit': limit, 'offset': offset })
     return logs
 
 @player.route('/')
 def overview(steamid):
     c = get_db()
-    filters = get_filters(flask.request.args)
+    filters = get_filter_params(flask.request.args)
+    filter_clauses = get_filter_clauses(filters, *surrogate_filter_columns)
     classes = c.cursor()
     classes.execute(
         """WITH classes AS MATERIALIZED (
@@ -121,10 +127,7 @@ def overview(steamid):
                    shots
                FROM player_stats
                JOIN class_stats cs USING (logid, steamid64)
-               LEFT JOIN class USING (classid)
                JOIN log_nodups USING (logid)
-               JOIN format USING (formatid)
-               JOIN map USING (mapid)
                JOIN (SELECT
                        logid,
                        steamid64,
@@ -154,7 +157,7 @@ def overview(steamid):
                WHERE class = %(class)s OR %(class)s ISNULL
                GROUP BY classid
                ORDER BY classid
-           ) AS classes;""".format(player_clauses),
+           ) AS classes;""".format(filter_clauses),
         { 'steamid': steamid, **filters})
     event_stats = c.cursor()
     event_stats.execute(
@@ -172,14 +175,11 @@ def overview(steamid):
                FROM event
                LEFT JOIN event_stats USING (eventid)
                LEFT JOIN log_nodups AS log USING (logid)
-               LEFT JOIN format USING (formatid)
-               LEFT JOIN map USING (mapid)
                LEFT JOIN player_stats USING (logid, steamid64)
-               LEFT JOIN class ON (primary_classid=classid)
                WHERE steamid64 = %(steamid)s
                    {}
                GROUP BY event
-               ORDER BY event DESC;""".format(player_clauses),
+               ORDER BY event DESC;""".format(filter_clauses),
                { 'steamid': steamid, **filters })
     aliases = c.cursor()
     aliases.execute(
@@ -204,7 +204,7 @@ def overview(steamid):
 def logs(steamid):
     limit = flask.request.args.get('limit', 100, int)
     offset = flask.request.args.get('offset', 0, int)
-    filters = get_filters(flask.request.args)
+    filters = get_filter_params(flask.request.args)
     order, order_clause = get_order(flask.request.args, {
         'logid': "logid",
         'wins': "wins",
@@ -221,15 +221,16 @@ def logs(steamid):
         'acc': "acc",
         'date': "time",
 	}, 'logid')
-    logs = get_logs(get_db(), steamid, filters, order_clause, limit=limit, offset=offset).fetchall()
-    return flask.render_template("player/logs.html", logs=logs, filters=filters, order=order,
-                                 limit=limit, offset=offset)
+    logs = get_logs(get_db(), steamid, filters, order_clause, limit=limit, offset=offset)
+    return flask.render_template("player/logs.html", logs=logs.fetchall(), filters=filters,
+                                 order=order, limit=limit, offset=offset)
 
 @player.route('/peers')
 def peers(steamid):
     limit = flask.request.args.get('limit', 100, int)
     offset = flask.request.args.get('offset', 0, int)
-    filters = get_filters(flask.request.args)
+    filters = get_filter_params(flask.request.args)
+    filter_clauses = get_filter_clauses(filters, 'class', *base_filter_columns)
     order, order_clause = get_order(flask.request.args, {
         'logs': "count(*)",
         'with': '"with"',
@@ -283,8 +284,6 @@ def peers(steamid):
                        hs2.healing AS healing_from,
                        log.duration
                    FROM log_nodups AS log
-                   LEFT JOIN map USING (mapid)
-                   LEFT JOIN format USING (formatid)
                    JOIN player_stats AS p1 USING (logid)
                    JOIN player_stats AS p2 USING (logid)
                    LEFT JOIN class ON (p1.primary_classid=classid)
@@ -306,14 +305,14 @@ def peers(steamid):
                LIMIT %(limit)s OFFSET %(offset)s
            ) AS peers
            JOIN player USING (steamid64)
-           JOIN name USING (nameid);""".format(player_clauses, order_clause),
-           { 'steamid': steamid, **filters, 'limit': limit, 'offset': offset })
-    return flask.render_template("player/peers.html", peers=peers.fetchall(), filters=filters,order=order,
-                                 limit=limit, offset=offset)
+           JOIN name USING (nameid);""".format(filter_clauses, order_clause),
+        { 'steamid': steamid, **filters, 'limit': limit, 'offset': offset })
+    return flask.render_template("player/peers.html", peers=peers.fetchall(), filters=filters,
+                                 order=order, limit=limit, offset=offset)
 
 @player.route('/totals')
 def totals(steamid):
-    filters = get_filters(flask.request.args)
+    filters = get_filter_params(flask.request.args)
     totals = get_db().cursor()
     totals.execute(
         """SELECT
@@ -377,8 +376,6 @@ def totals(steamid):
            FROM player_stats AS ps
            LEFT JOIN player_stats_extra AS pse USING (logid, steamid64)
            JOIN log_nodups AS log USING (logid)
-           JOIN format USING (formatid)
-           JOIN map USING (mapid)
            LEFT JOIN medic_stats AS ms USING (logid, steamid64)
            LEFT JOIN (SELECT
                    logid,
@@ -387,15 +384,15 @@ def totals(steamid):
                FROM heal_stats
                GROUP BY logid, steamid64
            ) AS hs USING (logid, steamid64)
-           LEFT JOIN class ON (primary_classid=classid)
            WHERE ps.steamid64 = %(steamid)s
-               {};""".format(player_clauses),
+               {};""".format(get_filter_clauses(filters, *surrogate_filter_columns)),
         {'steamid': steamid, **filters})
     return flask.render_template("player/totals.html", totals=totals.fetchone(), filters=filters)
 
 @player.route('/weapons')
 def weapons(steamid):
-    filters = get_filters(flask.request.args)
+    filters = get_filter_clauses(flask.request.args)
+    filter_clauses = get_filter_clauses(filters, 'classid', *base_filter_columns)
     order, order_clause = get_order(flask.request.args, {
         'weapon': 'weapon',
         'kills': 'k30',
@@ -420,21 +417,18 @@ def weapons(steamid):
            FROM weapon_stats AS ws
            JOIN weapon USING (weaponid)
            JOIN class_stats AS cs USING (logid, steamid64, classid)
-           JOIN class USING (classid)
            JOIN log_nodups AS log USING (logid)
-           JOIN format USING (formatid)
-           JOIN map USING (mapid)
            WHERE steamid64 = %(steamid)s
                {}
            GROUP BY weapon
-           ORDER BY {} NULLS LAST;""".format(player_clauses, order_clause),
+           ORDER BY {} NULLS LAST;""".format(filter_clauses, order_clause),
         {'steamid': steamid, **filters})
     return flask.render_template("player/weapons.html", weapons=weapons, filters=filters,
                                  order=order)
 
 @player.route('/trends')
 def trends(steamid):
-    filters = get_filters(flask.request.args)
+    filters = get_filter_params(flask.request.args)
     window = clamp(flask.request.args.get('window', 20, int), 1, 500)
     cur = get_db().cursor()
     cur.execute(
@@ -460,9 +454,6 @@ def trends(steamid):
                    nullif(sum(log.duration) OVER win, 0) AS hpm_recieved
            FROM log_nodups AS log
            JOIN player_stats AS ps USING (logid)
-           JOIN format USING (formatid)
-           JOIN map USING (mapid)
-           LEFT JOIN class ON (primary_classid=classid)
            LEFT JOIN heal_stats_given AS hsg USING (logid, steamid64)
            LEFT JOIN heal_stats_received AS hsr USING (logid, steamid64)
            WHERE ps.steamid64 = %(steamid)s
@@ -472,7 +463,7 @@ def trends(steamid):
                ORDER BY log.logid
                GROUPS BETWEEN %(window)s - 1 PRECEDING AND CURRENT ROW
            ) ORDER BY log.logid DESC
-           LIMIT 10000;""".format(player_clauses),
+           LIMIT 10000;""".format(get_filter_clauses(filters, *surrogate_filter_columns)),
            {'steamid': steamid, 'window': window, **filters})
     trends = list(dict(row) for row in cur)
     trends.reverse()

trends/site/common.py
@@ -6,13 +6,14 @@ from dateutil import tz
 
 import flask
 
-from .util import common_clauses, get_filters, get_order
+from .util import get_filter_params, get_filter_clauses, get_order
 from ..sql import get_db
 
 def logs(api):
     limit = flask.request.args.get('limit', 100, int)
     offset = flask.request.args.get('offset', 0, int)
-    filters = get_filters(flask.request.args)
+    filters = get_filter_params(flask.request.args)
+    filter_clauses = get_filter_clauses(filters, 'title', 'format', 'map', 'time', 'logid')
     order, order_clause = get_order(flask.request.args, {
         'logid': "logid",
         'duration': "duration",
@@ -29,17 +30,10 @@ def logs(api):
                     FROM log
                     JOIN map USING (mapid)
                     LEFT JOIN format USING (formatid)
-                    LEFT JOIN (SELECT
-                            logid
-                        FROM player_stats
-                        WHERE steamid64 IN %(players)s
-                        GROUP BY logid
-                    ) AS ps USING (logid)
-                    WHERE (ps.logid NOTNULL OR %(players)s ISNULL)
-                        AND (title ILIKE %(title)s OR %(title)s ISNULL)
+                    WHERE TRUE
                         {}
                     ORDER BY {}
-                    LIMIT %(limit)s OFFSET %(offset)s;""".format(common_clauses, order_clause),
+                    LIMIT %(limit)s OFFSET %(offset)s;""".format(filter_clauses, order_clause),
                 { **filters, 'limit': limit, 'offset': offset })
 
     logs = logs.fetchall()

trends/site/root.py
@@ -5,7 +5,7 @@ import flask
 import sqlite3
 
 from . import common
-from .util import get_filters, get_order
+from .util import get_filter_params, get_filter_clauses, get_order
 from ..sql import get_db
 from ..steamid import SteamID
 
@@ -98,7 +98,16 @@ def search():
 def leaderboard():
     limit = flask.request.args.get('limit', 100, int)
     offset = flask.request.args.get('offset', 0, int)
-    filters = get_filters(flask.request.args)
+    filters = get_filter_params(flask.request.args)
+    filter_clauses = get_filter_clauses(filters, 'classid', 'formatid', 'mapid')
+
+    # Since we are using a cube, we need to explicitly select the NULL rows
+    cube_clauses = []
+    for (name, column) in (('class', 'classid'), ('format', 'formatid'), ('map', 'mapid')):
+        if not filters[name]:
+            cube_clauses.append("AND {} ISNULL".format(column))
+    cube_clauses = '\n'.join(cube_clauses)
+
     order, order_clause = get_order(flask.request.args, {
         'duration': "duration",
         'logs': "logs",
@@ -107,19 +116,6 @@ def leaderboard():
     }, 'rating')
 
     db = get_db()
-    ids = db.cursor()
-    ids.execute("""SELECT
-                       (SELECT
-                               classid
-                           FROM class
-                           WHERE class = %(class)s
-                       ) AS classid,
-                       (SELECT
-                               formatid
-                           FROM format
-                           WHERE format = %(format)s
-                       ) AS formatid;""", filters)
-    ids = ids.fetchone()
     leaderboard = db.cursor()
     leaderboard.execute("""SELECT
                                name,
@@ -138,21 +134,17 @@ def leaderboard():
                                    (50 + sum(0.5 * ties + wins)) /
                                        (100 + sum(wins + losses + ties)) AS rating
                                FROM leaderboard_cube
-                               LEFT JOIN map USING (mapid)
                                WHERE steamid64 NOTNULL
-                                   AND (classid = %(classid)s
-                                       OR (%(classid)s ISNULL AND classid ISNULL))
-                                   AND (formatid = %(formatid)s
-                                       OR (%(formatid)s ISNULL AND formatid ISNULL))
-                                   AND (map ILIKE %(map)s
-                                       OR (%(map)s ISNULL AND map ISNULL))
+                                   {}
+                                   {}
                                GROUP BY steamid64
                                ORDER BY {} NULLS LAST
                                LIMIT %(limit)s OFFSET %(offset)s
                            ) AS leaderboard
                            LEFT JOIN player USING (steamid64)
-                           LEFT JOIN name USING (nameid);""".format(order_clause),
-                           { **filters, **ids, 'limit': limit, 'offset': offset })
+                           LEFT JOIN name USING (nameid);"""
+                           .format(filter_clauses, cube_clauses, order_clause),
+                        { **filters, 'limit': limit, 'offset': offset })
     return flask.render_template("leaderboard.html", leaderboard=leaderboard.fetchall(),
                                  filters=filters, order=order, offset=offset, limit=limit)
 

trends/site/util.py
@@ -4,37 +4,84 @@
 from datetime import datetime, timedelta
 from dateutil import tz
 
-def get_filters(args):
-    ret = {}
+def get_filter_params(args):
+    params = {}
 
-    # We need the or None to turn false-y values into NULLs
-    ret['class'] = args.get('class', None, str) or None
-    ret['format'] = args.get('format', None, str) or None
-    map = args.get('map', None, str)
-    ret['map'] = "%{}%".format(map) if map else None
-    title = args.get('title', None, str)
-    ret['title'] = "%{}%".format(title) if title else None
-    ret['players'] = tuple(args.getlist('steamid64', int)) or (None,)
+    params['class'] = args.get('class', type=str)
+    params['format'] = args.get('format', type=str)
+    params['players'] = args.getlist('steamid64', type=int)
+
+    def set_like_param(name):
+        if val := args.get(name, type=str):
+            params[name] = "%{}%".format(val)
+        else:
+            params[name] = None
+
+    set_like_param('map')
+    set_like_param('title')
 
     timezone = args.get('timezone', tz.UTC, tz.gettz)
-    def parse_date(name):
-        date = args.get(name, None, datetime.fromisoformat)
-        if date:
-            date = date.replace(tzinfo=timezone).astimezone(tz.UTC)
-            return (date.date().isoformat(), date.timestamp())
-        return (None, None)
+    def set_date_params(name):
+        name_ts = "{}_ts".format(name)
+        if date := args.get(name, None, datetime.fromisoformat):
+            utcdate = date.replace(tzinfo=timezone).astimezone(tz.UTC)
+            params[name] = date.date().isoformat()
+            params[name_ts] = utcdate.timestamp()
+        else:
+            params[name] = None
+            params[name_ts] = None
 
-    (ret['date_from'], ret['date_from_ts']) = parse_date('date_from')
-    (ret['date_to'], ret['date_to_ts']) = parse_date('date_to')
+    set_date_params('date_from')
+    set_date_params('date_to')
 
-    return ret
+    return params
 
-# These are common filter clauses which can be added to any query
-common_clauses = \
-    """AND (format = %(format)s OR %(format)s ISNULL)
-       AND (map ILIKE %(map)s OR %(map)s ISNULL)
-       AND (time >= %(date_from_ts)s::BIGINT OR %(date_from_ts)s ISNULL)
-       AND (time <= %(date_to_ts)s::BIGINT OR %(date_to_ts)s ISNULL)"""
+def get_filter_clauses(params, *valid_columns):
+    clauses = []
+
+    def simple_clause(name, column):
+        if not params[name]:
+            return
+
+        if name in valid_columns:
+            clauses.append("AND {0} = %({0})s".format(name))
+        elif column in valid_columns:
+            clauses.append("AND {0} = (SELECT {0} FROM {1} WHERE {1} = %({1})s)"
+                           .format(column, name))
+
+    simple_clause('class', 'classid')
+    simple_clause('format', 'formatid')
+
+    if 'primary_classid' in valid_columns and params['class']:
+        clauses.append("AND primary_classid = (SELECT classid FROM class WHERE class = %(class)s)")
+
+    def like_clause(name):
+        if name in valid_columns and params[name]:
+            clauses.append("AND {0} ILIKE %({0})s".format(name))
+    like_clause('title')
+    like_clause('map')
+
+    if 'mapid' in valid_columns and params['map']:
+        clauses.append("AND mapid IN (SELECT mapid FROM map WHERE map ILIKE %(map)s)")
+
+    def date_clause(name, op):
+        if 'time' in valid_columns and params[name]:
+            clauses.append("AND time {} %({})s::BIGINT".format(op, name))
+
+    date_clause('date_from_ts', '>=')
+    date_clause('date_to_ts', '<=')
+
+    if 'logid' in valid_columns:
+        for i, player in enumerate(params['players']):
+            key = "player_{}".format(i)
+            params[key] = player
+            clauses.append("""AND logid IN (
+                                  SELECT logid
+                                  FROM player_stats
+                                  WHERE steamid64 = %({})s
+                           )""".format(key))
+
+    return "\n".join(clauses)
 
 def get_order(args, column_map, default_column, default_dir='desc'):
     column = args.get('sort', None, str)

commit 62b1350ef3eb88c8a12b5687176467e1aefc1b00

site: weapons: Fix failure to load
This fixes the wrong function being used.

Fixes: b0a42a1 ("site: Dynamically generate filter clauses")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/player.py
@@ -394,7 +394,7 @@ def totals(steamid):
 
 @player.route('/weapons')
 def weapons(steamid):
-    filters = get_filter_clauses(flask.request.args)
+    filters = get_filter_params(flask.request.args)
     filter_clauses = get_filter_clauses(filters, 'classid', *base_filter_columns)
     order, order_clause = get_order(flask.request.args, {
         'weapon': 'weapon',