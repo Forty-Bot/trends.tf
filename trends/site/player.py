Forty-Bot/trends.tf

commit 2db7380620f92d4fea267ae8285f8ccb425cba62

site: Mark duplicate logs
This adds a link to the primary log in log lists. In addition, duplicate
logs are hidden on the player overview.

Closes: #68
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/player.py
@@ -48,9 +48,11 @@ base_filter_columns = frozenset({'formatid', 'title', 'mapid', 'time', 'logid'})
 # These columns filters should be used when pretty names for class, format, and map are not used
 surrogate_filter_columns = base_filter_columns.union({'primary_classid'})
 
-def get_logs(c, steamid, filters, order_clause="logid DESC", limit=100, offset=0):
+def get_logs(c, steamid, filters, duplicates=True, order_clause="logid DESC", limit=100, offset=0):
     filter_clauses = get_filter_clauses(filters, 'primary_classid', 'format', 'title', 'map',
                                         'time', 'logid')
+    if not duplicates:
+        filter_clauses += "\nAND duplicate_of ISNULL"
     logs = c.cursor()
     logs.execute(
         """SELECT
@@ -75,6 +77,7 @@ def get_logs(c, steamid, filters, order_clause="logid DESC", limit=100, offset=0
                acc,
                hsg.healing * 60.0 / log.duration AS hpm_given,
                hsr.healing * 60.0 / log.duration AS hpm_recieved,
+               duplicate_of,
                time
            FROM log
            JOIN player_stats AS ps USING (logid)
@@ -196,7 +199,7 @@ def overview(steamid):
                    LIMIT 10
                ) AS names
                JOIN name USING (nameid)""", (steamid,))
-    logs = get_logs(c, steamid, filters, limit=25)
+    logs = get_logs(c, steamid, filters, limit=25, duplicates=False)
     return flask.render_template("player/overview.html", logs=logs, classes=classes,
                                  event_stats=event_stats, aliases=aliases, filters=filters)
 

trends/site/common.py
@@ -26,7 +26,8 @@ def logs(api):
                         duration,
                         title,
                         map,
-                        format
+                        format,
+                        duplicate_of
                     FROM log
                     JOIN map USING (mapid)
                     LEFT JOIN format USING (formatid)

trends/site/templates/logs.html
@@ -22,6 +22,7 @@
 	<th>Format</th>
 	<th>Duration</th>
 	<th>Date</th>
+	<th><abbr title="Some logs are duplicates of other logs">Duplicate of</abbr></th>
 </tr>
 {% endmacro %}
 	<table>
@@ -37,6 +38,13 @@
 				<td class="left">{{ log['format'] or '' }}</td>
 				<td>{{ log['duration'] | duration }}</td>
 				<td class="left date" timestamp="{{ log['time'] }}"></td>
+				<td>
+					{% if log['duplicate_of'] %}
+					<a href="{{ url_for('root.log', logids=log['duplicate_of']) }}">
+						{{ log['duplicate_of'] }}
+					</a>
+					{% endif %}
+				</td>
 			</tr>
 			{% endfor %}
 		</tbody>

trends/site/templates/macros/logs.html
@@ -19,14 +19,15 @@
 	<th><abbr title="Kills">K</abbr></th>
 	<th><abbr title="Deaths">D</abbr></th>
 	<th><abbr title="Assists">A</abbr></th>
-	{% if extra %}
 	<th><abbr title="Damage per Minute">D/M</abbr></th>
 	<th><abbr title="Damage Taken per Minute">DT/M</abbr></th>
 	<th><abbr title="Healing Given per Minute ">HG/M</abbr></th>
 	<th><abbr title="Healing Recieved per Minute ">HR/M</abbr></th>
 	<th><abbr title="Accuracy">Acc</abbr></th>
-	{% endif %}
 	<th>Date</th>
+	{% if extra %}
+	<th><abbr title="Some logs are duplicates of other logs">Duplicate of</abbr></th>
+	{% endif %}
 </tr>
 {% endmacro %}
 {% macro log_table(logs, extra=False) %}
@@ -56,14 +57,21 @@
 			<td>{{ log['kills'] }}</td>
 			<td>{{ log['deaths'] }}</td>
 			<td>{{ log['assists'] }}</td>
-			{% if extra %}
 			<td>{{ log['dpm'] | int }}</td>
 			<td>{{ optint(log['dtm']) }}</td>
 			<td>{{ log['hpm_given'] | int }}</td>
 			<td>{{ log['hpm_recieved'] | int }}</td>
 			<td>{{ optformat("{:.0%}", log['acc']) }}</td>
-			{% endif %}
 			<td class="left date" timestamp="{{ log['time'] }}"></td>
+			{% if extra %}
+			<td>
+				{% if log['duplicate_of'] %}
+				<a href="{{ url_for('root.log', logids=log['duplicate_of']) }}">
+					{{ log['duplicate_of'] }}
+				</a>
+				{% endif %}
+			</td>
+			{% endif %}
 		</tr>
 		{% endfor %}
 	</tbody>

commit 7df525150a7c0a69d7e3db4d6f9ea7cfa2ecc7a8

site: player: Fix logs not sorting
Despite order_clause being a positional parameter, logs() was using it
positionally, causing it to be ignored.

Fixes: 2db7380 ("site: Mark duplicate logs")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/player.py
@@ -223,7 +223,7 @@ def logs(steamid):
         'acc': "acc",
         'date': "time",
 	}, 'logid')
-    logs = get_logs(get_db(), steamid, filters, order_clause, limit=limit, offset=offset)
+    logs = get_logs(get_db(), steamid, filters, order_clause=order_clause, limit=limit, offset=offset)
     return flask.render_template("player/logs.html", logs=logs.fetchall(), filters=filters,
                                  order=order, limit=limit, offset=offset)
 