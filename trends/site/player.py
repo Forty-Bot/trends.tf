Forty-Bot/trends.tf

commit 4eb5a2cc3897477862fcf2a13fc33f23b466ae95

site: overview: Fix class stats not rendering
False-y values are no longer coalesced to None/NULL, which breaks the class
stats if the user doesn't specify a class when filtering. Fix this by
generating a filter clause just for class.

Fixes: b0a42a1 ("site: Dynamically generate filter clauses")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/player.py
@@ -157,10 +157,10 @@ def overview(steamid):
                    total(dmg) / nullif(sum(shots), 0.0) AS dps
                FROM class
                LEFT JOIN classes USING (classid)
-               WHERE class = %(class)s OR %(class)s ISNULL
+                   {}
                GROUP BY classid
                ORDER BY classid
-           ) AS classes;""".format(filter_clauses),
+           ) AS classes;""".format(filter_clauses, get_filter_clauses(filters, 'class')),
         { 'steamid': steamid, **filters})
     event_stats = c.cursor()
     event_stats.execute(

commit 91992b7b5325173b749302ab7a29af48655aaa11

site: overview: Fix filtering by class
We were missing a TRUE clause so we can append "AND class = ...".

Fixes: 4eb5a2c ("site: overview: Fix class stats not rendering")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/player.py
@@ -156,6 +156,7 @@ def overview(steamid):
                    total(dmg) / nullif(sum(shots), 0.0) AS dps
                FROM class
                LEFT JOIN classes USING (classid)
+               WHERE TRUE
                    {}
                GROUP BY classid
                ORDER BY classid