Forty-Bot/trends.tf

commit fa3b55ee89e1d0671b203d6565662cd51b8aafae

site: log: Add list-like canonical form
This changes the canonical url for combined logs from

	/log?id=1&id=2

to
	/log/1+2

which is more aesthetically pleasing. We redirect from the old url to allow
for form submissions and to preserve compatibility.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/wsgi.py
@@ -83,6 +83,17 @@ class StaticHashDefaults:
         self.cache[filename] = (mtime, hash)
         values['h'] = hash
 
+class IntListConverter(werkzeug.routing.BaseConverter):
+    def to_python(self, value):
+        return [int(val) for val in value.split('+')]
+
+    def to_url(self, values):
+        try:
+            return '+'.join(str(value) for value in values)
+        # flask turns (foo) into bare foo, so handle this special case
+        except TypeError:
+            return str(values)
+
 def create_app():
     app = flask.Flask(__name__)
     app.config.from_object(DefaultConfig)
@@ -90,6 +101,7 @@ def create_app():
 
     app.teardown_appcontext(put_db)
     app.url_defaults(StaticHashDefaults(app))
+    app.url_map.converters['intlist'] = IntListConverter
 
     app.add_template_filter(any)
     app.add_template_filter(all)

trends/site/root.py
@@ -149,9 +149,13 @@ def leaderboard():
                                  filters=filters, order=order, offset=offset, limit=limit)
 
 @root.route('/log')
-def log():
-    db = get_db()
+def log_form():
     logids = flask.request.args.getlist('id', int)
+    return flask.redirect(flask.url_for('.log', logids=logids))
+
+@root.route('/log/<intlist:logids>')
+def log(logids):
+    db = get_db()
     if not logids:
         flask.abort(404)
     elif len(logids) > 5:

trends/site/templates/log.html
@@ -181,7 +181,7 @@
 	       {% set comma = joiner(", ") %}
 	       {% for logid in logids -%}
 		       {{ comma() }}
-		       <a href="{{ url_for('root.log', id=logid) }}">{{ logid }}</a>
+		       <a href="{{ url_for('root.log', logids=logid) }}">{{ logid }}</a>
 	       {%- endfor %}
 	</h1>
 	<table><thead>
@@ -207,7 +207,7 @@
 		<td>{{ log['duration'] | duration }}</td>
 		<td>
 			{% if log['duplicate_of'] %}
-			<a href="{{ url_for('root.log', id=log['duplicate_of']) }}">
+			<a href="{{ url_for('root.log', logids=log['duplicate_of']) }}">
 				{{ log['duplicate_of'] }}
 			</a>
 			{% endif %}

trends/site/templates/logs.html
@@ -30,7 +30,7 @@
 			{% for log in logs %}
 			<tr>
 				<td class="left">
-					<a href="{{ url_for('root.log', id=log['logid']) }}">
+					<a href="{{ url_for('root.log', logids=log['logid']) }}">
 						{{ log['title'] }}
 				</a></td>
 				<td class="left">{{ log['map'] }}</td>

trends/site/templates/macros/logs.html
@@ -41,7 +41,7 @@
 				 value="{{ log['logid'] }}">
 			</td>
 			{% endif %}
-			<td class="left"><a href="{{ url_for('root.log', id=log['logid']) }}">
+			<td class="left"><a href="{{ url_for('root.log', logids=log['logid']) }}">
 				{{ log['title'] }}
 			</a></td>
 			<td class="left">{{ log['map'] }}</td>

trends/site/templates/player/logs.html
@@ -31,7 +31,7 @@
 		'acc': "Accuracy",
 		'date': "Date",
 	   }) }}
-	<form id="combine" action="{{ url_for('root.log') }}">
+	<form id="combine" action="{{ url_for('root.log_form') }}">
 		<button type="submit">Combine selected logs</button>
 	</form>
 	{{ navigation(logs, limit, offset) }}

commit 194c6028052ab0a112af7e6d8c7072e47af8eab1

site: Catch value errors in intlists
If someone passes a non-integer, we need to catch the error.

Fixes: fa3b55e ("site: log: Add list-like canonical form")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/wsgi.py
@@ -86,7 +86,10 @@ class StaticHashDefaults:
 
 class IntListConverter(werkzeug.routing.BaseConverter):
     def to_python(self, value):
-        return [int(val) for val in value.split('+')]
+        try:
+            return [int(val) for val in value.split('+')]
+        except ValueError:
+            return []
 
     def to_url(self, values):
         try: