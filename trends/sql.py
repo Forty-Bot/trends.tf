df5b017e2d47e2123c042a61cdf38f4854da5ee2
Sean Anderson <seanga2@gmail.com>
2021-12-29 17:06:18 -0500

    sql: Add tracing cursor
    This adds tracing to SQL queries using sentry. Hopefully this can give me
    some better insight into what to optimize.
    
    Signed-off-by: Sean Anderson <seanga2@gmail.com>

--- trends/sql.py
+++ trends/sql.py
@@ -8,6 +8,25 @@ import psycopg2, psycopg2.extras
 
 from .steamid import SteamID
 
+try:
+    from sentry_sdk import Hub, tracing_utils
+
+    class TracingCursor(psycopg2.extras.DictCursor):
+        def _log(self, query, vars, paramstyle=psycopg2.paramstyle):
+            return tracing_utils.record_sql_queries(Hub.current, self, query, vars,
+                                                    paramstyle, False)
+
+        def execute(self, query, vars=None):
+            with self._log(query, vars):
+                super().execute(query, vars)
+
+        def callproc(self, procname, vars=None):
+            with self._log(procname, vars, paramstyle=None):
+                super().callproc(procname, vars)
+
+except ImportError:
+    TracingCursor=psycopg2.extras.DictCursor
+
 def db_connect(url, name=None):
     """Setup a database connection
 
@@ -18,7 +37,7 @@ def db_connect(url, name=None):
 
     psycopg2.extensions.register_adapter(SteamID, psycopg2.extensions.AsIs)
     psycopg2.extensions.set_wait_callback(psycopg2.extras.wait_select)
-    c = psycopg2.connect(url, cursor_factory=psycopg2.extras.DictCursor,
+    c = psycopg2.connect(url, cursor_factory=TracingCursor,
                          application_name=name or " ".join(sys.argv))
     return c
 
