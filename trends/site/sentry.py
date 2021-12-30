253aa78ed6b060a6176205455fd90017abab6001
Sean Anderson <seanga2@gmail.com>
2021-12-29 21:01:12 -0500

    sentry: End template spans on exception
    If there is an exception while rendering a template, the span will never be
    exited. Fix this by listening for exceptions, and ending the span there. We
    need to use the global context because the exception will not have the
    template's context.
    
    Signed-off-by: Sean Anderson <seanga2@gmail.com>

--- trends/site/sentry.py
+++ trends/site/sentry.py
@@ -1,6 +1,8 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # Copyright (C) 2021 Sean Anderson <seanga2@gmail.com>
 
+import sys
+
 import blinker
 import flask
 import sentry_sdk
@@ -23,13 +25,18 @@ sentry_sdk.init(
 @flask.before_render_template.connect_via(blinker.ANY)
 def trace_template_start(app, template, context):
     span = sentry_sdk.Hub.current.start_span(op='render', description=template.name)
-    context['span'] = span
     span.set_data('context', context)
+    setattr(flask.g, 'span', span)
     span.__enter__()
 
 @flask.template_rendered.connect_via(blinker.ANY)
 def trace_template_finish(app, template, context):
-    context['span'].__exit__(None, None, None)
+    flask.g.pop('span').__exit__(None, None, None)
+
+@flask.got_request_exception.connect_via(blinker.ANY)
+def trace_template_error(app, exception):
+    if span := flask.g.pop('span', None):
+        span.__exit__(*sys.exc_info())
 
 class TracingCursor(psycopg2.extras.DictCursor):
     def _log(self, query, vars, paramstyle=psycopg2.paramstyle):
