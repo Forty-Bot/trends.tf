Forty-Bot/trends.tf

commit 35bf34f76d8eb013631e86b24cf9fe81b1c4ac22

site: logs: Limit subqueries to avoid poor estimates
Postgresql lacks the ability to consider foreign keys when joining tables,
and always considers columsn in separate tables to be completely unrelated.
This can have disastrous results when those columns are *not*, in fact,
unrelated, but instead have a 1-to-1 or 1-to-many relationship. This can
cause postgres to underestimate the number of rows returned by a join by
over 1000x. Unfortunately, there is no way to correct this through the
creation of additional statistics. It is possible to use adaptive query
optimization [1] to address this, but it involves training on specific
queries and is not a general solution. Additionally, I don't think the
linked extension is ready for production use (judging by the quality of
their code, documentation, and comments in said documentation). An attempt
was made [2] to upstream some of these strategies, but it has stalled.

So for now, all we can do is limit the rows on one side of the join. This
ensures that a 1000x underestimate is only a LIMITx overestimate. Obviously
not ideal, but hopefully this will reduce query times.

[1] https://github.com/postgrespro/aqo
[2] https://commitfest.postgresql.org/35/2958/
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/player.py
@@ -89,23 +89,29 @@ def get_logs(c, steamid, filters, duplicates=True, order_clause="logid DESC", li
                      array_agg(duration ORDER BY duration DESC) AS class_durations
                  FROM class_stats
                  JOIN class USING (classid)
-                 -- Duplicate of below, but sqlite is dumb...
+                 -- Duplicated to reduce the fallout from postgres's abysmal join estimates
                  WHERE steamid64 = %(steamid)s
                  GROUP BY logid, steamid64
+                 ORDER BY {1} NULLS LAST
+                 LIMIT %(limit)s OFFSET %(offset)s
            ) AS classes USING (logid, steamid64)
            LEFT JOIN (SELECT
                    logid,
                    steamid64,
                    total(hits) / nullif(sum(shots), 0.0) AS acc
                FROM weapon_stats
+               -- ditto
+               WHERE steamid64 = %(steamid)s
                GROUP BY logid, steamid64
+               ORDER BY {1} NULLS LAST
+               LIMIT %(limit)s OFFSET %(offset)s
            ) AS ws USING (logid, steamid64)
            LEFT JOIN heal_stats_given AS hsg USING (logid, steamid64)
            LEFT JOIN heal_stats_received AS hsr USING (logid, steamid64)
            LEFT JOIN class ON (primary_classid=classid)
            WHERE ps.steamid64 = %(steamid)s
-               {}
-           ORDER BY {} NULLS LAST
+               {0}
+           ORDER BY {1} NULLS LAST
            LIMIT %(limit)s OFFSET %(offset)s;""".format(filter_clauses, order_clause),
         { 'steamid': steamid, **filters, 'limit': limit, 'offset': offset })
     return logs

commit 6b33d97bcf5ac82d04e4ead2a3f328d88b2c03d0

site: log: Fix subsequent pages not rendering
There is some kind of subtle limit/offset interaction going on with
subqueries here. I am not sure what causes it, but it is fixed by joining
to classes with LEFT JOIN. This is ok, especially in light of 1d18a7b
("site: log: Display players without any classes correctly"). I am just not
very sure why it doesn't work in the first place. With this change, we also
need to ensure that we don't render the class list if the player has no
classes.

Fixes: 35bf34f ("site: logs: Limit subqueries to avoid poor estimates")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/player.py
@@ -82,7 +82,7 @@ def get_logs(c, steamid, filters, duplicates=True, order_clause="logid DESC", li
            JOIN player_stats AS ps USING (logid)
            JOIN map USING (mapid)
            LEFT JOIN format USING (formatid)
-           JOIN (SELECT
+           LEFT JOIN (SELECT
                      logid,
                      steamid64,
                      array_agg(class ORDER BY duration DESC) AS classes,

trends/site/templates/macros/logs.html
@@ -50,9 +50,11 @@
 			</td>
 			<td class="left">{{ log['map'] }}</td>
 			<td class="left">
+			{% if log['classes'] %}
 			{% for (cls, pct) in zip(log['classes'], log['class_pct']) %}
 				{{ classicon(cls, pct) }}
 			{% endfor %}
+			{% endif %}
 			</td>
 			<td>{{ wlt(log['wins'], log['losses'], log['ties']) }}</td>
 			<td class="left">{{ log['format'] }}</td>