Forty-Bot/trends.tf

commit 8e0b859d57a7a40a1138ba8afd88001c2cd8095b

Make teamid NOT NULL
This makes teamid NOT NULL, eliminating dummy rows for spectators. As a
consequence, chat now has a split foreign key, so that players do not have
to have played in the log to have chatted. This finally implements #35 for
real.

Temporary table foreign keys cannot reference real tables, so we can no
longer detect when a player is only present in chat by waiting for a
foreign key error. Work around this by always inserting into player. To
alleviate some of the impact of this, do nothing on conflict instead of
upserting.

I am experimenting with including the migrations for database changes.
Hopefully this will make it easier for those following along at home. In
the future these may be applied automatically, but for now they must be
applied manually.

Examples: 2844704, 2860321, 2788885, 3053999
Closes: #79
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/importer/logs.py
@@ -137,7 +137,11 @@ def import_log(cctx, c, logid, log):
                      );""", round)
 
     for steamid_str, player in log['players'].items():
-        steamid = None
+        # Some players don't have teams (they do actually have teams but they weren't parsed
+        # properly). Just ignore them, since we have no way to tell what team they were actually on.
+        if not player['team']:
+            continue
+
         try:
             steamid = SteamID(steamid_str)
         except ValueError:
@@ -338,45 +342,24 @@ def import_log(cctx, c, logid, log):
                              );""", weapon)
 
     for (seq, msg) in enumerate(log['chat']):
-        # poor man's goto...
-        first = True
-        while True:
-            try:
-                steamid = SteamID(msg['steamid']) if msg['steamid'] != 'Console' else None
-                c.execute("SAVEPOINT before_chat;")
-                c.execute("INSERT INTO chat (logid, steamid64, seq, msg) VALUES (%s, %s, %s, %s);",
-                          (logid, steamid, seq, msg['msg']))
+        try:
+            steamid = SteamID(msg['steamid']) if msg['steamid'] != 'Console' else None
+        except ValueError:
+            continue
 
-            # Spectator?
-            except psycopg2.errors.ForeignKeyViolation:
-                if not first:
-                    raise
-                first = False
-
-                c.execute("ROLLBACK TO SAVEPOINT before_chat;")
-                c.execute("INSERT INTO name (name) VALUES (%(name)s) ON CONFLICT DO NOTHING;", msg)
-                c.execute("""INSERT INTO player (
-                                 steamid64,
-                                 nameid,
-                                 last_active
-                             ) VALUES (
-                                 %s,
-                                 (SELECT nameid FROM name WHERE name = %s),
-                                 %s
-                             ) ON CONFLICT (steamid64)
-                             DO UPDATE SET
-                                 last_active = greatest(player.last_active, EXCLUDED.last_active);""",
-                          (steamid, msg['name'], info['date']))
-                c.execute("""INSERT INTO player_stats (
-                               logid, steamid64, nameid, kills, assists, deaths, dmg
-                           ) VALUES (
-                               %s, %s, (SELECT nameid FROM name WHERE name = %s), 0, 0, 0, 0
-                           );""",
-                          (logid, steamid, msg['name']))
-                continue
-            except ValueError:
-                break
-            break
+        c.execute("INSERT INTO name (name) VALUES (%(name)s) ON CONFLICT DO NOTHING;", msg)
+        c.execute("""INSERT INTO player (
+                         steamid64,
+                         nameid,
+                         last_active
+                     ) VALUES (
+                         %s,
+                         (SELECT nameid FROM name WHERE name = %s),
+                         %s
+                     ) ON CONFLICT (steamid64) DO NOTHING;""",
+                  (steamid, msg['name'], info['date']))
+        c.execute("INSERT INTO chat (logid, steamid64, seq, msg) VALUES (%s, %s, %s, %s);",
+                  (logid, steamid, seq, msg['msg']))
 
     for (healer, healees) in log['healspread'].items():
         try:
@@ -695,8 +678,6 @@ def import_logs(args, c):
     cur.execute("""ALTER TABLE heal_stats
                    ADD FOREIGN KEY (logid, healer) REFERENCES player_stats (logid, steamid64),
 	               ADD FOREIGN KEY (logid, healee) REFERENCES player_stats (logid, steamid64);""")
-    cur.execute("""ALTER TABLE chat
-                   ADD FOREIGN KEY (logid, steamid64) REFERENCES player_stats (logid, steamid64)""")
     # We need this index to calculate formats efficiently
     cur.execute("CREATE INDEX class_stats_logid ON class_stats (logid);")
     # And this index to limit dupes

trends/migrations/chat_fk_split.sql
@@ -1,0 +1,4 @@
+BEGIN;
+ALTER TABLE chat ADD FOREIGN KEY (steamid64) REFERENCES player (steamid64);
+ALTER TABLE chat DROP CONSTRAINT chat_logid_steamid64_fkey;
+COMMIT;

trends/migrations/teamid_notnull.sql
@@ -1,0 +1,15 @@
+-- depends chat_fk_split.sql
+
+BEGIN;
+CREATE TEMP TABLE to_delete ON COMMIT DROP AS
+	SELECT logid, steamid64 FROM player_stats WHERE teamid ISNULL FOR UPDATE;
+DELETE FROM weapon_stats WHERE (logid, steamid64) IN (SELECT logid, steamid64 FROM to_delete);
+DELETE FROM class_stats WHERE (logid, steamid64) IN (SELECT logid, steamid64 FROM to_delete);
+DELETE FROM event_stats WHERE (logid, steamid64) IN (SELECT logid, steamid64 FROM to_delete);
+DELETE FROM medic_stats WHERE (logid, steamid64) IN (SELECT logid, steamid64 FROM to_delete);
+DELETE FROM heal_stats WHERE (logid, healer) IN (SELECT logid, steamid64 FROM to_delete);
+DELETE FROM heal_stats WHERE (logid, healee) IN (SELECT logid, steamid64 FROM to_delete);
+DELETE FROM player_stats_extra WHERE (logid, steamid64) IN (SELECT logid, steamid64 FROM to_delete);
+DELETE FROM player_stats WHERE (logid, steamid64) IN (SELECT logid, steamid64 FROM to_delete);
+ALTER TABLE player_stats ALTER teamid SET NOT NULL;
+COMMIT;

trends/schema.sql
@@ -152,7 +152,7 @@ CREATE TABLE IF NOT EXISTS player_stats (
 	logid INT REFERENCES log (logid) NOT NULL,
 	steamid64 BIGINT REFERENCES player (steamid64) NOT NULL,
 	nameid INT NOT NULL REFERENCES name (nameid),
-	teamid INT REFERENCES team (teamid), -- May be NULL for spectators
+	teamid INT NOT NULL REFERENCES team (teamid),
 	kills INT NOT NULL,
 	assists INT NOT NULL,
 	deaths INT NOT NULL,
@@ -363,11 +363,10 @@ CREATE INDEX IF NOT EXISTS event_logid ON event_stats (logid);
 
 CREATE TABLE IF NOT EXISTS chat (
 	logid INT NOT NULL REFERENCES log (logid),
-	steamid64 BIGINT, -- May be NULL for Console messages
+	steamid64 BIGINT REFERENCES player (steamid64), -- May be NULL for Console messages
 	seq INT NOT NULL, -- Message sequence, starting at 0; earlier messages have lower sequences
 	msg TEXT NOT NULL,
-	PRIMARY KEY (logid, seq),
-	FOREIGN KEY (logid, steamid64) REFERENCES player_stats (logid, steamid64)
+	PRIMARY KEY (logid, seq)
 );
 
 -- Reverse index for lookups by steamid64

commit 91927c05427896f062b40b2df6be8fbf3e501377

importer: Fix importing logs with console messages
Console messages have a NULL steamid, so we cannot unconditionally try to
insert an entry for them into players.

Fixes: 8e0b859 ("Make teamid NOT NULL")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/importer/logs.py
@@ -348,16 +348,17 @@ def import_log(cctx, c, logid, log):
             continue
 
         c.execute("INSERT INTO name (name) VALUES (%(name)s) ON CONFLICT DO NOTHING;", msg)
-        c.execute("""INSERT INTO player (
-                         steamid64,
-                         nameid,
-                         last_active
-                     ) VALUES (
-                         %s,
-                         (SELECT nameid FROM name WHERE name = %s),
-                         %s
-                     ) ON CONFLICT (steamid64) DO NOTHING;""",
-                  (steamid, msg['name'], info['date']))
+        if steamid:
+            c.execute("""INSERT INTO player (
+                             steamid64,
+                             nameid,
+                             last_active
+                         ) VALUES (
+                             %s,
+                             (SELECT nameid FROM name WHERE name = %s),
+                             %s
+                         ) ON CONFLICT (steamid64) DO NOTHING;""",
+                      (steamid, msg['name'], info['date']))
         c.execute("INSERT INTO chat (logid, steamid64, seq, msg) VALUES (%s, %s, %s, %s);",
                   (logid, steamid, seq, msg['msg']))
 