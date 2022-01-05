Forty-Bot/trends.tf

commit 0c966317550dc2dd0dda7b0454a3f45bb7a087f2

Use Salt to manage configuration
This adds salt files to configure an entire new trends.tf server from just
a root login over ssh. This includes bootstrapping a new "admin" user,
setting up the database, configuring nginx and uwsgi, and setting up all
the other miscellaneous configs (such as scheduled services). Finally,
almost all configuration is in git, and setting up a new server is (almost)
as easy as `salt-ssh foo state.apply' (missing top.sls notwithstanding).

Phew.

Things still to do include:
- Document the whole process
- Reload uwsgi automatically
- Fix the ugliness wrt where to make nginx look for static files
- Use more variables to make changing things easier
- Grab certificates (from pillar?)
- Try not to run module.run and cmd.run states

Closes: #10
Signed-off-by: Sean Anderson <seanga2@gmail.com>

salt/database.sls
@@ -1,0 +1,181 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+# Copyright (C) 2021 Sean Anderson <seanga2@gmail.com>
+
+# Set up the postgres database
+# There are a lot of exceptions for Debian because it comes with "batteries included"
+
+xfsprogs:
+  pkg.installed:
+    - refresh: False
+
+{% if grains.manufacturer == "DigitalOcean" %}
+  {% set db_dev = "/dev/sda" %}
+{% endif %}
+
+{{ db_dev }}:
+  blockdev.formatted:
+    - fs_type: xfs
+    - require:
+      - xfsprogs
+  mount.fstab_present:
+    - fs_file: /srv
+    - fs_vfstype: xfs
+    - fs_passno: 2
+    - require:
+      - blockdev: {{ db_dev }}
+
+{% set pg_version = 13 %}
+{% if grains.os_family == 'Debian' %}
+{% set pg_confdir = "/etc/postgresql/13/data" %}
+postgresql-common:
+  pkg.installed:
+    - refresh: False
+
+/etc/postgresql-common/createcluster.d/override.conf:
+  file.managed:
+    - makedirs: True
+    - contents: |
+        create_main_cluster = false
+        waldir = '/var/lib/postgres/data/pg_wal'
+    - require:
+      - postgresql-common
+{% else %}
+{% set pg_confdir = "/srv/postgres/data" %}
+{% endif %}
+
+postgresql:
+  pkg.installed:
+    - refresh: False
+    - version: '{{ pg_version }}*'
+    {% if grains.os_family == 'Debian' %}
+    - require:
+      - /etc/postgresql-common/createcluster.d/override.conf
+    {% endif %}
+
+/var/lib/postgres:
+  file.directory:
+    - require:
+      - pkg: postgresql
+
+/var/lib/postgres/data:
+  file.directory:
+    - user: postgres
+    - group: postgres
+    - mode: 700
+    - require:
+      - /var/lib/postgres
+
+/srv/postgres:
+  file.directory:
+    - user: postgres
+    - group: postgres
+    - mode: 700
+    - require:
+      - pkg: postgresql
+
+/srv/postgres/data:
+  file.directory:
+    - user: postgres
+    - group: postgres
+    - mode: 700
+    - require:
+      - /srv/postgres
+
+initdb:
+  {% if grains.os_family == 'Debian' %}
+  postgres_cluster.present:
+    - name: data
+    - version: {{ pg_version }}
+    - datadir: /srv/postgres/data
+  {% else %}
+  postgres_initdb.present:
+    - name: /srv/postgres/data
+    - runas: postgres
+    - auth: peer
+    - waldir: /var/lib/postgres/data/pg_wal
+  {% endif %}
+    - require:
+      - /var/lib/postgres/data
+      - file: /srv/postgres/data
+
+pg_includedir:
+  file.replace:
+    - name: {{ pg_confdir }}/postgresql.conf
+    - pattern: "^.?include_dir.*$"
+    - repl: include_dir = 'conf.d'
+    - append_if_not_found: True
+    - require:
+      - initdb
+
+# Budget pgtune
+{%- set connections = 20 %}
+{%- set mem = grains.mem_total * 1024 %}
+{%- set shared_buffers = (mem / 4) | int %}
+{%- set work_mem = ((mem - shared_buffers) / connections / 2) | int %}
+{{ pg_confdir }}/conf.d/override.conf:
+  file.managed:
+    - makedirs: True
+    - user: postgres
+    - group: postgres
+    - contents: |
+        max_connections = {{ connections }}
+        shared_buffers = {{ shared_buffers }}kB
+        work_mem = {{ work_mem }}kB
+        maintenance_work_mem = {{ (shared_buffers * 2, 2 * 1024 * 1024) | min }}kB
+        effective_io_concurrency = 200
+        wal_buffers = 16MB
+        min_wal_size = 1GB
+        max_wal_size = 4GB
+        checkpoint_completion_target = 0.9
+        random_page_cost = 2
+        effective_cache_size = {{ (mem * 3 / 4) | int }}kB
+        default_statistics_target = 500
+        shared_preload_libraries = 'pg_stat_statements'
+
+postgresql.service:
+  service.running:
+    - enable: True
+    - watch:
+      - initdb
+    - require:
+      - pg_includedir
+      - {{ pg_confdir }}/conf.d/override.conf
+{% if grains.os_family != 'Debian' %}
+      - pg_service
+
+/etc/systemd/system/postgresql.service.d/override.conf:
+  file.managed:
+    - makedirs: True
+    - contents: |
+        [Service]
+        Environment=PGROOT=/srv/postgres
+        PIDFile=/srv/postgres/data/postmaster.pid
+
+pg_service:
+  module.run:
+    - name: service.systemctl_reload
+    - onchanges:
+      - /etc/systemd/system/postgresql.service.d/override.conf
+    - require:
+      - postgresql
+{% endif %}
+
+daemon:
+  postgres_user.present:
+    - runas: postgres
+    - require:
+      - postgresql.service
+
+trends:
+  postgres_database.present:
+    - runas: postgres
+    - owner: daemon
+    - require:
+      - daemon
+
+salt://schema.sql:
+  cmd.script:
+    - args: -d trends
+    - runas: daemon
+    - require:
+      - trends

Saltfile
@@ -1,0 +1,2 @@
+salt-ssh:
+  config_dir: salt

salt/.gitignore
@@ -1,0 +1,5 @@
+# Files created by salt
+etc/salt/
+var/
+# Files the user should create
+id_rsa.pub

salt/backend.sls
@@ -1,0 +1,96 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+# Copyright (C) 2021 Sean Anderson <seanga2@gmail.com>
+
+# Set up the backend (the import scripts)
+
+{% set prefix = "/srv/uwsgi/trends" %}
+
+/etc/systemd/system/log_import.service:
+  file.managed:
+    - contents: |
+        [Unit]
+        Description=Import logs from logs.tf
+        
+        [Service]
+        Type=oneshot
+        ExecStart={{ prefix }}/bin/trends_importer -vv logs bulk -c 1000 postgres:///trends
+        User=daemon
+
+/etc/systemd/system/log_import.timer:
+  file.managed:
+    - contents: |
+        [Unit]
+        Description=Import from logs.tf every 5 minutes
+        
+        [Timer]
+        OnCalendar=*:0/5
+        
+        [Install]
+        WantedBy=timers.target
+
+/etc/systemd/system/player_import.service:
+  file.managed:
+    - contents: |
+        [Unit]
+        Description=Import players from steam
+        
+        [Service]
+        Type=simple
+        EnvironmentFile=/etc/default/trends
+        ExecStart={{ prefix }}/bin/trends_importer players -k ${STEAMKEY} -w 1 random postgres:///trends
+        User=daemon
+        Restart=on-failure
+        
+        [Install]
+        WantedBy=multi-user.target
+
+/etc/systemd/system/leaderboard_refresh.service:
+  file.managed:
+    - contents: |
+        [Unit]
+        Description=Refresh leaderboard
+        
+        [Service]
+        Type=oneshot
+        ExecStart=/usr/bin/psql -c 'REFRESH MATERIALIZED VIEW CONCURRENTLY leaderboard_cube' postgres:///trends
+        User=daemon
+
+/etc/systemd/system/leaderboard_refresh.timer:
+  file.managed:
+    - contents: |
+        [Unit]
+        Description=Daily leaderboard refresh
+        
+        [Timer]
+        OnCalendar=7:00
+        
+        [Install]
+        WantedBy=timers.target
+
+backend_services:
+  module.run:
+    - name: service.systemctl_reload
+    - onchanges:
+      - /etc/systemd/system/log_import.service
+      - /etc/systemd/system/log_import.timer
+      - /etc/systemd/system/player_import.service
+      - /etc/systemd/system/leaderboard_refresh.service
+      - /etc/systemd/system/leaderboard_refresh.timer
+
+log_import.timer:
+  service.running:
+    - enable: True
+    - require:
+      - backend_services
+
+player_import.service:
+  service.running:
+    - enable: True
+    - require:
+      - backend_services
+
+leaderboard_refresh.timer:
+  service.running:
+    - enable: True
+    - require:
+      - backend_services

salt/bootstrap.sls
@@ -1,0 +1,69 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+# Copyright (C) 2021 Sean Anderson <seanga2@gmail.com>
+
+# Set up an admin user 'sean' with sudo and ssh access, then disable root login
+
+sudo:
+  pkg.installed:
+    - refresh: False
+
+/etc/sudoers.d/wheel:
+  file.managed:
+    - contents: |
+        %wheel ALL=(ALL) NOPASSWD: ALL
+    - require:
+      - sudo
+
+wheel:
+  group.present:
+    - system: True
+
+sean:
+  user.present:
+    - empty_password: True
+    - groups:
+      - wheel
+      - systemd-journal
+    - require:
+      - wheel
+
+ssh key:
+  ssh_auth.present:
+    - user: sean
+    - source: salt://id_rsa.pub
+    - require:
+      - sean
+
+ssh:
+  pkg.installed:
+    - name:
+       {% if grains.os_family == 'Debian' %}
+       openssh-server
+       {% else %}
+       openssh
+       {% endif %}
+    - refresh: False
+
+sshd:
+  service.running:
+    - enable: True
+    - require:
+      - ssh
+
+/root/.ssh/authorized_keys:
+  file.absent:
+    - require:
+      - sshd
+      - ssh key
+      - /etc/sudoers.d/wheel
+
+shadow.lock_password:
+  module.run:
+    - m_name: root
+    - watch:
+      - /root/.ssh/authorized_keys
+
+arch:
+  user.absent:
+    - require:
+      - sshd

salt/common.sls
@@ -1,0 +1,25 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+# Copyright (C) 2021 Sean Anderson <seanga2@gmail.com>
+
+update_system:
+  pkg.uptodate:
+    - refresh: True
+{% if grains.os_family != 'Debian' %}
+    - require:
+      - kernel-modules-hook
+
+# Prevent pacman from removing modules for the running kernel
+kernel-modules-hook:
+  pkg.installed:
+    - refresh: False
+{% endif %}
+
+vim:
+  pkg.installed:
+    - refresh: False
+
+nano:
+  pkg.purged:
+    - refresh: False
+    - require:
+      - vim

salt/etc/nginx/nginx.conf
@@ -1,0 +1,62 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+# Copyright (C) 2021 Sean Anderson <seanga2@gmail.com>
+
+{%- if os == 'Debian' %}
+user www-data;
+pid /run/nginx.pid;
+{% else %}
+user http;
+{% endif %}
+worker_processes auto;
+include /etc/nginx/modules-enabled/*.conf;
+
+events {
+	multi_accept on;
+	worker_connections 65535;
+}
+
+http {
+	# Basic Settings
+	charset utf-8;
+	sendfile on;
+	tcp_nopush on;
+	tcp_nodelay on;
+	server_tokens off;
+	log_not_found off;
+	keepalive_timeout 65;
+	types_hash_max_size 4096;
+	client_max_body_size 16M;
+
+	include /etc/nginx/mime.types;
+	default_type application/octet-stream;
+
+	# SSL Settings
+	ssl_protocols TLSv1.3;
+	ssl_session_timeout 1d;
+	ssl_session_cache shared:SSL:10m;
+	ssl_session_tickets off;
+	ssl_prefer_server_ciphers off;
+
+	ssl_stapling on;
+	ssl_stapling_verify on;
+	resolver 127.0.0.53;
+	resolver_timeout 2s;
+
+	# Logging Settings
+	access_log /var/log/nginx/access.log;
+	error_log /var/log/nginx/error.log;
+
+	# Gzip Settings
+	gzip on;
+	gzip_vary on;
+	gzip_proxied any;
+	gzip_comp_level 6;
+	gzip_types text/plain text/css application/json application/javascript text/xml
+	           application/xml application/xml+rss text/javascript;
+
+	include /etc/nginx/conf.d/*.conf;
+	include /etc/nginx/sites-enabled/*;
+
+	# uWSGI response caching
+	uwsgi_cache_path /var/cache/nginx/ keys_zone=cache:10m;
+}

salt/etc/nginx/sites-available/trends.tf
@@ -1,0 +1,93 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+# Copyright (C) 2021 Sean Anderson <seanga2@gmail.com>
+
+{% set certdir = "/etc/letsencrypt/live/trends.tf" %}
+server {
+	listen                  443 ssl http2;
+	listen                  [::]:443 ssl http2;
+	server_name             trends.tf;
+
+	# SSL
+	ssl_certificate         {{ certdir }}/fullchain.pem;
+	ssl_certificate_key     {{ certdir }}/privkey.pem;
+	ssl_trusted_certificate {{ certdir }}/chain.pem;
+
+	# security
+	add_header X-Frame-Options           "SAMEORIGIN" always;
+	add_header X-XSS-Protection          "1; mode=block" always;
+	add_header X-Content-Type-Options    "nosniff" always;
+	add_header Referrer-Policy           "no-referrer-when-downgrade" always;
+	add_header Content-Security-Policy   "default-src 'self' http: https: data: blob: 'unsafe-inline'" always;
+	add_header Strict-Transport-Security "max-age=31536000; includeSubDomains" always;
+	
+	# . files
+	location ~ /\.(?!well-known) {
+		deny all;
+	}
+
+	location / {
+		# default uwsgi_params
+		include                       uwsgi_params;
+
+		# uwsgi settings
+		uwsgi_pass                    unix:{{ uwsgi_socket }};
+		uwsgi_param Host              $host;
+		uwsgi_param X-Real-IP         $remote_addr;
+		uwsgi_param X-Forwarded-For   $proxy_add_x_forwarded_for;
+		uwsgi_param X-Forwarded-Proto $http_x_forwarded_proto;
+
+		# cache settings
+		uwsgi_cache cache;
+		uwsgi_cache_key $request_uri;
+		uwsgi_cache_lock on;
+		uwsgi_cache_use_stale updating;
+		uwsgi_cache_valid 200 5m;
+	}
+
+	location ^~ /static/ {
+		root       {{ site_packages }}/trends/site/;
+		expires    365d;
+		access_log off;
+	}
+
+	# favicon.ico
+	location = /favicon.ico {
+		access_log off;
+		return 301 https://$host/static/img/favicon.ico;
+	}
+
+	# robots.txt
+	location = /robots.txt {
+		access_log off;
+		return 301 https://$host/static/robots.txt;
+	}
+}
+
+# subdomains redirect
+server {
+	listen                  443 ssl http2;
+	listen                  [::]:443 ssl http2;
+	server_name             *.trends.tf;
+
+	# SSL
+	ssl_certificate         {{ certdir }}/fullchain.pem;
+	ssl_certificate_key     {{ certdir }}/privkey.pem;
+	ssl_trusted_certificate {{ certdir }}/chain.pem;
+	return                  301 https://trends.tf$request_uri;
+}
+
+# HTTP redirect
+server {
+	listen      80;
+	listen      [::]:80;
+	server_name .trends.tf;
+
+	# ACME-challenge
+	location ^~ /.well-known/acme-challenge/ {
+		root /var/lib/letsencrypt/;
+	}
+
+	location / {
+		return 301 https://$host$request_uri;
+	}
+}

commit 30de8fa0d28324dce0349bafa0fcd15ade8b1ea0

salt: Reduce maintenance work memory
When converting pgtune to salt, I accidentally used page_cache multiplied
by two instead of divided by two. This has been causing maintenance
operations like refreshing materialized views, clustering tables, and
performing migrations to fail.

Fixes: 0c96631 ("Use Salt to manage configuration")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

salt/database.sls
@@ -128,7 +128,7 @@ pg_includedir:
         max_connections = {{ connections }}
         shared_buffers = {{ shared_buffers }}kB
         work_mem = {{ work_mem }}kB
-        maintenance_work_mem = {{ (shared_buffers * 2, 2 * 1024 * 1024) | min }}kB
+        maintenance_work_mem = {{ ((shared_buffers / 2 | int), 2 * 1024 * 1024) | min }}kB
         effective_io_concurrency = 200
         wal_buffers = 16MB
         min_wal_size = 1GB