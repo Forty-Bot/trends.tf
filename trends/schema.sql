Forty-Bot/trends.tf

commit c545eac074de15359d73c66b9cfa7cf2960b676b

Use materialized view for map popularity
Map suggestions are rarely requested, so they tend not to be in cache. They
also take a while to calculate, since we need to do a full scan of log.
However, they are not very time-sensitive (popularity does not change much
over the course of a day), so we can use a materialized view instead.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/schema.sql
@@ -84,6 +84,21 @@ CREATE INDEX IF NOT EXISTS log_title ON log USING gin (title gin_trgm_ops);
 -- To filter by date
 CREATE INDEX IF NOT EXISTS log_time ON log (time);
 
+CREATE MATERIALIZED VIEW map_popularity AS
+SELECT
+	mapid,
+	popularity,
+	map
+FROM map
+JOIN (SELECT
+		mapid,
+		count(*) AS popularity
+	FROM log
+	GROUP BY mapid
+) AS map_popularity USING (mapid)
+ORDER BY popularity DESC, mapid ASC
+WITH NO DATA;
+
 -- The original json, zstd compressed
 CREATE TABLE IF NOT EXISTS log_json (
 	logid INTEGER PRIMARY KEY REFERENCES log (logid),

salt/backend.sls
@@ -60,12 +60,36 @@
     - contents: |
         [Unit]
         Description=Daily leaderboard refresh
-        
+
         [Timer]
         OnCalendar=7:00
-        
+
         [Install]
         WantedBy=timers.target
+
+/etc/systemd/system/map_refresh.service:
+  file.managed:
+    - contents: |
+        [Unit]
+        Description=Refresh map popularity
+
+        [Service]
+        Type=oneshot
+        ExecStart=/usr/bin/psql -c 'REFRESH MATERIALIZED VIEW CONCURRENTLY map_popularity' postgres:///trends
+        User=daemon
+
+/etc/systemd/system/map_refresh.timer:
+  file.managed:
+    - contents: |
+        [Unit]
+        Description=Daily map popularity refresh
+
+        [Timer]
+        OnCalendar=6:45
+
+        [Install]
+        WantedBy=timers.target
+
 
 /etc/systemd/system/weapon_import.service:
   file.managed:
@@ -99,6 +123,8 @@ backend_services:
       - /etc/systemd/system/player_import.service
       - /etc/systemd/system/leaderboard_refresh.service
       - /etc/systemd/system/leaderboard_refresh.timer
+      - /etc/systemd/system/map_refresh.service
+      - /etc/systemd/system/map_refresh.timer
       - /etc/systemd/system/weapon_import.service
       - /etc/systemd/system/weapon_import.timer
 
@@ -115,6 +141,12 @@ player_import.service:
       - backend_services
 
 leaderboard_refresh.timer:
+  service.running:
+    - enable: True
+    - require:
+      - backend_services
+
+map_refresh.timer:
   service.running:
     - enable: True
     - require:

trends/site/api.py
@@ -31,18 +31,11 @@ def logs():
 def maps():
     limit, offset = get_pagination(limit=500)
     maps = get_db().cursor()
-    maps.execute("""SELECT
-                     map
-                 FROM map
-                 JOIN (SELECT
-                         mapid,
-                         count(*) AS popularity
-                     FROM log
-                     GROUP BY mapid
-                 ) AS log_nodups USING (mapid)
-                 ORDER BY popularity DESC, mapid ASC
-                 LIMIT %s OFFSET %s;""",
-                 (limit, offset))
+    maps.execute(
+        """SELECT map
+           FROM map_popularity
+           ORDER BY popularity DESC, mapid ASC
+           LIMIT %s OFFSET %s;""", (limit, offset))
     return flask.jsonify(maps=[row[0] for row in maps])
 
 @api.route('/players')

commit 60bb6206b881aecbd1e0f015e4900340b2f1436d

schema: Create map_popularity only if it does not exist
This will result in an error otherwise.

Fixes: c545eac ("Use materialized view for map popularity")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/schema.sql
@@ -84,7 +84,7 @@ CREATE INDEX IF NOT EXISTS log_title ON log USING gin (title gin_trgm_ops);
 -- To filter by date
 CREATE INDEX IF NOT EXISTS log_time ON log (time);
 
-CREATE MATERIALIZED VIEW map_popularity AS
+CREATE MATERIALIZED VIEW IF NOT EXISTS map_popularity AS
 SELECT
 	mapid,
 	popularity,