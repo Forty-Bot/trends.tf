Forty-Bot/trends.tf

commit f883a4daa3cb5f6bd54a127d69d16b366f2df2d6

schema: Add accuracy to player_stats
This allows calculation of total accuracy directly from player_stats,
without joining into weapon_stats.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/schema.sql
@@ -165,7 +165,10 @@ CREATE TABLE IF NOT EXISTS player_stats (
 	classids INT[] CHECK (array_position(classids, NULL) ISNULL AND array_ndims(classids) = 1),
 	class_durations INT[] CHECK (array_position(class_durations, NULL) ISNULL
 				     AND array_ndims(class_durations) = 1),
+	shots INT,
+	hits INT,
 	PRIMARY KEY (steamid64, logid),
+	CHECK ((shots NOTNULL AND hits NOTNULL) OR (shots ISNULL AND hits ISNULL)),
 	CHECK ((classids NOTNULL AND class_durations NOTNULL
 		AND array_length(classids, 1) = array_length(class_durations, 1))
 	       OR (classids ISNULL AND class_durations ISNULL))

trends/importer/logs.py
@@ -604,6 +604,45 @@ def update_player_classes(cur, bounds=None):
                    .format("WHERE logid BETWEEN %s AND %s" if bounds else ""),
                 bounds)
 
+def update_acc(cur, bounds=None):
+    cur.execute("""UPDATE class_stats AS cs SET
+                        hits = new.hits,
+                        shots = new.shots
+                    FROM (SELECT
+                            logid,
+                            steamid64,
+                            classid,
+                            sum(hits) AS hits,
+                            sum(shots) AS shots
+                        FROM weapon_stats
+                        {}
+                        GROUP BY logid, steamid64, classid
+                        ORDER BY steamid64, logid, classid
+                    ) AS new
+                    WHERE cs.logid = new.logid
+                        AND cs.steamid64 = new.steamid64
+                        AND cs.classid = new.classid"""
+                 .format("WHERE logid BETWEEN %s AND %s" if bounds else ""),
+              bounds)
+
+    cur.execute("""UPDATE player_stats AS ps SET
+                        hits = new.hits,
+                        shots = new.shots
+                    FROM (SELECT
+                            logid,
+                            steamid64,
+                            sum(hits) AS hits,
+                            sum(shots) AS shots
+                        FROM weapon_stats
+                        {}
+                        GROUP BY logid, steamid64
+                        ORDER BY steamid64, logid
+                    ) AS new
+                    WHERE ps.logid = new.logid
+                        AND ps.steamid64 = new.steamid64"""
+                 .format("WHERE logid BETWEEN %s AND %s" if bounds else ""),
+              bounds)
+
 def create_logs_parser(sub):
     class LogAction(argparse.Action):
         def __init__(self, option_strings, dest, **kwargs):
@@ -724,6 +763,7 @@ def import_logs(args, c):
         update_formats(cur)
         update_wlt(cur)
         update_player_classes(cur)
+        update_acc(cur)
         for table in temp_tables:
             set_clause = ", ".join("{}=EXCLUDED.{}".format(col, col)
                                    for col in table_columns(c, table[0]))

trends/migrations/stats_acc.py
@@ -1,0 +1,35 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+# Copyright (C) 2022 Sean Anderson <seanga2@gmail.com>
+
+import logging
+import sys
+
+from ..sql import db_connect, db_init
+from ..importer.cli import init_logging
+from ..importer.logs import update_acc
+
+def migrate():
+    init_logging(logging.DEBUG)
+    with db_connect(sys.argv[1]) as c:
+        logging.info("BEGIN")
+        cur = c.cursor()
+        cur.execute("BEGIN;")
+        for table in ("player_stats", "class_stats"):
+            for statement in (
+                "ALTER TABLE {} ADD IF NOT EXISTS hits INT;",
+                "ALTER TABLE {} ADD IF NOT EXISTS shots INT;",
+                "ALTER TABLE {} ADD " \
+                    "CHECK ((shots NOTNULL AND hits NOTNULL) OR (shots ISNULL AND hits ISNULL));",
+            ):
+                cur.execute(statement.format(table))
+        cur.execute("COMMIT;")
+        logging.info("ALTER TABLES")
+
+        step = 100000
+        cur.execute("SELECT min(logid), max(logid) FROM log;")
+        for i in range(*cur.fetchone(), step):
+            update_acc(cur, bounds=(i, i + step))
+            logging.info("UPDATE {}".format(i))
+
+if __name__ == "__main__":
+    migrate()

commit 96bf13e5fe6ef7dba0f5b306f562ec8222db1328

Add accuracy columsn to class_stats
We also need accuracy columns in class stats.

Fixes: f883a4d ("schema: Add accuracy to player_stats")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/schema.sql
@@ -309,8 +309,11 @@ CREATE TABLE IF NOT EXISTS class_stats (
 	deaths INT NOT NULL,
 	dmg INT NOT NULL,
 	duration INT NOT NULL,
+	shots INT,
+	hits INT,
 	PRIMARY KEY (steamid64, logid, classid),
-	FOREIGN KEY (logid, steamid64) REFERENCES player_stats_backing (logid, steamid64)
+	FOREIGN KEY (logid, steamid64) REFERENCES player_stats_backing (logid, steamid64),
+	CHECK ((shots NOTNULL AND hits NOTNULL) OR (shots ISNULL AND hits ISNULL))
 );
 
 -- For logs page