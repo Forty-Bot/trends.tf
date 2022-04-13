Forty-Bot/trends.tf

commit a53d72c2d5929cfa009417d14b7450620e324863

schema: Remove primary_classid from player_stats
We can now calculate this information from classids and class_durations. Or
rather, we can calculate it relative to the other classes a player played.
I think this is likely good enough, outside of some strange logs with
negative duration.

To accomplish this, we create a view in place of the player stats table,
and rename the table to player_stats_backing. The view also contains some
commonly-used stats like class names and percentages.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/schema.sql
@@ -5,6 +5,11 @@
 CREATE EXTENSION IF NOT EXISTS pg_trgm;
 CREATE EXTENSION IF NOT EXISTS tsm_system_rows;
 
+-- Compute the sum of an array
+CREATE OR REPLACE FUNCTION array_sum(anyarray) RETURNS anyelement
+	AS 'SELECT sum(val) FROM unnest($1) AS val'
+	LANGUAGE SQL IMMUTABLE STRICT PARALLEL SAFE;
+
 -- Compatibility functions for migration from SQLite
 
 CREATE OR REPLACE FUNCTION add(FLOAT, anyelement) RETURNS FLOAT
@@ -148,7 +153,7 @@ INSERT INTO class (class) VALUES
 	('spy')
 ON CONFLICT DO NOTHING;
 
-CREATE TABLE IF NOT EXISTS player_stats (
+CREATE TABLE IF NOT EXISTS player_stats_backing (
 	logid INT REFERENCES log (logid) NOT NULL,
 	steamid64 BIGINT REFERENCES player (steamid64) NOT NULL,
 	nameid INT NOT NULL REFERENCES name (nameid),
@@ -161,7 +166,6 @@ CREATE TABLE IF NOT EXISTS player_stats (
 	wins INT NOT NULL DEFAULT 0, -- rounds won
 	losses INT NOT NULL DEFAULT 0, -- rounds lost
 	ties INT NOT NULL DEFAULT 0, -- rounds tied
-	primary_classid INT REFERENCES class (classid), -- Class played more than 2/3 of the time
 	classids INT[] CHECK (array_position(classids, NULL) ISNULL AND array_ndims(classids) = 1),
 	class_durations INT[] CHECK (array_position(class_durations, NULL) ISNULL
 				     AND array_ndims(class_durations) = 1),
@@ -176,10 +180,24 @@ CREATE TABLE IF NOT EXISTS player_stats (
 
 -- This index includes steamid64 and team so that it can be used as a covering index for the peers
 -- query. This avoids a bunch of costly random reads to player_stats.
-CREATE INDEX IF NOT EXISTS player_stats_peers ON player_stats (logid) INCLUDE (steamid64, teamid);
+CREATE INDEX IF NOT EXISTS player_stats_peers
+	ON player_stats_backing (logid)
+	INCLUDE (steamid64, teamid);
 
 -- Covering index for name FTS queries
-CREATE INDEX IF NOT EXISTS player_stats_names ON player_stats (nameid) INCLUDE (steamid64);
+CREATE INDEX IF NOT EXISTS player_stats_names ON player_stats_backing (nameid) INCLUDE (steamid64);
+
+CREATE OR REPLACE VIEW player_stats AS SELECT
+	player_stats_backing.*,
+	CASE WHEN class_durations[1] * 1.5 > array_sum(class_durations)
+		THEN classids[1]
+	END AS primary_classid,
+	(SELECT array_agg(class)
+	 FROM unnest(classids) AS classid
+	 JOIN class USING (classid)) AS classes,
+	(SELECT array_agg(duration * 1.0 / array_sum(class_durations))
+	 FROM unnest(class_durations) AS duration) AS class_pct
+FROM player_stats_backing;
 
 CREATE TABLE IF NOT EXISTS player_stats_extra (
 	logid INT NOT NULL,
@@ -200,7 +218,7 @@ CREATE TABLE IF NOT EXISTS player_stats_extra (
 	cpc INT, -- Capture Point Captures
 	ic INT, -- Intel Captures
 	PRIMARY KEY (steamid64, logid),
-	FOREIGN KEY (logid, steamid64) REFERENCES player_stats (logid, steamid64),
+	FOREIGN KEY (logid, steamid64) REFERENCES player_stats_backing (logid, steamid64),
 	CHECK ((dmg_real NOTNULL AND dt_real NOTNULL) OR (dmg_real ISNULL AND dt_real ISNULL))
 );
 
@@ -221,7 +239,7 @@ CREATE TABLE IF NOT EXISTS medic_stats (
 	deaths_after_uber INT, -- within 20s
 	deaths_before_uber INT, -- 95-99%
 	PRIMARY KEY (steamid64, logid),
-	FOREIGN KEY (logid, steamid64) REFERENCES player_stats (logid, steamid64),
+	FOREIGN KEY (logid, steamid64) REFERENCES player_stats_backing (logid, steamid64),
 	CHECK ((medigun_ubers ISNULL AND kritz_ubers ISNULL AND other_ubers ISNULL) OR
 	       (medigun_ubers NOTNULL AND kritz_ubers NOTNULL AND other_ubers NOTNULL)),
 	CHECK (medigun_ubers ISNULL OR ubers = medigun_ubers + kritz_ubers + other_ubers),
@@ -239,8 +257,8 @@ CREATE TABLE IF NOT EXISTS heal_stats (
 	healing INT NOT NULL,
 	PRIMARY KEY (logid, healer, healee),
 	-- Should reference medic_stats, but some very old logs only report one class per player
-	FOREIGN KEY (logid, healer) REFERENCES player_stats (logid, steamid64),
-	FOREIGN KEY (logid, healee) REFERENCES player_stats (logid, steamid64)
+	FOREIGN KEY (logid, healer) REFERENCES player_stats_backing (logid, steamid64),
+	FOREIGN KEY (logid, healee) REFERENCES player_stats_backing (logid, steamid64)
 );
 
 -- Index for looking up people healed in logs
@@ -277,7 +295,7 @@ CREATE TABLE IF NOT EXISTS class_stats (
 	dmg INT NOT NULL,
 	duration INT NOT NULL,
 	PRIMARY KEY (steamid64, logid, classid),
-	FOREIGN KEY (logid, steamid64) REFERENCES player_stats (logid, steamid64)
+	FOREIGN KEY (logid, steamid64) REFERENCES player_stats_backing (logid, steamid64)
 );
 
 -- For logs page
@@ -297,7 +315,7 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS leaderboard_cube AS SELECT
 	sum((wins = losses)::INT) AS ties,
 	sum((wins < losses)::INT) AS losses
 FROM log_nodups AS log
-JOIN player_stats USING (logid)
+JOIN player_stats_backing USING (logid)
 LEFT JOIN class_stats USING (logid, steamid64)
 WHERE class_stats.duration * 1.5 >= log.duration
 GROUP BY CUBE (steamid64, formatid, classid, mapid)
@@ -364,7 +382,7 @@ CREATE TABLE IF NOT EXISTS event_stats (
 	soldier INT NOT NULL,
 	spy INT NOT NULL,
 	PRIMARY KEY (steamid64, logid, eventid),
-	FOREIGN KEY (logid, steamid64) REFERENCES player_stats (logid, steamid64)
+	FOREIGN KEY (logid, steamid64) REFERENCES player_stats_backing (logid, steamid64)
 );
 
 -- For logs page

trends/importer/logs.py
@@ -195,7 +195,7 @@ def import_log(cctx, c, logid, log):
                      DO UPDATE SET
                          last_active = greatest(player.last_active, EXCLUDED.last_active);""",
                   player)
-        c.execute("""INSERT INTO player_stats (
+        c.execute("""INSERT INTO player_stats_backing (
                          logid, steamid64, teamid, nameid, kills, assists, deaths, dmg, dt
                      ) VALUES (
                          %(logid)s, %(steamid)s, (SELECT teamid FROM team WHERE team = %(team)s),
@@ -540,7 +540,7 @@ def update_formats(c):
                  WHERE log.logid = new.logid;""")
 
 def update_wlt(c):
-    c.execute("""UPDATE player_stats AS ps
+    c.execute("""UPDATE player_stats_backing AS ps
                  SET wins = CASE new.teamid
                          WHEN 1 THEN new.red_score
                          WHEN 2 THEN new.blue_score
@@ -571,7 +571,7 @@ def update_wlt(c):
                              THEN 0
                              ELSE coalesce(round.ties, 0)
                          END AS ties
-                     FROM player_stats
+                     FROM player_stats_backing
                      JOIN log USING (logid)
                      LEFT JOIN (SELECT
                              logid,
@@ -586,7 +586,7 @@ def update_wlt(c):
                      AND ps.steamid64 = new.steamid64""")
 
 def update_player_classes(cur, bounds=None):
-    cur.execute("""UPDATE player_stats AS ps SET
+    cur.execute("""UPDATE player_stats_backing AS ps SET
                        classids = new.classids,
                        class_durations = new.durations
                    FROM (SELECT
@@ -625,7 +625,7 @@ def update_acc(cur, bounds=None):
                  .format("WHERE logid BETWEEN %s AND %s" if bounds else ""),
               bounds)
 
-    cur.execute("""UPDATE player_stats AS ps SET
+    cur.execute("""UPDATE player_stats_backing AS ps SET
                         hits = new.hits,
                         shots = new.shots
                     FROM (SELECT
@@ -705,7 +705,7 @@ def import_logs(args, c):
     # They may also be used to know what to delete on failure
     # The second element of the tuple is what to order by when inserting (e.g. the primary key)
     temp_tables = (('log', 'logid'), ('log_json', 'logid'), ('round', 'logid, seq'),
-                   ('round_extra', 'logid, seq'), ('player_stats', 'steamid64, logid'),
+                   ('round_extra', 'logid, seq'), ('player_stats_backing', 'steamid64, logid'),
                    ('player_stats_extra', 'steamid64, logid'), ('medic_stats', 'steamid64, logid'),
                    ('heal_stats', 'logid, healer, healee'),
                    ('class_stats', 'steamid64, logid, classid'),
@@ -720,8 +720,10 @@ def import_logs(args, c):
                        );""".format(table[0], table[0], table[1]))
     # This doesn't include foreign keys, so include some which we want to handle in import_log
     cur.execute("""ALTER TABLE heal_stats
-                   ADD FOREIGN KEY (logid, healer) REFERENCES player_stats (logid, steamid64),
-	               ADD FOREIGN KEY (logid, healee) REFERENCES player_stats (logid, steamid64);""")
+                   ADD FOREIGN KEY (logid, healer)
+                       REFERENCES player_stats_backing (logid, steamid64),
+	               ADD FOREIGN KEY (logid, healee)
+                       REFERENCES player_stats_backing (logid, steamid64);""")
     # We need this index to calculate formats efficiently
     cur.execute("CREATE INDEX class_stats_logid ON class_stats (logid);")
     # And this index to limit dupes

trends/migrations/player_stats_backing.sql
@@ -1,0 +1,20 @@
+-- depends player_stats_classes.py
+
+BEGIN;
+CREATE FUNCTION array_sum(anyarray) RETURNS anyelement
+	AS 'SELECT sum(val) FROM unnest($1) AS val'
+	LANGUAGE SQL IMMUTABLE STRICT PARALLEL SAFE;
+ALTER TABLE player_stats RENAME TO player_stats_backing;
+ALTER TABLE player_stats_backing DROP primary_classid;
+CREATE VIEW player_stats AS SELECT
+	player_stats_backing.*,
+	CASE WHEN class_durations[1] * 1.5 > array_sum(class_durations)
+		THEN classids[1]
+	END AS primary_classid,
+	(SELECT array_agg(class)
+	 FROM unnest(classids) AS classid
+	 JOIN class USING (classid)) AS classes,
+	(SELECT array_agg(duration * 1.0 / array_sum(class_durations))
+	 FROM unnest(class_durations) AS duration) AS class_pct
+FROM player_stats_backing;
+COMMIT;

commit 323465dc672cd555c8f28e2c55445f9f34e9b253

schema: Fix division by zero error in player_stats
This division is missing a nullif.

Fixes: a53d72c ("schema: Remove primary_classid from player_stats")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/schema.sql
@@ -210,7 +210,7 @@ CREATE OR REPLACE VIEW player_stats AS SELECT
 	(SELECT array_agg(class)
 	 FROM unnest(classids) AS classid
 	 JOIN class USING (classid)) AS classes,
-	(SELECT array_agg(duration * 1.0 / array_sum(class_durations))
+	(SELECT array_agg(duration * 1.0 / nullif(array_sum(class_durations), 0.0))
 	 FROM unnest(class_durations) AS duration) AS class_pct
 FROM player_stats_backing;
 