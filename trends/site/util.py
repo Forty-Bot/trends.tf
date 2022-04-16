Forty-Bot/trends.tf

commit df581ad7d5f82119bd5287d2adba37471d2f8b46

Add caching for player overview
Calculating the statistics for the player overview requires reading all the
rows in player_stats for that player. Try to mitigate this by caching the
results of the query for 5 minutes. We use memcached since it seems fairly
simple to deploy, and we don't need any more advanced reatures for the
moment.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/util.py
@@ -15,6 +15,11 @@ from ..util import clamp
 from ..sql import db_connect
 
 try:
+    import pylibmc
+except ImportError:
+    pylibmc = None
+
+try:
     from .sentry import TracingCursor
 except ImportError:
     TracingCursor = None
@@ -42,6 +47,22 @@ def get_db():
 def put_db(exception):
     if db := flask.g.pop('db_conn', None):
         db.close()
+
+class NoopClient:
+    def get(self, key, default=None):
+        return default
+
+    def set(self, key, value, **kwargs):
+        pass
+
+@global_context('mc_conn')
+def get_mc():
+    if pylibmc:
+        try:
+            return pylibmc.Client((flask.current_app.config['MEMCACHED_SERVERS'],), binary=True)
+        except pylibmc.Error as error:
+            flask.current_app.logger.exception("Could not connect to memcached")
+    return NoopClient()
 
 @global_context('filters')
 def get_filter_params():

salt/frontend.sls
@@ -41,6 +41,17 @@ uwsgi_{{ name }}_privs:
 
 # TODO: ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO uwsgi;
 
+memcached:
+  pkg.installed:
+    - refresh: False
+
+memcached.service:
+  service.running:
+    - enable: True
+    - reload: True
+    - requires:
+      - memcached
+
 {% if grains.os_family == 'Debian' %}
  {% set python = "python3" %}
  {% set uwsgi_socket = "/run/uwsgi/app/trends/socket" %}
@@ -86,10 +97,12 @@ virtualenv:
     - pkgs:
 {% if grains.os_family == 'Debian' %}
       - libpq-dev
+      - libmemcached-dev
       - python3-dev
       - python3
       - virtualenv
 {% else %}
+      - libmemcached
       - python
       - python-virtualenv
     - require:
@@ -115,7 +128,7 @@ setuptools_scm:
     - require:
       - /srv/uwsgi/trends/venv
 
-trends.tf[sentry]:
+trends.tf[cache,sentry]:
   pip.installed:
     - user: sean
     - editable: /srv/uwsgi/trends
@@ -145,6 +158,7 @@ uwsgi_service:
     - enable: True
     - reload: True
     - requires:
+      - memcached
       - /srv/uwsgi/trends
       - uwsgi_installed
       - uwsgi_config

setup.py
@@ -29,6 +29,7 @@ setuptools.setup(
         'zstandard',
     ],
     extras_require = {
+        'cache': "pylibmc",
         'sentry': "sentry-sdk[flask]",
     },
     entry_points = {

trends/site/player.py
@@ -3,7 +3,7 @@
 
 import flask
 
-from .util import get_db, get_filter_params, get_filter_clauses, get_order, get_pagination
+from .util import get_db, get_mc, get_filter_params, get_filter_clauses, get_order, get_pagination
 from ..util import clamp
 
 player = flask.Blueprint('player', __name__)
@@ -11,6 +11,12 @@ player = flask.Blueprint('player', __name__)
 @player.url_value_preprocessor
 def get_player(endpoint, values):
     flask.g.steamid = values['steamid']
+    mc = get_mc()
+    key = "overview_{}".format(values['steamid'])
+    if player_overview := mc.get(key):
+        flask.g.player = player_overview
+        return
+
     cur = get_db().cursor()
     cur.execute(
         """SELECT
@@ -38,6 +44,7 @@ def get_player(endpoint, values):
 
     for row in cur:
         flask.g.player = row
+        mc.set(key, row, time=300)
         break
     else:
         flask.abort(404)

trends/site/wsgi.py
@@ -25,10 +25,11 @@ except ImportError:
 class DefaultConfig:
     DATABASE = "postgresql:///trends"
     TIMEOUT = 60000
+    MEMCACHED_SERVERS = "127.0.0.1:11211"
 
 class EnvConfig:
     def __init__(self):
-        for name in ("DATABASE", "TIMEOUT"):
+        for name in ("DATABASE", "TIMEOUT", "MEMCACHED_SERVERS"):
             val = os.environ.get(name)
             if val is not None:
                 setattr(self, name, val)

commit 1835a5a70a155775d7680a1edcc9dc3b51804557

site: util: Properly handle a missing memcached
When we can't connect to the memcached server (such as unit tests, or when
it's just not running), we crash upon calling get(). This is because
creating a connection doesn't actually cause any network requests, so we
will never get an exception.

First, if there is no memcached server configured at all, don't bother
connecting in the first place. Second, make an immediate request for a
nonexistant key. This will cause the request to fail immediately. An
alternative strategy could be to wait for the caller to call get() (or
set()) and swap things out then. However, sometimes pylibmc returns
SomeErrors, which makes it difficult to disambiguate between connection
errors and other problems (which shouldn't be called). An extra call isn't
too bad, and is much simpler.

Fixes: df581ad ("Add caching for player overview")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/util.py
@@ -49,10 +49,16 @@ class NoopClient:
 
 @global_context('mc_conn')
 def get_mc():
-    try:
-        return pylibmc.Client(flask.current_app.config['MEMCACHED_SERVERS'].split(','), binary=True)
-    except pylibmc.Error as error:
-        flask.current_app.logger.exception("Could not connect to memcached")
+    servers = flask.current_app.config['MEMCACHED_SERVERS']
+    if servers:
+        try:
+            mc = pylibmc.Client(servers.split(','), binary=True)
+            # pylibmc doesn't actually connect until we make a request. Force a connection failure
+            # up front so we can log it and use a fallback client
+            mc.get('connection_test')
+            return mc
+        except pylibmc.ConnectionError as error:
+            flask.current_app.logger.exception("Could not connect to memcached")
     return NoopClient()
 
 @global_context('filters')