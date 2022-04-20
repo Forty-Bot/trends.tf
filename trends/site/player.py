Forty-Bot/trends.tf

commit cf39cc7482794760e169f2b90f44e0c7e79ed0cf

site: player: Use cached overview only if it is valid
Use the last_active column to determine if the player has been updated
since the overview was cached. With this in place, we no longer have to add
a timeout to prevent stale overviews.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/player.py
@@ -25,8 +25,8 @@ def get_overview():
         flask.abort(404)
 
     mc = get_mc()
-    key = "overview_{}".format(values['steamid'])
-    if player_overview := mc.get(key):
+    key = "overview_{}".format(flask.g.steamid)
+    if player_overview := mc.get(key) and player_overview['last_active'] == last_active:
         flask.g.player = player_overview
         return
 
@@ -57,7 +57,7 @@ def get_overview():
 
     for row in cur:
         flask.g.player = row
-        mc.set(key, row, time=300)
+        mc.set(key, row)
         break
     else:
         flask.abort(404)

commit fd17ca270ea04654abb51c6a16434bdbdfa56b2b

site: player: Fix error when getting cached overview
Looks like this construct is supported in 3.10 but not 3.9.

Fixes: cf39cc7 ("site: player: Use cached overview only if it is valid")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/player.py
@@ -26,7 +26,8 @@ def get_overview():
 
     mc = get_mc()
     key = "overview_{}".format(flask.g.steamid)
-    if player_overview := mc.get(key) and player_overview['last_active'] == last_active:
+    player_overview = mc.get(key)
+    if player_overview and player_overview['last_active'] == last_active:
         flask.g.player = player_overview
         return
 