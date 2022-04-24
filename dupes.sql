eef09c1dbe8a3cc11246a26760f6c550bb938a5b
Sean Anderson <seanga2@gmail.com>
2022-04-24 14:31:20 -0400

    Add helper script for deduplicating all logs
    We deduplicate when importing logs, but it's handy to be able to
    deduplicate globally once in a while (to catch dupes outside of the +-1
    day, and to catch older dupes which might not have used the same rules).
    
    Signed-off-by: Sean Anderson <seanga2@gmail.com>

---
+++ dupes.sql
@@ -0,0 +1,28 @@
+-- SPDX-License-Identifier: AGPL-3.0-only
+-- Copyright (C) 2020 Sean Anderson <seanga2@gmail.com>
+-- This sets ALL duplicates
+
+BEGIN;
+
+CREATE TEMP VIEW round_full AS
+SELECT *
+FROM round
+JOIN round_extra USING (logid, seq);
+
+CREATE TEMP TABLE dupes AS
+SELECT
+	r1.logid AS logid,
+	max(r2.logid) AS of
+FROM round_full AS r1
+JOIN round_full AS r2 USING (
+	time, duration, firstcap, red_score, blue_score, red_kills, blue_kills, red_dmg, blue_dmg,
+	red_ubers, blue_ubers
+) WHERE r2.logid > r1.logid
+GROUP BY r1.logid;
+
+UPDATE log
+SET duplicate_of=dupes.of
+FROM dupes
+WHERE log.logid=dupes.logid;
+
+COMMIT;
