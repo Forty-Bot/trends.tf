Forty-Bot/trends.tf

commit 119fa42206caa18cbfcd50ba449d232e42d2f2a2

importer: logs: Refactor logid filter to be batched
Instead of issuing one query per logid, batch filter queries. This reduces the number of rounds
trips to the server.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/importer/logs.py
@@ -14,6 +14,7 @@ from .fetch import ListFetcher, BulkFetcher, FileFetcher, ReverseFetcher, CloneL
 from ..steamid import SteamID
 from ..sql import table_columns, disable_tracing
 from .. import util
+from ..util import chunk
 
 def filter_logids(c, logids, update_only=False):
     """Filter log ids to exclude those already present in the database.
@@ -26,27 +27,22 @@ def filter_logids(c, logids, update_only=False):
     :raises sqlite3.DatabaseError: if there was a problem accessing the database
     """
 
-    for logid in logids:
-        try:
-            time = logid[1]
-            logid = logid[0]
-        except TypeError:
-            time = None
+    for logids in chunk(logids, 100):
+        logids = (logid if type(logid) is tuple else (logid, None) for logid in logids)
 
         cur = c.cursor()
-        with disable_tracing():
-            cur.execute("""SELECT
-                               time < %s
-                           FROM public.log
-                           WHERE logid = %s""", (time, logid))
+        psycopg2.extras.execute_values(cur,
+            """SELECT
+                new.logid,
+                log.logid AS old,
+                log.logid NOTNULL AS exists,
+                coalesce(log.time < new.time::BIGINT, TRUE) AS newer
+            FROM (VALUES %s) AS new(logid, time)
+            LEFT JOIN public.log USING (logid);""", logids, "(%s, %s)")
 
         for row in cur:
-            if row[0]:
-                yield logid
-            break
-        else:
-            if not update_only:
-                yield logid
+            if not update_only if not row['exists'] else row['newer']:
+                yield row['logid']
 
 def import_log(cctx, c, logid, log):
     """Import a log into the database.

commit deea9ab28d1acec67b8685ce0cc3d748a4e483f3

importer: Make log adapter more robust
This allows yielding things other than tuples (such as rows) from
get_logids. This fixes CloneLogsFetcher not working.

Fixes: 119fa42 ("importer: logs: Refactor logid filter to be batched")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/importer/logs.py
@@ -26,8 +26,15 @@ def filter_logids(c, logids, update_only=False):
     :raises sqlite3.DatabaseError: if there was a problem accessing the database
     """
 
+    def adapt(logids):
+        for logid in logids:
+            try:
+                yield logid[0], logid[1]
+            except TypeError:
+                yield logid, None
+
     for logids in chunk(logids, 100):
-        logids = (logid if type(logid) is tuple else (logid, None) for logid in logids)
+        logids = adapt(logids)
 
         cur = c.cursor()
         psycopg2.extras.execute_values(cur,