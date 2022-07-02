Forty-Bot/trends.tf

commit 9ac034c818590788e72058a846eacbe704d42664

site: Move sentry template rendering hooks to wsgi.py
These are mostly related to flask initialization, so move them to an
appropriate spot.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/wsgi.py
@@ -7,7 +7,9 @@ import gettext
 import hashlib
 import os, os.path
 
+import blinker
 import flask
+import sentry_sdk
 import werkzeug.exceptions
 import werkzeug.routing
 import werkzeug.utils
@@ -16,6 +18,22 @@ from .api import api, json_handler
 from .player import player
 from .root import root
 from .util import put_db
+
+@flask.before_render_template.connect_via(blinker.ANY)
+def trace_template_start(app, template, context):
+    span = sentry_sdk.start_span(op='render', description=template.name)
+    span.set_data('context', context)
+    flask.g.span = span
+    span.__enter__()
+
+@flask.template_rendered.connect_via(blinker.ANY)
+def trace_template_finish(app, template, context):
+    flask.g.pop('span').__exit__(None, None, None)
+
+@flask.got_request_exception.connect_via(blinker.ANY)
+def trace_template_error(app, exception):
+    if span := flask.g.pop('span', None):
+        span.__exit__(*sys.exc_info())
 
 class DefaultConfig:
     DATABASE = "postgresql:///trends"

trends/site/sentry.py
@@ -11,22 +11,6 @@ from sentry_sdk.integrations.flask import FlaskIntegration
 import pkg_resources
 import psycopg2.extras
 
-@flask.before_render_template.connect_via(blinker.ANY)
-def trace_template_start(app, template, context):
-    span = sentry_sdk.start_span(op='render', description=template.name)
-    span.set_data('context', context)
-    flask.g.span = span
-    span.__enter__()
-
-@flask.template_rendered.connect_via(blinker.ANY)
-def trace_template_finish(app, template, context):
-    flask.g.pop('span').__exit__(None, None, None)
-
-@flask.got_request_exception.connect_via(blinker.ANY)
-def trace_template_error(app, exception):
-    if span := flask.g.pop('span', None):
-        span.__exit__(*sys.exc_info())
-
 class TracingCursor(psycopg2.extras.DictCursor):
     def _log(self, query, vars, paramstyle=psycopg2.paramstyle):
         return tracing_utils.record_sql_queries(Hub.current, self, query, vars,

commit 6227854283a7b0fae239af0706a62b5052c7e446

site: Fix exception when tracing template errors
When there is an exception in a template, we will raise a second exception
because sys doesn't exist. Import it.

Fixes: 9ac034c ("site: Move sentry template rendering hooks to wsgi.py")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/wsgi.py
@@ -6,6 +6,7 @@ from decimal import Decimal
 import gettext
 import hashlib
 import os, os.path
+import sys
 
 import blinker
 import flask