5804e971833384c17406753acfc3ad6df84bd967
Sean Anderson <seanga2@gmail.com>
2022-10-19 17:52:01 -0400

    site: combine: Disable combine button when no logs are selected
    We can't combine no logs, and users will get an error if they try. Disable
    the combine logs button until some logs get selected.
    
    Closes: #88
    Signed-off-by: Sean Anderson <seanga2@gmail.com>

--- trends/site/static/js/combine.js
+++ trends/site/static/js/combine.js
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 // Copyright (C) 2021 Sean Anderson <seanga2@gmail.com>
 
-var checkboxes;
+var checkboxes, submit;
 
 function update_checkboxes() {
 	let checked = 0;
@@ -11,6 +11,7 @@ function update_checkboxes() {
 		}
 	}
 
+	submit.disabled = !checked;
 	if (checked >= 10) {
 		for (let checkbox of checkboxes) {
 			if (!checkbox.checked) {
@@ -27,6 +28,7 @@ function update_checkboxes() {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
+	submit = document.querySelector("form#combine button");
 	checkboxes = document.querySelectorAll("input[form=combine]");
 	for (let checkbox of checkboxes) {
 		checkbox.addEventListener('change', update_checkboxes);
