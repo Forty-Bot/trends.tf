Forty-Bot/trends.tf

commit 400ca990f48e2f062958f19e32fd6688f0386163

Add demo importer
This adds a preliminary demo importer. It imports a lot of information,
probably more than we will need. However, I'm still not sure of the
performance characteristics of the eventual reconciliation of logs and
demos, so I am waiting on a larger import of demos before simplifying
things.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/importer/fetch.py
@@ -367,4 +367,85 @@ class CloneLogsFetcher:
         ret['chat'] = [extract(msg, (('steam_id', 'steamid'), 'name', ('message', 'msg')))
                        for msg in chat]
 
+        killstreaks = self.c.execute("SELECT * FROM killstreak WHERE log_id = ? ORDER BY time ASC",
+                                     (logid,))
+        ret['killstreaks'] = [extract(killstreak, (('steam_id', 'steamid'), 'streak', 'time'))
+                              for killstreak in killstreaks]
+
         return ret
+
+class DemoFileFetcher:
+    def __init__(self, /, demos, **kwargs):
+        self.demos = {}
+        for demoname in demos:
+            with open(demoname) as demofile:
+                demo = json.load(demofile)
+                self.demos[demo['id']] = demo
+
+    def get_logids(self):
+        return self.demos.keys()
+
+    def get_log(self, demoid):
+        return self.demos[demoid]
+
+class DemoListFetcher(ListFetcher):
+    def get_log(self, demoid):
+        try:
+            url = "https://api.demos.tf/demos/{}".format(demoid)
+            resp = self.s.get(url)
+            resp.raise_for_status()
+            return resp.json()
+        except (OSError, urllib3.exceptions.HTTPError):
+            logging.exception("Could not fetch demo %s", logid)
+        except (ValueError, KeyError):
+            logging.exception("Could not parse demo %s", logid)
+
+class DemoBulkFetcher(DemoListFetcher):
+    def __init__(self, since=None, until=None, count=None, page=1, **kwargs):
+        self.s = create_session()
+        self.since = since
+        self.until = until
+        self.count = count
+        self.page = page
+        super().__init__(**kwargs)
+
+    def get_logids(self):
+        # Number of demoids yielded (up to a maximum of count)
+        yielded = 0
+        # The lowest demoid we've seen. We assume new demos will all have higher demoids.
+        last_demoid = None
+        # Our current page
+        page = self.page
+
+        try:
+            while True:
+                params = { 'page': page }
+                if self.since:
+                    params['after'] = self.since
+                if self.until:
+                    params['before'] = self.until
+                resp = self.s.get("https://api.demos.tf/demos", params=params)
+                resp.raise_for_status()
+                demo_list = resp.json()
+
+                page_demos = 0
+                for demo in demo_list:
+                    if last_demoid is not None and demo['id'] >= last_demoid:
+                        continue
+                    else:
+                        yield demo['id']
+                        last_demoid = demo['id']
+                        page_demos += 1
+                        yielded += 1
+                        if self.count is not None and yielded >= self.count:
+                            return
+
+                # No new demos this page; give up
+                if not page_demos:
+                    return
+
+                page += 1
+        except (OSError, urllib3.exceptions.HTTPError):
+            logging.exception("Could not fetch demo list")
+        except (ValueError, KeyError):
+            logging.exception("Could not parse demo list")

test/fetch_test.py
@@ -10,7 +10,7 @@ from hypothesis import given, strategies as st
 import pytest
 import responses, responses.registries
 
-from trends.importer.fetch import ListFetcher, BulkFetcher, ReverseFetcher
+from trends.importer.fetch import ListFetcher, BulkFetcher, ReverseFetcher, DemoBulkFetcher
 
 def response_200(logid):
     return responses.Response(method=responses.GET, url=f"https://logs.tf/api/v1/log/{logid}",
@@ -135,3 +135,47 @@ def test_bulk(args):
         if last_logid is not None:
             assert logid[0] < last_logid
         last_logid = logid[0]
+
+@st.composite
+def demo_args(draw):
+    demos = draw(st.lists(integers(32), min_size=10, max_size=200, unique=True))
+    demos = sorted(demos, reverse=True)
+    pagesize = draw(st.integers(1, 50))
+    growth = draw(st.integers(0, pagesize - 1))
+    count = draw(st.integers(1, 2 * len(demos) + 1))
+
+    return { k: v for k, v in locals().items() if k != 'draw' }
+
+@given(args=demo_args())
+@responses.activate
+def test_demo(args):
+    # Make a copy so we can still see the original parameters on failure
+    demos = args['demos'][:]
+    pagesize = args['pagesize']
+    growth = args['growth']
+    count = args['count']
+    fetcher = DemoBulkFetcher(count=count)
+
+    def get_demo(request):
+        page = int(request.params['page'])
+
+        resp_demos = demos[(page - 1) * pagesize:page * pagesize]
+        resp = [{ 'id': demo } for demo in resp_demos]
+
+        for _ in range(growth):
+            demos.insert(0, demos[0] + 1)
+
+        return 200, {'content_type': 'application/json'}, json.dumps(resp)
+
+    responses.add_callback(method=responses.GET,
+                           url=re.compile(r"https://api.demos.tf/demos.*"),
+                           callback=get_demo)
+
+    demoids = list(fetcher.get_logids())
+    assert demoids == args['demos'][:count]
+
+    last_demoid = None
+    for demoid in demoids:
+        if last_demoid is not None:
+            assert demoid < last_demoid
+        last_demoid = demoid

trends/importer/cli.py
@@ -12,6 +12,7 @@ from ..util import sentry_init
 from .ad import create_ad_parser
 from .json import create_json_parser
 from .logs import create_logs_parser
+from .demos import create_demos_parser
 from .players import create_players_parser
 from .uploader import create_uploader_parser
 from .weapons import create_weapons_parser
@@ -20,6 +21,7 @@ def create_parser():
     parser = argparse.ArgumentParser()
     sub = parser.add_subparsers()
     create_logs_parser(sub)
+    create_demos_parser(sub)
     create_players_parser(sub)
     create_ad_parser(sub)
     create_json_parser(sub)

trends/importer/demos.py
@@ -1,0 +1,154 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+# Copyright (C) 2022 Sean Anderson <seanga2@gmail.com>
+
+import argparse
+from datetime import datetime
+import json
+import logging
+
+import psycopg2
+import sentry_sdk
+
+from .fetch import DemoFileFetcher, DemoListFetcher, DemoBulkFetcher
+from ..steamid import SteamID
+from ..sql import disable_tracing, publicize
+from .. import util
+from ..util import chunk
+
+def filter_demoids(c, demoids):
+    for demoids in chunk(demoids, 100):
+        with c.cursor() as cur:
+            psycopg2.extras.execute_values(cur,
+                """SELECT
+                       demoid
+                   FROM (VALUES %s) AS new (demoid)
+                   LEFT JOIN public.demo USING (demoid)
+                   WHERE public.demo.demoid IS NULL""", ((demoid,) for demoid in demoids))
+            yield from (row[0] for row in cur)
+
+def import_demo(c, demo):
+    c.execute("INSERT INTO map (map) VALUES (%(map)s) ON CONFLICT DO NOTHING;", demo)
+    c.execute("""INSERT INTO demo (
+                     demoid, url, server, duration, mapid, time, red_name, blue_name, red_score,
+                     blue_score
+                 ) VALUES (
+                     %(id)s, %(url)s, %(server)s, %(duration)s,
+                     (SELECT mapid FROM map WHERE map = %(map)s), %(time)s, %(red)s, %(blue)s,
+                     %(redScore)s, %(blueScore)s
+                 )""", demo);
+    for player in demo['players']:
+        # Probably nothing useful here
+        if player['team'] not in ('red', 'blue'):
+            continue
+
+        try:
+            player['steamid'] = SteamID(player['steamid'])
+        except ValueError:
+            continue
+
+        player['demoid'] = demo['id']
+        player['time'] = demo['time']
+        c.execute("INSERT INTO name (name) VALUES (%(name)s) ON CONFLICT DO NOTHING;", player)
+        c.execute("""INSERT INTO player (
+                         steamid64,
+                         nameid,
+                         last_active
+                     ) VALUES (
+                         %(steamid)s,
+                         (SELECT nameid FROM name WHERE name = %(name)s),
+                         %(time)s
+                     ) ON CONFLICT (steamid64)
+                     DO UPDATE SET
+                         last_active = greatest(player.last_active, EXCLUDED.last_active);""",
+                  player)
+        c.execute("""INSERT INTO demo_player_stats (
+                         demoid, steamid64, team, kills, deaths, assists
+                     ) VALUES (
+                         %(demoid)s, %(steamid)s,
+                         (SELECT teamid FROM team WHERE %(team)s ILIKE team), %(kills)s, %(deaths)s,
+                         %(assists)s
+                     ) ON CONFLICT (demoid, steamid64)
+                     DO UPDATE SET
+                         kills = demo_player_stats.kills + excluded.kills,
+                         deaths = demo_player_stats.deaths + excluded.deaths,
+                         assists = demo_player_stats.assists + excluded.assists;""", player)
+
+def create_demos_parser(sub):
+    demos = sub.add_parser("demos", help="Import demos")
+    demos.set_defaults(importer=import_demos_cli)
+    demo_sub = demos.add_subparsers()
+    f = demo_sub.add_parser("file", help="Import from the local filesystem")
+    f.set_defaults(fetcher=DemoFileFetcher)
+    f.add_argument("-l", "--demo", action='append', dest='demos',
+                   help="Import a demo from a file. May be specified multiple times")
+    b = demo_sub.add_parser("bulk", help="Bulk import from demos.tf")
+    b.set_defaults(fetcher=DemoBulkFetcher)
+    b.add_argument("-s", "--since", type=datetime.fromisoformat,
+                   default=None, metavar="DATE",
+                   help="Only fetch demos created since DATE")
+    b.add_argument("-u", "--until", type=datetime.fromisoformat,
+                   default=None, metavar="DATE",
+                   help="Only fetch demos created before DATE")
+    b.add_argument("-c", "--count", type=int, default=None,
+                   help="Fetch up to COUNT demos, defaults to unlimited")
+    b.add_argument("-p", "--page", type=int, default=1,
+                   help="Start at a particular page")
+    l = demo_sub.add_parser("list", help="Import a list of demos from demos.tf")
+    l.set_defaults(fetcher=DemoListFetcher)
+    l.add_argument("-i", "--id", action='append', type=int, metavar="DEMOID",
+                   dest='demoids', help="Fetch demo DEMOID")
+
+def import_demos_cli(args, c):
+    with sentry_sdk.start_transaction(op="import", name="demos"):
+        return import_demos(c, args.fetcher(**vars(args)))
+
+tables = (('demo', 'demoid'), ('demo_player_stats', 'demoid, steamid64'))
+
+def import_demos(c, fetcher):
+    cur = c.cursor()
+
+    # Create some temporary tables for bulk inserts
+    for table in tables:
+        cur.execute("""CREATE TEMP TABLE {} (
+                           LIKE {} INCLUDING ALL EXCLUDING INDEXES,
+                           PRIMARY KEY ({})
+                       );""".format(table[0], table[0], table[1]))
+
+    def commit():
+        with sentry_sdk.start_span(op='db.transaction', description="commit"):
+            cur.execute("BEGIN;")
+            publicize(c, tables)
+            cur.execute("COMMIT;")
+            logging.info("Committed %s imported demo(s)...", count)
+
+    count = 0
+    start = datetime.now()
+    for demoid in filter_demoids(c, fetcher.get_logids()):
+        demo = fetcher.get_log(demoid)
+        if demo is None:
+            continue
+
+        with sentry_sdk.start_span(op='db.transaction', description=f"import {demoid}"), \
+             disable_tracing():
+            cur.execute("BEGIN;")
+            try:
+                import_demo(c.cursor(), demo)
+            except (IndexError, KeyError, psycopg2.errors.NumericValueOutOfRange):
+                logging.exception("Could not parse demo %s", demoid)
+                cur.execute("ROLLBACK;")
+            except psycopg2.Error:
+                logging.error("Could not import demo %s", demoid)
+                raise
+            else:
+                count += 1
+            cur.execute("COMMIT;")
+
+        now = datetime.now()
+        if (now - start).total_seconds() > 60 or count > 500:
+            commit()
+            cur.execute("BEGIN;")
+            count = 0
+            # Committing may take a while, so start the timer when we can actually import stuff
+            start = datetime.now()
+
+    commit()

trends/schema.sql
@@ -57,12 +57,42 @@ INSERT INTO format (format, players) VALUES
 	('other', NULL)
 ON CONFLICT DO NOTHING;
 
+CREATE TABLE IF NOT EXISTS team (
+	teamid SERIAL PRIMARY KEY,
+	team TEXT NOT NULL UNIQUE
+);
+
+INSERT INTO team (team) VALUES ('Red'), ('Blue') ON CONFLICT DO NOTHING;
+
 CREATE TABLE IF NOT EXISTS map (
 	mapid SERIAL PRIMARY KEY,
 	map TEXT NOT NULL UNIQUE
 );
 
 CREATE INDEX IF NOT EXISTS map_names ON map USING gin (map gin_trgm_ops);
+
+CREATE TABLE IF NOT EXISTS demo (
+	demoid INTEGER PRIMARY KEY,
+	url TEXT NOT NULL,
+	server TEXT NOT NULL,
+	duration INT NOT NULL,
+	mapid INT NOT NULL REFERENCES map (mapid),
+	time BIGINT NOT NULL,
+	red_name TEXT NOT NULL,
+	blue_name TEXT NOT NULL,
+	red_score INT NOT NULL,
+	blue_score INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS demo_player_stats (
+	demoid INTEGER REFERENCES demo (demoid),
+	steamid64 BIGINT REFERENCES player (steamid64),
+	team INT NOT NULL REFERENCES team (teamid),
+	kills INT NOT NULL,
+	deaths INT NOT NULL,
+	assists INT NOT NULL,
+	PRIMARY KEY (demoid, steamid64)
+);
 
 CREATE TABLE IF NOT EXISTS log (
 	logid INTEGER PRIMARY KEY, -- SQLite won't infer a rowid alias unless the type is INTEGER
@@ -132,13 +162,6 @@ CREATE TABLE IF NOT EXISTS log_json_default
 PARTITION OF log_json (
 	CONSTRAINT minimum CHECK (logid >= 0)
 ) DEFAULT;
-
-CREATE TABLE IF NOT EXISTS team (
-	teamid SERIAL PRIMARY KEY,
-	team TEXT NOT NULL UNIQUE
-);
-
-INSERT INTO team (team) VALUES ('Red'), ('Blue') ON CONFLICT DO NOTHING;
 
 CREATE TABLE IF NOT EXISTS round (
 	logid INT NOT NULL REFERENCES log (logid),

commit 574a74096588806b1d4353f9e86d89b2bc435df2

importer: demos: Use demoid for errors
We don't have a logid to report, use demoid instead.

Fixes: 400ca99 ("Add demo importer")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/importer/fetch.py
@@ -377,9 +377,9 @@ class DemoListFetcher(ListFetcher):
             resp.raise_for_status()
             return resp.json()
         except (OSError, urllib3.exceptions.HTTPError):
-            logging.exception("Could not fetch demo %s", logid)
+            logging.exception("Could not fetch demo %s", demoid)
         except (ValueError, KeyError):
-            logging.exception("Could not parse demo %s", logid)
+            logging.exception("Could not parse demo %s", demoid)
 
 class DemoBulkFetcher(DemoListFetcher):
     def __init__(self, since=None, until=None, count=None, page=1, **kwargs):