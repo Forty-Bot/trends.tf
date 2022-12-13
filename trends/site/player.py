Forty-Bot/trends.tf

commit c567b5663ef14afa05830ce7c1e416cbe9d28fd4

site: logs: Limit/filter earlier
Postgresql isn't smart enough to push down the filter/limit clauses to just
the log and player_stats part of the query, so do it ourselves. Previously,
it would fetch all rows matching the player in all tables, which was not
very efficient, especially since we usually just want a page (or less) of
logs. All the filter columns are in log/player_stats, so we can move those
clauses there unconditionally. However, the order clauses may depend on
stuff from hsg/hsr if the user is sorting by these columns. In that
(uncommon) case, just let the outer order/limit clause handle it.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/player.py
@@ -75,14 +75,20 @@ base_filter_columns = frozenset({'formatid', 'title', 'mapid', 'time', 'logid'})
 surrogate_filter_columns = base_filter_columns.union({'primary_classid'})
 
 def get_logs(c, steamid, filters, duplicates=True, order_clause="logid DESC", limit=100, offset=0):
-    filter_clauses = get_filter_clauses(filters, 'primary_classid', 'format', 'title', 'map',
+    filter_clauses = get_filter_clauses(filters, 'primary_classid', 'formatid', 'title', 'mapid',
                                         'time', 'logid')
     if not duplicates:
         filter_clauses += "\nAND duplicate_of ISNULL"
+    if 'hpm' not in order_clause:
+        filter_clauses += """
+            ORDER BY {} NULLS LAST
+            LIMIT %(limit)s OFFSET %(offset)s
+        """.format(order_clause)
+
     logs = c.cursor()
     logs.execute(
         """SELECT
-               log.logid,
+               ps.logid,
                title,
                map,
                classes,
@@ -91,26 +97,29 @@ def get_logs(c, steamid, filters, duplicates=True, order_clause="logid DESC", li
                losses,
                ties,
                format,
-               log.duration,
+               ps.duration,
                ps.kills,
                ps.deaths,
                ps.assists,
-               ps.dmg * 60.0 / log.duration AS dpm,
-               ps.dt * 60.0 / log.duration AS dtm,
+               ps.dmg * 60.0 / ps.duration AS dpm,
+               ps.dt * 60.0 / ps.duration AS dtm,
                hits * 1.0 / nullif(shots, 0.0) AS acc,
-               hsg.healing * 60.0 / log.duration AS hpm_given,
-               hsr.healing * 60.0 / log.duration AS hpm_recieved,
+               hsg.healing * 60.0 / ps.duration AS hpm_given,
+               hsr.healing * 60.0 / ps.duration AS hpm_recieved,
                duplicate_of,
                demoid,
                time
-           FROM log
-           JOIN player_stats AS ps USING (logid)
+           FROM (SELECT *
+               FROM log
+               JOIN player_stats USING (logid)
+               WHERE steamid64 = %(steamid)s
+                   {}
+           ) as ps
            JOIN map USING (mapid)
            LEFT JOIN format USING (formatid)
            LEFT JOIN heal_stats_given AS hsg USING (logid, steamid64)
            LEFT JOIN heal_stats_received AS hsr USING (logid, steamid64)
            WHERE ps.steamid64 = %(steamid)s
-               {}
            ORDER BY {} NULLS LAST
            LIMIT %(limit)s OFFSET %(offset)s;""".format(filter_clauses, order_clause),
         { 'steamid': steamid, **filters, 'limit': limit, 'offset': offset })

commit 440ebd96d33f4a96776edac0111201c33438ca9f

site: player: Fix sorting logs on dpm/dtm
I forgot about these columns. They don't exist in the log/player_stats
subselect, so we can't sort on them directly. As these are dereived from
just those two tables, push the calculation down.

Fixes: c567b56 ("site: logs: Limit/filter earlier")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/player.py
@@ -101,17 +101,20 @@ def get_logs(c, steamid, filters, duplicates=True, order_clause="logid DESC", li
                ps.kills,
                ps.deaths,
                ps.assists,
-               ps.dmg * 60.0 / ps.duration AS dpm,
-               ps.dt * 60.0 / ps.duration AS dtm,
+               dpm,
+               dtm,
                hits * 1.0 / nullif(shots, 0.0) AS acc,
                hsg.healing * 60.0 / ps.duration AS hpm_given,
                hsr.healing * 60.0 / ps.duration AS hpm_recieved,
                duplicate_of,
                demoid,
                time
-           FROM (SELECT *
+           FROM (SELECT
+                   *,
+                   ps.dmg * 60.0 / log.duration AS dpm,
+                   ps.dt * 60.0 / log.duration AS dtm
                FROM log
-               JOIN player_stats USING (logid)
+               JOIN player_stats AS ps USING (logid)
                WHERE steamid64 = %(steamid)s
                    {}
            ) as ps