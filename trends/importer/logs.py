Forty-Bot/trends.tf

commit 4b6c8b2a62592aab7de5a5a07210457bf5470b9d

Rework duplicate log calculation
This begins the work to start fixing how duplicates are calculated.
Currently, the logs which combine into another log are marked as
duplicates, not the combined log. We get much better data from uncombined
logs, so mark the combined stuff as duplicate. To support this, we are
switching to having duplicate_of be an array. This begins the process.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/importer/logs.py
@@ -443,16 +443,16 @@ def delete_dup_logs(c):
 
     cur = c.cursor()
     cur.execute("""CREATE TEMP TABLE dupes AS SELECT
-                     r1.logid AS logid,
-                     max(r2.logid) AS of
+                     r2.logid AS logid,
+                     array_agg(DISTINCT r1.logid) AS of
                  FROM round AS r1
                  JOIN combined_rounds AS r2 USING (time, duration) WHERE r2.logid > r1.logid
                      AND (r2.logid > %(min)s OR %(min)s ISNULL)
                      AND (r2.logid < %(max)s OR %(max)s ISNULL)
-                 GROUP BY r1.logid;""", { 'min': min, 'max': max})
+                 GROUP BY r2.logid;""", { 'min': min, 'max': max})
 
     cur.execute("""UPDATE log
-                 SET duplicate_of=dupes.of
+                 SET new_duplicate_of = coalesce(new_duplicate_of, ARRAY[]::INT[]) | dupes.of
                  FROM dupes
                  WHERE log.logid=dupes.logid;""")
     cur.execute("DROP TABLE dupes;")

trends/migrations/dupes_part1.sql
@@ -1,0 +1,25 @@
+-- SPDX-License-Identifier: AGPL-3.0-only
+-- Copyright (C) 2022 Sean Anderson <seanga2@gmail.com>
+
+BEGIN;
+
+ALTER TABLE log ADD new_duplicate_of INT[]
+	CHECK (new_duplicate_of = uniq(sort(new_duplicate_of)));
+ALTER TABLE log ADD CHECK (logid > new_duplicate_of[#new_duplicate_of]);
+
+CREATE TEMP TABLE dupes AS SELECT
+    r2.logid AS logid,
+    array_agg(DISTINCT r1.logid) AS of
+FROM round AS r1
+JOIN round AS r2 USING (time, duration) WHERE r2.logid > r1.logid
+GROUP BY r2.logid;
+
+UPDATE log
+SET new_duplicate_of = dupes.of
+FROM dupes
+WHERE log.logid=dupes.logid;
+
+CREATE INDEX IF NOT EXISTS new_log_nodups_pkey ON log (logid)
+	WHERE new_duplicate_of ISNULL;
+
+COMMIT;

trends/schema.sql
@@ -2,6 +2,7 @@
 -- SPDX-License-Identifier: AGPL-3.0-only
 -- Copyright (C) 2020 Sean Anderson <seanga2@gmail.com>
 
+CREATE EXTENSION IF NOT EXISTS intarray;
 CREATE EXTENSION IF NOT EXISTS pg_trgm;
 CREATE EXTENSION IF NOT EXISTS tsm_system_rows;
 
@@ -110,6 +111,7 @@ CREATE TABLE IF NOT EXISTS log (
 	ad_scoring BOOLEAN, -- Whether attack/defense scoring is enabled
 	-- Some logs may be duplicates or subsets of another log
 	duplicate_of INT REFERENCES log (logid),
+	new_duplicate_of INT[] CHECK (new_duplicate_of = uniq(sort(new_duplicate_of))),
 	uploader BIGINT REFERENCES player (steamid64),
 	uploader_nameid INT REFERENCES name (nameid),
 	demoid INT REFERENCES demo (demoid),
@@ -117,7 +119,8 @@ CREATE TABLE IF NOT EXISTS log (
 		OR (uploader NOTNULL AND uploader_nameid NOTNULL)),
 	-- All duplicates must be earlier (and have smaller logids) than what they are duplicates of
 	-- This prevents cycles (though it does admit chains of finite length)
-	CHECK (logid < duplicate_of)
+	CHECK (logid < duplicate_of),
+	CHECK (logid > new_duplicate_of[#new_duplicate_of])
 );
 
 -- For the below view
@@ -134,7 +137,7 @@ CREATE OR REPLACE VIEW log_nodups AS SELECT
 	blue_score,
 	formatid
 FROM log
-WHERE duplicate_of ISNULL;
+WHERE new_duplicate_of ISNULL;
 
 -- For log search
 CREATE INDEX IF NOT EXISTS log_title ON log USING gin (title gin_trgm_ops);

commit 42f46a21dd55ad4f56e643bed83e2239880f547e

importer: logs: Fix demo deduplication
Instead of looking for dupes in the rest of the logs, we should look to see
if the logs we are adding are dupes.

Fixes: 4b6c8b2 ("Rework duplicate log calculation")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/importer/logs.py
@@ -441,13 +441,14 @@ def delete_dup_logs(c):
 
     cur = c.cursor()
     cur.execute("""CREATE TEMP TABLE dupes AS SELECT
-                     r2.logid AS logid,
-                     array_agg(DISTINCT r1.logid) AS of
+                     r1.logid AS logid,
+                     array_agg(DISTINCT r2.logid) AS of
                  FROM round AS r1
-                 JOIN combined_rounds AS r2 USING (time, duration) WHERE r2.logid > r1.logid
+                 JOIN combined_rounds AS r2 USING (time, duration)
+                 WHERE r1.logid > r2.logid
                      AND (r2.logid > %(min)s OR %(min)s ISNULL)
                      AND (r2.logid < %(max)s OR %(max)s ISNULL)
-                 GROUP BY r2.logid;""", { 'min': min, 'max': max})
+                 GROUP BY r1.logid;""", { 'min': min, 'max': max})
 
     cur.execute("""UPDATE log
                  SET duplicate_of = coalesce(duplicate_of, ARRAY[]::INT[]) | dupes.of