Forty-Bot/trends.tf

commit e3784a7e948a453e9167dd47cf57c436dd2615db

Makefile: Update deploy to work with git
`make deploy` used to work by building a package on the host machine,
pushing that package to prod, and then installing it. However, we switched
to keeping the source in git on the remote machine in 996cc66 ("salt: Clone
site into /srv/uwsgi/trends"). This is a much more convenient solution,
because it lets me precisely specify what I want in prod, without worrying
about what I am doing in the working directory. However, it also meant that
the package wasn't being reinstalled regularly. This meant I had to
manually install new deps, update the version, and reload uwsgi. Update the
makefile to do these tasks automatically.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

Makefile
@@ -13,10 +13,16 @@ PACKAGE := dist/$(shell $(PYTHON) setup.py --fullname)-py3-none-any.whl
 $(PACKAGE): FORCE
 	$(PYTHON) setup.py $(if $(V),,-q) bdist_wheel --plat-name any
 
-.PHONY: deploy
-deploy: $(PACKAGE)
-	scp $< $(PROD):/tmp
-	ssh $(PROD) '$(PROD_PREFIX)/bin/pip $(if $(V),,-q) install /tmp/$(<F)'
+.PHONY: build
+build: $(PACKAGE)
+
+.PHONY: deploy deploy/%
+deploy/%:
+	git push --force ssh://$(PROD)$(PROD_PREFIX) $*
+	ssh $(PROD) '$(PROD_PREFIX)/venv/bin/pip $(if $(V),,-q) install -Ue $(PROD_PREFIX) && \
+		sudo systemctl reload uwsgi'
+
+deploy: deploy/HEAD
 
 .PHONY: clean
 clean:

commit e9f36645f6d6944df9365928021d190ddb0078dd

Makefile: Fix deploying non-master refs
Specify the foreigh branch, letting us deploy arbitrary refs.

Fixes: e3784a7 ("Makefile: Update deploy to work with git")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

Makefile
@@ -18,7 +18,7 @@ build: $(PACKAGE)
 
 .PHONY: deploy deploy/%
 deploy/%:
-	git push --force ssh://$(PROD)$(PROD_PREFIX) $*
+	git push --force ssh://$(PROD)$(PROD_PREFIX) $*:master
 	ssh $(PROD) '$(PROD_PREFIX)/venv/bin/pip $(if $(V),,-q) install -Ue $(PROD_PREFIX) && \
 		sudo systemctl reload uwsgi'
 