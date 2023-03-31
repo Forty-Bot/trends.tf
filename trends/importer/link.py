Forty-Bot/trends.tf

commit 8ad94109c59cd54bd32e4fcc62b861490ca5e131

Add link tool
This adds a tool to link logs and demos. It is separate from either
process, since the associated logs/demos are not necessarily added at the
same time.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/importer/link.py
@@ -1,0 +1,46 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+# Copyright (C) 2022 Sean Anderson <seanga2@gmail.com>
+
+from datetime import datetime, timedelta
+import logging
+
+def create_link_parser(sub):
+    link = sub.add_parser("link", help="Link logs and demos")
+    link.set_defaults(importer=link_logs)
+    link.add_argument("-s", "--since", type=datetime.fromisoformat,
+                   default=datetime.now() - timedelta(hours=8), metavar="DATE",
+                   help="Only link logs created before DATE")
+
+def link_logs(args, c):
+    with c.cursor() as cur:
+        since = int(args.since.timestamp())
+        cur.execute("BEGIN;")
+        cur.execute("SELECT min(logid) FROM log WHERE time > %s", (since,))
+        min_logid = cur.fetchone()[0]
+        cur.execute("""CREATE TEMP TABLE linked AS
+                       SELECT
+                           logid,
+                           demo.demoid
+                       FROM log
+                       JOIN (SELECT
+                               logid,
+                               array_agg(steamid64) AS players
+                           FROM player_stats
+                           WHERE logid > %s
+                           GROUP BY logid
+                       ) AS log_players USING (logid)
+                       CROSS JOIN demo
+                       WHERE (log_players.players @> demo.players
+                              OR demo.players @> log_players.players)
+                           AND demo.time BETWEEN log.time - 300 AND log.time + 300
+                           AND demo.time > %s
+                           AND log.demoid ISNULL;""",
+                    (min_logid, since))
+        cur.execute("SELECT count(*) from linked;");
+        count = cur.fetchone()[0]
+        cur.execute("""UPDATE log
+                       SET demoid = linked.demoid
+                       FROM linked
+                       WHERE log.logid = linked.logid;""")
+        cur.execute("COMMIT;")
+        logging.info(f"Linked {count} logs")

trends/importer/cli.py
@@ -10,9 +10,10 @@ import psycopg2
 from ..sql import db_connect, db_init
 from ..util import sentry_init
 from .ad import create_ad_parser
+from .demos import create_demos_parser
 from .json import create_json_parser
 from .logs import create_logs_parser
-from .demos import create_demos_parser
+from .link import create_link_parser
 from .players import create_players_parser
 from .uploader import create_uploader_parser
 from .weapons import create_weapons_parser
@@ -20,11 +21,12 @@ from .weapons import create_weapons_parser
 def create_parser():
     parser = argparse.ArgumentParser()
     sub = parser.add_subparsers()
-    create_logs_parser(sub)
-    create_demos_parser(sub)
-    create_players_parser(sub)
     create_ad_parser(sub)
+    create_demos_parser(sub)
     create_json_parser(sub)
+    create_link_parser(sub)
+    create_logs_parser(sub)
+    create_players_parser(sub)
     create_uploader_parser(sub)
     create_weapons_parser(sub)
     parser.add_argument("database", default="postgresql:///trends", metavar="DATABASE",

commit 0533877c686a2bebc0304d95b06b47e86245caa1

importer: Don't link demos without players
Demos without players are strict subsets of all logs, which could cause
them to be incorrectly linked if done at the wrong time. Add an explicit
check for this.

Fixes: 8ad9410 ("Add link tool")
Closes: #92
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/importer/link.py
@@ -34,6 +34,8 @@ def link_logs(args, c):
                               OR demo.players @> log_players.players)
                            AND demo.time BETWEEN log.time - 300 AND log.time + 300
                            AND demo.time > %s
+                           AND log_players.players NOTNULL
+                           AND demo.players NOTNULL
                            AND log.demoid ISNULL;""",
                     (min_logid, since))
         cur.execute("SELECT count(*) from linked;");