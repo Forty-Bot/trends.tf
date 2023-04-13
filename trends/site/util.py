Forty-Bot/trends.tf

commit 47d53e8ace82564562afed7e237142fdd6d736b6

site: Convert to playerid
This converts the front-end to use playerid instead of steamid64. We still
use steamid64 for our urls and templates, so generally this conversion only
affects queries. For players' pages, we look up the playerid up front in
the same query where we determine last_active. In general, not too much
modification was required, as in many places we already joined to player
for avatarhash.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/util.py
@@ -81,7 +81,7 @@ def get_filter_params():
     if val := tuple(args.getlist('steamid64', type=int)[:5]):
         players = get_db().cursor()
         players.execute("""SELECT
-                               steamid64,
+                               playerid,
                                avatarhash,
                                name
                            FROM player
@@ -164,11 +164,11 @@ def get_filter_clauses(params, *valid_columns, player_prefix='', log_prefix=''):
     if 'logid' in valid_columns:
         for i, player in enumerate(params['players']):
             key = "player_{}".format(i)
-            params[key] = player['steamid64']
+            params[key] = player['playerid']
             clauses.append("""AND {}logid IN (
                                   SELECT logid
                                   FROM player_stats
-                                  WHERE steamid64 = %({})s
+                                  WHERE playerid = %({})s
                            )""".format(log_prefix, key))
 
     return "\n".join(clauses)

test/site_test.py
@@ -117,7 +117,11 @@ def test_filter(client, logs, players, titles, maps, names, data):
 
 def test_search(client, connection):
     players = connection.cursor()
-    players.execute("SELECT steamid64, name FROM player_stats JOIN name USING (nameid) LIMIT 10")
+    players.execute("""SELECT steamid64, name
+                       FROM player_stats
+                       JOIN name USING (nameid)
+                       JOIN player USING (playerid)
+                       LIMIT 10""")
     players = players.fetchall()
 
     for path in ("/search", "/api/v1/players"):

trends/importer/demos.py
@@ -61,7 +61,8 @@ def import_demo(c, demo):
                  ) VALUES (
                      %(id)s, %(url)s, %(server)s, %(duration)s,
                      (SELECT mapid FROM map WHERE map = %(map)s), %(time)s, %(red)s, %(blue)s,
-                     %(redScore)s, %(blueScore)s, %(players)s
+                     %(redScore)s, %(blueScore)s,
+                     (SELECT array_agg(playerid) FROM player WHERE steamid64 = ANY(%(players)s))
                  )""", demo);
 
 def create_demos_parser(sub):

trends/importer/link.py
@@ -24,7 +24,7 @@ def link_logs(args, c):
                        FROM log
                        JOIN (SELECT
                                logid,
-                               array_agg(steamid64) AS players
+                               array_agg(playerid) AS players
                            FROM player_stats
                            WHERE logid > %s
                            GROUP BY logid

trends/importer/logs.py
@@ -85,13 +85,15 @@ def import_log(c, logid, log):
                      %(uploader_steamid)s,
                      (SELECT nameid FROM name WHERE name = %(uploader_name)s)
                  ) ON CONFLICT (steamid64) DO NOTHING;""", info)
+    c.execute("SELECT playerid FROM player WHERE steamid64 = %(uploader_steamid)s;", info)
+    info['uploader_playerid'] = c.fetchone()[0]
     c.execute("""INSERT INTO log (
                      logid, time, duration, title, mapid, red_score, blue_score, ad_scoring,
                      uploader, uploader_nameid
                  ) VALUES (
                      %(logid)s, %(date)s, %(total_length)s, %(title)s,
                      (SELECT mapid FROM map WHERE map = %(map)s),
-                     %(red_score)s, %(blue_score)s, %(AD_scoring)s, %(uploader_steamid)s,
+                     %(red_score)s, %(blue_score)s, %(AD_scoring)s, %(uploader_playerid)s,
                      (SELECT nameid FROM name WHERE name = %(uploader_name)s)
                  );""",
               info)
@@ -101,7 +103,7 @@ def import_log(c, logid, log):
     c.execute("SAVEPOINT import;")
 
     # Ignore logs from banned players
-    c.execute("SELECT banned FROM player WHERE steamid64 = %(uploader_steamid)s;", info);
+    c.execute("SELECT banned FROM player WHERE playerid = %(uploader_playerid)s;", info);
     for row in c:
         if row[0]:
             return
@@ -167,10 +169,12 @@ def import_log(c, logid, log):
                      DO UPDATE SET
                          last_active = greatest(player.last_active, EXCLUDED.last_active);""",
                   player)
+        c.execute("SELECT playerid FROM player WHERE steamid64 = %(steamid)s;", player)
+        player['playerid'] = c.fetchone()[0]
         c.execute("""INSERT INTO player_stats_backing (
-                         logid, steamid64, team, nameid, kills, assists, deaths, dmg, dt
+                         logid, playerid, team, nameid, kills, assists, deaths, dmg, dt
                      ) VALUES (
-                         %(logid)s, %(steamid)s, %(team)s,
+                         %(logid)s, %(playerid)s, %(team)s,
                          (SELECT nameid FROM name WHERE name = %(name)s), %(kills)s, %(assists)s,
                          %(deaths)s, %(dmg)s, %(dt)s
                      );""", player)
@@ -178,11 +182,11 @@ def import_log(c, logid, log):
                                         'medkits', 'medkits_hp', 'backstabs', 'headshots',
                                         'headshots_hit', 'sentries', 'heal', 'cpc', 'ic'))):
             c.execute("""INSERT INTO player_stats_extra (
-                             logid, steamid64, suicides, dmg_real, dt_real, hr, lks, airshots,
+                             logid, playerid, suicides, dmg_real, dt_real, hr, lks, airshots,
                              medkits, medkits_hp, backstabs, headshots, headshots_hit, sentries,
                              healing, cpc, ic
                          ) VALUES (
-                             %(logid)s, %(steamid)s, %(suicides)s, %(dmg_real)s, %(dt_real)s,
+                             %(logid)s, %(playerid)s, %(suicides)s, %(dmg_real)s, %(dt_real)s,
                              %(hr)s, %(lks)s, %(as)s, %(medkits)s, %(medkits_hp)s, %(backstabs)s,
                              %(headshots)s, %(headshots_hit)s, %(sentries)s, %(heal)s, %(cpc)s,
                              %(ic)s
@@ -198,7 +202,7 @@ def import_log(c, logid, log):
                 continue
 
             events['logid'] = logid
-            events['steamid'] = steamid
+            events['playerid'] = player['playerid']
             events['event'] = event
             for cls in util.classes:
                 events[cls] = events.get(cls, 0)
@@ -206,10 +210,10 @@ def import_log(c, logid, log):
             # There are also 'unknown' events, but we skip them; they can be determined by the
             # difference between the sum of this event and the event in player_stats
             c.execute("""INSERT INTO event_stats (
-                             logid, steamid64, eventid, demoman, engineer, heavyweapons, medic,
+                             logid, playerid, eventid, demoman, engineer, heavyweapons, medic,
                              pyro, scout, sniper, soldier, spy
                          ) VALUES (
-                             %(logid)s, %(steamid)s,
+                             %(logid)s, %(playerid)s,
                              (SELECT eventid FROM event WHERE event = %(event)s), %(demoman)s,
                              %(engineer)s, %(heavyweapons)s, %(medic)s, %(pyro)s, %(scout)s,
                              %(sniper)s, %(soldier)s, %(spy)s
@@ -223,7 +227,7 @@ def import_log(c, logid, log):
             if cls['type'] == 'medic':
                 medic = player.get('medicstats', {})
                 medic['logid'] = logid
-                medic['steamid'] = steamid
+                medic['playerid'] = player['playerid']
                 medic['ubers'] = player['ubers']
                 medic['drops'] = player['drops']
 
@@ -259,13 +263,13 @@ def import_log(c, logid, log):
                     medic[prop] = medic.get(prop)
 
                 c.execute("""INSERT INTO medic_stats (
-                                 logid, steamid64, ubers, medigun_ubers, kritz_ubers,
+                                 logid, playerid, ubers, medigun_ubers, kritz_ubers,
                                  other_ubers, drops, advantages_lost, biggest_advantage_lost,
                                  avg_time_before_healing, avg_time_before_using,
                                  avg_time_to_build, avg_uber_duration, deaths_after_uber,
                                  deaths_before_uber
                              ) VALUES (
-                                 %(logid)s, %(steamid)s, %(ubers)s, %(medigun_ubers)s,
+                                 %(logid)s, %(playerid)s, %(ubers)s, %(medigun_ubers)s,
                                  %(kritz_ubers)s, %(other_ubers)s, %(drops)s, %(advantages_lost)s,
                                  %(biggest_advantage_lost)s, %(avg_time_before_healing)s,
                                  %(avg_time_before_using)s, %(avg_time_to_build)s,
@@ -274,7 +278,7 @@ def import_log(c, logid, log):
                             );""", medic)
 
             cls['logid'] = logid
-            cls['steamid'] = steamid
+            cls['playerid'] = player['playerid']
 
             # Some logs accidentally have a timestamp instead of a duration. Try and fix this up as
             # best we can... This may also fix some logs where players have slightly more time
@@ -282,9 +286,9 @@ def import_log(c, logid, log):
             cls['total_time'] = min(cls['total_time'], info['total_length'])
 
             c.execute("""INSERT INTO class_stats (
-                             logid, steamid64, classid, kills, assists, deaths, dmg, duration
+                             logid, playerid, classid, kills, assists, deaths, dmg, duration
                          ) VALUES (
-                             %(logid)s, %(steamid)s,
+                             %(logid)s, %(playerid)s,
                              (SELECT classid FROM class WHERE class = %(type)s), %(kills)s,
                              %(assists)s, %(deaths)s, %(dmg)s, %(total_time)s
                          );""", cls);
@@ -302,7 +306,7 @@ def import_log(c, logid, log):
                     weapon = { 'kills': weapon }
 
                 weapon['logid'] = logid
-                weapon['steamid'] = steamid
+                weapon['playerid'] = player['playerid']
                 weapon['class'] = cls['type']
                 weapon['name'] = weapon_name
 
@@ -316,10 +320,10 @@ def import_log(c, logid, log):
                 c.execute("INSERT INTO weapon (weapon) VALUES (%(name)s) ON CONFLICT DO NOTHING;",
                           weapon)
                 c.execute("""INSERT INTO weapon_stats (
-                                 logid, steamid64, classid, weaponid, kills, dmg, avg_dmg, shots,
+                                 logid, playerid, classid, weaponid, kills, dmg, avg_dmg, shots,
                                  hits
                              ) VALUES (
-                                 %(logid)s, %(steamid)s,
+                                 %(logid)s, %(playerid)s,
                                  (SELECT classid FROM class WHERE class = %(class)s),
                                  (SELECT weaponid FROM weapon WHERE weapon = %(name)s), %(kills)s,
                                  %(dmg)s, %(avg_dmg)s, %(shots)s, %(hits)s
@@ -335,10 +339,11 @@ def import_log(c, logid, log):
         if steamid:
             c.execute("""INSERT INTO player (steamid64, nameid) VALUES (
                              %s, (SELECT nameid FROM name WHERE name = %s)
-                         ) ON CONFLICT (steamid64) DO NOTHING;""",
-                      (steamid, msg['name']))
-        c.execute("INSERT INTO chat (logid, steamid64, seq, msg) VALUES (%s, %s, %s, %s);",
-                  (logid, steamid, seq, msg['msg']))
+                         ) ON CONFLICT (steamid64) DO NOTHING;""", (steamid, msg['name']))
+        c.execute("""INSERT INTO chat (logid, playerid, seq, msg)
+                     VALUES (
+                         %s, (SELECT playerid FROM player WHERE steamid64 = %s), %s, %s
+                     );""", (logid, steamid, seq, msg['msg']))
 
     for (healer, healees) in log['healspread'].items():
         try:
@@ -358,11 +363,13 @@ def import_log(c, logid, log):
                 # healing being logged more than once, and aren't distinct instances of healing.
                 c.execute("SAVEPOINT before_heal_stats;")
                 c.execute("""INSERT INTO heal_stats (logid, healer, healee, healing)
-                             VALUES (%s, %s, %s, %s)
-                             ON CONFLICT DO NOTHING;""",
+                             VALUES (
+                                 %s, (SELECT playerid FROM player WHERE steamid64 = %s),
+                                 (SELECT playerid FROM player WHERE steamid64 = %s), %s
+                             ) ON CONFLICT DO NOTHING;""",
                           (logid, healer, healee, healing))
             # Sometimes a player only shows up in rounds and healspread...
-            except psycopg2.errors.ForeignKeyViolation:
+            except (psycopg2.errors.ForeignKeyViolation, psycopg2.errors.NotNullViolation):
                 logging.warning("Either %s or %s is only present in healspread for log %s",
                                 healer, healee, logid)
                 c.execute("ROLLBACK TO SAVEPOINT before_heal_stats;")
@@ -560,7 +567,7 @@ def update_formats(c):
                          FROM log
                          JOIN (SELECT
                                  logid,
-                                 count(DISTINCT steamid64) AS total_players,
+                                 count(DISTINCT playerid) AS total_players,
                                  total(duration) as total_duration
                              FROM class_stats
                              GROUP BY logid
@@ -598,7 +605,7 @@ def update_wlt(c):
                      END
                  FROM (SELECT
                          logid,
-                         steamid64,
+                         playerid,
                          team,
                          CASE WHEN ad_scoring
                              THEN log.red_score
@@ -624,7 +631,7 @@ def update_wlt(c):
                      ) AS round USING (logid)
                  ) AS new
                  WHERE ps.logid = new.logid
-                     AND ps.steamid64 = new.steamid64""")
+                     AND ps.playerid = new.playerid""")
 
 def update_player_classes(cur, bounds=None):
     cur.execute("""UPDATE player_stats_backing AS ps SET
@@ -632,16 +639,16 @@ def update_player_classes(cur, bounds=None):
                        class_durations = new.durations
                    FROM (SELECT
                            logid,
-                           steamid64,
+                           playerid,
                            array_agg(classid ORDER BY duration DESC) AS classids,
                            array_agg(duration ORDER BY duration DESC) AS durations
                        FROM class_stats
                        {}
-                       GROUP BY logid, steamid64
-                       ORDER BY steamid64, logid
+                       GROUP BY logid, playerid
+                       ORDER BY playerid, logid
                    ) AS new
                    WHERE ps.logid = new.logid
-                       AND ps.steamid64 = new.steamid64;"""
+                       AND ps.playerid = new.playerid;"""
                    .format("WHERE logid BETWEEN %s AND %s" if bounds else ""),
                 bounds)
 
@@ -651,17 +658,17 @@ def update_acc(cur, bounds=None):
                         shots = new.shots
                     FROM (SELECT
                             logid,
-                            steamid64,
+                            playerid,
                             classid,
                             sum(hits) AS hits,
                             sum(shots) AS shots
                         FROM weapon_stats
                         {}
-                        GROUP BY logid, steamid64, classid
-                        ORDER BY steamid64, logid, classid
+                        GROUP BY logid, playerid, classid
+                        ORDER BY playerid, logid, classid
                     ) AS new
                     WHERE cs.logid = new.logid
-                        AND cs.steamid64 = new.steamid64
+                        AND cs.playerid = new.playerid
                         AND cs.classid = new.classid"""
                  .format("WHERE logid BETWEEN %s AND %s" if bounds else ""),
               bounds)
@@ -671,16 +678,16 @@ def update_acc(cur, bounds=None):
                         shots = new.shots
                     FROM (SELECT
                             logid,
-                            steamid64,
+                            playerid,
                             sum(hits) AS hits,
                             sum(shots) AS shots
                         FROM weapon_stats
                         {}
-                        GROUP BY logid, steamid64
-                        ORDER BY steamid64, logid
+                        GROUP BY logid, playerid
+                        ORDER BY playerid, logid
                     ) AS new
                     WHERE ps.logid = new.logid
-                        AND ps.steamid64 = new.steamid64"""
+                        AND ps.playerid = new.playerid"""
                  .format("WHERE logid BETWEEN %s AND %s" if bounds else ""),
               bounds)
 
@@ -754,9 +761,9 @@ def import_logs(c, fetcher, update_only):
     # This doesn't include foreign keys, so include some which we want to handle in import_log
     cur.execute("""ALTER TABLE heal_stats
                    ADD FOREIGN KEY (logid, healer)
-                       REFERENCES player_stats_backing (logid, steamid64),
+                       REFERENCES player_stats_backing (logid, playerid),
 	               ADD FOREIGN KEY (logid, healee)
-                       REFERENCES player_stats_backing (logid, steamid64);""")
+                       REFERENCES player_stats_backing (logid, playerid);""")
     # We need this index to calculate formats efficiently
     cur.execute("CREATE INDEX class_stats_logid ON class_stats (logid);")
     # And this index to limit dupes

trends/schema.sql
@@ -48,7 +48,8 @@ CREATE TABLE IF NOT EXISTS name (
 CREATE INDEX IF NOT EXISTS name_tgrm ON name USING GIN (name gin_trgm_ops);
 
 CREATE TABLE IF NOT EXISTS player (
-	steamid64 BIGINT PRIMARY KEY,
+	playerid SERIAL PRIMARY KEY,
+	steamid64 BIGINT NOT NULL UNIQUE,
 	nameid INT NOT NULL REFERENCES name (nameid),
 	avatarhash TEXT,
 	-- May be NULL if the player has only spectated or uploaded
@@ -104,7 +105,7 @@ CREATE TABLE IF NOT EXISTS demo (
 	blue_score INT NOT NULL,
 	-- There should be a foreign key here, but postgres doesn't support it
 	-- See https://commitfest.postgresql.org/17/1252/
-	players BIGINT[] NOT NULL CHECK (
+	players INT[] NOT NULL CHECK (
 		array_position(players, NULL) ISNULL
 		AND array_ndims(players) = 1
 	)
@@ -124,7 +125,7 @@ CREATE TABLE IF NOT EXISTS log (
 	ad_scoring BOOLEAN, -- Whether attack/defense scoring is enabled
 	-- Some logs may be duplicates or subsets of another log
 	duplicate_of INT[] CHECK (duplicate_of = uniq(sort(duplicate_of))),
-	uploader BIGINT REFERENCES player (steamid64),
+	uploader INT REFERENCES player (playerid),
 	uploader_nameid INT REFERENCES name (nameid),
 	demoid INT REFERENCES demo (demoid),
 	CHECK ((uploader ISNULL) = (uploader_nameid ISNULL)),
@@ -219,7 +220,7 @@ ON CONFLICT DO NOTHING;
 
 CREATE TABLE IF NOT EXISTS player_stats_backing (
 	logid INT REFERENCES log (logid) NOT NULL,
-	steamid64 BIGINT REFERENCES player (steamid64) NOT NULL,
+	playerid INT REFERENCES player (playerid) NOT NULL,
 	nameid INT NOT NULL REFERENCES name (nameid),
 	team TEAM NOT NULL,
 	kills INT NOT NULL,
@@ -235,21 +236,21 @@ CREATE TABLE IF NOT EXISTS player_stats_backing (
 				     AND array_ndims(class_durations) = 1),
 	shots INT,
 	hits INT,
-	PRIMARY KEY (steamid64, logid),
+	PRIMARY KEY (playerid, logid),
 	CHECK ((shots ISNULL) = (hits ISNULL)),
 	CHECK ((classids NOTNULL AND class_durations NOTNULL
 		AND array_length(classids, 1) = array_length(class_durations, 1))
 	       OR (classids ISNULL AND class_durations ISNULL))
 );
 
--- This index includes steamid64 and team so that it can be used as a covering index for the peers
+-- This index includes playerid and team so that it can be used as a covering index for the peers
 -- query. This avoids a bunch of costly random reads to player_stats.
 CREATE INDEX IF NOT EXISTS player_stats_peers
 	ON player_stats_backing (logid)
-	INCLUDE (steamid64, team);
+	INCLUDE (playerid, team);
 
 -- Covering index for name FTS queries
-CREATE INDEX IF NOT EXISTS player_stats_names ON player_stats_backing (nameid) INCLUDE (steamid64);
+CREATE INDEX IF NOT EXISTS player_stats_names ON player_stats_backing (nameid) INCLUDE (playerid);
 
 CREATE OR REPLACE VIEW player_stats AS SELECT
 	player_stats_backing.*,
@@ -265,7 +266,7 @@ FROM player_stats_backing;
 
 CREATE TABLE IF NOT EXISTS player_stats_extra (
 	logid INT NOT NULL,
-	steamid64 BIGINT NOT NULL,
+	playerid INT NOT NULL,
 	suicides INT,
 	dmg_real INT, -- Damage dealt just before/after a kill, cap, or uber
 	dt_real INT,
@@ -281,14 +282,14 @@ CREATE TABLE IF NOT EXISTS player_stats_extra (
 	healing INT,
 	cpc INT, -- Capture Point Captures
 	ic INT, -- Intel Captures
-	PRIMARY KEY (steamid64, logid),
-	FOREIGN KEY (logid, steamid64) REFERENCES player_stats_backing (logid, steamid64),
+	PRIMARY KEY (playerid, logid),
+	FOREIGN KEY (logid, playerid) REFERENCES player_stats_backing (logid, playerid),
 	CHECK ((dmg_real ISNULL) = (dt_real ISNULL))
 );
 
 CREATE TABLE IF NOT EXISTS medic_stats (
 	logid INT NOT NULL,
-	steamid64 BIGINT NOT NULL,
+	playerid INT NOT NULL,
 	ubers INT NOT NULL,
 	medigun_ubers INT,
 	kritz_ubers INT,
@@ -302,8 +303,8 @@ CREATE TABLE IF NOT EXISTS medic_stats (
 	avg_uber_duration REAL,
 	deaths_after_uber INT, -- within 20s
 	deaths_before_uber INT, -- 95-99%
-	PRIMARY KEY (steamid64, logid),
-	FOREIGN KEY (logid, steamid64) REFERENCES player_stats_backing (logid, steamid64),
+	PRIMARY KEY (playerid, logid),
+	FOREIGN KEY (logid, playerid) REFERENCES player_stats_backing (logid, playerid),
 	CHECK (equal(medigun_ubers ISNULL, kritz_ubers ISNULL, other_ubers ISNULL)),
 	CHECK (medigun_ubers ISNULL OR ubers = medigun_ubers + kritz_ubers + other_ubers),
 	CHECK ((advantages_lost ISNULL) = (biggest_advantage_lost ISNULL))
@@ -314,13 +315,13 @@ CREATE INDEX IF NOT EXISTS medic_stats_logid ON medic_stats (logid);
 
 CREATE TABLE IF NOT EXISTS heal_stats (
 	logid INT NOT NULL,
-	healer BIGINT NOT NULL,
-	healee BIGINT NOT NULL,
+	healer INT NOT NULL,
+	healee INT NOT NULL,
 	healing INT NOT NULL,
 	PRIMARY KEY (logid, healer, healee),
 	-- Should reference medic_stats, but some very old logs only report one class per player
-	FOREIGN KEY (logid, healer) REFERENCES player_stats_backing (logid, steamid64),
-	FOREIGN KEY (logid, healee) REFERENCES player_stats_backing (logid, steamid64)
+	FOREIGN KEY (logid, healer) REFERENCES player_stats_backing (logid, playerid),
+	FOREIGN KEY (logid, healee) REFERENCES player_stats_backing (logid, playerid)
 );
 
 -- Index for looking up people healed in logs
@@ -335,21 +336,21 @@ FROM heal_stats;
 
 CREATE OR REPLACE VIEW heal_stats_given AS SELECT
 	logid,
-	healer AS steamid64,
+	healer AS playerid,
 	sum(healing) AS healing
 FROM heal_stats
 GROUP BY logid, healer;
 
 CREATE OR REPLACE VIEW heal_stats_received AS SELECT
 	logid,
-	healee AS steamid64,
+	healee AS playerid,
 	sum(healing) AS healing
 FROM heal_stats
 GROUP BY logid, healee;
 
 CREATE TABLE IF NOT EXISTS class_stats (
 	logid INT NOT NULL,
-	steamid64 BIGINT NOT NULL,
+	playerid INT NOT NULL,
 	classid INT NOT NULL REFERENCES class (classid),
 	kills INT NOT NULL,
 	assists INT NOT NULL,
@@ -358,8 +359,8 @@ CREATE TABLE IF NOT EXISTS class_stats (
 	duration INT NOT NULL,
 	shots INT,
 	hits INT,
-	PRIMARY KEY (steamid64, logid, classid),
-	FOREIGN KEY (logid, steamid64) REFERENCES player_stats_backing (logid, steamid64),
+	PRIMARY KEY (playerid, logid, classid),
+	FOREIGN KEY (logid, playerid) REFERENCES player_stats_backing (logid, playerid),
 	CHECK ((shots ISNULL) = (hits ISNULL))
 );
 
@@ -367,11 +368,11 @@ CREATE TABLE IF NOT EXISTS class_stats (
 CREATE INDEX IF NOT EXISTS class_stats_logid ON class_stats (logid);
 
 CREATE STATISTICS IF NOT EXISTS class_stats (ndistinct)
-ON logid, steamid64
+ON logid, playerid
 FROM class_stats;
 
 CREATE MATERIALIZED VIEW IF NOT EXISTS leaderboard_cube AS SELECT
-	steamid64,
+	playerid,
 	formatid,
 	classid,
 	mapid,
@@ -381,14 +382,14 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS leaderboard_cube AS SELECT
 	sum((wins < losses)::INT) AS losses
 FROM log_nodups AS log
 JOIN player_stats_backing USING (logid)
-LEFT JOIN class_stats USING (logid, steamid64)
+LEFT JOIN class_stats USING (logid, playerid)
 WHERE class_stats.duration * 1.5 >= log.duration
-GROUP BY CUBE (steamid64, formatid, classid, mapid)
-ORDER BY mapid, classid, formatid, steamid64
+GROUP BY CUBE (playerid, formatid, classid, mapid)
+ORDER BY mapid, classid, formatid, playerid
 WITH NO DATA;
 
 CREATE UNIQUE INDEX IF NOT EXISTS leaderboard_pkey
-	ON leaderboard_cube (mapid, classid, formatid, steamid64);
+	ON leaderboard_cube (mapid, classid, formatid, playerid);
 
 CREATE INDEX IF NOT EXISTS leaderboard_classid ON leaderboard_cube (classid, formatid);
 
@@ -405,7 +406,7 @@ FROM weapon;
 
 CREATE TABLE IF NOT EXISTS weapon_stats (
 	logid INT NOT NULL,
-	steamid64 BIGINT NOT NULL,
+	playerid INT NOT NULL,
 	classid INT NOT NULL,
 	weaponid INT NOT NULL REFERENCES weapon (weaponid),
 	kills INT NOT NULL,
@@ -413,14 +414,14 @@ CREATE TABLE IF NOT EXISTS weapon_stats (
 	avg_dmg REAL,
 	shots INT,
 	hits INT,
-	PRIMARY KEY (steamid64, logid, classid, weaponid),
-	FOREIGN KEY (logid, steamid64, classid) REFERENCES class_stats (logid, steamid64, classid),
+	PRIMARY KEY (playerid, logid, classid, weaponid),
+	FOREIGN KEY (logid, playerid, classid) REFERENCES class_stats (logid, playerid, classid),
 	CHECK ((shots ISNULL) = (hits ISNULL)),
 	CHECK ((dmg ISNULL) = (avg_dmg ISNULL))
 );
 
 CREATE STATISTICS IF NOT EXISTS weapon_stats (ndistinct)
-ON logid, steamid64, classid
+ON logid, playerid, classid
 FROM weapon_stats;
 
 -- For logs page
@@ -435,7 +436,7 @@ INSERT INTO event (event) VALUES ('kill'), ('death'), ('assist') ON CONFLICT DO 
 
 CREATE TABLE IF NOT EXISTS event_stats (
 	logid INT NOT NULL,
-	steamid64 BIGINT NOT NULL,
+	playerid INT NOT NULL,
 	eventid INT REFERENCES event (eventid),
 	demoman INT NOT NULL,
 	engineer INT NOT NULL,
@@ -446,8 +447,8 @@ CREATE TABLE IF NOT EXISTS event_stats (
 	sniper INT NOT NULL,
 	soldier INT NOT NULL,
 	spy INT NOT NULL,
-	PRIMARY KEY (steamid64, logid, eventid),
-	FOREIGN KEY (logid, steamid64) REFERENCES player_stats_backing (logid, steamid64)
+	PRIMARY KEY (playerid, logid, eventid),
+	FOREIGN KEY (logid, playerid) REFERENCES player_stats_backing (logid, playerid)
 );
 
 -- For logs page
@@ -455,12 +456,12 @@ CREATE INDEX IF NOT EXISTS event_logid ON event_stats (logid);
 
 CREATE TABLE IF NOT EXISTS chat (
 	logid INT NOT NULL REFERENCES log (logid),
-	steamid64 BIGINT REFERENCES player (steamid64), -- May be NULL for Console messages
+	playerid INT REFERENCES player (playerid), -- May be NULL for Console messages
 	seq INT NOT NULL, -- Message sequence, starting at 0; earlier messages have lower sequences
 	msg TEXT NOT NULL,
 	PRIMARY KEY (logid, seq)
 );
 
--- Reverse index for lookups by steamid64
+-- Reverse index for lookups by playerid
 -- Don't index NULLs since we don't generally want to look them up.
---CREATE INDEX IF NOT EXISTS chat_steamid64 ON chat (steamid64, logid) WHERE steamid64 NOTNULL;
+--CREATE INDEX IF NOT EXISTS chat_playerid ON chat (playerid, logid) WHERE playerid NOTNULL;

commit 18eac3ea77d5dd751f2bdd5425222556d2b1bd75

util: Select steamid64 into players
In order to determine the value for the player filter, we need to also
select the steamid64 from players. WIthout this, currently-filtered players
are not shown.

Fixes: 47d53e8 ("site: Convert to playerid")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/util.py
@@ -83,6 +83,7 @@ def get_filter_params():
     if val := tuple(args.getlist('steamid64', type=int)[:5]):
         players = get_db().cursor()
         players.execute("""SELECT
+                               steamid64,
                                playerid,
                                avatarhash,
                                name