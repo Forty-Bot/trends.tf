Forty-Bot/trends.tf

commit d98755c1b6a714493468f2a3ca067a14fc4fe045

site: Display logs
This adds a column to the log page containing a link to the associated
demo. I'm not sure if there's something better to use for the link text, so
I just used the demos.tf id.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/root.py
@@ -130,17 +130,19 @@ def log(logids):
     logs = db.cursor()
     logs.execute("""SELECT
                         logid,
-                        time,
+                        log.time,
                         title,
                         map,
                         format,
-                        duration,
-                        red_score,
-                        blue_score,
-                        duplicate_of
+                        log.duration,
+                        log.red_score,
+                        log.blue_score,
+                        duplicate_of,
+                        demoid
                     FROM log
                     LEFT JOIN format USING (formatid)
                     JOIN map USING (mapid)
+                    LEFT JOIN demo USING (demoid)
                     WHERE logid IN %s
                     ORDER BY array_position(%s, logid);""", (tuple(logids), logids))
     logs = logs.fetchall()

trends/schema.sql
@@ -108,6 +108,7 @@ CREATE TABLE IF NOT EXISTS log (
 	duplicate_of INT REFERENCES log (logid),
 	uploader BIGINT REFERENCES player (steamid64),
 	uploader_nameid INT REFERENCES name (nameid),
+	demoid INT REFERENCES demo (demoid),
 	CHECK ((uploader ISNULL AND uploader_nameid ISNULL)
 		OR (uploader NOTNULL AND uploader_nameid NOTNULL)),
 	-- All duplicates must be earlier (and have smaller logids) than what they are duplicates of

trends/site/player.py
@@ -99,6 +99,7 @@ def get_logs(c, steamid, filters, duplicates=True, order_clause="logid DESC", li
                hsg.healing * 60.0 / log.duration AS hpm_given,
                hsr.healing * 60.0 / log.duration AS hpm_recieved,
                duplicate_of,
+               demoid,
                time
            FROM log
            JOIN player_stats AS ps USING (logid)

trends/site/templates/log.html
@@ -219,9 +219,12 @@
 		<th>Red Score</th>
 		<th>Blue Score</th>
 		<th>Duration</th>
-		<th><abbr title="Some logs may be uploaded more than once">Duplicate of</abbr></th>
 		<th>Date</th>
+		<th>Demo</th>
+		<th><abbr title="Some logs may be uploaded more than once">Duplicate of</abbr></th>
+		{% if logs | count > 1 %}
 		<th></th>
+		{% endif %}
 	</thead>
 	<tbody class="hier">
 	{% for log in logs %}
@@ -243,6 +246,14 @@
 			{{ log['blue_score'] }}
 		</td>
 		<td>{{ log['duration'] | duration }}</td>
+		<td class="left date" timestamp="{{ log['time'] }}"></td>
+		<td>
+			{% if log['demoid'] %}
+			<a href="https://demos.tf/{{ log['demoid'] }}">
+				{{ log['demoid'] }}
+			</a>
+			{% endif %}
+		</td>
 		<td>
 			{% if log['duplicate_of'] %}
 			<a href="{{ url_for('.log', logids=log['duplicate_of']) }}">
@@ -250,15 +261,14 @@
 			</a>
 			{% endif %}
 		</td>
-		<td class="left date" timestamp="{{ log['time'] }}"></td>
+		{% if logs | count > 1 %}
 		<td class="left">
-			{% if logs | count > 1 %}
 			<a href="{{ url_for('.log',
 					    logids=(logids | reject('eq', log['logid']))) }}">
 				Remove log
 			</a>
-			{% endif %}
 		</td>
+		{% endif %}
 	</tr>
 		{% for round in rounds if round['logid'] == log['logid'] %}
 			{% if loop.previtem is defined %}

trends/site/templates/macros/logs.html
@@ -26,6 +26,7 @@
 	<th><abbr title="Accuracy">Acc</abbr></th>
 	<th>Date</th>
 	{% if extra %}
+	<th>Demo</th>
 	<th><abbr title="Some logs are duplicates of other logs">Duplicate of</abbr></th>
 	{% endif %}
 </tr>
@@ -77,6 +78,13 @@
 			<td>{{ optformat("{:.0%}", log['acc']) }}</td>
 			<td class="left date" timestamp="{{ log['time'] }}"></td>
 			{% if extra %}
+			<td>
+				{% if log['demoid'] %}
+				<a href="https://demos.tf/{{ log['demoid'] }}">
+					{{ log['demoid'] }}
+				</a>
+				{% endif %}
+			</td>
 			<td>
 				{% if log['duplicate_of'] %}
 				<a href="{{ url_for('root.log', logids=log['duplicate_of']) }}">

commit 0f61e493214fd7c0c251a611ca870741cf1ebf69

site: log: Remove unnecessary join to demos
We don't need to join to the demos table to get the demoid. Remove this
join and the (now unnecessary) log qualifications.

Fixes: d98755c ("site: Display logs")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/root.py
@@ -134,13 +134,13 @@ def log(logids):
     logs = db.cursor()
     logs.execute("""SELECT
                         logid,
-                        log.time,
+                        time,
                         title,
                         map,
                         format,
-                        log.duration,
-                        log.red_score,
-                        log.blue_score,
+                        duration,
+                        red_score,
+                        blue_score,
                         duplicate_of,
                         demoid,
                         league,
@@ -149,7 +149,6 @@ def log(logids):
                     FROM log
                     LEFT JOIN format USING (formatid)
                     JOIN map USING (mapid)
-                    LEFT JOIN demo USING (demoid)
                     WHERE logid IN %s
                     ORDER BY array_position(%s, logid);""", (tuple(logids), logids))
     logs = logs.fetchall()