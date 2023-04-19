Forty-Bot/trends.tf

commit 0cfc7d07669642193c77b3fb11fcc3ade05e57a4

site: Add team overview
Add a team overview. Just shows players and recent competitions for now.
I'd also like to add some status in the future.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/team.py
@@ -3,8 +3,138 @@
 
 import flask
 
+from .util import get_db, get_filter_params, get_filter_clauses, get_order, get_pagination, \
+                  last_modified
+from ..util import leagues
+
 team = flask.Blueprint('team', __name__)
+
+@team.url_value_preprocessor
+def get_compid(endpoint, values):
+    flask.g.teamid = values['teamid']
+
+@team.before_request
+def get_comp():
+    db = get_db()
+    cur = db.cursor()
+    cur.execute(
+        """SELECT
+               team_name AS name,
+               avatarhash,
+               fetched,
+               compid
+           FROM team_comp
+           WHERE league = %s AND teamid = %s
+           ORDER by FETCHED DESC
+           LIMIT 1;""", (flask.g.league, flask.g.teamid))
+
+    flask.g.team = cur.fetchone()
+    if flask.g.team is None:
+        flask.abort(404)
+
+    if resp := last_modified(flask.g.team['fetched']):
+        return resp
+
+    cur.execute(
+        """SELECT
+               *,
+               (wins + 0.5 * ties) / nullif(wins + losses + ties, 0) AS winrate,
+               rounds_won / nullif(rounds_won + rounds_lost, 0) AS round_winrate
+           FROM (SELECT
+                   total(win) AS wins,
+                   total(loss) AS losses,
+                   total(tie) AS ties,
+                   total(rounds_won) AS rounds_won,
+                   total(rounds_lost) AS rounds_lost
+               FROM match_wlt
+               WHERE league = %s AND teamid = %s
+           ) AS wlt;""", (flask.g.league, flask.g.teamid))
+    flask.g.team_wlt = cur.fetchone()
 
 @team.route('/')
 def overview(league, teamid):
-    return ""
+    db = get_db()
+    roster = db.cursor()
+    roster.execute(
+        """SELECT
+               steamid64,
+               avatarhash,
+               name,
+               lower(rostered) AS joined
+           FROM team_player
+           JOIN player USING (playerid)
+           JOIN name USING (nameid)
+           WHERE league = %s
+               AND teamid = %s
+               AND (CASE WHEN league_team_per_comp(league) THEN
+                        compid = %s
+                    ELSE
+                        compid ISNULL
+                    END)
+               AND upper(rostered) ISNULL
+           ORDER BY lower(rostered);""", (league, teamid, flask.g.team['compid']))
+
+    old_roster = db.cursor()
+    old_roster.execute(
+        """SELECT
+               steamid64,
+               avatarhash,
+               name,
+               lower(rostered) AS joined,
+               upper(rostered) AS left
+           FROM (SELECT
+                   playerid,
+                   range_max(rostered) AS rostered
+               FROM team_player
+               WHERE league = %s
+                   AND teamid = %s
+                   AND (CASE WHEN league_team_per_comp(league) THEN
+                            compid = %s
+                        ELSE
+                            compid ISNULL
+                        END)
+               GROUP BY league, compid, teamid, playerid
+               HAVING upper(range_max(rostered)) NOTNULL
+           ) AS team_player
+           JOIN player USING (playerid)
+           JOIN name USING (nameid)
+           ORDER BY upper(rostered) DESC
+           LIMIT 10;""", (league, teamid, flask.g.team['compid']))
+
+    comps = db.cursor()
+    comps.execute(
+        """SELECT
+               compid,
+               competition.name AS comp,
+               division AS div,
+               format,
+               wins,
+               losses,
+               ties,
+               (wins + 0.5 * ties) / nullif(wins + losses + ties, 0) AS winrate,
+               rounds_won,
+               rounds_lost,
+               rounds_won / nullif(rounds_won + rounds_lost, 0) AS round_winrate
+           FROM team_comp
+           JOIN competition USING (league, compid)
+           JOIN division USING (league, compid, divid)
+           JOIN div_name USING (div_nameid)
+           JOIN format USING (formatid)
+           JOIN (SELECT
+                   league,
+                   compid,
+                   teamid,
+                   total(win) AS wins,
+                   total(loss) AS losses,
+                   total(tie) AS ties,
+                   total(rounds_won) AS rounds_won,
+                   total(rounds_lost) AS rounds_lost
+               FROM match_wlt
+               GROUP BY league, compid, teamid
+           ) AS match USING (league, compid, teamid)
+           WHERE league = %s AND teamid = %s
+           ORDER BY compid desc
+           LIMIT 10;""", (league, teamid))
+
+    return flask.render_template("league/team/overview.html", roster=roster, old_roster=old_roster,
+                                 comps=comps)

trends/site/templates/league/team/base.html
@@ -1,0 +1,40 @@
+{# SPDX-License-Identifier: AGPL-3.0-only #}
+{# Copyright (C) 2023 Sean Anderson <seanga2@gmail.com> #}
+{% from "macros/nav.html" import navbar %}
+{% from "macros/pretty.html" import league_map %}
+{% from "macros/wlt.html" import wlt %}
+{% extends "base.html" %}
+{% block title %}{{ g.team.name }} - {{ league_map[g.league] }} - {{ super() }}{% endblock %}
+{% macro stats() %}
+<table>
+	<thead><tr>
+		<td class="left"><abbr title="Wins-Losses-Ties">W-L-T</abbr></td>
+		<td class="left"><abbr title="Ties count as 0.5 wins">Winrate</abbr></td>
+		<td class="left"><abbr title="Wins-Losses">Round W-L</abbr></td>
+		<td class="left"><abbr title="Ties count as 0.5 wins">Round Winrate</abbr></td>
+	</tr></thead>
+	<tbody class="noshade"><tr>
+		<td>{{ wlt(g.team_wlt.wins, g.team_wlt.losses, g.team_wlt.ties) }}</td>
+		<td>{{ "{:.0%}".format(g.team_wlt.winrate) }}</td>
+		<td>{{ "{:.0f}-{:.0f}".format(g.team_wlt.rounds_won, g.team_wlt.rounds_lost) }}</td>
+		<td>{{ "{:.1%}".format(g.team_wlt.round_winrate) }}</td>
+	</tr></tbody>
+</table>
+{% endmacro %}
+{% block content %}
+	{{ super() }}
+	{% set league_url_map = {
+		'etf2l': "https://etf2l.org/teams/{}/"
+	} %}
+	<div class="avatar">
+		<img src="{{ g.team['avatarhash'] | avatar('full', g.league) }}">
+		<div class="box">
+			<h1><a href="{{ league_url_map[g.league].format(g.teamid) }}">
+				{{ g.team.name }}
+			</a></h1>
+			{{ stats() }}
+		</div>
+	</div>
+
+	{{ navbar(('.overview', "Overview"), league=g.league, teamid=g.teamid) }}
+{% endblock %}

trends/site/templates/league/team/overview.html
@@ -1,0 +1,104 @@
+{# SPDX-License-Identifier: AGPL-3.0-only #}
+{# Copyright (C) 2023 Sean Anderson <seanga2@gmail.com> #}
+{% from "macros/format.html" import optformat %}
+{% from "macros/pretty.html" import format_map %}
+{% from "macros/wlt.html" import wlt %}
+{% extends "league/team/base.html" %}
+{% block title %}Overview - {{ super() }}{% endblock %}
+{% block content %}
+	{{ super() }}
+	<h2>Overview</h2>
+	<div class="box">
+	<div class="float">
+	<h3>Current Players</h3>
+	<table>
+	<thead>
+		<tr>
+			<th>Player</th>
+			<th>Joined</th>
+		</tr>
+	</thead>
+	<tbody>
+		{% for player in roster %}
+		<tr>
+			<td class="left">
+				<img class="avatar_small"
+				 src="{{ player.avatarhash | avatar('small') }}">
+				<a href="{{ url_for('player.overview',
+				                    steamid=player.steamid64) }}">
+					{{ player.name }}
+				</a>
+			</td>
+			<td class="left date" timestamp="{{ player.joined }}"></td>
+		</tr>
+		{% endfor %}
+	</tbody>
+	</table>
+	</div>
+
+	<div class="float">
+	<h3>Recent Players</h3>
+	<table>
+	<thead>
+		<tr>
+			<th>Player</th>
+			<th><abbr title="Most-recent join date">Joined</th>
+			<th><abbr title="Most-recent leave date">Left</th>
+		</tr>
+	</thead>
+	<tbody>
+		{% for player in old_roster %}
+		<tr>
+			<td class="left">
+				<img class="avatar_small"
+				 src="{{ player.avatarhash | avatar('small') }}">
+				<a href="{{ url_for('player.overview',
+				                    steamid=player.steamid64) }}">
+					{{ player.name }}
+				</a>
+			</td>
+			<td class="left date" timestamp="{{ player.joined }}"></td>
+			<td class="left date" timestamp="{{ player.left }}"></td>
+		</tr>
+		{% endfor %}
+	</tbody>
+	</table>
+	</div>
+
+	<div class="float">
+	<h3>Recent Competitions</h3>
+	<table>
+	<thead>
+		<tr>
+			<th>Competition</th>
+			<th>Division</th>
+			<th>Format</th>
+			<th><abbr title="Wins-Losses-Ties">W-L-T</abbr></th>
+			<th><abbr title="Winrate; Ties count as 0.5 wins">WR</abbr></th>
+			<th><abbr title="Round Wins">RW</abbr></th>
+			<th><abbr title="Round Losses">RL</abbr></th>
+			<th><abbr title="Round Winrate">RWR</abbr></th>
+		</tr>
+	</thead>
+	<tbody>
+		{% for comp in comps %}
+		<tr>
+			<td class="left">
+			<a href="{{ url_for('league.comp.overview', league=g.league,
+			                    compid=comp.compid) }}">
+				{{ comp.comp }}
+			</a></td>
+			<td class="left">{{ comp.div }}</td>
+			<td class="left">{{ format_map[comp.format] }}</td>
+			<td>{{ wlt(comp.wins, comp.losses, comp.ties) }}</td>
+			<td>{{ optformat("{:.0%}", comp.winrate) }}</td>
+			<td>{{ comp.rounds_won }}</td>
+			<td>{{ comp.rounds_lost }}</td>
+			<td>{{ optformat("{:.0%}", comp.round_winrate) }}</td>
+		</tr>
+		{% endfor %}
+	</tbody>
+	</table>
+	</div>
+	</div>
+{% endblock %}

trends/site/util.py
@@ -205,6 +205,10 @@ def get_filter_clauses(params, *valid_columns, **column_map):
                                    WHERE playerid = %({key})s
                             )""")
 
+    if 'playerid' in column_map and len(params['players']):
+        params['playerids'] = tuple(player['playerid'] for player in params['players'])
+        clauses.append(f"AND {column_map['playerid']} IN %(playerids)s")
+
     return "\n".join(clauses)
 
 dir_map = {'desc': "DESC", 'asc': "ASC"}

commit 6a119eb3cbc319831db2ee0a09b3782392b98b0e

team: Use sum instead of total
These are always goign to be whole numbers, and this makes the formatting easier.

Fixes: 0cfc7d0 ("site: Add team overview")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/team.py
@@ -114,7 +114,7 @@ def overview(league, teamid):
                (wins + 0.5 * ties) / nullif(wins + losses + ties, 0) AS winrate,
                rounds_won,
                rounds_lost,
-               rounds_won / nullif(rounds_won + rounds_lost, 0) AS round_winrate
+               rounds_won::NUMERIC / nullif(rounds_won + rounds_lost, 0) AS round_winrate
            FROM team_comp
            JOIN competition USING (league, compid)
            JOIN division USING (league, compid, divid)
@@ -124,11 +124,11 @@ def overview(league, teamid):
                    league,
                    compid,
                    teamid,
-                   total(win) AS wins,
-                   total(loss) AS losses,
-                   total(tie) AS ties,
-                   total(rounds_won) AS rounds_won,
-                   total(rounds_lost) AS rounds_lost
+                   sum(win) AS wins,
+                   sum(loss) AS losses,
+                   sum(tie) AS ties,
+                   sum(rounds_won) AS rounds_won,
+                   sum(rounds_lost) AS rounds_lost
                FROM match_wlt
                GROUP BY league, compid, teamid
            ) AS match USING (league, compid, teamid)