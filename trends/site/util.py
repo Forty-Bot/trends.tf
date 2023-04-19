Forty-Bot/trends.tf

commit db8dd50276e06e84b359671e3ab82be927d503e7

site: Add league competitions page
Add a page with all the competitions in a particular league. Pretty
standard stuff. I'm not really sure what I want in the league overview, so
make this the "default" page for the moment.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/util.py
@@ -80,6 +80,7 @@ def get_filter_params():
     params['class'] = args.get('class', type=str)
     params['format'] = args.get('format', type=str)
     params['league'] = args.get('league', type=str)
+    params['comp'] = args.get('comp', type=str)
     if val := tuple(args.getlist('steamid64', type=int)[:5]):
         players = get_db().cursor()
         players.execute("""SELECT
@@ -103,6 +104,7 @@ def get_filter_params():
 
     set_like_param('map')
     set_like_param('title')
+    set_like_param('name')
 
     timezone = args.get('timezone', tz.UTC, tz.gettz)
     def set_date_params(name):
@@ -154,6 +156,7 @@ def get_filter_clauses(params, *valid_columns, **column_map):
 
     simple_clause('class', 'classid')
     simple_clause('format', 'formatid')
+    simple_clause('comp', 'compid')
 
     if 'primary_classid' in column_map and params['class']:
         clauses.append(f"""AND {column_map['primary_classid']} = (
@@ -167,6 +170,7 @@ def get_filter_clauses(params, *valid_columns, **column_map):
             clauses.append(f"AND {column_map[name]} ILIKE %({name})s")
 
     like_clause('title')
+    like_clause('name')
     like_clause('map')
 
     if 'mapid' in column_map and params['map']:

trends/util.py
@@ -25,6 +25,8 @@ events = {
     'classkillassists': 'assist',
 }
 
+leagues = ('etf2l',)
+
 def clamp(n, lower, upper):
     return max(lower, min(n, upper))
 

trends/site/league.py
@@ -5,11 +5,80 @@ import flask
 
 from .comp import comp
 from .team import team
+from .util import get_db, get_filter_params, get_filter_clauses, get_order, get_pagination
+from ..util import leagues
 
 league = flask.Blueprint('league', __name__)
 league.register_blueprint(comp, url_prefix='/comp/<int:compid>')
 league.register_blueprint(team, url_prefix='/team/<int:teamid>')
 
+@league.url_value_preprocessor
+def get_league(endpoint, values):
+    flask.g.league = values['league']
+
+@league.before_request
+def verify_league():
+    if flask.g.league not in leagues:
+        flask.abort(404)
+
 @league.route('/comps')
 def comps(league):
-    return ""
+    limit, offset = get_pagination()
+    filters = get_filter_params()
+    filter_clauses = get_filter_clauses(filters, 'name', 'format', 'timespan')
+    order, order_clause = get_order({
+        'compid': "compid",
+        'from': "comp_match.from",
+        'to': "comp_match.to",
+    }, 'compid')
+
+    db = get_db()
+    comps = db.cursor()
+    comps.execute(
+        """SELECT
+               compid,
+               name,
+               format,
+               comp_match.from,
+               comp_match.to,
+               coalesce(divs, ARRAY[]::JSON[]) AS divs
+           FROM competition
+           JOIN format USING (formatid)
+           LEFT JOIN (SELECT
+               league,
+               compid,
+               array_agg(json_build_object(
+                   'divid', divid,
+                   'name', division,
+                   'from', div_match.from,
+                   'to', div_match.to
+               ) ORDER BY tier ASC, divid ASC) AS divs
+               FROM division
+               JOIN div_name USING (div_nameid)
+               LEFT JOIN (SELECT
+                       league,
+                       compid,
+                       divid,
+                       min(scheduled) AS from,
+                       max(scheduled) AS to
+                   FROM match
+                   GROUP BY league, compid, divid
+               ) AS div_match USING (league, compid, divid)
+               GROUP BY league, compid
+           ) AS division USING (league, compid)
+           LEFT JOIN (SELECT
+                   league,
+                   compid,
+                   min(scheduled) AS from,
+                   max(scheduled) AS to,
+                   int8range(min(scheduled), max(scheduled)) AS timespan
+               FROM match
+               GROUP BY league, compid
+           ) AS comp_match USING (league, compid)
+           WHERE league = %(league)s
+               {}
+           ORDER BY {} NULLS LAST
+           LIMIT %(limit)s OFFSET %(offset)s;""".format(filter_clauses, order_clause),
+       { 'limit': limit, 'offset': offset, **filters, 'league': league, })
+
+    return flask.render_template("league/comps.html", comps=comps.fetchall())

trends/site/templates/base.html
@@ -29,6 +29,7 @@
 		<h3><a href="{{ url_for('root.index') }}">Trends.tf</a></h3>
 		<a href="{{ url_for('root.logs') }}">Logs</a>
 		<a href="{{ url_for('root.leaderboard') }}">Leaderboards</a>
+		<a href="{{ url_for('league.comps', league='etf2l') }}">ETF2L</a>
 		<form action="{{ url_for('root.search') }}" role="search">
 			<input type="search" name="q" placeholder="Player name or SteamID"
 			 minlength="3" value="{{ q }}">

trends/site/templates/league/base.html
@@ -1,0 +1,17 @@
+{# SPDX-License-Identifier: AGPL-3.0-only #}
+{# Copyright (C) 2023 Sean Anderson <seanga2@gmail.com> #}
+{% from "macros/nav.html" import navbar %}
+{% from "macros/pretty.html" import league_map %}
+{% extends "base.html" %}
+{% block title %}{{ league_map[g.league] }} - {{ super() }}{% endblock %}
+{% block content %}
+	{{ super() }}
+	{% set league_url_map = {
+		'etf2l': "https://etf2l.org/"
+	} %}
+	<h1><a href="{{ league_url_map[g.league] }}">
+		{{ league_map[g.league] }}
+	</a></h1>
+
+	{{ navbar(('.comps', "Competitions"), league=g.league) }}
+{% endblock %}

trends/site/templates/league/comps.html
@@ -1,0 +1,73 @@
+{# SPDX-License-Identifier: AGPL-3.0-only #}
+{# Copyright (C) 2021 Sean Anderson <seanga2@gmail.com> #}
+{% from "macros/filter.html" import filter %}
+{% from "macros/format.html" import date_col %}
+{% from "macros/pagination.html" import navigation %}
+{% from "macros/pretty.html" import format_map, league_map %}
+{% from "macros/sort.html" import sort %}
+{% extends "league/base.html" %}
+{% block title %}Competitions - {{ super() }}{% endblock %}
+{% block head %}
+	{{ super() }}
+	{{ local_js("js/hider.js") }}
+{% endblock %}
+{% block content %}
+	{{ super() }}
+	<h2>Competitions</h2>
+	{{ filter('name', 'format', 'date') }}
+	{{ sort({
+		'compid': "Competition ID",
+		'from': "From",
+		'to': "To",
+	   }) }}
+	{{ navigation(comps) }}
+{% macro header() %}
+<tr>
+	<th></th>
+	<th>Name</th>
+	<th>Format</th>
+	<th><abbr title="Date of oldest match">From</abbr></th>
+	<th><abbr title="Date of most-recent match">To</abbr></th>
+</tr>
+{% endmacro %}
+	<table>
+	<thead>{{ header() }}</thead>
+	<tbody class="hier">
+		{% for comp in comps %}
+		<tr id="{{ comp.compid }}"
+		 class="{{ "hider hiding" if comp.divs | count }}">
+			<td class="tree"></td>
+			<td class="left"><a href="{{ url_for('.comp.overview', league=g.league,
+							     compid=comp.compid) }}">
+				{{ comp.name }}
+			</a></td>
+			<td class="left">{{ format_map[comp.format] }}</td>
+			{{ date_col(comp.from) }}
+			{{ date_col(comp.to) }}
+		</tr>
+		{% if comp.divs | count %}
+		<tr class="{{ comp.compid }} padding-row hidden"></tr>
+		{% endif %}
+		{% for div in comp.divs %}
+		<tr class="hidable hidden {{ comp.compid }}">
+			<td></td>
+			<td class="left"><a href="{{ url_for('.comp.overview', league=g.league,
+				   			     compid=comp.compid,
+							     _anchor=(div.name |
+							              replace(' ', '_'))) }}">
+				{{ div.name }}
+			</a></td>
+			<td></td>
+			{{ date_col(div.from) }}
+			{{ date_col(div.to) }}
+		</tr>
+		{% endfor %}
+		{% if comp.divs | count %}
+		<tr class="{{ comp.compid }} padding-row hidden"></tr>
+		{% endif %}
+		{% endfor %}
+	</tbody>
+	<tfoot>{{ header() }}</tfoot>
+	</table>
+	{{ navigation(comps) }}
+{% endblock %}

trends/site/templates/macros/filter.html
@@ -18,12 +18,14 @@
 {% endif %}
 <form>
 	{{ hidden_excluding(*params) }}
-	{% if 'title' in params %}
-	<label>Title:
-		<input type="text" name="title"
-		       value="{{ (g.filters['title'] or "") | trim("%") }}">
+	{% for param in ('title', 'name') %}
+	{% if param in params %}
+		<label>{{ param | capitalize }}:
+			<input type="text" name="{{ param }}"
+		       value="{{ (g.filters[param] or "") | trim("%") }}">
 	</label>
 	{% endif %}
+	{% endfor %}
 	{% if 'class' in params %}
 	<label>
 		<abbr title="Games with at least &frac23; time on this class">Class</abbr>:
@@ -39,6 +41,12 @@
 			<option></option>
 			{{ _create_options(g.filters['league'], league_map) }}
 		</select>
+	</label>
+	{% endif %}
+	{% if 'comp' in params %}
+	<label><abbr title="Competition">Comp</abbr>:
+		<input type="text" name="comp" list="comps"
+		 value="{{ (g.filters['comp'] or "") | trim("%") }}">
 	</label>
 	{% endif %}
 	{% if 'format' in params %}

commit 8880c382f0a4341a8728ec604920e50c45ee5301

site: Fix filtering by compid directly
If we say we have compid, we will try to match against comp.comp instead of
competition name. Fix things up. This is a bit ugly, but it keeps most of
the code common...

Fixes: db8dd50 ("site: Add league competitions page")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/util.py
@@ -146,7 +146,7 @@ def get_filter_clauses(params, *valid_columns, **column_map):
     id_clause('league')
     id_clause('divid')
 
-    def simple_clause(name, column):
+    def simple_clause(name, column, table=None, table_col=None):
         if not params[name]:
             return
 
@@ -155,13 +155,13 @@ def get_filter_clauses(params, *valid_columns, **column_map):
         elif column in column_map:
             clauses.append(f"""AND {column_map[column]} = (
                                    SELECT {column}
-                                   FROM {name}
-                                   WHERE {name} = %({name})s
+                                   FROM {name if table is None else table}
+                                   WHERE {name if table_col is None else table_col} = %({name})s
                                )""")
 
     simple_clause('class', 'classid')
     simple_clause('format', 'formatid')
-    simple_clause('comp', 'compid')
+    simple_clause('comp', 'compid', 'competition', 'name')
 
     if 'primary_classid' in column_map and params['class']:
         clauses.append(f"""AND {column_map['primary_classid']} = (