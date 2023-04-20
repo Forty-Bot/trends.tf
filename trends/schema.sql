Forty-Bot/trends.tf

commit fe5410db81b02e0ebbdfe7d5168dd4718f0974a4

site: Add competition overview
Add an overview of the competition. This is just the league standings (for
all divisions) and recent matches. For some leagues, the divisions can take
up a lot of space, but some leagues only have e.g.
8 teams in 2 divisions, so I don't know if there's a general solution. h3s
and h4s look a little odd with the default spacing, so reduce it a bit.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/schema.sql
@@ -301,15 +301,95 @@ CREATE TABLE IF NOT EXISTS match (
 	CHECK (league_round_optional(league) OR (round_seq NOTNULL))
 );
 
-CREATE UNIQUE INDEX IF NOT EXISTS match_unique ON match (
+CREATE UNIQUE INDEX IF NOT EXISTS match_unique_div ON match (
 	league, divid, round_seq, teamid1, teamid2
 ) WHERE divid NOTNULL;
-CREATE UNIQUE INDEX IF NOT EXISTS match_unique_nodiv ON match (
+CREATE UNIQUE INDEX IF NOT EXISTS match_unique_comp ON match (
 	league, compid, round_seq, teamid1, teamid2
 ) WHERE divid ISNULL;
 CREATE INDEX IF NOT EXISTS match_team1 ON match (teamid1);
 CREATE INDEX IF NOT EXISTS match_team2 ON match (teamid2);
 CREATE INDEX IF NOT EXISTS match_time ON match (scheduled);
+
+CREATE OR REPLACE VIEW match_wlt AS SELECT
+	league,
+	matchid,
+	compid,
+	divid,
+	teamid1 AS teamid,
+	teamid2 AS opponent,
+	round_seq,
+	mapids,
+	score1 AS rounds_won,
+	score2 AS rounds_lost,
+	forfeit,
+	scheduled,
+	submitted,
+	fetched,
+	(score1 > score2)::INT AS win,
+	(score1 < score2)::INT AS loss,
+	(score1 = score2)::INT AS tie
+FROM match
+UNION ALL
+SELECT
+	league,
+	matchid,
+	compid,
+	divid,
+	teamid2 AS teamid,
+	teamid1 AS opponent,
+	round_seq,
+	mapids,
+	score1 AS rounds_won,
+	score2 AS rounds_lost,
+	forfeit,
+	scheduled,
+	submitted,
+	fetched,
+	(score2 > score1)::INT AS win,
+	(score2 < score1)::INT AS loss,
+	(score2 = score1)::INT AS tie
+FROM match;
+
+CREATE OR REPLACE VIEW match_wlt AS SELECT
+	league,
+	matchid,
+	compid,
+	divid,
+	teamid1 AS teamid,
+	teamid2 AS opponent,
+	round_seq,
+	mapids,
+	score1 AS rounds_won,
+	score2 AS rounds_lost,
+	forfeit,
+	scheduled,
+	submitted,
+	fetched,
+	(score1 > score2)::INT AS win,
+	(score1 < score2)::INT AS loss,
+	(score1 = score2)::INT AS tie
+FROM match
+UNION ALL
+SELECT
+	league,
+	matchid,
+	compid,
+	divid,
+	teamid2 AS teamid,
+	teamid1 AS opponent,
+	round_seq,
+	mapids,
+	score1 AS rounds_won,
+	score2 AS rounds_lost,
+	forfeit,
+	scheduled,
+	submitted,
+	fetched,
+	(score2 > score1)::INT AS win,
+	(score2 < score1)::INT AS loss,
+	(score2 = score1)::INT AS tie
+FROM match;
 
 CREATE OR REPLACE VIEW match_pretty AS SELECT
 	match.league,

trends/site/comp.py
@@ -3,8 +3,77 @@
 
 import flask
 
+from .common import get_matches
+from .util import get_db, get_filter_params, get_filter_clauses, get_order, get_pagination
+from ..util import leagues
+
 comp = flask.Blueprint('comp', __name__)
 
+@comp.url_value_preprocessor
+def get_compid(endpoint, values):
+    flask.g.compid = values['compid']
+
+@comp.before_request
+def get_comp():
+    db = get_db()
+    comp = db.cursor()
+    comp.execute(
+        """SELECT
+               name,
+               format
+           FROM competition
+           JOIN format USING (formatid)
+           WHERE league = %s AND compid = %s;""", (flask.g.league, flask.g.compid))
+    flask.g.comp = comp.fetchone()
+    if flask.g.comp is None:
+        flask.abort(404)
 @comp.route('/')
 def overview(league, compid):
-    return ""
+    db = get_db()
+    divs = db.cursor()
+    divs.execute(
+        """SELECT
+               divid,
+               division AS name,
+               teams
+           FROM (SELECT
+                   league,
+                   compid,
+                   divid,
+                   array_agg(json_build_object(
+                       'teamid', teamid,
+                       'name', team_name,
+                       'avatarhash', avatarhash,
+                       'wins', wins,
+                       'losses', losses,
+                       'ties', ties,
+                       'winrate', (wins + 0.5 * ties) / nullif(wins + losses + ties, 0),
+                       'rounds_won', rounds_won,
+                       'rounds_lost', rounds_lost,
+                       'round_winrate', rounds_won / nullif(rounds_won + rounds_lost, 0)
+                   ) ORDER BY wins + 0.5 * ties DESC,
+                              rounds_won / (rounds_lost + 1) DESC,
+                              team_name) AS teams
+               FROM team_comp
+               JOIN (SELECT
+                       league,
+                       compid,
+                       teamid,
+                       total(win) AS wins,
+                       total(loss) AS losses,
+                       total(tie) AS ties,
+                       total(rounds_won) AS rounds_won,
+                       total(rounds_lost) AS rounds_lost
+                   FROM match_wlt
+                   GROUP BY league, compid, teamid
+               ) AS match USING (league, compid, teamid)
+               GROUP BY league, compid, divid
+           ) AS teams
+           LEFT JOIN division USING (league, compid, divid)
+           LEFT JOIN div_name USING (div_nameid)
+           WHERE league = %s AND compid = %s
+           ORDER BY tier ASC, divid ASC;""", (league, compid))
+
+    matches = get_matches(compid, get_filter_params(), limit=25)
+
+    return flask.render_template("league/comp/overview.html", divs=divs, matches=matches)

trends/site/static/css/style.css
@@ -288,3 +288,11 @@ form {
 	display: inline !important;
 	padding: 8px !important;
 }
+
+h3 {
+	margin: .5em 0;
+}
+
+h4 {
+	margin: .5em 0;
+}

trends/site/templates/league/comp/base.html
@@ -1,0 +1,17 @@
+{# SPDX-License-Identifier: AGPL-3.0-only #}
+{# Copyright (C) 2023 Sean Anderson <seanga2@gmail.com> #}
+{% from "macros/nav.html" import navbar %}
+{% from "macros/pretty.html" import league_map %}
+{% extends "base.html" %}
+{% block title %}{{ g.comp.name }} - {{ league_map[g.league] }} - {{ super() }}{% endblock %}
+{% block content %}
+	{{ super() }}
+	{% set league_url_map = {
+		'etf2l': "https://etf2l.org/etf2l/archives/{}/"
+	} %}
+	<h1><a href="{{ league_url_map[g.league].format(g.compid) }}">
+		{{ league_map[g.league] }} {{ g.comp.name }}
+	</a></h1>
+
+	{{ navbar(('.overview', "Overview"), league=g.league, compid=g.compid) }}
+{% endblock %}

trends/site/templates/league/comp/overview.html
@@ -1,0 +1,59 @@
+{# SPDX-License-Identifier: AGPL-3.0-only #}
+{# Copyright (C) 2021 Sean Anderson <seanga2@gmail.com> #}
+{% from "macros/format.html" import date_col, optformat %}
+{% from "macros/matches.html" import match_table %}
+{% from "macros/pretty.html" import format_map, league_map %}
+{% from "macros/wlt.html" import wlt %}
+{% extends "league/comp/base.html" %}
+{% block head %}
+	{{ super() }}
+	{{ local_js("js/hider.js") }}
+{% endblock %}
+{% block title %}Overview - {{ super() }}{% endblock %}
+{% block content %}
+	{{ super() }}
+	<h2>Overview</h2>
+	<h3>Teams</h3>
+	<div class="box">
+	{% for div in divs %}
+	<div class="float">
+	{% if div.divid != None %}
+	<h4 id="{{ div.name | replace(' ', '_') }}">{{ div.name }}</h4>
+	{% endif %}
+	<table>
+	<thead>
+		<tr>
+			<th>Team</th>
+			<th><abbr title="Wins-Losses-Ties">W-L-T</abbr></th>
+			<th><abbr title="Winrate; Ties count as 0.5 wins">WR</abbr></th>
+			<th><abbr title="Round Wins">RW</abbr></th>
+			<th><abbr title="Round Losses">RL</abbr></th>
+			<th><abbr title="Round Winrate">RWR</abbr></th>
+		</tr>
+	</thead>
+	<tbody>
+	{% for team in div.teams %}
+		<tr>
+			<td class="left">
+				<img class="avatar_small"
+				     src="{{ team.avatarhash | avatar('small', g.league) }}">
+				<a href="{{ url_for('league.team.overview', league=g.league,
+					 	    teamid=team.teamid) }}">
+					{{ team.name }}
+				</a>
+			</td>
+			<td>{{ wlt(team.wins, team.losses, team.ties) }}</td>
+			<td>{{ optformat("{:.0%}", team.winrate) }}</td>
+			<td>{{ team.rounds_won }}</td>
+			<td>{{ team.rounds_lost }}</td>
+			<td>{{ optformat("{:.0%}", team.round_winrate) }}</td>
+		</tr>
+	{% endfor %}
+	</tbody>
+	</table>
+	</div>
+	{% endfor %}
+	</div>
+	<h3>Recent Matches</h3>
+	{{ match_table(matches) }}
+{% endblock %}

trends/site/wsgi.py
@@ -63,9 +63,15 @@ def duration_filter(timestamp):
     else:
         return "{:.0f}:{:02.0f}".format(mm, ss)
 
-def avatar_filter(hash, size='full'):
+def avatar_filter(hash, size='full', league=None):
     if not hash:
         return ''
+
+    if league == 'etf2l':
+        return "https://etf2l.org/wp-content/uploads/avatars/{}".format(hash)
+    elif league is not None:
+        raise ValueError("Invalid league")
+
     url  = "https://avatars.steamstatic.com/{}{}.jpg"
     return url.format(hash, {
             'small': '',

commit c4d8966a7f7c65570db4f9aeabad5d44d1290616

schema: Fix match_wlt scores
The scores for the team2 perspective are backwards. Fix them.

Fixes: fe5410d ("site: Add competition overview")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/schema.sql
@@ -342,8 +342,8 @@ SELECT
 	teamid1 AS opponent,
 	round_seq,
 	mapids,
-	score1 AS rounds_won,
-	score2 AS rounds_lost,
+	score2 AS rounds_won,
+	score1 AS rounds_lost,
 	forfeit,
 	scheduled,
 	submitted,