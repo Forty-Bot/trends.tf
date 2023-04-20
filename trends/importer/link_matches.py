Forty-Bot/trends.tf

commit 602d7912044987180eeb916847e87447b416cb59

importer: Add match linker
This adds an importer to link logs and matches. To avoid truly awful query
plans, create a temporary table with all the logs we want to attempt to
link along with their teams. Then link them with matches

We match logs which took place around the scheduled time that have 2/3
overlap of players on each team (without too many extras).  We use
scheduled instead of submitted because submitted is sometimes done months
after the match takes place by the admins. I don't know why this isn't
automated...

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/importer/link_matches.py
@@ -1,0 +1,96 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+# Copyright (C) 2023 Sean Anderson <seanga2@gmail.com>
+
+from datetime import datetime, timedelta
+import logging
+
+def create_link_matches_parser(sub):
+    link = sub.add_parser("link_matches", help="Link logs and matches")
+    link.set_defaults(importer=link_matches)
+    link.add_argument("-s", "--since", type=datetime.fromisoformat,
+                   default=datetime.now() - timedelta(days=7), metavar="DATE",
+                   help="Only link logs created before DATE")
+
+def link_matches(args, c):
+    with c.cursor() as cur:
+        since = int(args.since.timestamp())
+        cur.execute("BEGIN;")
+        cur.execute(
+            """CREATE TEMP TABLE log_teams AS SELECT
+                   logid,
+                   time,
+                   mapid,
+                   red,
+                   blue
+               FROM log
+               JOIN (SELECT
+                       logid,
+                       array_agg(playerid) AS red
+                   FROM player_stats
+                   WHERE team = 'Red'
+                   GROUP BY logid
+               ) AS red USING (logid)
+               JOIN (SELECT
+                       logid,
+                       array_agg(playerid) AS blue
+                   FROM player_stats
+                   WHERE team = 'Blue'
+                   GROUP BY logid
+               ) AS blue USING (logid)
+               WHERE league ISNULL AND time > %s;""", (since,))
+        cur.execute("CREATE INDEX log_teams_time ON log_teams (time);")
+        cur.execute("ANALYZE log_teams;")
+        cur.execute("SELECT * FROM log_teams;")
+
+        cur.execute(
+            """CREATE TEMP TABLE log_matches AS SELECT
+                   logid,
+                   league,
+                   matchid,
+                   (match1.players & log.red) >= (match2.players & log.blue) AS team1_is_red
+               FROM (SELECT
+                       match.league,
+                       matchid,
+                       match.compid,
+                       scheduled AS time,
+                       mapids,
+                       array_agg(playerid) AS players
+                   FROM match
+                   JOIN team_player AS tp ON (
+                       tp.league = match.league
+                       AND tp.teamid = match.teamid1
+                       AND tp.rostered @> match.scheduled
+                   ) WHERE scheduled > %s
+                   GROUP BY match.league, matchid, match.compid, match.scheduled, mapids
+               ) AS match1
+               JOIN (SELECT
+                       match.league,
+                       matchid,
+                       array_agg(playerid) AS players
+                   FROM match
+                   JOIN team_player AS tp ON (
+                       tp.league = match.league
+                       AND tp.teamid = match.teamid2
+                       AND tp.rostered @> match.scheduled
+                   ) GROUP BY match.league, matchid
+               ) AS match2 USING (league, matchid)
+               JOIN competition USING (league, compid)
+               JOIN format USING (formatid)
+               CROSS JOIN log_teams AS log
+               WHERE log.time BETWEEN match1.time - 12 * 60 * 60 AND match1.time + 12 * 60 * 60
+                   AND (#(match1.players & (log.red | log.blue)) >= format.players / 3
+                        AND #(match2.players & (log.red | log.blue)) >= format.players / 3)
+                   AND #((log.red | log.blue) - match1.players - match2.players) <=
+                         format.players / 3
+                   AND (match1.mapids @> ARRAY[log.mapid] OR match1.mapids ISNULL);""", (since,))
+
+        cur.execute("SELECT count(*) from log_matches;");
+        count = cur.fetchone()[0]
+        cur.execute("""UPDATE log SET
+                           league = log_matches.league,
+                           matchid = log_matches.matchid,
+                           team1_is_red = log_matches.team1_is_red
+                       FROM log_matches
+                       WHERE log.logid = log_matches.logid;""")
+        cur.execute("COMMIT;")
+        logging.info(f"Linked {count} logs")

salt/backend.sls
@@ -189,6 +189,30 @@
         [Install]
         WantedBy=timers.target
 
+/etc/systemd/system/link_matches.service:
+  file.managed:
+    - contents: |
+        [Unit]
+        Description=Link logs and matches
+
+        [Service]
+        Type=oneshot
+        EnvironmentFile=/etc/default/trends
+        ExecStart={{ prefix }}/bin/trends_importer -vv link_matches postgres:///trends
+        User=daemon
+
+/etc/systemd/system/link_matches.timer:
+  file.managed:
+    - contents: |
+        [Unit]
+        Description=Link logs and matches every 5 minutes
+
+        [Timer]
+        OnCalendar=*:3/5
+
+        [Install]
+        WantedBy=timers.target
+
 backend_services:
   module.run:
     - name: service.systemctl_reload
@@ -208,6 +232,8 @@ backend_services:
       - /etc/systemd/system/etf2l_import.timer
       - /etc/systemd/system/link.service
       - /etc/systemd/system/link.timer
+      - /etc/systemd/system/link_matches.service
+      - /etc/systemd/system/link_matches.timer
 
 log_import.timer:
   service.running:
@@ -252,6 +278,12 @@ etf2l_import.timer:
       - backend_services
 
 link.timer:
+  service.running:
+    - enable: True
+    - require:
+      - backend_services
+
+link_matches.timer:
   service.running:
     - enable: True
     - require:

test/conftest.py
@@ -2,6 +2,7 @@
 # Copyright (C) 2022 Sean Anderson <seanga2@gmail.com>
 
 from contextlib import contextmanager
+from datetime import datetime
 import os
 import logging
 
@@ -10,6 +11,7 @@ from testing.postgresql import Postgresql
 
 import trends.importer.logs
 import trends.importer.etf2l
+import trends.importer.link_matches
 from trends.importer.fetch import ETF2LFileFetcher, FileFetcher
 from trends.sql import db_connect, db_init
 
@@ -34,6 +36,8 @@ def database(request):
             logfiles = { logid: f"{os.path.dirname(__file__)}/logs/log_{logid}.json"
                 for logid in (
                     30099,
+                    2408458,
+                    2408491,
                     2600722,
                     2818814,
                     2844704,
@@ -65,6 +69,11 @@ def database(request):
             db_init(c)
             cur.execute("REFRESH MATERIALIZED VIEW leaderboard_cube;")
             cur.execute("REFRESH MATERIALIZED VIEW map_popularity;")
+
+        with db_connect(database.url()) as c:
+            class args:
+                since = datetime.fromtimestamp(0)
+            trends.importer.link_matches.link_matches(args, c)
 
         yield database
 

test/site_test.py
@@ -139,3 +139,11 @@ def test_search(client, connection):
             if len(player['name']) > 3:
                 resp = client.get(path, query_string={'q': player['name']})
                 assert str(player['steamid64']) in resp.get_data(as_text=True)
+
+def test_linked(connection):
+    cur = connection.cursor()
+    cur.execute("SELECT 1 FROM log WHERE league NOTNULL;")
+    for _ in cur:
+        break
+    else:
+        pytest.fail("No linked matches")

trends/importer/cli.py
@@ -15,6 +15,7 @@ from .etf2l import create_etf2l_parser
 from .json import create_json_parser
 from .logs import create_logs_parser
 from .link_demos import create_link_demos_parser
+from .link_matches import create_link_matches_parser
 from .players import create_players_parser
 from .uploader import create_uploader_parser
 from .weapons import create_weapons_parser
@@ -27,6 +28,7 @@ def create_parser():
     create_etf2l_parser(sub)
     create_json_parser(sub)
     create_link_demos_parser(sub)
+    create_link_matches_parser(sub)
     create_logs_parser(sub)
     create_players_parser(sub)
     create_uploader_parser(sub)

commit 38f2f2a6df2b8087610218eb66d6478fca927bb0

importer: link_matches: Fix team determination
We need to consider whether team1 and team2 have more overlap with red and
blue than with blue and red. The previous comparison is bogus. Fix it.

Fixes: 602d791 ("importer: Add match linker")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/importer/link_matches.py
@@ -45,7 +45,8 @@ def link_matches(args, c):
                    logid,
                    league,
                    matchid,
-                   (match1.players & log.red) >= (match2.players & log.blue) AS team1_is_red
+                   #(match1.players & log.red) + #(match2.players & log.blue) >
+                       #(match1.players & log.blue) + #(match2.players & log.red) AS team1_is_red
                FROM (SELECT
                        match.league,
                        matchid,