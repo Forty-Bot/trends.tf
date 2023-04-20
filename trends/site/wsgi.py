Forty-Bot/trends.tf

commit 6971fedac79a00ed925694f7adb9f918c0b8260f

site: comp: Add matches page
Add a page with matches in the competition. Pretty straightforward. Query
the divisions so we have them available to filter on.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/templates/league/comp/matches.html
@@ -1,0 +1,28 @@
+{# SPDX-License-Identifier: AGPL-3.0-only #}
+{# Copyright (C) 2021 Sean Anderson <seanga2@gmail.com> #}
+{% from "macros/filter.html" import filter %}
+{% from "macros/matches.html" import match_table %}
+{% from "macros/pagination.html" import navigation %}
+{% from "macros/sort.html" import sort %}
+{% extends "league/comp/base.html" %}
+{% block head %}
+	{{ super() }}
+	{{ local_js("js/hider.js") }}
+{% endblock %}
+{% block title %}Matches - {{ super() }}{% endblock %}
+{% block content %}
+	{{ super() }}
+	<h2>Matches</h2>
+	{% set filters = ['map', 'date'] %}
+	{% if g.divs | count %}
+		{% set filters = filters + ['divid'] %}
+	{% endif %}
+	{{ filter(*filters) }}
+	{{ sort({
+		'round': "Round",
+		'date': "Date",
+	}) }}
+	{{ navigation(matches) }}
+	{{ match_table(matches, div=g.divs | count) }}
+	{{ navigation(matches) }}
+{% endblock %}

trends/schema.sql
@@ -351,46 +351,6 @@ SELECT
 	(score2 = score1)::INT AS tie
 FROM match;
 
-CREATE OR REPLACE VIEW match_wlt AS SELECT
-	league,
-	matchid,
-	compid,
-	divid,
-	teamid1 AS teamid,
-	teamid2 AS opponent,
-	round_seq,
-	mapids,
-	score1 AS rounds_won,
-	score2 AS rounds_lost,
-	forfeit,
-	scheduled,
-	submitted,
-	fetched,
-	(score1 > score2)::INT AS win,
-	(score1 < score2)::INT AS loss,
-	(score1 = score2)::INT AS tie
-FROM match
-UNION ALL
-SELECT
-	league,
-	matchid,
-	compid,
-	divid,
-	teamid2 AS teamid,
-	teamid1 AS opponent,
-	round_seq,
-	mapids,
-	score1 AS rounds_won,
-	score2 AS rounds_lost,
-	forfeit,
-	scheduled,
-	submitted,
-	fetched,
-	(score2 > score1)::INT AS win,
-	(score2 < score1)::INT AS loss,
-	(score2 = score1)::INT AS tie
-FROM match;
-
 CREATE OR REPLACE VIEW match_pretty AS SELECT
 	match.league,
 	matchid,

trends/site/comp.py
@@ -27,6 +27,15 @@ def get_comp():
     flask.g.comp = comp.fetchone()
     if flask.g.comp is None:
         flask.abort(404)
+
+    divs = db.cursor()
+    divs.execute(
+        """SELECT json_object_agg(divid, division ORDER BY tier ASC, division DESC)
+           FROM division
+           JOIN div_name USING (div_nameid)
+           WHERE league=%s AND compid=%s;""", (flask.g.league, flask.g.compid))
+    flask.g.divs = divs.fetchone()[0] or {}
+
 @comp.route('/')
 def overview(league, compid):
     db = get_db()
@@ -77,3 +86,12 @@ def overview(league, compid):
     matches = get_matches(compid, get_filter_params(), limit=25)
 
     return flask.render_template("league/comp/overview.html", divs=divs, matches=matches)
+
+@comp.route('/matches')
+def matches(league, compid):
+    limit, offset = get_pagination()
+    filters = get_filter_params()
+
+    matches = get_matches(compid, filters, limit, offset)
+
+    return flask.render_template("league/comp/matches.html", matches=matches.fetchall())

trends/site/templates/league/comp/base.html
@@ -13,5 +13,6 @@
 		{{ league_map[g.league] }} {{ g.comp.name }}
 	</a></h1>
 
-	{{ navbar(('.overview', "Overview"), league=g.league, compid=g.compid) }}
+	{{ navbar(('.overview', "Overview"), ('.matches', "Matches"), league=g.league,
+	          compid=g.compid) }}
 {% endblock %}

commit ee4e22f5a9d63abb6acd24d5052433d7b02a41fd

site: matches: Fix log alignment
Fix the log columns not always aligning with the match columns.

Fixes: 6971fed ("site: comp: Add matches page")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/templates/league/comp/matches.html
@@ -23,6 +23,6 @@
 		'date': "Date",
 	}) }}
 	{{ navigation(matches) }}
-	{{ match_table(matches, div=g.divs | count) }}
+	{{ match_table(matches, div=g.divs | count | bool) }}
 	{{ navigation(matches) }}
 {% endblock %}

trends/site/templates/macros/matches.html
@@ -100,8 +100,8 @@
 		{% endif %}
 		<td></td>
 		<td class="left">{{ demoicon(log.demoid) }}</td>
-		<td colspan="2"></td>
-		<td class="left" colspan="2">
+		<td colspan="{{ comp + div + 1 }}"></td>
+		<td class="left" colspan="{{ 1 + team1 }}">
 			<a href="{{ url_for('root.log', logids=log.logid) }}">
 				{{ log['title'] }}
 			</a>

trends/site/wsgi.py
@@ -162,6 +162,7 @@ def create_app():
 
     app.add_template_filter(any)
     app.add_template_filter(all)
+    app.add_template_filter(bool)
     app.add_template_filter(anynone)
     app.add_template_filter(opacit)
     app.add_template_filter(duration_filter, 'duration')