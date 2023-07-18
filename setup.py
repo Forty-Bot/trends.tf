Forty-Bot/trends.tf

commit 13541e0e90d45297ce00e60d52c95e857ce0b5a1

Add integration tests
This adds some basic integration tests. I'm not too happy with the way
pytest does fixtures (at the same time both magical and rigid), so this may
be reworked. I would like to be able to swap in a real database and test on
a much large sample size as well. For the moment, I just have some logs
which have been problematic in the past (old, broken/missing properties,
etc.).

There are two big tests added here, and one small one. The first test
crawls the site and follows all the links it can find. The primary purpose
of this test is to ensure that I haven't introduced any broken links or
gross errors. I use a random selector to limit the amount of pages
crawled. Unfortunately, hidden form inputs are not filled in properly, so
no forms are crawled for now.

The second test crawls the site as well, but it fills in form parameters.
We use knowledge of data to guide hypothesis. I am not really happy with
the way the fixtures are set up here. I think it would be nice to be able
to create module-level strategies which can query the database, but AFIAK
this is very unweildy with fixtures. I would like to extend this test to
also exercise pagination (should be simple, we just don't have enough logs
to have multiple pages without setting limit) and ordering (I have no idea
where we would get the acceptable keys from). I'd also like to be able to
exclude 404 errors here as well. At the moment we allow them because of
spectators.

Finally, we have a bit more traditional testing for the search
functionality. Unlike most other pages on the site, it has a sort of API
(including /api) with predictable responses. I would like to have more
tests like this, but I don't know if it's possible for most pages.

Closes: #74
Signed-off-by: Sean Anderson <seanga2@gmail.com>

setup.py
@@ -34,6 +34,7 @@ setuptools.setup(
         'tests': [
             'pytest',
             'hypothesis',
+            'python-testing-crawler',
         ],
     },
     entry_points = {

test/conftest.py
@@ -1,0 +1,89 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+# Copyright (C) 2022 Sean Anderson <seanga2@gmail.com>
+
+from contextlib import contextmanager
+import os
+import logging
+
+import pytest
+from testing.postgresql import Postgresql
+
+import trends.importer.logs
+from trends.importer.fetch import FileFetcher
+from trends.sql import db_connect, db_init
+
+@contextmanager
+def caplog_session(request):
+    request.node.add_report_section = lambda *args: None
+    logging_plugin = request.config.pluginmanager.getplugin('logging-plugin')
+    for _ in logging_plugin.pytest_runtest_setup(request.node):
+       yield pytest.LogCaptureFixture(request.node, _ispytest=True)
+
+@pytest.fixture(scope='session')
+def database(request):
+    postgres_args = Postgresql.DEFAULT_SETTINGS['postgres_args']
+    postgres_args += " -c full_page_writes=off"
+    with Postgresql(postgres_args=postgres_args) as database:
+        # This must happen in a separate connection because we use temporary tables which will alias
+        # other queries.
+        with db_connect(database.url()) as c:
+            db_init(c)
+            cur = c.cursor()
+
+            logfiles = { logid: f"{os.path.dirname(__file__)}/logs/log_{logid}.json"
+                for logid in (
+                    30099,
+                    2600722,
+                    2818814,
+                    2844704,
+                    3027588,
+                    3069780,
+                )
+            }
+
+            with caplog_session(request) as caplog:
+                with caplog.at_level(logging.ERROR):
+                    trends.importer.logs.import_logs(c, FileFetcher(logs=logfiles), False)
+                if caplog.records:
+                    pytest.fail("Error importing logs")
+
+            cur.execute("ANALYZE;")
+            cur.execute("REFRESH MATERIALIZED VIEW leaderboard_cube;")
+            cur.execute("REFRESH MATERIALIZED VIEW map_popularity;")
+
+        yield database
+
+@pytest.fixture(scope='session')
+def connection(database):
+    with db_connect(database.url()) as c:
+        yield c
+
+@pytest.fixture(scope='session')
+def logs(connection):
+    cur = connection.cursor()
+    cur.execute("SELECT logid FROM log LIMIT 1000;")
+    return [row[0] for row in cur]
+
+@pytest.fixture(scope='session')
+def players(connection):
+    cur = connection.cursor()
+    cur.execute("SELECT steamid64 FROM player LIMIT 1000;")
+    return [row[0] for row in cur]
+
+@pytest.fixture(scope='session')
+def titles(connection):
+    cur = connection.cursor()
+    cur.execute("SELECT title FROM log LIMIT 1000;")
+    return [row[0] for row in cur]
+
+@pytest.fixture(scope='session')
+def maps(connection):
+    cur = connection.cursor()
+    cur.execute("SELECT map FROM map LIMIT 1000;")
+    return [row[0] for row in cur]
+
+@pytest.fixture(scope='session')
+def names(connection):
+    cur = connection.cursor()
+    cur.execute("SELECT name FROM name WHERE length(name) >= 3 LIMIT 1000;")
+    return [row[0] for row in cur]

test/site_test.py
@@ -1,0 +1,135 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+# Copyright (C) 2022 Sean Anderson <seanga2@gmail.com>
+
+import random
+
+import hypothesis
+from hypothesis import assume, given, strategies as st
+import pytest
+from python_testing_crawler import Crawler, Rule, Request
+from werkzeug.datastructures import MultiDict
+
+from trends.site.wsgi import create_app
+from trends.util import classes
+
+@pytest.fixture(scope='session')
+def app(database):
+    app = create_app()
+    app.config.update({
+        'TESTING': True,
+        'DATABASE': database.url(),
+        'MEMCACHED_SERVERS': "",
+    })
+    return app
+
+@pytest.fixture(scope='session')
+def client(app):
+    return app.test_client()
+
+class RandomSelector:
+    def __init__(self, scale):
+        self.scale = scale
+        self.remaining = scale
+
+    def __call__(self, node):
+        if random.randrange(0, self.scale) < self.remaining:
+            self.remaining -= 1
+            return True
+        return False
+
+def test_crawl(client):
+    crawler = Crawler(
+        client=client,
+        initial_paths=('/',),
+        path_attrs=('href', 'src'),
+        rules=(
+            Rule('a', '/.*', 'GET', Request()),
+            Rule('link', '/.*', 'GET', Request()),
+            Rule('img', '/.*', 'GET', Request()),
+        ),
+        should_process_handlers=(RandomSelector(100),),
+    )
+    crawler.crawl()
+
+    for node in crawler.graph.map.values():
+        if node.requested:
+            print(node.path)
+
+class PlayerPage:
+    def __init__(self, endpoint):
+        self.endpoint = endpoint
+
+@st.composite
+def substrings(draw, strings, min_size=0):
+    s = draw(strings)
+    l = len(s)
+    assume(l >= min_size)
+    start = draw(st.integers(0, l - min_size))
+    # Integers shrink to 0, so try and shrink to the whole string
+    end = l - draw(st.integers(0, l - start - min_size))
+    return s[start:end]
+
+@given(st.data())
+def test_filter(client, logs, players, titles, maps, names, data):
+    players = st.sampled_from(players).map(str)
+
+    path = data.draw(st.one_of(
+        st.builds(lambda steamid, rule: rule.format(steamid), players, st.sampled_from((
+            "/player/{}",
+            "/player/{}/logs",
+            "/player/{}/peers",
+            "/player/{}/trends",
+            "/player/{}/totals",
+            "/player/{}/weapons",
+        ))),
+        st.lists(st.sampled_from(logs).map(str))
+            .map(lambda logs: "/log/{}".format("+".join(logs))),
+        st.sampled_from((
+            "/search",
+            "/logs",
+            "/leaderboard",
+            "/api/v1/players",
+            "/api/v1/logs",
+        )),
+    ))
+
+    params = MultiDict([
+        ('class', data.draw(st.sampled_from(('',) + classes))),
+        ('format', data.draw(st.sampled_from((
+            '',
+            'ultiduo',
+            'fours',
+            'sixes',
+            'prolander',
+            'highlander',
+            'other',
+        )))),
+        ('map', data.draw(substrings(st.sampled_from([''] + maps)))),
+        ('title', data.draw(substrings(st.sampled_from([''] + titles)))),
+        ('timezone', data.draw(st.timezone_keys())),
+        ('date_to_ts', data.draw(st.one_of(st.just(''), st.datetimes().map(str)))),
+        ('date_from_ts', data.draw(st.one_of(st.just(''), st.datetimes().map(str)))),
+        ('q', data.draw(st.one_of(players, substrings(st.sampled_from(names))))),
+    ] + [('steamid64', steamid) for steamid in data.draw(st.lists(players))])
+
+    hypothesis.note(f"path={path} params={params}")
+    assert client.get(path, query_string=params, follow_redirects=True).status_code < 500
+
+def test_search(client, connection):
+    players = connection.cursor()
+    players.execute("SELECT steamid64, name FROM player_stats JOIN name USING (nameid) LIMIT 10")
+    players = players.fetchall()
+
+    for path in ("/search", "/api/v1/players"):
+        for i in range(3):
+            assert client.get(path, query_string={'q': 'x' * i}).status_code == 400
+
+        for player in players:
+            if path == "/search":
+                resp = client.get(path, query_string={'q': player['steamid64']},
+                                  follow_redirects=True)
+                assert resp.history
+
+            if len(player['name']) > 3:
+                resp = client.get(path, query_string={'q': player['name']})
+                assert str(player['steamid64']) in resp.get_data(as_text=True)

commit 098726366421a350f7665efb4931b96a8e41f5f2

setup.py: Add missing tests dependencies
Add some test dependencies which were missing when tests were added.

Fixes: 13541e0 ("Add integration tests")
Fixes: 5d1dd94 ("Add test for fetchers")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

setup.py
@@ -35,9 +35,11 @@ setuptools.setup(
     ],
     extras_require = {
         'tests': [
-            'pytest',
             'hypothesis',
+            'pytest',
             'python-testing-crawler',
+            'responses',
+            'testing.postgresql'
         ],
     },
     entry_points = {