Forty-Bot/trends.tf

commit d0a4ce1b5ac597dbccb50414756b1c76d861e7f7

salt: Switch to Munin for metrics
Netdata takes up a lot of memory/cpu all the time (around 100M or so). We
are already memory-constrained, so this is too much. This switches to
Munin. I'm not really happy with it, so I may switch to e.g.
prometheus+grafana instead. At the very least it is much lighter.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

salt/frontend.sls
@@ -150,86 +150,75 @@ uwsgi_service:
       - uwsgi_config
 
 # Metrics
-netdata:
+munin:
   pkg.installed:
     - refresh: False
-  postgres_user.present:
-    - runas: postgres
-    - require:
-      - pkg: netdata
 
-psycopg2:
-  pkg.installed:
-{% if grains.os_family == 'Debian' %}
-    - name: python3-psycopg2
-{% else %}
-    - name: python-psycopg2
-{% endif %}
-    - refresh: False
-
-/etc/netdata/.opt-out-from-anonymous-statistics:
+munin_conf:
   file.managed:
-    - require:
-      - netdata
-
-/etc/netdata/netdata.conf:
-  file.managed:
+    - name: /etc/munin/munin-conf.d/override.conf
     - contents: |
-        [global]
-          run as user = netdata
-          access log = none
-          debug log = none
-
-          update every = 5
-          memory mode = dbengine
-          page cache size = 32
-          dbengine multihost disk space = 4096
-
-          process scheduling policy = idle
-          OOM score = 1000
-
-        [web]
-          web files owner = root
-{% if grains.os_family == 'Debian' %}
-          web files group = root
-{% else %}
-          web files group = netdata
-{% endif %}
-          bind to = unix:/run/netdata/netdata.sock
+        html_strategy cgi
+        graph_strategy cgi
+        cgiurl_graph /graph
     - require:
-      - netdata
+      - munin
 
-/etc/netdata/python.d/web_log.conf:
+/etc/systemd/system/munin-graph.service:
+  file.symlink:
+    - target: /usr/share/doc/munin/examples/systemd-fastcgi/munin-graph.service
+    - require:
+      - munin_conf
+
+/etc/systemd/system/munin-graph.socket:
+  file.symlink:
+    - target: /usr/share/doc/munin/examples/systemd-fastcgi/munin-graph.socket
+    - require:
+      - munin_conf
+
+/var/log/munin/munin-cgi-graph.log:
   file.managed:
-    - contents: |
-        nginx_log:
-          name: 'nginx'
-          path: '/var/log/nginx/access.log'
-          histogram: [1,10,100,1000]
+    - user: munin
+    - group: munin
+    - replace: False
     - require:
-      - netdata
+      - munin
 
-/etc/netdata/python.d/postgres.conf:
+/etc/systemd/system/munin-html.service:
+  file.symlink:
+    - target: /usr/share/doc/munin/examples/systemd-fastcgi/munin-html.service
+    - require:
+      - munin_conf
+
+/etc/systemd/system/munin-html.socket:
+  file.symlink:
+    - target: /usr/share/doc/munin/examples/systemd-fastcgi/munin-html.socket
+    - require:
+      - munin_conf
+
+/var/log/munin/munin-cgi-html.log:
   file.managed:
-    - contents: |
-        socket:
-          name: 'local'
-          user: 'netdata'
-          database: 'trends'
+    - user: munin
+    - group: munin
+    - replace: False
     - require:
-      - netdata
+      - munin
 
-netdata.service:
+munin-graph.socket:
   service.running:
     - enable: True
-    - reload: True
-    - requires:
-      - netdata
-      - psycopg2
-      - /etc/netdata/.opt-out-from-anonymous-statistics
-      - /etc/netdata/netdata.conf
-      - /etc/netdata/python.d/web_log.conf
-      - /etc/netdata/python.d/postgres.conf
+    - require:
+      - /etc/systemd/system/munin-graph.service
+      - /etc/systemd/system/munin-graph.socket
+      - /var/log/munin/munin-cgi-graph.log
+
+munin-html.socket:
+  service.running:
+    - enable: True
+    - require:
+      - /etc/systemd/system/munin-html.service
+      - /etc/systemd/system/munin-html.socket
+      - /var/log/munin/munin-cgi-html.log
 
 certbot:
   pkg.installed:
@@ -325,4 +314,21 @@ nginx.service:
       - /srv/uwsgi/trends
       - nginx_service
       - uwsgi_service
-      - netdata.service
+      - munin-graph.socket
+      - munin-html.socket
+
+munin_node:
+  pkg.installed:
+    - pkgs:
+      - libdbd-pg-perl
+      - munin-node
+      - munin-plugins-core
+    - refresh: False
+    - require:
+      - nginx.service
+
+munin-node.service:
+  service.running:
+    - enable: True
+    - require:
+      - munin_node

salt/etc/nginx/sites-available/trends.tf
@@ -72,7 +72,7 @@ server {
 	}
 }
 
-# netdata metrics
+# munin metrics
 server {
 	listen                  443 ssl http2;
 	listen                  [::]:443 ssl http2;
@@ -85,14 +85,23 @@ server {
 	add_header Content-Security-Policy "default-src 'self' http: https: data: blob: 'unsafe-inline' 'unsafe-eval'" always;
 
 	location / {
-		proxy_pass                        http://unix:/run/netdata/netdata.sock;
-		proxy_set_header Connection       $connection_upgrade;
-		proxy_set_header Host             $host;
-		proxy_set_header X-Forwarded-Host $host;
-		proxy_set_header X-Forwarded-Port $server_port;
+		include fastcgi_params;
 
-		proxy_http_version 1.1;
-		proxy_cache_bypass $http_upgrade;
+		fastcgi_pass unix:/run/munin/fcgi-html.sock;
+		fastcgi_param PATH_INFO $fastcgi_script_name;
+	}
+
+	location ^~ /graph/ {
+		include fastcgi_params;
+
+		fastcgi_split_path_info ^(/graph)(.*);
+		fastcgi_param PATH_INFO $fastcgi_path_info;
+		fastcgi_pass unix:/run/munin/fcgi-graph.sock;
+	}
+
+	location ^~ /static/ {
+		root /etc/munin/;
+		access_log off;
 	}
 }
 
@@ -105,6 +114,14 @@ server {
 	# ACME-challenge
 	location ^~ /.well-known/acme-challenge/ {
 		root /var/lib/letsencrypt/;
+	}
+
+	# Local stat collection
+	location = /nginx_status {
+		stub_status on;
+		access_log off;
+		allow 127.0.0.1;
+		deny all;
 	}
 
 	location / {

commit e9e384bea76119a30cc5d380027f680c07c8b6bf

salt: Add missing munin dependency on CGI::Fast
We use fastcgi for munin, but this was never added.

Fixes: d0a4ce1 ("salt: Switch to Munin for metrics")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

salt/frontend.sls
@@ -169,6 +169,9 @@ uwsgi_service:
 # Metrics
 munin:
   pkg.installed:
+    - pkgs:
+      - munin
+      - libcgi-fast-perl
     - refresh: False
 
 munin_conf: