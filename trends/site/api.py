Forty-Bot/trends.tf

commit 15bedaac63d36b1a28ec29986b060c2556264c9f

site: Reduce default cache valid time
Now that we implement last-modified, make nginx use a shorter valid time.
When a cache item expires, it will revalidate it upon a new request. For
pages backed by materialized views we set a longer timeout (as they are not
updated very frequently in the first place).

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/api.py
@@ -38,7 +38,10 @@ def maps():
            FROM map_popularity
            ORDER BY popularity DESC, mapid ASC
            LIMIT %s OFFSET %s;""", (limit, offset))
-    return flask.jsonify(maps=[row[0] for row in maps])
+
+    resp = flask.make_response(flask.jsonify(maps=[row[0] for row in maps]))
+    resp.cache_control.max_age = 86400
+    return resp
 
 @api.route('/players')
 def players():

salt/etc/nginx/sites-available/trends.tf
@@ -50,7 +50,8 @@ server {
 		uwsgi_cache_key $request_uri;
 		uwsgi_cache_lock on;
 		uwsgi_cache_use_stale updating;
-		uwsgi_cache_valid 200 5m;
+		uwsgi_cache_revalidate on;
+		uwsgi_cache_valid 30s;
 	}
 
 	location ^~ /static/ {

trends/site/root.py
@@ -102,7 +102,10 @@ def leaderboard():
                            LEFT JOIN name USING (nameid);"""
                            .format(filter_clauses, cube_clauses, order_clause),
                         { **filters, 'limit': limit, 'offset': offset })
-    return flask.render_template("leaderboard.html", leaderboard=leaderboard.fetchall())
+    resp = flask.make_response(flask.render_template("leaderboard.html",
+                               leaderboard=leaderboard.fetchall()))
+    resp.cache_control.max_age = 3600
+    return resp
 
 @root.route('/logs')
 def logs():

commit 237fb6b6eb4c22ee495c19230ae5b239278eb688

site: api: Fix cache-control max-age
Fix all API endpoints having the same max age.

Fixes: 15bedaa ("site: Reduce default cache valid time")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/api.py
@@ -20,7 +20,8 @@ def json_handler(error):
 @api.after_request
 def do_cache(resp):
     resp.add_etag()
-    resp.cache_control.max_age = 300
+    if resp.cache_control.max_age is None:
+        resp.cache_control.max_age = 300
     return resp
 
 @api.route('/logs')