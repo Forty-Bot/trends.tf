Forty-Bot/trends.tf

commit 71d660b3c0a1bcb68347a00a479233f9223656aa

site: Add maps page
This page displays stats broken down by map. This was already a somewhat
possible by using the totals page, but I think this is a much nicer
interface. We use the same hierarchal format as used on the log page.
Because each row has the same number of columns, I added some helper arrows
on the left side to distiguish branch rows from leaf nodes. This is
inspired by Thunar's tree view.

Maps names are divided up into parts, using non-alphanumerics as
separators. Generally, the first part is the map type, the second is the
map, and the rest is some kind of version. Heuristicly, this works quite
well, as long as things like workshop prefixes are excluded. There are
still a lot of nonconforming (manually-entered) map names, so we collapse
them by default.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/player.py
@@ -459,3 +459,77 @@ def trends(steamid):
     trends = list(dict(row) for row in cur)
     trends.reverse()
     return flask.render_template("player/trends.html", trends=trends, window=window)
+
+@player.route('/maps')
+def maps(steamid):
+    filters = get_filter_params()
+    filter_clauses = get_filter_clauses(filters, *surrogate_filter_columns)
+    maps = get_db().cursor()
+    maps.execute(
+        """SELECT
+               grouping(parts[1], parts[2], parts[3:]) AS grouping,
+               parts[1] AS part1,
+               parts[2] AS part2,
+               (array_agg(map ORDER BY logs DESC))[1] AS map,
+               (sum(wins) + 0.5 * sum(ties)) /
+                   nullif(sum(wins + losses + ties), 0) AS winrate,
+               (sum(round_wins) + 0.5 * sum(round_losses)) /
+                   nullif(sum(round_wins + round_losses + round_ties), 0) AS round_winrate,
+               sum(kills) AS kills,
+               sum(deaths) AS deaths,
+               sum(assists) AS assists,
+               sum(dmg) AS dmg,
+               sum(dt) AS dt,
+               total(kills) * 30 * 60 / nullif(total(duration), 0) AS k30,
+               total(deaths) * 30 * 60 / nullif(total(duration), 0) AS d30,
+               total(assists) * 30 * 60 / nullif(total(duration), 0) AS a30,
+               total(dmg) * 60 / nullif(total(duration), 0) AS dpm,
+               total(dt) * 60 / nullif(total(duration), 0) AS dtm,
+               total(hits) / nullif(sum(shots), 0.0) AS acc,
+               sum(logs) AS logs,
+               total(duration) AS duration
+           FROM (WITH map_unprefixed AS (SELECT
+                   mapid,
+                   replace(map, 'workshop/', '') AS map
+               FROM map) SELECT
+                   map,
+                   (SELECT
+                        array_agg(part)
+                    FROM unnest(regexp_split_to_array(lower(map), '[^a-z0-9]+')) AS part
+                    WHERE part != '') AS parts,
+                   stats.*
+               FROM (SELECT
+                       mapid,
+                       count(*) AS logs,
+                       sum(wins) AS round_wins,
+                       sum(losses) AS round_losses,
+                       sum(ties) AS round_ties,
+                       sum((wins > losses)::INT) AS wins,
+                       sum((wins < losses)::INT) AS losses,
+                       sum((wins = losses)::INT) AS ties,
+                       total(duration) AS duration,
+                       total(kills) AS kills,
+                       total(deaths) AS deaths,
+                       total(assists) AS assists,
+                       total(dmg) AS dmg,
+                       total(dt) AS dt,
+                       total(hits) AS hits,
+                       total(shots) AS shots
+                   FROM log_nodups
+                   JOIN player_stats using (logid)
+                   WHERE steamid64 = %(steamid)s
+                       {}
+                   GROUP BY mapid
+               ) AS stats
+               JOIN map USING (mapid)
+           ) AS maps
+           GROUP BY ROLLUP (parts[1], parts[2], parts[3:])
+           HAVING parts[1] IS NOT NULL
+           ORDER BY parts[1] NULLS FIRST,
+               CASE
+                   WHEN grouping(parts[2]) = 0 THEN coalesce(parts[2], '')
+                   ELSE NULL
+               END NULLS FIRST,
+               parts[3:] COLLATE numeric NULLS FIRST;""".format(filter_clauses),
+        {'steamid': steamid, **filters})
+    return flask.render_template("player/maps.html", maps=maps.fetchall())

trends/schema.sql
@@ -5,6 +5,9 @@
 CREATE EXTENSION IF NOT EXISTS pg_trgm;
 CREATE EXTENSION IF NOT EXISTS tsm_system_rows;
 
+-- Numeric sort
+CREATE COLLATION IF NOT EXISTS numeric (provider = icu, locale = 'en-u-kn-true');
+
 -- Compute the sum of an array
 CREATE OR REPLACE FUNCTION array_sum(anyarray) RETURNS anyelement
 	AS 'SELECT sum(val) FROM unnest($1) AS val'

trends/site/static/css/style.css
@@ -219,6 +219,22 @@ a.sort {
 	display: none;
 }
 
+.sortable th.tree {
+	background-image: none;
+	cursor: auto;
+}
+
+.hider .tree {
+	background-image: url(data:image/gif;base64,R0lGODlhFQAEAIAAACMtMP///yH5BAEAAAEALAAAAAAVAAQAAAINjB+gC+jP2ptn0WskLQA7);
+	background-repeat: no-repeat;
+	background-position: center right;
+	padding-right: 1em;
+}
+
+.hider.hiding .tree {
+	background-image: url(data:image/gif;base64,R0lGODlhFQAHAPABAAAAAP///yH5BAUKAAEALAAAAAAVAAcAAAISjI+Aywva4nlQNlptevrmPlkFADs=);
+}
+
 .padding-row {
 	height: 1em;
 }

trends/site/static/js/hider.js
@@ -1,7 +1,19 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 // Copyright (C) 2021 Sean Anderson <seanga2@gmail.com>
 
+/**
+ * update_hidden() - Update hidden rows after a click
+ * @hider - The row which was clicked
+ * @hide - Whether to unconditionally hide rows, or toggle them
+ * @recursive - Whether to recurse into child rows
+ */
 function update_hidden(hider, hide, recursive) {
+	if (hide) {
+		hider.classList.add('hiding');
+	} else {
+		hider.classList.toggle('hiding');
+	}
+
 	let elements = document.getElementsByClassName(hider.id);
 	for (let element of elements) {
 		let hidden = true;

trends/site/templates/player/base.html
@@ -46,7 +46,8 @@
 	<ul class="nav">
 	{% for (endpoint, name) in (('.overview', "Overview"), ('.logs', "Logs"),
 	                            ('.peers', "Peers"), ('.totals', "Totals"),
-	                            ('.weapons', "Weapons"), ('.trends', "Trends")) %}
+				    ('.weapons', "Weapons"), ('.trends', "Trends"),
+				    ('.maps', "Maps")) %}
 		{% set url = url_for(endpoint, steamid=g.player['steamid64']) %}
 		<li class="nav"><a class="{{ "active" if url == request.path }}" href="{{ url }}">
 			{{ name }}

trends/site/templates/player/maps.html
@@ -1,0 +1,114 @@
+{# SPDX-License-Identifier: AGPL-3.0-only #}
+{# Copyright (C) 2022 Sean Anderson <seanga2@gmail.com> #}
+{% from "macros/filter.html" import filter %}
+{% from "macros/format.html" import comma_col, duration_col, optformat, optint %}
+{% from "macros/js.html" import local_js %}
+{% from "macros/wlt.html" import wlt%}
+{% extends "player/base.html" %}
+{% block title %}Maps - {{ super() }}{% endblock %}
+{% block head %}
+	{{ super() }}
+	{{ local_js("js/hider.js") }}
+	{{ local_js("js/sort.js") }}
+{% endblock %}
+{% macro map_cols(map, logs) %}
+	<td class="tree"></td>
+	<td class="left">{{ map }}</td>
+	<td data-value="{{ logs }}">
+		{% set args = request.args.copy() %}
+		{% do args.update(request.view_args) %}
+		{% do args.__setitem__('map', map) %}
+		<a href="{{ url_for('.logs', **args) }}">
+			{{ optformat("{:,.0f}", logs) }}
+		</a>
+	</td>
+{% endmacro %}
+{% block content %}
+	{{ super() }}
+	<h2>Maps</h2>
+	{{ filter() }}
+	<table>
+		<thead class="sortable">
+		<tr>
+			<th class="tree"></th>
+			<th>Map</th>
+			<th>Logs</th>
+			<th><abbr title="Winrate; ties count as 0.5 wins">WR</abbr></th>
+			<th><abbr title="Round winrate; ties count as 0.5 wins">RWR</abbr></th>
+			<th><abbr title="Kills per 30 minutes">K/30</abbr></th>
+			<th><abbr title="Deaths per 30 minutes">D/30</abbr></th>
+			<th><abbr title="Assists per 30 minutes">A/30</abbr></th>
+			<th><abbr title="Damage per Minute">DA/M</abbr></th>
+			<th><abbr title="Damage Taken per Minute">DT/M</abbr></th>
+			<th><abbr title="Accuracy">Acc</abbr></th>
+			<th><abbr title="Kills">K</abbr></th>
+			<th><abbr title="Deaths">D</abbr></th>
+			<th><abbr title="Assists">A</abbr></th>
+			<th><abbr title="Damage">DA</abbr></th>
+			<th><abbr title="Damage Taken">DT</abbr></th>
+			<th>Time played</th>
+		</tr>
+		</thead>
+		<tbody class="hier">
+			{% set prefixes = (
+				"arena",
+				"cp",
+				"ctf",
+				"jump",
+				"koth",
+				"mge",
+				"mvm",
+				"pass",
+				"pd",
+				"pl",
+				"plr",
+				"rd",
+				"sd",
+				"tc",
+				"tr",
+				"ultiduo",
+				"vsh",
+			) %}
+			{% for map in maps %}
+			{% set part12 = "{}_{}".format(map['part1'], map['part2']) %}
+			{% if map['grouping'] == 3 %}
+			<tr id="{{ map['part1'] }}"
+			    class="hider {{ 'hiding' if map['part1'] not in prefixes }}">
+				{{ map_cols(map['part1'], map['logs']) }}
+			{% elif map['grouping'] == 1 %}
+			<tr id="{{ part12 }}"
+			    class="{{ map['part1'] }} hider hidable hiding
+			           {{ 'hidden' if map['part1'] not in prefixes }}">
+				{% if map['part2'] == None %}
+					{{ map_cols(map['part1'], map['logs']) }}
+				{% else %}
+					{{ map_cols(part12, map['logs']) }}
+				{% endif %}
+			{% else %}
+			<tr class="{{ part12 }} hidden hidable2">
+				{{ map_cols(map['map'], map['logs']) }}
+			{% endif %}
+				<td>{{ optformat("{:.2%}", map['winrate']) }}</td>
+				<td>{{ optformat("{:.2%}", map['round_winrate']) }}</td>
+				<td>{{ "{:,.1f}".format(map['k30']) }}</td>
+				<td>{{ "{:,.1f}".format(map['d30']) }}</td>
+				<td>{{ "{:,.1f}".format(map['a30']) }}</td>
+				<td>{{ optint(map['dpm']) }}</td>
+				<td>{{ optint(map['dtm']) }}</td>
+				<td>{{ optformat("{:.0%}", map['acc']) }}</td>
+				{{ comma_col(map['kills']) }}
+				{{ comma_col(map['deaths']) }}
+				{{ comma_col(map['assists']) }}
+				{{ comma_col(map['dmg']) }}
+				{{ comma_col(map['dt']) }}
+				{{ duration_col(map['duration']) }}
+			</tr>
+			{% if loop.last or map['grouping'] == 3 or
+			      loop.nextitem['grouping'] == 3 %} 
+			      <tr class="{{ map['part1'] }} padding-row
+			                 {{ 'hidden' if map['part1'] not in prefixes }}"></tr>
+		        {% endif %}
+			{% endfor %}
+		</tbody>
+	</table>
+{% endblock %}

commit 1f73a68aa519caac3a927cbed22666bd2db9c555

site: maps: Fix round winrate calculation
Losses were counting towards wins instead of ties. Fix this.

Fixes: 71d660b ("site: Add maps page")
Closes: #103
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/player.py
@@ -611,7 +611,7 @@ def maps(steamid):
                (array_agg(map ORDER BY logs DESC))[1] AS map,
                (sum(wins) + 0.5 * sum(ties)) /
                    nullif(sum(wins + losses + ties), 0) AS winrate,
-               (sum(round_wins) + 0.5 * sum(round_losses)) /
+               (sum(round_wins) + 0.5 * sum(round_ties)) /
                    nullif(sum(round_wins + round_losses + round_ties), 0) AS round_winrate,
                sum(kills) AS kills,
                sum(deaths) AS deaths,