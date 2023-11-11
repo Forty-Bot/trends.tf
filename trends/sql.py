Forty-Bot/trends.tf

commit 35f74b04289e334043956d36b0bbdd6dbac6f076

importer: log: Don't trace some sql statements
Each player import results in around 300 individual SQL statements. This
causes us to exceed the span limit. While we should try and reduce this,
for now just disable tracing these statements. Additionally, we do a select
for each logid, so don't trace that either.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/sql.py
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # Copyright (C) 2020 Sean Anderson <seanga2@gmail.com>
 
+import contextlib
 import os
 import sys
 
@@ -9,8 +10,19 @@ from sentry_sdk import Hub, tracing_utils
 
 from .steamid import SteamID
 
+tracing_disabled = False
+
+@contextlib.contextmanager
+def disable_tracing():
+    global tracing_disabled
+    tracing_disabled = True
+    yield
+    tracing_disabled = False
+
 class TracingCursor(psycopg2.extras.DictCursor):
     def _log(self, query, vars, paramstyle=psycopg2.paramstyle):
+        if tracing_disabled:
+            return contextlib.nullcontext()
         return tracing_utils.record_sql_queries(Hub.current, self, query, vars,
                                                 paramstyle, False)
 

trends/importer/logs.py
@@ -12,7 +12,7 @@ import zstandard
 
 from .fetch import ListFetcher, BulkFetcher, FileFetcher, ReverseFetcher, CloneLogsFetcher
 from ..steamid import SteamID
-from ..sql import table_columns
+from ..sql import table_columns, disable_tracing
 from .. import util
 
 def filter_logids(c, logids, update_only=False):
@@ -34,10 +34,11 @@ def filter_logids(c, logids, update_only=False):
             time = None
 
         cur = c.cursor()
-        cur.execute("""SELECT
-                           time < %s
-                       FROM public.log
-                       WHERE logid = %s""", (time, logid))
+        with disable_tracing():
+            cur.execute("""SELECT
+                               time < %s
+                           FROM public.log
+                           WHERE logid = %s""", (time, logid))
 
         for row in cur:
             if row[0]:
@@ -794,7 +795,8 @@ def import_logs(c, fetcher, update_only):
         if log is None:
             continue
 
-        with sentry_sdk.start_span(op='db.transaction', description=f"import {logid}"):
+        with sentry_sdk.start_span(op='db.transaction', description=f"import {logid}"), \
+             disable_tracing():
             cur.execute("BEGIN;")
             cur.execute("SAVEPOINT import;")
             try:

commit f8bed245c68b6705f004247861a5efa2d519c2f4

sql: Enable tracing after exceptions
Ensure tracing gets enabled even after there is an exception.

Fixes: 35f74b0 ("importer: log: Don't trace some sql statements")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/sql.py
@@ -17,8 +17,10 @@ tracing_disabled = False
 def disable_tracing():
     global tracing_disabled
     tracing_disabled = True
-    yield
-    tracing_disabled = False
+    try:
+        yield
+    finally:
+        tracing_disabled = False
 
 class TracingCursor(psycopg2.extras.DictCursor):
     def _log(self, query, vars, paramstyle=psycopg2.paramstyle):