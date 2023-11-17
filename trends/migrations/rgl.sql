273e5e3c16cbca3d6711124cb56ed1b8292a50fa
Sean Anderson <seanga2@gmail.com>
2023-11-17 16:41:07 -0500

    Add migration for converting scores
    When modifying the schema, I forgot to add a migration. Do so now.
    
    Fixes: ad2f5d7 ("Add RGL support")
    Fixes: aab6939 ("Store match score as a float")
    Signed-off-by: Sean Anderson <seanga2@gmail.com>

--- trends/migrations/rgl.sql
+++ trends/migrations/rgl.sql
@@ -3,6 +3,10 @@ ALTER TABLE league_team ADD CHECK (
 	equal(league_team_per_comp(league), fetched ISNULL, team_nameid ISNULL)
 );
 ALTER TABLE league_team ALTER team_nameid DROP NOT NULL;
+DROP VIEW match_pretty;
+DROP VIEW match_wlt;
+ALTER TABLE match ALTER score1 TYPE REAL;
+ALTER TABLE match ALTER score2 TYPE REAL;
 DROP INDEX match_team1;
 DROP INDEX match_team2;
 DROP INDEX match_comp;
