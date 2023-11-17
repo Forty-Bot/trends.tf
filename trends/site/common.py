d31b800c1e9d740d3f1272ccdeecd8d1a16c0f27
Sean Anderson <seanga2@gmail.com>
2023-11-17 17:17:49 -0500

    site: league: comp: Refactor players page into a function/macro
    Refactor out the common parts of the players page into a function/macro
    (depending on the language) in preparation for adding it to the teams page.
    
    Signed-off-by: Sean Anderson <seanga2@gmail.com>

--- trends/site/common.py
+++ trends/site/common.py
@@ -155,3 +155,96 @@ def get_matches(compid, filters, limit=100, offset=0):
         { **filters, 'league': flask.g.league, 'compid': compid, 'limit': limit,
           'offset': offset })
     return matches
+
+def get_players(league, compid, filters, limit=100, offset=0):
+    filter_clauses = get_filter_clauses(filters, 'divid', 'primary_classid', 'map', 'time', 'logid')
+    filter_clauses += "\nAND compid = %(compid)s"
+
+    order, order_clause = get_order({
+        'name': "name",
+        'k30': "k30",
+        'd30': "d30",
+        'a30': "a30",
+        'kd': "kd",
+        'kad': "kad",
+        'dpm': "dpm",
+        'dtm': "dtm",
+        'ddm': "ddm",
+        'dr': "dr",
+        'acc': "acc",
+        'dps': "dps",
+        'kills': "kills",
+        'deaths': "deaths",
+        'assists': "assists",
+        'dmg': "dmg",
+        'dt': "dt",
+        'duration': "duration",
+    }, 'name', 'asc')
+
+    players = get_db().cursor()
+    players.execute(
+        """SELECT
+               steamid64,
+               avatarhash,
+               name,
+               (SELECT json_object_agg(class, d * 1.0 / duration ORDER BY d DESC)
+                FROM unnest(classes, durations) AS c(class, d)) AS classes,
+               kills * 30.0 * 60 / duration AS k30,
+               deaths * 30.0 * 60 / duration AS d30,
+               assists * 30.0 * 60 / duration AS a30,
+               kills * 1.0 / nullif(deaths, 0) AS kd,
+               (kills + assists) * 1.0 / nullif(deaths, 0) AS kad,
+               dmg * 60.0 / duration AS dpm,
+               dt * 60.0 / duration AS dtm,
+               (dmg - dt) * 60.0 / duration AS ddm,
+               dmg * 1.0 / nullif(dt, 0) AS dr,
+               hits * 1.0 / nullif(shots, 0) AS acc,
+               dmg * 1.0 / nullif(shots, 0) AS dps,
+               kills,
+               deaths,
+               assists,
+               dmg,
+               dt,
+               duration
+           FROM(SELECT
+                   playerid,
+                   sum(kills) AS kills,
+                   sum(deaths) AS deaths,
+                   sum(assists) AS assists,
+                   sum(dmg) AS dmg,
+                   sum(dt) AS dt,
+                   sum(hits) AS hits,
+                   sum(shots) AS shots,
+                   sum(duration) AS duration,
+                   array_agg(class ORDER BY duration DESC) AS classes,
+                   array_agg(duration ORDER BY duration DESC) AS durations
+               FROM (SELECT
+                       playerid,
+                       primary_classid AS classid,
+                       sum(kills) AS kills,
+                       sum(deaths) AS deaths,
+                       sum(assists) AS assists,
+                       sum(dmg) AS dmg,
+                       sum(dt) AS dt,
+                       sum(hits) AS hits,
+                       sum(shots) AS shots,
+                       sum(duration) AS duration
+                   FROM match
+                   JOIN log USING (league, matchid)
+                   JOIN player_stats USING (logid)
+                   JOIN map USING (mapid)
+                   WHERE league = %(league)s
+                       {}
+                   GROUP BY playerid, primary_classid
+               ) AS player_stats
+               JOIN class USING (classid)
+               GROUP BY playerid
+           ) AS player_stats
+           JOIN player USING (playerid)
+           JOIN name USING (nameid)
+           ORDER BY {} NULLS LAST
+           LIMIT %(limit)s OFFSET %(offset)s;""".format(filter_clauses, order_clause),
+        { **filters, 'league': league, 'compid': compid, 'teamid': teamid, 'limit': limit,
+          'offset': offset })
+
+    return players
