a7eee69954ce8458ecd5b9ef3897bd154c832b7c
Sean Anderson <seanga2@gmail.com>
2023-11-24 15:26:21 -0500

    site: Don't display timestamps for all dates
    Dates with timestamps are helpful for identifying logs and matches, as
    multiple may be played in one day. However, they just add noise to certain
    elements (such as season starts/ends and roster dates) which generally
    (aside from some exceedingly-quick rosters) take place over weeks or
    longer. Create a second macro for time-less dates and convert existing uses
    to the timed version.
    
    Signed-off-by: Sean Anderson <seanga2@gmail.com>

--- trends/site/static/js/dates.js
+++ trends/site/static/js/dates.js
@@ -1,11 +1,16 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 // Copyright (C) 2020 Sean Anderson <seanga2@gmail.com>
 
-var time_formatter = new Intl.DateTimeFormat('und', {dateStyle: 'medium', timeStyle: 'short'});
+var date_formatter = new Intl.DateTimeFormat('und', {dateStyle: 'medium'});
+var datetime_formatter = new Intl.DateTimeFormat('und', {dateStyle: 'medium', timeStyle: 'short'});
 
 document.addEventListener('DOMContentLoaded', () => {
+	for (date of document.getElementsByClassName("datetime"))
+		date.textContent = datetime_formatter.format(
+			new Date(date.getAttribute('timestamp') * 1000));
+
 	for (date of document.getElementsByClassName("date"))
-		date.textContent = time_formatter.format(
+		date.textContent = date_formatter.format(
 			new Date(date.getAttribute('timestamp') * 1000));
 });
 
