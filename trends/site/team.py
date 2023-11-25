Forty-Bot/trends.tf

commit 513352cefbc63f85c76f7a418aa9d91282887a27

site: league: team: Add competitions tab
Most teams have only a few seasons. But it is nice to have a full list of
competitions, especially for teams with extensive histories.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/team.py
@@ -164,6 +164,12 @@ def get_matches(league, teamid, filters, limit=100, offset=0):
     return matches
 
 def get_comps(league, teamid, limit=100, offset=0):
+    order, order_clause = get_order({
+        'compid': "compid",
+        'from': "date_from",
+        'to': "date_to",
+    }, 'compid')
+
     comps = get_db().cursor()
     comps.execute(
         """SELECT
@@ -179,7 +185,9 @@ def get_comps(league, teamid, limit=100, offset=0):
                (wins + 0.5 * ties) / nullif(wins + losses + ties, 0) AS winrate,
                rounds_won,
                rounds_lost,
-               rounds_won::NUMERIC / nullif(rounds_won + rounds_lost, 0) AS round_winrate
+               rounds_won::NUMERIC / nullif(rounds_won + rounds_lost, 0) AS round_winrate,
+               date_from AS from,
+               date_to AS to
            FROM team_comp
            JOIN competition USING (league, compid)
            LEFT JOIN division USING (league, compid, divid)
@@ -193,13 +201,15 @@ def get_comps(league, teamid, limit=100, offset=0):
                    sum(loss) AS losses,
                    sum(tie) AS ties,
                    sum(rounds_won) AS rounds_won,
-                   sum(rounds_lost) AS rounds_lost
+                   sum(rounds_lost) AS rounds_lost,
+                   max(scheduled) AS date_to,
+                   min(scheduled) AS date_from
                FROM match_wlt
                GROUP BY league, compid, teamid
            ) AS match USING (league, compid, teamid)
            WHERE league = %s AND teamid = %s
-           ORDER BY compid desc
-           LIMIT %s OFFSET %s;""", (league, teamid, limit, offset))
+           ORDER BY {}
+           LIMIT %s OFFSET %s;""".format(order_clause), (league, teamid, limit, offset))
 
     return comps
 
@@ -285,6 +295,14 @@ def roster(league, teamid):
        { **filters, 'league': league, 'teamid': teamid, 'limit': limit, 'offset': offset })
 
     return flask.render_template("league/team/roster.html", roster=roster.fetchall())
+
+@team.route('/comps')
+def comps(league, teamid):
+    limit, offset = get_pagination()
+
+    comps = get_comps(league, teamid, limit, offset)
+
+    return flask.render_template("league/team/comps.html", comps=comps.fetchall())
 
 @team.route('/matches')
 def matches(league, teamid):

trends/site/templates/league/team/base.html
@@ -43,6 +43,6 @@
 		</div>
 	</div>
 
-	{{ navbar(('.overview', "Overview"), ('.roster', "Roster"), ('.matches', "Matches"),
-		  league=g.league, teamid=g.teamid) }}
+	{{ navbar(('.overview', "Overview"), ('.comps', "Competitions"), ('.roster', "Roster"),
+		  ('.matches', "Matches"), league=g.league, teamid=g.teamid) }}
 {% endblock %}

trends/site/templates/league/team/comps.html
@@ -1,0 +1,19 @@
+{# SPDX-License-Identifier: AGPL-3.0-only #{# SPDX-License-Identifier: AGPL-3.0-only #}
+{# Copyright (C) 2021 Sean Anderson <seanga2@gmail.com> #}
+{% from "macros/comps.html" import comp_table %}
+{% from "macros/pagination.html" import navigation %}
+{% from "macros/sort.html" import sort %}
+{% extends "league/team/base.html" %}
+{% block title %}Competitions - {{ super() }}{% endblock %}
+{% block content %}
+	{{ super() }}
+	<h2>Competitions</h2>
+	{{ sort({
+		'compid': "Competition ID",
+		'from': "From",
+		'to': "To",
+	}) }}
+	{{ navigation(comps) }}
+	{{ comp_table(comps, date=True) }}
+	{{ navigation(comps) }}
+{% endblock %}

trends/site/templates/macros/comps.html
@@ -1,9 +1,9 @@
 {# SPDX-License-Identifier: AGPL-3.0-only #}
 {# Copyright (C) 2023 Sean Anderson <seanga2@gmail.com> #}
-{% from "macros/format.html" import optformat %}
+{% from "macros/format.html" import date_col, optformat %}
 {% from "macros/pretty.html" import format_map %}
 {% from "macros/wlt.html" import wlt %}
-{% macro comp_table(comps) %}
+{% macro comp_table(comps, date=False) %}
 <table>
 <thead>
 	<tr>
@@ -18,6 +18,10 @@
 		<th><abbr title="Points Won">PW</abbr></th>
 		<th><abbr title="Points Lost">PL</abbr></th>
 		<th><abbr title="Point Ratio">PR</abbr></th>
+		{% if date %}
+		<th><abbr title="Date of oldest match">From</abbr></th>
+		<th><abbr title="Date of newest match">To</abbr></th>
+		{% endif %}
 	</tr>
 </thead>
 <tbody>
@@ -44,6 +48,10 @@
 		<td>{{ comp.rounds_won | score }}</td>
 		<td>{{ comp.rounds_lost | score }}</td>
 		<td>{{ optformat("{:.0%}", comp.round_winrate) }}</td>
+		{% if date %}
+		{{ date_col(comp.from) }}
+		{{ date_col(comp.to) }}
+		{% endif %}
 	</tr>
 	{% endfor %}
 </tbody>

commit c24ab48ec8295a85dc58f4647b2fa8b0dfc96881

site: teams: Fix incorrect ordering of matches on team overview
There can only be one call to get_order per page. This is because we cache the selected order for
the sort macro. Therefore, we can't call get_order from helpers (such as get_comps and get_matches)
which might be called multiple times (such as on the overview page). Provide an explicit default
order clause instead.

Fixes: 513352c ("site: league: team: Add competitions tab")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/team.py
@@ -63,7 +63,7 @@ def get_comp():
            ) AS wlt;""", (flask.g.league, flask.g.teamid))
     flask.g.team_wlt = cur.fetchone()
 
-def get_matches(league, teamid, filters, limit=100, offset=0):
+def get_matches(league, teamid, filters, order_clause="scheduled DESC", limit=100, offset=0):
     filter_clauses = get_filter_clauses(filters, comp='competition.name', time='scheduled')
     if filters['map']:
         maps = """JOIN (SELECT
@@ -78,11 +78,6 @@ def get_matches(league, teamid, filters, limit=100, offset=0):
                   ) AS maps ON (mapid = ANY(mapids))"""
     else:
         maps = ""
-    order, order_clause = get_order({
-        'round': "round_seq",
-        'date': "scheduled",
-        'matchid': "matchid",
-    }, 'date')
 
     matches = get_db().cursor()
     matches.execute(
@@ -176,13 +171,7 @@ def get_matches(league, teamid, filters, limit=100, offset=0):
 
     return matches
 
-def get_comps(league, teamid, limit=100, offset=0):
-    order, order_clause = get_order({
-        'compid': "compid",
-        'from': "date_from",
-        'to': "date_to",
-    }, 'compid')
-
+def get_comps(league, teamid, order_clause="compid DESC", limit=100, offset=0):
     comps = get_db().cursor()
     comps.execute(
         """SELECT
@@ -312,8 +301,13 @@ def roster(league, teamid):
 @team.route('/comps')
 def comps(league, teamid):
     limit, offset = get_pagination()
+    order, order_clause = get_order({
+        'compid': "compid",
+        'from': "date_from",
+        'to': "date_to",
+    }, 'compid')
 
-    comps = get_comps(league, teamid, limit, offset)
+    comps = get_comps(league, teamid, order_clause, limit, offset)
 
     return flask.render_template("league/team/comps.html", comps=comps.fetchall())
 
@@ -331,9 +325,14 @@ def get_comp_list(league, teamid):
 @team.route('/matches')
 def matches(league, teamid):
     limit, offset = get_pagination()
+    order, order_clause = get_order({
+        'round': "round_seq",
+        'date': "scheduled",
+        'matchid': "matchid",
+    }, 'date')
 
     comps = get_comp_list(league, teamid)
-    matches = get_matches(league, teamid, get_filter_params(), limit, offset)
+    matches = get_matches(league, teamid, get_filter_params(), order_clause, limit, offset)
 
     return flask.render_template("league/team/matches.html", matches=matches.fetchall(),
                                  comps=comps)