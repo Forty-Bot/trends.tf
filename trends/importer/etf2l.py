Forty-Bot/trends.tf

commit ad2f5d721f567b93f7a0b3689ad50641f1b51455

Add RGL support
At long last, add RGL support. There are still a lot of API bugs and data
errors on the RGL side, requiring several hacks on our end:

- There is no way to differentiate rounds except by name. We must create
  our own round_seq upon insertion. There are still several cases where
  rounds have the same name...
- Most teams have incorrect divisions, so we must fix them up from the
  match data. This doesn't always work, so there are several special cases.
- Sometimes the timestamp for when a player joined is earlier than for when
  they left. We just ignore these for now.

The rest of this commit is fairly straightforward. The table checks are
adjusted a bit to reflect the data as-is. Additionally, some of the indices
need to be modified to still apply.

The site itself is mostly the same. The team page is adjusted to link to an
appropriate rgl_teamid (or the most-recent one).

At the moment there is no scheduled import, since the round stuff is still
unresolved. I hope that this can be addressed quickly so I can deploy this
feature.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/importer/fetch.py
@@ -2,10 +2,14 @@
 # Copyright (C) 2020-21 Sean Anderson <seanga2@gmail.com>
 
 import collections
+from datetime import datetime
+from dateutil import tz
+from glob import glob
 import itertools
 import json
 import logging
 import os
+import re
 import sqlite3
 import time
 
@@ -510,3 +514,95 @@ class ETF2LBulkFetcher:
     def get_xfers(self, teamid, since=0):
         return self._get_data(f"https://api.etf2l.org/team/{teamid}/transfers", 'transfers',
                               since=since)
+
+class RGLFileFetcher:
+    def __init__(self, dir, **kwargs):
+        self.dir = dir
+
+    RE_MATCH_FILE = re.compile(r"^.*match_([0-9]*).json$")
+    def get_matchids(self):
+        for file in glob(f"{self.dir}/match_*.json"):
+            match = self.RE_MATCH_FILE.match(file)
+            if match:
+                yield int(match.group(1))
+
+    def get_json(self, path):
+        try:
+            logging.debug("GET %s", path)
+            with open(path) as file:
+                data = json.load(file)
+            fetched = int(os.path.getmtime(path))
+            data['fetched'] = fetched
+            return data
+        except FileNotFoundError:
+            pass
+
+    def get_season(self, seasonid):
+        return self.get_json(f"{self.dir}/season_{seasonid}.json")
+
+    def get_match(self, matchid):
+        return self.get_json(f"{self.dir}/match_{matchid}.json")
+
+    def get_team(self, teamid):
+        return self.get_json(f"{self.dir}/team_{teamid}.json")
+
+class RGLBulkFetcher:
+    def __init__(self, since=0, count=None, skip=0, **kwargs):
+        self.s = create_session()
+        self.count = count
+        self.skip = skip
+
+    PREFIX = "https://api.rgl.gg/v0"
+
+    def get_matchids(self):
+        # Number of datums yielded (up to a maximum of count)
+        yielded = 0
+        # The id we've seen. We assume new matches will all have higher matchids.
+        last_id = None
+        skip = self.skip
+
+        try:
+            while True:
+                fetched = int(time.time())
+                resp = self.s.post(f"{self.PREFIX}/matches/paged", params={
+                    'take': 100,
+                    'skip': skip,
+                }, json={
+                    'after': datetime.fromtimestamp(0, tz.UTC).isoformat(),
+                })
+                resp.raise_for_status()
+
+                resp = resp.json()
+                for result in resp:
+                    yield result['matchId']
+                    yielded += 1
+                    if self.count is not None and yielded >= self.count:
+                        return
+
+                skip += len(resp)
+        except (OSError, urllib3.exceptions.HTTPError):
+            logging.exception("Could not fetch matches")
+        except (ValueError, KeyError):
+            logging.exception("Could not parse matches")
+
+    def _get_data(self, url):
+        try:
+            fetched = int(time.time())
+            resp = self.s.get(url)
+            resp.raise_for_status()
+            resp = resp.json()
+            resp['fetched'] = fetched
+            return resp
+        except (OSError, urllib3.exceptions.HTTPError):
+            logging.exception("Could not fetch %s", url)
+        except (ValueError, KeyError):
+            logging.exception("Could not parse %s", url)
+
+    def get_season(self, seasonid):
+        return self._get_data(f"{self.PREFIX}/seasons/{seasonid}")
+
+    def get_match(self, matchid):
+        return self._get_data(f"{self.PREFIX}/matches/{matchid}")
+
+    def get_team(self, teamid):
+        return self._get_data(f"{self.PREFIX}/teams/{teamid}")

trends/importer/rgl.py
@@ -1,0 +1,242 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+# Copyright (C) 2022 Sean Anderson <seanga2@gmail.com>
+
+from datetime import datetime
+import functools
+import re
+import json
+import logging
+import time
+
+from psycopg2.extras import NumericRange
+import sentry_sdk
+
+from .fetch import RGLBulkFetcher, RGLFileFetcher
+from .league import *
+from ..sql import db_connect
+from ..steamid import SteamID
+from ..util import chunk
+
+def filter_matchids(c, matchids):
+    with c.cursor() as cur:
+        for matchids in chunk(matchids, 100):
+            psycopg2.extras.execute_values(cur,
+                """SELECT new.matchid
+                   FROM (VALUES %s) AS new (matchid)
+                   LEFT JOIN match ON (league = 'rgl' AND match.matchid = new.matchid)
+                   WHERE match.matchid IS NULL""", ((matchid,) for matchid in matchids))
+            for row in cur:
+                yield row[0]
+
+rgl_format_map = {
+    'Sixes': 'sixes',
+    'NR Sixes': 'sixes',
+    'P7': 'prolander',
+    'Prolander': 'prolander',
+    'Fresh Meat': 'prolander',
+    'HL': 'highlander',
+    'Highlander': 'highlander',
+    'Newcomer Cup': 'sixes',
+}
+
+RE_NAME_FORMAT = re.compile(r"(Sixes|NR Sixes|P7|Prolander|Fresh Meat|HL|Newcomer Cup)")
+def parse_season(season):
+    if season['formatName'] is None:
+        m = RE_NAME_FORMAT.match(season['name'])
+        if not m:
+            raise ValueError(f"Unknown format for {season['name']}")
+        fmt = rgl_format_map[m.group(1)]
+    else:
+        fmt = rgl_format_map[season['formatName']]
+
+    return {
+        'format': fmt,
+        'div_tiers': { int(div): -order for div, order in season['divisionSorting'].items() },
+    }
+
+def parse_date(date):
+    if date is None:
+        return None
+    # datetime doesn't properly parse Z as a timezone
+    return int(datetime.fromisoformat(date.replace('Z', "+00:00")).timestamp())
+
+def parse_match(result):
+    # FIXME: Dirty data, dirty hacks
+    if result['matchId'] == 1418:
+        result['isForfeit'] = True
+    elif result['matchId'] == 1419:
+        result['matchDate'] = "2018-09-27T01:30:00.000Z"
+    elif result['matchId'] == 6495:
+        result['matchDate'] = "2020-05-04T01:30:00.000Z"
+    elif result['divisionId'] == 462:
+        result['divisionName'] = "Solo Queue A"
+    elif result['divisionId'] == 629:
+        result['divisionId'] = 603
+        result['divisionName'] = "Advanced"
+    elif result['divisionId'] == 642:
+        result['divisionId'] = 635
+        result['divisionName'] = "Invite"
+
+    return {
+        'league': 'rgl',
+        'compid': result['seasonId'],
+        'competition': result['seasonName'],
+        'divid': result['divisionId'],
+        'division': result['divisionName'],
+        'teams': tuple({
+            'rgl_teamid': team['teamId'],
+            'score': team['points'],
+        } for team in result['teams']),
+        'seq': ..., # FIXME
+        'round': result['matchName'],
+        'matchid': result['matchId'],
+        'scheduled': parse_date(result['matchDate']),
+        'submitted': None,
+        'forfeit': result['isForfeit'],
+        'fetched': result['fetched'],
+        'maps': [map['mapName'] for map in result['maps']] or None,
+    }
+
+def parse_team(team):
+    updates = []
+    for player in team['players']:
+        updates.append({
+            'player': {
+                'name': player['name'],
+                'steamid64': SteamID(player['steamId']),
+                'avatarhash': None,
+                'eu_playerid': None,
+            },
+            'rostered': NumericRange(
+                lower=parse_date(player['joinedAt']),
+                upper=parse_date(player['leftAt'])
+            ),
+        })
+        rostered = updates[-1]['rostered']
+        if rostered.upper is not None and rostered.lower is not None \
+            and rostered.upper <= rostered.lower:
+            logging.info("Player %s (%s) on team %s joined before they left: %s <= %s",
+                         player['name'], player['steamId'], team['teamId'], player['joinedAt'],
+                         player['leftAt'])
+            del updates[-1] # FIXME
+
+    return {
+        'league': 'rgl',
+        'rgl_teamid': team['teamId'],
+        'rgl_teamids': (team['teamId'], *team['linkedTeams']),
+        'compid': team['seasonId'],
+        'divid': team['divisionId'],
+        'end_rank': team['finalRank'],
+        'fetched': team['fetched'],
+        'name': team['name'],
+        'avatarhash': None, # TODO
+        'updates': updates,
+    }
+
+def create_rgl_parser(sub):
+    rgl = sub.add_parser("rgl", help="Import rgl matches")
+    rgl.set_defaults(importer=import_rgl_cli)
+    rgl_sub = rgl.add_subparsers()
+    f = rgl_sub.add_parser("file", help="Import from the local filesystem")
+    f.set_defaults(fetcher=RGLFileFetcher)
+    f.add_argument("dir", metavar="DIR",
+                   help="Directory containing season, match, and team responses.")
+    b = rgl_sub.add_parser("bulk", help="Bulk import from api.rgl.gg")
+    b.set_defaults(fetcher=RGLBulkFetcher)
+    b.add_argument("-s", "--since", type=datetime.fromisoformat,
+                   default=0, metavar="DATE",
+                   help="Only fetch matches created since DATE")
+    b.add_argument("-N", "--new", action='store_true',
+                   help="Only fetch matches created since the newest imported rgl")
+    b.add_argument("-c", "--count", type=int, default=None,
+                   help="Fetch up to COUNT matches, defaults to unlimited")
+    b.add_argument("-p", "--skip", type=int, default=0,
+                   help="Skip the first SKIP matches")
+
+def import_rgl_cli(args, c):
+    with sentry_sdk.start_transaction(op="import", name="rgl_matches"):
+        if 'new' in args and args.new:
+            with c.cursor() as cur:
+                cur.execute(
+                    """SELECT max(fetched) - 6 * 60 * 60
+                       FROM match
+                       WHERE league = 'rgl';""");
+                args.since = datetime.fromtimestamp(cur.fetchone()[0])
+        return import_rgl(c, args.fetcher(**vars(args)))
+
+def import_rgl(c, fetcher):
+    @functools.cache
+    def get_season(seasonid):
+        return parse_season(fetcher.get_season(seasonid))
+
+    cur = c.cursor()
+    count = 0
+    for matchid in filter_matchids(c, fetcher.get_matchids()):
+        try:
+            result = fetcher.get_match(matchid)
+            res = parse_match(result)
+            if res['teams'][0]['score'] is None and res['teams'][1]['score'] is None:
+                continue
+
+            if res['divid'] == 558:
+                logging.info("Skipping inter-division match")
+                continue
+
+            cur.execute("""SELECT 1
+                           FROM division
+                           WHERE league = 'rgl'
+                               AND compid = %(compid)s
+                               AND divid = %(divid)s""", res)
+            for _ in cur:
+                season = None
+                break
+            else:
+                season = get_season(res['compid'])
+                res['format'] = season['format']
+                res['tier'] = season['div_tiers'][res['divid']]
+
+            for team in res['teams']:
+                cur.execute(
+                    """SELECT teamid, coalesce(fetched, 0)
+                       FROM team_comp_backing
+                       WHERE rgl_teamid = %(rgl_teamid)s
+                       UNION ALL
+                       SELECT NULL::INT, 0;""", team)
+                row = cur.fetchone()
+
+                if row[0] is None or row[1] <= (res['scheduled'] or 0) + 12 * 60 * 60:
+                    team |= parse_team(fetcher.get_team(team['rgl_teamid']))
+                    # FIXME broken divs!
+                    if team['divid'] != res['divid']:
+                        logging.info("Bad team division %s for team %s; should be %s",
+                                     team['divid'], team['rgl_teamid'], res['divid'])
+                    team['divid'] = res['divid']
+                else:
+                    team['teamid'] = row[0]
+
+            with sentry_sdk.start_span(op='db.transaction', description=f"import {matchid}"):
+                cur.execute("BEGIN;")
+                if season:
+                    import_compdiv(cur, res)
+
+                for team in res['teams']:
+                    if 'league' in team:
+                        import_team(cur, team)
+
+                if res['teams'][0]['teamid'] > res['teams'][1]['teamid']:
+                    res['teams'] = (res['teams'][1], res['teams'][0])
+                res['teamid1'] = res['teams'][0]['teamid']
+                res['teamid2'] = res['teams'][1]['teamid']
+                res['score1'] = res['teams'][0]['score']
+                res['score2'] = res['teams'][1]['score']
+                import_match(cur, res)
+                cur.execute("COMMIT;")
+        except (IndexError, KeyError, psycopg2.errors.UniqueViolation):
+            logging.exception("Could not parse match %s", matchid)
+            cur.execute("ROLLBACK;")
+        except psycopg2.Error:
+            logging.error("Could not import match %s", matchid)
+            raise
+        else:
+            count += 1
+    logging.info("Imported %s matches", count)

trends/importer/cli.py
@@ -17,6 +17,7 @@ from .logs import create_logs_parser
 from .link_demos import create_link_demos_parser
 from .link_matches import create_link_matches_parser
 from .players import create_players_parser
+from .rgl import create_rgl_parser
 from .uploader import create_uploader_parser
 from .weapons import create_weapons_parser
 
@@ -31,6 +32,7 @@ def create_parser():
     create_link_matches_parser(sub)
     create_logs_parser(sub)
     create_players_parser(sub)
+    create_rgl_parser(sub)
     create_uploader_parser(sub)
     create_weapons_parser(sub)
     parser.add_argument("database", default="postgresql:///trends", metavar="DATABASE",

trends/importer/league.py
@@ -266,6 +266,25 @@ def import_match(c, m):
     if m['seq'] is not None:
         c.execute("INSERT INTO round_name (round) VALUES (%(round)s) ON CONFLICT DO NOTHING;", m)
         col = "div" if m['divid'] else "comp"
+
+        # FIXME
+        if m['league'] == 'rgl':
+            c.execute(
+                f"""WITH round AS (SELECT
+                            round_seq,
+                            round_nameid
+                        FROM {col}_round
+                        WHERE league = %(league)s
+                            AND {col}id = %({col}id)s
+                    ) SELECT round_seq
+                    FROM round
+                    WHERE round_nameid =
+                        (SELECT round_nameid FROM round_name WHERE round = %(round)s)
+                    UNION ALL
+                    SELECT coalesce(max(round_seq) + 1, 0)
+                    FROM round;""", m)
+            m['seq'] = c.fetchone()[0]
+
         c.execute(
             f"""INSERT INTO {col}_round (league, {col}id, round_seq, round_nameid)
                 VALUES (

trends/migrations/rgl.sql
@@ -1,0 +1,10 @@
+ALTER TABLE league_team DROP CONSTRAINT league_team_check;
+ALTER TABLE league_team ADD CHECK (
+	equal(league_team_per_comp(league), fetched ISNULL, team_nameid ISNULL)
+);
+ALTER TABLE league_team ALTER team_nameid DROP NOT NULL;
+DROP INDEX match_team1;
+DROP INDEX match_team2;
+DROP INDEX match_comp;
+CREATE INDEX match_team1 ON match (teamid1);
+CREATE INDEX match_team2 ON match (teamid2);

trends/schema.sql
@@ -1,6 +1,6 @@
 --() { :; }; exec psql -f "$0" "$@"
 -- SPDX-License-Identifier: AGPL-3.0-only
--- Copyright (C) 2020 Sean Anderson <seanga2@gmail.com>
+-- Copyright (C) 2020-23 Sean Anderson <seanga2@gmail.com>
 
 CREATE EXTENSION IF NOT EXISTS btree_gist;
 CREATE EXTENSION IF NOT EXISTS intarray;
@@ -173,8 +173,7 @@ CREATE TABLE IF NOT EXISTS league_team (
 	-- When team_player (if not RGL) was last fetched
 	fetched BIGINT,
 	PRIMARY KEY (league, teamid),
-	CHECK (equal(league_team_per_comp(league), fetched ISNULL, team_nameid ISNULL)),
-	CHECK (NOT league_team_per_comp(league) OR avatarhash NOTNULL)
+	CHECK (equal(league_team_per_comp(league), fetched ISNULL, team_nameid ISNULL))
 );
 
 CREATE TABLE IF NOT EXISTS team_comp_backing (
@@ -297,7 +296,7 @@ CREATE TABLE IF NOT EXISTS match (
 	FOREIGN KEY (league, compid, teamid2) REFERENCES team_comp_backing (league, compid, teamid),
 	CHECK (teamid1 < teamid2),
 	CHECK (league_div_optional(league) OR (divid NOTNULL)),
-	CHECK (league_time_optional(league) OR (scheduled NOTNULL)),
+	CHECK (league_time_optional(league) OR forfeit OR (scheduled NOTNULL)),
 	CHECK (league_round_optional(league) OR (round_seq NOTNULL))
 );
 
@@ -307,10 +306,9 @@ CREATE UNIQUE INDEX IF NOT EXISTS match_unique_div ON match (
 CREATE UNIQUE INDEX IF NOT EXISTS match_unique_comp ON match (
 	league, compid, round_seq, teamid1, teamid2
 ) WHERE divid ISNULL;
-CREATE INDEX IF NOT EXISTS match_team1 ON match (league, teamid1);
-CREATE INDEX IF NOT EXISTS match_team2 ON match (league, teamid2);
+CREATE INDEX IF NOT EXISTS match_team1 ON match (teamid1);
+CREATE INDEX IF NOT EXISTS match_team2 ON match (teamid2);
 CREATE INDEX IF NOT EXISTS match_time ON match (scheduled);
-CREATE INDEX IF NOT EXISTS match_comp ON match (league, compid);
 
 CREATE OR REPLACE VIEW match_wlt AS SELECT
 	league,

trends/site/team.py
@@ -10,7 +10,7 @@ from ..util import leagues
 team = flask.Blueprint('team', __name__)
 
 @team.url_value_preprocessor
-def get_compid(endpoint, values):
+def get_teamid(endpoint, values):
     flask.g.teamid = values['teamid']
 
 @team.before_request
@@ -212,6 +212,8 @@ def overview(league, teamid):
                competition.name AS comp,
                division AS div,
                format,
+               rgl_teamid,
+               team_name,
                wins,
                losses,
                ties,

trends/site/templates/base.html
@@ -30,6 +30,7 @@
 		<a href="{{ url_for('root.logs') }}">Logs</a>
 		<a href="{{ url_for('root.leaderboard') }}">Leaderboards</a>
 		<a href="{{ url_for('league.comps', league='etf2l') }}">ETF2L</a>
+		<a href="{{ url_for('league.comps', league='rgl') }}">RGL</a>
 		<form action="{{ url_for('root.search') }}" role="search">
 			<input type="search" name="q" placeholder="Player name or SteamID"
 			 minlength="3" value="{{ q }}">

commit 668070d842bd1cb55dc33496aacbe53b8ff1339a

importer: Don't crash when there's an error getting a match
When there's a network (or other) error getting a match, we return None.
This is fine for methods which yield results, since that turns into an
empty iterator. But for regular methods that means we have to check for
None every time. Just re-raise the exception and catch it later on.

Fixes: ad2f5d7 ("Add RGL support")
Fixes: 481a320 ("Support importing ETF2L matches")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/importer/etf2l.py
@@ -10,7 +10,7 @@ import time
 from psycopg2.extras import NumericRange
 import sentry_sdk
 
-from .fetch import ETF2LFileFetcher, ETF2LBulkFetcher
+from .fetch import ETF2LFileFetcher, ETF2LBulkFetcher, FetchError
 from .league import *
 from ..sql import db_connect
 from ..steamid import SteamID
@@ -200,6 +200,8 @@ def import_etf2l(c, fetcher):
                     import_team(cur, team)
                 import_match(cur, res)
                 cur.execute("COMMIT;")
+        except FetchError:
+            continue
         except (IndexError, KeyError, psycopg2.errors.UniqueViolation):
             logging.exception("Could not parse result %s", result['id'])
             cur.execute("ROLLBACK;")

trends/importer/fetch.py
@@ -16,7 +16,10 @@ import urllib3.util, urllib3.exceptions
 
 from .. import util
 
-class APIError(OSError):
+class FetchError(OSError):
+    pass
+
+class APIError(FetchError):
     """The logs.tf API returned a failure"""
     def __init__(self, msg):
         super().__init__("logs.tf API request failed: %s".format(msg))
@@ -462,8 +465,8 @@ class ETF2LFileFetcher:
                     xfer['fetched'] = fetched
                     yield xfer
                 page += 1
-        except FileNotFoundError:
-            pass
+        except FileNotFoundError as e:
+            raise FetchError from e
 
 class ETF2LBulkFetcher:
     def __init__(self, since=0, count=None, page=1, **kwargs):
@@ -500,10 +503,12 @@ class ETF2LBulkFetcher:
                     return
 
                 page += 1
-        except (OSError, urllib3.exceptions.HTTPError):
+        except (OSError, urllib3.exceptions.HTTPError) as e:
             logging.exception("Could not fetch from %s", url)
-        except (ValueError, KeyError):
+            raise FetchError from e
+        except (ValueError, KeyError) as e:
             logging.exception("Could not parse %s", url)
+            raise FetchError from e
 
     def get_results(self):
         return self._get_data("https://api.etf2l.org/results", 'results', self.count,
@@ -532,8 +537,8 @@ class RGLFileFetcher:
             fetched = int(os.path.getmtime(path))
             data['fetched'] = fetched
             return data
-        except FileNotFoundError:
-            pass
+        except FileNotFoundError as e:
+            raise FetchError from e
 
     def get_season(self, seasonid):
         return self.get_json(f"{self.dir}/season_{seasonid}.json")
@@ -596,10 +601,12 @@ class RGLBulkFetcher:
             resp = resp.json()
             resp['fetched'] = fetched
             return resp
-        except (OSError, urllib3.exceptions.HTTPError):
+        except (OSError, urllib3.exceptions.HTTPError) as e:
             logging.exception("Could not fetch %s", url)
-        except (ValueError, KeyError):
+            raise FetchError from e
+        except (ValueError, KeyError) as e:
             logging.exception("Could not parse %s", url)
+            raise FetchError from e
 
     def get_season(self, seasonid):
         return self._get_data(f"{self.PREFIX}/seasons/{seasonid}")

trends/importer/rgl.py
@@ -12,7 +12,7 @@ import time
 from psycopg2.extras import NumericRange
 import sentry_sdk
 
-from .fetch import RGLBulkFetcher, RGLFileFetcher
+from .fetch import FetchError, RGLBulkFetcher, RGLFileFetcher
 from .league import *
 from ..sql import db_connect
 from ..steamid import SteamID
@@ -233,6 +233,8 @@ def import_rgl(c, fetcher):
                 res['score2'] = res['teams'][1]['score']
                 import_match(cur, res)
                 cur.execute("COMMIT;")
+        except FetchError:
+            continue
         except (IndexError, KeyError, psycopg2.errors.UniqueViolation):
             logging.exception("Could not parse match %s", matchid)
             cur.execute("ROLLBACK;")