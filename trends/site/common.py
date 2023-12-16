Forty-Bot/trends.tf

commit 2df4539e6dae0afcf1eb5c583c1486c23d12a916

site: league: team: Add players page
Add a players tag to the team page. This makes it easy to compare players
in a team. Unlike the comp version, we only show stats for players on the
team. IMO this makes most sense, since we don't usually want to compare
e.g. the best enemy player in our match.

Closes: #96
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/common.py
@@ -156,9 +156,23 @@ def get_matches(compid, filters, limit=100, offset=0):
           'offset': offset })
     return matches
 
-def get_players(league, compid, filters, limit=100, offset=0):
-    filter_clauses = get_filter_clauses(filters, 'divid', 'primary_classid', 'map', 'time', 'logid')
-    filter_clauses += "\nAND compid = %(compid)s"
+def get_players(league, compid, teamid, filters, limit=100, offset=0):
+    if compid is not None:
+        assert teamid is None
+        filter_clauses = get_filter_clauses(filters, 'divid', 'primary_classid', 'map', 'time',
+                                            'logid')
+        filter_clauses += "\nAND compid = %(compid)s"
+    elif teamid is not None:
+        filter_clauses = get_filter_clauses(filters, 'compid', 'primary_classid', 'map', 'time',
+                                            'logid')
+        filter_clauses += """AND %(teamid)s in (teamid1, teamid2)
+                             AND team = CASE WHEN equal(team1_is_red, %(teamid)s = teamid1) THEN
+                                 'Red'::TEAM
+                             ELSE
+                                 'Blue'::TEAM
+                             END"""
+    else:
+        assert False
 
     order, order_clause = get_order({
         'name': "name",

trends/site/comp.py
@@ -101,6 +101,6 @@ def players(league, compid):
     limit, offset = get_pagination()
     filters = get_filter_params()
 
-    players = get_players(league, compid, filters, limit, offset)
+    players = get_players(league, compid, None, filters, limit, offset)
 
     return flask.render_template("league/comp/players.html", players=players.fetchall())

trends/site/team.py
@@ -3,6 +3,7 @@
 
 import flask
 
+from .common import get_players
 from .util import get_db, get_filter_params, get_filter_clauses, get_order, get_pagination, \
                   last_modified
 from ..util import leagues
@@ -304,10 +305,7 @@ def comps(league, teamid):
 
     return flask.render_template("league/team/comps.html", comps=comps.fetchall())
 
-@team.route('/matches')
-def matches(league, teamid):
-    limit, offset = get_pagination()
-
+def get_comp_list(league, teamid):
     comps = get_db().cursor()
     comps.execute(
         """SELECT name
@@ -316,6 +314,25 @@ def matches(league, teamid):
            WHERE league = %s AND teamid = %s;
         """, (league, teamid));
 
+    return comps
+
+@team.route('/matches')
+def matches(league, teamid):
+    limit, offset = get_pagination()
+
+    comps = get_comp_list(league, teamid)
     matches = get_matches(league, teamid, get_filter_params(), limit, offset)
 
-    return flask.render_template("league/team/matches.html", matches=matches.fetchall(), comps=comps)
+    return flask.render_template("league/team/matches.html", matches=matches.fetchall(),
+                                 comps=comps)
+
+@team.route('/players')
+def players(league, teamid):
+    limit, offset = get_pagination()
+    filters = get_filter_params()
+
+    comps = get_comp_list(league, teamid)
+    players = get_players(league, None, teamid, filters, limit, offset)
+
+    return flask.render_template("league/team/players.html", players=players.fetchall(),
+                                 comps=comps)

trends/site/templates/league/team/base.html
@@ -44,5 +44,6 @@
 	</div>
 
 	{{ navbar(('.overview', "Overview"), ('.comps', "Competitions"), ('.roster', "Roster"),
-		  ('.matches', "Matches"), league=g.league, teamid=g.teamid) }}
+		  ('.matches', "Matches"), ('.players', "Players"), league=g.league,
+		  teamid=g.teamid) }}
 {% endblock %}

trends/site/templates/league/team/players.html
@@ -1,0 +1,22 @@
+{# SPDX-License-Identifier: AGPL-3.0-only #}
+{# Copyright (C) 2023 Sean Anderson <seanga2@gmail.com> #}
+{% from "macros/filter.html" import filter %}
+{% from "macros/pagination.html" import navigation %}
+{% from "macros/players.html" import player_sort, player_table %}
+{% from "macros/sort.html" import sort %}
+{% extends "league/team/base.html" %}
+{% block title %}Players - {{ super() }}{% endblock %}
+{% block content %}
+	{{ super() }}
+	<h2>Players</h2>
+	<datalist id="comps">
+	{% for comp in comps %}
+		<option value="{{ comp[0] }}"></option>
+	{% endfor %}
+	</datalist>
+	{{ filter('comp', 'class', 'map', 'date', 'steamid64') }}
+	{{ sort(player_sort) }}
+	{{ navigation(players) }}
+	{{ player_table(players) }}
+	{{ navigation(players) }}
+{% endblock %}

commit ff29f5d793a3970131e968690ae8f8af43bc629f

site: common: Fix filtering on the players page
We're missing a newline in the filter, which will cause filtering not to
work.

Fixes: 2df4539 ("site: league: team: Add players page")

trends/site/common.py
@@ -166,7 +166,7 @@ def get_players(league, compid, teamid, filters, limit=100, offset=0,
     elif teamid is not None:
         filter_clauses = get_filter_clauses(filters, 'compid', 'primary_classid', 'map', 'time',
                                             'logid')
-        filter_clauses += """AND %(teamid)s in (teamid1, teamid2)
+        filter_clauses += """\nAND %(teamid)s in (teamid1, teamid2)
                              AND team = CASE WHEN equal(team1_is_red, %(teamid)s = teamid1) THEN
                                  'Red'::TEAM
                              ELSE