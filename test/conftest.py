a9626d51423be41293441bc0dcb4aa0eec44e511
Sean Anderson <seanga2@gmail.com>
2024-02-18 12:47:34 -0500

    test: Add helper to create a test database
    To make it easier to get started, add a command to create a test database
    just like the one used for tests.
    
    Signed-off-by: Sean Anderson <seanga2@gmail.com>

--- test/conftest.py
+++ test/conftest.py
@@ -2,21 +2,13 @@
 # Copyright (C) 2022 Sean Anderson <seanga2@gmail.com>
 
 from contextlib import contextmanager
-from datetime import datetime
-import os
 import logging
 
 import pytest
 from testing.postgresql import Postgresql
 
-import trends.importer.demos
-import trends.importer.logs
-import trends.importer.etf2l
-import trends.importer.link_demos
-import trends.importer.link_matches
-import trends.importer.rgl
-from trends.importer.fetch import DemoFileFetcher, ETF2LFileFetcher, FileFetcher, RGLFileFetcher
-from trends.sql import db_connect, db_init, db_schema
+from trends.sql import db_connect
+from .create import create_test_db
 
 @contextmanager
 def caplog_session(request):
@@ -30,108 +22,11 @@ def database(request):
     postgres_args = Postgresql.DEFAULT_SETTINGS['postgres_args']
     postgres_args += " -c full_page_writes=off"
     with Postgresql(postgres_args=postgres_args) as database:
-        # This must happen in a separate connection because we use temporary tables which will alias
-        # other queries.
-        with db_connect(database.url()) as c:
-            cur = c.cursor()
-            db_schema(cur)
-            db_init(c)
-
-            logfiles = { logid: f"{os.path.dirname(__file__)}/logs/log_{logid}.json"
-                for logid in (
-                    30099,
-                    2297197,
-                    2297225,
-                    2344272,
-                    2344306,
-                    2344331,
-                    2344354,
-                    2344383,
-                    2344394,
-                    2344394,
-                    2392536,
-                    2392557,
-                    2401045,
-                    2408458,
-                    2408491,
-                    2506954,
-                    2600722,
-                    2818814,
-                    2844704,
-                    2878546,
-                    2931193,
-                    3027588,
-                    3069780,
-                    3124976,
-                    3302963,
-                    3302982,
-                    3384488,
-                )
-            }
-
-            with caplog_session(request) as caplog:
-                with caplog.at_level(logging.ERROR):
-                    trends.importer.logs.import_logs(c, FileFetcher(logs=logfiles), False)
-                if caplog.records:
-                    pytest.fail("Error importing logs")
-
-        with db_connect(database.url()) as c:
-            demofiles = (f"{os.path.dirname(__file__)}/demos/demo_{demoid}.json" for demoid in (
-                273469,
-                273477,
-                292844,
-                292859,
-                292868,
-                292885,
-                318447,
-                322265,
-                322285,
-                375937,
-                585088,
-                609093,
-                640794,
-                737954,
-                776712,
-                902137,
-                902150,
-            ))
-
-            with caplog_session(request) as caplog:
-                with caplog.at_level(logging.ERROR):
-                    trends.importer.demos.import_demos(c, DemoFileFetcher(demos=demofiles))
-                if caplog.records:
-                    pytest.fail("Error importing demos")
-
-        with db_connect(database.url()) as c:
-            fetcher = ETF2LFileFetcher(results=f"{os.path.dirname(__file__)}/etf2l/results.json",
-                                       xferdir=f"{os.path.dirname(__file__)}/etf2l/")
-            with caplog_session(request) as caplog:
-                with caplog.at_level(logging.ERROR):
-                    trends.importer.etf2l.import_etf2l(c, fetcher)
-                if caplog.records:
-                    pytest.fail("Error importing ETF2L files")
-
-        with db_connect(database.url()) as c:
-            fetcher = RGLFileFetcher(dir=f"{os.path.dirname(__file__)}/rgl")
-            with caplog_session(request) as caplog:
-                with caplog.at_level(logging.ERROR):
-                    trends.importer.rgl.import_rgl(c, fetcher)
-                if caplog.records:
-                    pytest.fail("Error importing RGL files")
-
-        with db_connect(database.url()) as c:
-            cur = c.cursor()
-            cur.execute("ANALYZE;")
-            # A second time to test partitioning log_json
-            db_init(c)
-            cur.execute("REFRESH MATERIALIZED VIEW leaderboard_cube;")
-            cur.execute("REFRESH MATERIALIZED VIEW map_popularity;")
-
-        with db_connect(database.url()) as c:
-            class args:
-                since = datetime.fromtimestamp(0)
-            trends.importer.link_demos.link_logs(args, c)
-            trends.importer.link_matches.link_matches(args, c)
+        with caplog_session(request) as caplog:
+            with caplog.at_level(logging.ERROR):
+                create_test_db(database.url())
+            if caplog.records:
+                pytest.fail("Error creating test database")
 
         yield database
 
---
+++ test/create.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+
+from datetime import datetime
+import os
+import sys
+
+import trends.importer.demos
+import trends.importer.logs
+import trends.importer.etf2l
+import trends.importer.link_demos
+import trends.importer.link_matches
+import trends.importer.rgl
+from trends.importer.fetch import DemoFileFetcher, ETF2LFileFetcher, FileFetcher, RGLFileFetcher
+from trends.sql import db_connect, db_init, db_schema
+
+def create_test_db(url):
+    # We use separate connections for importing because we use temporary tables which will alias
+    # other queries.
+    with db_connect(url) as c:
+        cur = c.cursor()
+        db_schema(cur)
+        db_init(c)
+
+        logfiles = { logid: f"{os.path.dirname(__file__)}/logs/log_{logid}.json"
+            for logid in (
+                30099,
+                2297197,
+                2297225,
+                2344272,
+                2344306,
+                2344331,
+                2344354,
+                2344383,
+                2344394,
+                2344394,
+                2392536,
+                2392557,
+                2401045,
+                2408458,
+                2408491,
+                2506954,
+                2600722,
+                2818814,
+                2844704,
+                2878546,
+                2931193,
+                3027588,
+                3069780,
+                3124976,
+                3302963,
+                3302982,
+                3384488,
+            )
+        }
+        trends.importer.logs.import_logs(c, FileFetcher(logs=logfiles), False)
+
+    with db_connect(url) as c:
+        demofiles = (f"{os.path.dirname(__file__)}/demos/demo_{demoid}.json" for demoid in (
+            273469,
+            273477,
+            292844,
+            292859,
+            292868,
+            292885,
+            318447,
+            322265,
+            322285,
+            375937,
+            585088,
+            609093,
+            640794,
+            737954,
+            776712,
+            902137,
+            902150,
+        ))
+        trends.importer.demos.import_demos(c, DemoFileFetcher(demos=demofiles))
+
+    with db_connect(url) as c:
+        fetcher = ETF2LFileFetcher(results=f"{os.path.dirname(__file__)}/etf2l/results.json",
+                                   xferdir=f"{os.path.dirname(__file__)}/etf2l/")
+        trends.importer.etf2l.import_etf2l(c, fetcher)
+
+    with db_connect(url) as c:
+        fetcher = RGLFileFetcher(dir=f"{os.path.dirname(__file__)}/rgl")
+        trends.importer.rgl.import_rgl(c, fetcher)
+
+    with db_connect(url) as c:
+        cur = c.cursor()
+        cur.execute("ANALYZE;")
+        # A second time to test partitioning log_json
+        db_init(c)
+        cur.execute("REFRESH MATERIALIZED VIEW leaderboard_cube;")
+        cur.execute("REFRESH MATERIALIZED VIEW map_popularity;")
+
+    with db_connect(url) as c:
+        class args:
+            since = datetime.fromtimestamp(0)
+        trends.importer.link_demos.link_logs(args, c)
+        trends.importer.link_matches.link_matches(args, c)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} <database url>", file=sys.stderr)
+        sys.exit(1)
+
+    create_test_db(sys.argv[1])
