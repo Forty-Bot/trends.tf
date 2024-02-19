Forty-Bot/trends.tf

commit 8017ab95c9e34b2160b9c11af5c2eef6b5ab4ec5

site: league: Add matches page
Add a matches page. It lives in common because it will be reused for
competitions as well. Of minor interest here is division filtering. This
isn't used for leagues (as divisions are heterogenous), but it will be used
for competitions.

The SQL here has some materialized CTEs and lateral joins because it's the
only way to force postgres to perform properly. The map filtering is also
done in a nonstandard way so it actually functions as a filter (as opposed
to introducing nested sequential scans). Once again, another curse on the
postgres query planner.

Each match has a drop-down showing the individual logs. This will allow
easy evaluation of the linking algorithm. Right now, it appears to be
around 80% effective, so it needs some tweaking. There's no dedicated match
page. Instead, I just link directly to the logs.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/util.py
@@ -81,6 +81,7 @@ def get_filter_params():
     params['format'] = args.get('format', type=str)
     params['league'] = args.get('league', type=str)
     params['comp'] = args.get('comp', type=str)
+    params['divid'] = args.get('divid', type=int)
     if val := tuple(args.getlist('steamid64', type=int)[:5]):
         players = get_db().cursor()
         players.execute("""SELECT
@@ -138,8 +139,12 @@ def get_filter_clauses(params, *valid_columns, **column_map):
     for col in valid_columns:
         column_map[col] = col
 
-    if params['league'] and 'league' in valid_columns:
-        clauses.append(f"AND {column_map['league']} = %(league)s")
+    def id_clause(column):
+      if params[column] and column in column_map:
+          clauses.append(f"AND {column_map[column]} = %({column})s")
+
+    id_clause('league')
+    id_clause('divid')
 
     def simple_clause(name, column):
         if not params[name]:

trends/schema.sql
@@ -417,7 +417,9 @@ CREATE OR REPLACE VIEW log_nodups AS SELECT
 	blue_score,
 	formatid,
 	league,
-	matchid
+	matchid,
+	team1_is_red,
+	demoid
 FROM log
 WHERE duplicate_of ISNULL;
 

trends/site/common.py
@@ -78,3 +78,78 @@ def get_players(q):
            LIMIT %(limit)s OFFSET %(offset)s;""",
         { 'q': "%{}%".format(q), 'limit': limit, 'offset': offset})
     return results
+
+def get_matches(compid, filters, limit=100, offset=0):
+    if compid is None:
+        filter_clauses = get_filter_clauses(filters, 'comp', 'divid', time='scheduled')
+    else:
+        filter_clauses = get_filter_clauses(filters, 'divid', time='scheduled')
+        filter_clauses += "\nAND compid = %(compid)s"
+    if filters['map']:
+        maps = """JOIN (SELECT
+                          mapid
+                      FROM (SELECT
+                              unnest(mapids) AS mapid
+                          FROM match_semifiltered
+                          GROUP BY mapid
+                      ) AS maps
+                      JOIN map USING (mapid)
+                      WHERE map ILIKE %(map)s
+                  ) AS maps ON (mapid = ANY(mapids))"""
+    else:
+        maps = ""
+    order, order_clause = get_order({
+        'round': "round_seq",
+        'date': "scheduled",
+        'matchid': "matchid",
+    }, 'matchid')
+
+    matches = get_db().cursor()
+    matches.execute(
+        """WITH match_semifiltered AS (SELECT *
+               FROM match_pretty
+               WHERE league = %(league)s
+                   {0}
+           ), match AS MATERIALIZED (SELECT *
+               FROM match_semifiltered
+               {1}
+               ORDER BY {2} NULLS LAST, compid DESC, tier ASC, matchid DESC
+               LIMIT %(limit)s OFFSET %(offset)s
+           ) SELECT
+               matchid,
+               comp,
+               div,
+               round,
+               teamid1,
+               teamid2,
+               team1,
+               team2,
+               score1,
+               score2,
+               maps,
+               scheduled,
+               logs
+           FROM match
+           LEFT JOIN LATERAL (SELECT
+                   league,
+                   matchid,
+                   json_object_agg(logid, json_build_object(
+                       'logid', logid,
+                       'time', time,
+                       'title', title,
+                       'map', map,
+                       'score1', CASE WHEN team1_is_red THEN red_score ELSE blue_score END,
+                       'score2', CASE WHEN team1_is_red THEN blue_score ELSE red_score END,
+                       'demoid', demoid
+                   ) ORDER BY time DESC) AS logs
+               FROM log_nodups
+               LEFT JOIN format USING (formatid)
+               JOIN map USING (mapid)
+               WHERE league=match.league AND matchid=match.matchid
+               GROUP BY league, matchid
+           ) AS log USING (league, matchid)
+           ORDER BY {2} NULLS LAST, compid DESC, tier ASC, matchid DESC;"""
+        .format(filter_clauses, maps, order_clause),
+        { **filters, 'league': flask.g.league, 'compid': compid, 'limit': limit,
+          'offset': offset })
+    return matches

trends/site/league.py
@@ -3,6 +3,7 @@
 
 import flask
 
+from .common import get_matches
 from .comp import comp
 from .team import team
 from .util import get_db, get_filter_params, get_filter_clauses, get_order, get_pagination
@@ -82,3 +83,14 @@ def comps(league):
        { 'limit': limit, 'offset': offset, **filters, 'league': league, })
 
     return flask.render_template("league/comps.html", comps=comps.fetchall())
+
+@league.route('/matches')
+def matches(league):
+    limit, offset = get_pagination()
+    filters = get_filter_params()
+
+    matches = get_matches(None, filters, limit, offset)
+    comps = get_db().cursor()
+    comps.execute("SELECT name FROM competition WHERE league=%s;", (league,))
+
+    return flask.render_template("league/matches.html", matches=matches.fetchall(), comps=comps)

trends/site/templates/league/base.html
@@ -13,5 +13,5 @@
 		{{ league_map[g.league] }}
 	</a></h1>
 
-	{{ navbar(('.comps', "Competitions"), league=g.league) }}
+	{{ navbar(('.comps', "Competitions"), ('.matches', "Matches"), league=g.league) }}
 {% endblock %}

trends/site/templates/league/matches.html
@@ -1,0 +1,30 @@
+{# SPDX-License-Identifier: AGPL-3.0-only #}
+{# Copyright (C) 2021 Sean Anderson <seanga2@gmail.com> #}
+{% from "macros/filter.html" import filter %}
+{% from "macros/matches.html" import match_table %}
+{% from "macros/pagination.html" import navigation %}
+{% from "macros/sort.html" import sort %}
+{% extends "league/base.html" %}
+{% block head %}
+	{{ super() }}
+	{{ local_js("js/hider.js") }}
+{% endblock %}
+{% block title %}Matches - {{ super() }}{% endblock %}
+{% block content %}
+	{{ super() }}
+	<h2>Matches</h2>
+	{{ filter('comp', 'map', 'date') }}
+	<datalist id="comps">
+	{% for comp in comps %}
+		<option value="{{ comp[0] }}"></option>
+	{% endfor %}
+	</datalist>
+	{{ sort({
+		'matchid': "MatchID",
+		'round': "Round",
+		'date': "Date",
+	}) }}
+	{{ navigation(matches) }}
+	{{ match_table(matches, comp=True) }}
+	{{ navigation(matches) }}
+{% endblock %}

trends/site/templates/macros/filter.html
@@ -49,6 +49,14 @@
 		 value="{{ (g.filters['comp'] or "") | trim("%") }}">
 	</label>
 	{% endif %}
+	{% if 'divid' in params %}
+	<label><abbr title="Division">Div</abbr>:
+		<select name="divid">
+			<option></option>
+			{{ _create_options(g.filters['divid'] | string, g.divs) }}
+		</select>
+	</label>
+	{% endif %}
 	{% if 'format' in params %}
 	<label>Format:
 		<select name="format">

trends/site/templates/macros/matches.html
@@ -1,0 +1,93 @@
+{# SPDX-License-Identifier: AGPL-3.0-only #}
+{# Copyright (C) 2023 Sean Anderson <seanga2@gmail.com> #}
+{% from "macros/format.html" import date_col, optformat %}
+{% from "macros/icons.html" import demoicon %}
+{% macro _team_link(team, teamid) %}
+<a href="{{ url_for('league.team.overview', league=g.league, teamid=teamid) }}">
+	{{ team }}
+</a>
+{% endmacro %}
+{% macro match_table(matches, comp=False, div=False) %}
+<table>
+<thead>
+	<tr>
+		<th></th>
+		<th><abbr title="Demo/Match">D/M</abbr></th>
+		{% if comp %}
+		<th>Competition</th>
+		{% endif %}
+		{% if div %}
+		<th>Division</th>
+		{% endif %}
+		<th>Round</th>
+		<th colspan="2">Teams</th>
+		<th colspan="2">Score</th>
+		<th>Maps</th>
+		<th>Date</th>
+	</tr>
+</thead>
+<tbody class="hier">
+{% for match in matches %}
+	<tr id="{{ match.matchid }}" class="{{ "hider hiding" if match.logs != None }}">
+	{% set team1class = '' %}
+	{% set team2class = '' %}
+	{% if match['score1'] > match['score2'] %}
+		{% set team1class = 'win' %}
+	{% elif match['score1'] < match['score2'] %}
+		{% set team2class = 'win' %}
+	{% endif %}
+	<td class="tree"></td>
+	<td class="left">
+		<a href="https://etf2l.org/matches/{{ match.matchid }}">
+		<img class="avatar_small" src="{{
+			url_for('static', filename="img/{}.png".format(g.league))
+		 }}">
+		</img>
+		</a>
+	</td>
+	{% if comp %}
+	<td class="left">{{ match.comp }}</td>
+	{% endif %}
+	{% if div %}
+	<td class="left">{{ optformat("{}", match.div) }}</td>
+	{% endif %}
+	<td class="left">
+		{% if match.logs != None %}
+		<a href="{{ url_for('root.log', logids=match.logs) }}">
+			{{ match.round }}
+		</a>
+		{% else %}
+		{{ match.round }}
+		{% endif %}
+	</td>
+	<td class="left">{{ _team_link(match.team1, match.teamid1) }}</td>
+	<td class="left">{{ _team_link(match.team2, match.teamid2) }}</td>
+	<td class="{{ team1class }}">{{ match.score1 }}</td>
+	<td class="{{ team2class }}">{{ match.score2 }}</td>
+	<td class="left">{{ match.maps | join(", ") if match.maps else "" }}</td>
+	{{ date_col(match.scheduled) }}
+</tr>
+{% if match.logs != None %}
+<tr class="{{ match.matchid }} padding-row hidden"></tr>
+{% for log in match.logs.values() %}
+	<tr class="{{ match.matchid }} hidable hidden">
+		<td></td>
+		<td class="left">{{ demoicon(log.demoid) }}</td>
+		<td colspan="2"></td>
+		<td class="left" colspan="2">
+			<a href="{{ url_for('root.log', logids=log.logid) }}">
+				{{ log['title'] }}
+			</a>
+		</td>
+		<td>{{ log.score1 }}</td>
+		<td>{{ log.score2 }}</td>
+		<td class="left">{{ log.map }}</td>
+		<td class="left date" timestamp="{{ log.time }}"></td>
+	</tr>
+{% endfor %}
+<tr class="{{ match.matchid }} padding-row hidden"></tr>
+{% endif %}
+{% endfor %}
+</tbody>
+</table>
+{% endmacro %}

commit 558b8847ce4a4ec0da51d977be9bbe281486209c

site: util: Fix indentation
The indentation here is wrong. Fix it.

Fixes: 8017ab9 ("site: league: Add matches page")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/util.py
@@ -140,8 +140,8 @@ def get_filter_clauses(params, *valid_columns, **column_map):
         column_map[col] = col
 
     def id_clause(column):
-      if params[column] and column in column_map:
-          clauses.append(f"AND {column_map[column]} = %({column})s")
+        if params[column] and column in column_map:
+            clauses.append(f"AND {column_map[column]} = %({column})s")
 
     id_clause('league')
     id_clause('divid')