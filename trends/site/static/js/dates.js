Forty-Bot/trends.tf

commit a7eee69954ce8458ecd5b9ef3897bd154c832b7c

site: Don't display timestamps for all dates
Dates with timestamps are helpful for identifying logs and matches, as
multiple may be played in one day. However, they just add noise to certain
elements (such as season starts/ends and roster dates) which generally
(aside from some exceedingly-quick rosters) take place over weeks or
longer. Create a second macro for time-less dates and convert existing uses
to the timed version.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/static/js/dates.js
@@ -1,11 +1,16 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 // Copyright (C) 2020 Sean Anderson <seanga2@gmail.com>
 
-var time_formatter = new Intl.DateTimeFormat('und', {dateStyle: 'medium', timeStyle: 'short'});
+var date_formatter = new Intl.DateTimeFormat('und', {dateStyle: 'medium'});
+var datetime_formatter = new Intl.DateTimeFormat('und', {dateStyle: 'medium', timeStyle: 'short'});
 
 document.addEventListener('DOMContentLoaded', () => {
+	for (date of document.getElementsByClassName("datetime"))
+		date.textContent = datetime_formatter.format(
+			new Date(date.getAttribute('timestamp') * 1000));
+
 	for (date of document.getElementsByClassName("date"))
-		date.textContent = time_formatter.format(
+		date.textContent = date_formatter.format(
 			new Date(date.getAttribute('timestamp') * 1000));
 });
 

trends/site/templates/index.html
@@ -14,9 +14,9 @@
 			{{ "{:,}".format(logs) }}
 		</a></dt>
 		<dt>Newest log</dt>
-		<dd class="date" timestamp="{{ minmax['newest'] }}"></dt>
+		<dd class="datetime" timestamp="{{ minmax['newest'] }}"></dt>
 		<dt>Oldest log</dt>
-		<dd class="date" timestamp="{{ minmax['oldest'] }}"></dt>
+		<dd class="datetime" timestamp="{{ minmax['oldest'] }}"></dt>
 	</dl>
 	<a href="https://github.com/Forty-Bot/trends.tf/issues/new">Suggestions</a>
 {% endblock %}

trends/site/templates/league/comp/overview.html
@@ -1,6 +1,6 @@
 {# SPDX-License-Identifier: AGPL-3.0-only #}
 {# Copyright (C) 2021 Sean Anderson <seanga2@gmail.com> #}
-{% from "macros/format.html" import date_col, optformat %}
+{% from "macros/format.html" import optformat %}
 {% from "macros/matches.html" import match_table %}
 {% from "macros/pretty.html" import format_map, league_map %}
 {% from "macros/wlt.html" import wlt %}

trends/site/templates/league/team/overview.html
@@ -1,6 +1,7 @@
 {# SPDX-License-Identifier: AGPL-3.0-only #}
 {# Copyright (C) 2023 Sean Anderson <seanga2@gmail.com> #}
 {% from "macros/comps.html" import comp_table %}
+{% from "macros/format.html" import date_col %}
 {% from "macros/matches.html" import match_table %}
 {% extends "league/team/base.html" %}
 {% block head %}
@@ -32,7 +33,7 @@
 					{{ player.name }}
 				</a>
 			</td>
-			<td class="left date" timestamp="{{ player.joined }}"></td>
+			{{ date_col(player.joined) }}
 		</tr>
 		{% endfor %}
 	</tbody>
@@ -60,8 +61,8 @@
 					{{ player.name }}
 				</a>
 			</td>
-			<td class="left date" timestamp="{{ player.joined }}"></td>
-			<td class="left date" timestamp="{{ player.left }}"></td>
+			{{ date_col(player.joined) }}
+			{{ date_col(player.left) }}
 		</tr>
 		{% endfor %}
 	</tbody>

trends/site/templates/log.html
@@ -1,7 +1,7 @@
 {# SPDX-License-Identifier: AGPL-3.0-only #}
 {# Copyright (C) 2021 Sean Anderson <seanga2@gmail.com> #}
 {% from "macros/dupes.html" import show_dupes %}
-{% from "macros/format.html" import duration_col, optformat, optint %}
+{% from "macros/format.html" import datetime_col, duration_col, optformat, optint %}
 {% from "macros/icons.html" import classicon, demoicon, matchicon, playerlink %}
 {% from "macros/js.html" import local_js %}
 {% from "macros/pretty.html" import class_map, league_map %}
@@ -340,7 +340,7 @@
 			{{ log['blue_score'] }}
 		</td>
 		<td>{{ log['duration'] | duration }}</td>
-		<td class="left date" timestamp="{{ log['time'] }}"></td>
+		{{ datetime_col(log['time']) }}
 		<td class="left">{{ show_dupes(log['duplicate_of']) }}</td>
 		{% if logs | count > 1 %}
 		<td class="left">

trends/site/templates/logs.html
@@ -2,6 +2,7 @@
 {# Copyright (C) 2021 Sean Anderson <seanga2@gmail.com> #}
 {% from "macros/dupes.html" import show_dupes %}
 {% from "macros/filter.html" import filter %}
+{% from "macros/format.html" import datetime_col %}
 {% from "macros/icons.html" import demoicon, matchicon %}
 {% from "macros/js.html" import local_js %}
 {% from "macros/pagination.html" import navigation %}
@@ -56,7 +57,7 @@
 				<td class="left">{{ log['map'] }}</td>
 				<td class="left">{{ log['format'] or '' }}</td>
 				<td>{{ log['duration'] | duration }}</td>
-				<td class="left date" timestamp="{{ log['time'] }}"></td>
+				{{ datetime_col(log['time']) }}
 				<td class="left">{{ show_dupes(log['duplicate_of']) }}</td>
 			</tr>
 			{% endfor %}

trends/site/templates/macros/comps.html
@@ -1,6 +1,6 @@
 {# SPDX-License-Identifier: AGPL-3.0-only #}
 {# Copyright (C) 2023 Sean Anderson <seanga2@gmail.com> #}
-{% from "macros/format.html" import date_col, optformat %}
+{% from "macros/format.html" import datetime_col, optformat %}
 {% from "macros/pretty.html" import format_map %}
 {% from "macros/wlt.html" import wlt %}
 {% macro comp_table(comps, date=False) %}
@@ -49,8 +49,8 @@
 		<td>{{ comp.rounds_lost | score }}</td>
 		<td>{{ optformat("{:.0%}", comp.round_winrate) }}</td>
 		{% if date %}
-		{{ date_col(comp.from) }}
-		{{ date_col(comp.to) }}
+		{{ datetime_col(comp.from) }}
+		{{ datetime_col(comp.to) }}
 		{% endif %}
 	</tr>
 	{% endfor %}

trends/site/templates/macros/format.html
@@ -23,10 +23,16 @@
 		{{ optformat("{:.{}%}", val, precision) }}
 	</td>
 {% endmacro %}
-{% macro date_col(date) %}
+{% macro _date_col(date, cls) %}
 {% if date != None %}
-	<td class="left date" timestamp="{{ date }}"></td>
+	<td class="left {{ cls }}" timestamp="{{ date }}"></td>
 {% else %}
 	<td></td>
 {% endif %}
+{% endmacro %}
+{% macro datetime_col(date) %}
+{{ _date_col(date, "datetime") }}
+{% endmacro %}
+{% macro date_col(date) %}
+{{ _date_col(date, "date") }}
 {% endmacro %}

commit 580e6f3dcb050f0f73ae016aec6b9fb6d53e2a47

site: Fix filtering by date
Filtering by date was not taking into account the user's time zone, since
the timezone field wasn't being filled in. Fix this.

Fixes: a7eee69 ("site: Don't display timestamps for all dates")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/static/js/dates.js
@@ -17,6 +17,6 @@ document.addEventListener('DOMContentLoaded', () => {
 document.addEventListener('DOMContentLoaded', () => {
 	let timezone = document.getElementById('timezone');
 	if (timezone) {
-		timezone.value = time_formatter.resolvedOptions().timeZone;
+		timezone.value = datetime_formatter.resolvedOptions().timeZone;
 	}
 });