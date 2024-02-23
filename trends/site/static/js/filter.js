Forty-Bot/trends.tf

commit 07c6e9219f740f50230eba9a2a72f1f98c83b91d

site: Add avatars to player filters
This adds avatars to the player selection dropdown.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/static/js/filter.js
@@ -23,6 +23,16 @@ function autocomplete_maps() {
 }
 document.getElementById('map_input').addEventListener('focus', autocomplete_maps, {once: true });
 
+function render_option(data, escape) {
+	div = document.createElement('div');
+	img = document.createElement('img');
+	img.classList.add('avatar_small');
+	img.setAttribute('src', "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/" +
+	                        `avatars/${data.avatarhash.slice(0, 2)}/${data.avatarhash}.jpg`);
+	div.append(img, " ", data.name);
+	return div;
+}
+
 document.addEventListener('DOMContentLoaded', () => {
 	new TomSelect(document.getElementById('players_input'), {
 		plugins: ['remove_button'],
@@ -38,5 +48,9 @@ document.addEventListener('DOMContentLoaded', () => {
 				});
 		},
 		shouldLoad: query => query.length > 3,
+		render: {
+			item: render_option,
+			option: render_option,
+		},
 	});
 })

trends/site/templates/macros/filter.html
@@ -45,7 +45,8 @@
 	<label>Players:
 		<select id="players_input" name="steamid64" multiple>
 			{% for player in g.filters['players'] %}
-			<option selected value="{{ player['steamid64'] }}">
+			<option selected value="{{ player['steamid64'] }}"
+			 data-avatarhash="{{ player['avatarhash'] }}">
 				{{ player['name'] }}
 			</option>
 			{% endfor %}

commit 8ab119f880f05460a96d8b0d8e12dcdb2307e742

site: Fix filtering for players without avatars
If a player doesn't hae an aatar, then avatarhash would be null, causing an
exception. Check for this, and just skip the avatar image in that case.

Fixes: 07c6e92 ("site: Add avatars to player filters")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/static/js/filter.js
@@ -24,11 +24,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function render_option(data, escape) {
 	let div = document.createElement('div');
-	let img = document.createElement('img');
-	img.classList.add('avatar_small');
-	img.setAttribute('src', "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/" +
-	                        `avatars/${data.avatarhash.slice(0, 2)}/${data.avatarhash}.jpg`);
-	div.append(img, " ", data.name);
+	if (data.avatarhash == null) {
+		div.append(data.name);
+	} else {
+		let img = document.createElement('img');
+		img.classList.add('avatar_small');
+		img.setAttribute('src', "https://steamcdn-a.akamaihd.net/steamcommunity/public/"
+					`images/avatars/${data.avatarhash.slice(0, 2)}/` +
+					`${data.avatarhash}.jpg`);
+		div.append(img, " ", data.name);
+	}
 	return div;
 }
 