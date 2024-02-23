Forty-Bot/trends.tf

commit 44be5a54bc5b619925db031c6725d7d519448276

Optimize leaderboard
Borrow from OLTP databases and create a "cube" view. Instead of
filtering/summing over round_wlt, just store those sums over everything we
want to filter. I don't know of a good way to filter by date for this, so
just leave that out.

We have to use sub-selects in our query to avoid postgres filtering after
the sequential scan over leaderboard_cube. However, we can't do this in the
query itself, since it causes indices to be skipped.

This also changes the display to remove the date filters (since they are
now useless). We also display the dating for better visibility into how we
sort.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

root.py
@@ -4,7 +4,7 @@
 import flask
 import sqlite3
 
-from common import get_filters, filter_clauses
+from common import get_filters
 from sql import get_db
 from steamid import SteamID
 
@@ -92,16 +92,24 @@ def leaderboard():
     sort = flask.request.args.get('sort', 'rating', str)
     limit = flask.request.args.get('limit', 100, int)
     offset = flask.request.args.get('offset', 0, int)
-
-    # This factor controls how much we weight the expected winrate for each player
-    # Increasing this factor will cause players with low numbers of logs to be rated closer to m
-    # This should be approximately the average number of logs per player
-    C = 100
-    # The expected winrate
-    m = 0.5
-
     filters = get_filters(flask.request.args)
-    leaderboard = get_db().cursor()
+
+    db = get_db()
+    ids = db.cursor()
+    ids.execute("""SELECT
+                       (SELECT
+                               classid
+                           FROM class
+                           WHERE class = %(class)s
+                       ) AS classid,
+                       (SELECT
+                               formatid
+                           FROM format
+                           WHERE format = %(format)s
+                       ) AS formatid;""", filters)
+    ids = ids.fetchone()
+    print(ids)
+    leaderboard = db.cursor()
     leaderboard.execute("""SELECT
                                name,
                                steamid64,
@@ -113,34 +121,22 @@ def leaderboard():
                                    *
                                FROM (SELECT
                                        steamid64,
-                                       duration,
-                                       logs,
-                                       (0.5 * ties + wins) / (logs) AS winrate,
-                                       (%(C)s * %(m)s + 0.5 * ties + wins) /
-                                           (%(C)s + logs) AS rating
-                                   FROM (SELECT
-                                           steamid64,
-                                           sum(round_wlt.duration) AS duration,
-                                           sum((wins > losses)::INT) AS wins,
-                                           sum((wins = losses)::INT) AS ties,
-                                           count(*) AS logs
-                                       FROM round_wlt
-                                       LEFT JOIN log USING (logid)
-                                       LEFT JOIN format USING (formatid)
-                                       LEFT JOIN map USING (mapid)
-                                       -- Hack to avoid joining this table if we don't filter
-                                       LEFT JOIN (SELECT
-                                               logid,
-                                               steamid64,
-                                               classid
-                                           FROM class_stats
-                                           WHERE %(class)s NOTNULL
-                                       ) AS class_stats USING (logid, steamid64)
-                                       LEFT JOIN class USING (classid)
-                                       WHERE TRUE
-                                           {}
-                                       GROUP BY steamid64
-                                   ) AS player_wlt
+                                       sum(duration) AS duration,
+                                       sum(wins + losses + ties) AS logs,
+                                       sum(0.5 * ties + wins) /
+                                           sum(wins + losses + ties) AS winrate,
+                                       (50 + sum(0.5 * ties + wins)) /
+                                           (100 + sum(wins + losses + ties)) AS rating
+                                   FROM leaderboard_cube
+                                   LEFT JOIN map USING (mapid)
+                                   WHERE steamid64 NOTNULL
+                                       AND (classid = %(classid)s
+                                           OR (%(classid)s ISNULL AND classid ISNULL))
+                                       AND (formatid = %(formatid)s
+                                           OR (%(formatid)s ISNULL AND formatid ISNULL))
+                                       AND (map ILIKE %(map)s
+                                           OR (%(map)s ISNULL AND map ISNULL))
+                                   GROUP BY steamid64
                                ) AS player_filtered
                                ORDER BY CASE %(sort)s
                                    WHEN 'logs' THEN logs
@@ -150,8 +146,7 @@ def leaderboard():
                                LIMIT %(limit)s OFFSET %(offset)s
                            ) AS players_sorted
                            LEFT JOIN player_last USING (steamid64)
-                           LEFT JOIN name USING (nameid);""".format(filter_clauses),
-                           { 'C': C, 'm': m, **filters, 'sort': sort, 'limit': limit,
-                             'offset': offset })
+                           LEFT JOIN name USING (nameid);""",
+                           { **filters, **ids, 'sort': sort, 'limit': limit, 'offset': offset })
     return flask.render_template("leaderboard.html", leaderboard=leaderboard.fetchall(),
                                  filters=filters, offset=offset, limit=limit)

schema.sql
@@ -160,27 +160,6 @@ JOIN (SELECT
 	GROUP BY ps.logid, steamid64
 ) AS round USING (logid);
 
-CREATE MATERIALIZED VIEW IF NOT EXISTS round_wlt AS SELECT
-	logid,
-	steamid64,
-	duration,
-	wins,
-	losses,
-	ties
-FROM (SELECT
-		logid,
-		teamid,
-		sum(duration) AS duration,
-		coalesce(sum((teamid = round.winner)::INT), 0::BIGINT) AS wins,
-		coalesce(sum((teamid != round.winner)::INT), 0::BIGINT) AS losses,
-		sum((round.winner ISNULL AND round.duration >= 60)::INT) AS ties
-	FROM round
-	CROSS JOIN team
-	GROUP BY logid, teamid
-) AS rounds
-JOIN player_stats USING (logid, teamid)
-ORDER BY steamid64, logid;
-
 CREATE TABLE IF NOT EXISTS player_stats_extra (
 	logid INT NOT NULL,
 	steamid64 BIGINT NOT NULL,
@@ -283,6 +262,36 @@ CREATE TABLE IF NOT EXISTS class_stats (
 CREATE STATISTICS IF NOT EXISTS class_stats (ndistinct)
 ON logid, steamid64
 FROM class_stats;
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS leaderboard_cube AS SELECT
+	steamid64,
+	formatid,
+	classid,
+	mapid,
+	sum(round_wlt.duration) AS duration,
+	sum((wins > losses)::INT) AS wins,
+	sum((wins = losses)::INT) AS ties,
+	sum((wins < losses)::INT) AS losses
+FROM (SELECT
+		logid,
+		teamid,
+		sum(duration) AS duration,
+		coalesce(sum((teamid = round.winner)::INT), 0::BIGINT) AS wins,
+		coalesce(sum((teamid != round.winner)::INT), 0::BIGINT) AS losses
+	FROM round
+	CROSS JOIN team
+	GROUP BY logid, teamid
+) AS round_wlt
+JOIN log USING (logid)
+JOIN player_stats USING (logid, teamid)
+JOIN class_stats USING (logid, steamid64)
+GROUP BY CUBE (steamid64, formatid, classid, mapid)
+ORDER BY mapid, classid, formatid, steamid64;
+
+CREATE UNIQUE INDEX IF NOT EXISTS leaderboard_pkey
+	ON leaderboard_cube (mapid, classid, formatid, steamid64);
+
+CREATE INDEX IF NOT EXISTS leaderboard_classid ON leaderboard_cube (classid, formatid);
 
 CREATE TABLE IF NOT EXISTS weapon (
 	weaponid SERIAL PRIMARY KEY,

templates/leaderboard.html
@@ -7,7 +7,7 @@
 {% block content %}
 	{{ super() }}
 	<h1>Leaderboard</h1>
-	{{ filter(filters) }}
+	{{ filter(filters, date=False) }}
 	{{ navigation(leaderboard, '.leaderboard', limit, offset, **request.args) }}
 {% macro header() %}
 <tr>
@@ -15,6 +15,7 @@
 	<th>Player</th>
 	<th><abbr title="Total logs">Logs</th>
 	<th><abbr title="Winrate, ties count as 0.5 wins">Winrate</abbr></th>
+	<th><abbr title="Winrate with 50 wins and 50 losses added">Rating</abbr></th>
 	<th>Time Played</td>
 </tr>
 {% endmacro %}
@@ -31,6 +32,7 @@
 				</td>
 				<td>{{ player['logs'] }}</td>
 				<td>{{ "{:.2%}".format(player['winrate']) }}</td>
+				<td>{{ "{:.3}".format(player['rating']) }}</td>
 				<td>{{ player['duration'] | duration }}</td>
 			</tr>
 			{% endfor %}

templates/macros/filter.html
@@ -6,7 +6,7 @@
 	<option {{ "selected" if key == default }} value="{{ key }}">{{ content }}</option>
 {% endfor %}
 {% endmacro %}
-{% macro filter(filters) %}
+{% macro filter(filters, date=True) %}
 <form>
 	<label>
 		<abbr title="Games with at least &frac23; time on this class">Class</abbr>:
@@ -32,6 +32,7 @@
 		       value="{{ (filters['map'] or "") | trim("%") }}">
 		<datalist id="maps"></datalist>
 	</label>
+	{% if date %}
 	<input id="timezone" type="hidden" name="timezone">
 	<label>From:
 		<input type="date" name="date_from" value="{{ filters['date_from'] or "" }}">
@@ -39,6 +40,7 @@
 	<label>To:
 		<input type="date" name="date_to" value="{{ filters['date_to'] or "" }}">
 	</label>
+	{% endif %}
 	<button type="submit">Filter</button>
 </form>
 {% endmacro %}

commit a2717f44d919d1d3e3e41c865c890d2e5b92ae85

Add groupings column to leaderboard_cube
When players have never (for example) played in a league match, the cube
groupings with league included will have NULLs in the league column, just
like the groupings without league. This will cause these players' stats to
be overcounted when not filtering by league. To prevent this, add a
groupings column so we can know for sure which rows we should include in
the results.

In addition to fixing correctness problems, the groupings column also lets
us rework our indexes to take advantage of bloom filters. Unlike b-tree
indices, which are only efficient when filtering columns in-order, bloom
indices have no ordering preference, and filter just as well as long as
enough columns are being filtered by. The bloom index implementation in
postgres does not support excluding NULLs. However, by including the
groupings column in the index we can filter to the correct rows without
requiring NULL support.

Following the general outline of [1], the entropy in each of the filtering
columns is:

column   entropy
======== =======
league      0.40
formatid    2.20
classid     2.69
mapid       7.07
grouping    3.56

As the information stored in the default signature length of 80 bits is
6.322, we can use one bit for each column (slightly shortchanging the
mapid). This gives us a total number of set bits (I) of 80. Using the
formula for signature length (s_r) assuming 4K pages and a 4x random read
cost, we find the optimal signature length for a given number of filters
(Q) is:

I Q s_r
= = =====
5 1 809.5
5 2 169.4
5 3  75.8
5 4  46.6
5 5  33.6

This indicates we will support efficient querying with the default
signature length of 80 when we are filtering by at least 3 columns. We will
always filter by at least one columns (groupings), so the bloom index will
be efficient for querying on 2 or more columns. This means we need
more-efficient indices for the 1 column case. Fortunately, b-tree indices
are a great fit here. In the case where we aren't filtering on any columns,
we still want to filter by groupings, so we can use a b-tree index for that
as well. This indexing strategy roughly halves the index space, and should
be much more robust to arbitrary filter combinations.

[1] https://web.archive.org/web/20190201134134/https://blog.coelho.net/database/2016/12/11/postgresql-bloom-index.html

Fixes: 44be5a5 ("Optimize leaderboard")

trends/bloom.sql
@@ -1,0 +1,9 @@
+CREATE EXTENSION IF NOT EXISTS bloom;
+
+DO $$ BEGIN
+	CREATE OPERATOR CLASS enum_ops DEFAULT FOR TYPE anyenum USING bloom AS
+		OPERATOR 1 =(anyenum, anyenum),
+		FUNCTION 1 hashenum(anyenum);
+EXCEPTION WHEN duplicate_object THEN
+	NULL;
+END $$;

trends/migrations/leaderboard_bloom.sql
@@ -1,0 +1,73 @@
+BEGIN;
+
+DROP MATERIALIZED VIEW leaderboard_cube;
+CREATE MATERIALIZED VIEW leaderboard_cube AS SELECT
+	playerid,
+	league,
+	formatid,
+	primary_classid AS classid,
+	mapid,
+	grouping(playerid, league, formatid, primary_classid, mapid) AS grouping,
+	sum(log.duration) AS duration,
+	sum((wins > losses)::INT) AS wins,
+	sum((wins = losses)::INT) AS ties,
+	sum((wins < losses)::INT) AS losses,
+	sum(kills) AS kills,
+	sum(deaths) AS deaths,
+	sum(assists) AS assists,
+	sum(dmg) AS dmg,
+	sum(dt) AS dt,
+	sum(shots) AS shots,
+	sum(hits) AS hits
+FROM log_nodups AS log
+JOIN player_stats USING (logid)
+GROUP BY CUBE (playerid, league, formatid, classid, mapid)
+ORDER BY mapid, classid, formatid, playerid, league;
+
+-- To help out the query planner
+CREATE STATISTICS IF NOT EXISTS leaderboard_stats (dependencies, ndistinct, mcv)
+	ON league, formatid, classid, mapid, grouping
+	FROM leaderboard_cube;
+
+-- When we have no filters (or nothing better)
+CREATE INDEX IF NOT EXISTS leaderboard_grouping ON leaderboard_cube (grouping);
+
+-- When we have a single filter
+CREATE INDEX IF NOT EXISTS leaderboard_league ON leaderboard_cube (league)
+	WHERE playerid NOTNULL
+		AND league NOTNULL
+		AND formatid ISNULL
+		AND classid ISNULL
+		AND mapid ISNULL
+		AND grouping = b'01110'::INT;
+CREATE INDEX IF NOT EXISTS leaderboard_format ON leaderboard_cube (formatid)
+	WHERE playerid NOTNULL
+		AND league ISNULL
+		AND formatid NOTNULL
+		AND classid ISNULL
+		AND mapid ISNULL
+		AND grouping = b'01110'::INT;
+CREATE INDEX IF NOT EXISTS leaderboard_class ON leaderboard_cube (classid)
+	WHERE playerid NOTNULL
+		AND league ISNULL
+		AND formatid ISNULL
+		AND classid NOTNULL
+		AND mapid ISNULL
+		AND grouping = b'01110'::INT;
+CREATE INDEX IF NOT EXISTS leaderboard_map ON leaderboard_cube (mapid)
+	WHERE playerid NOTNULL
+		AND league ISNULL
+		AND formatid ISNULL
+		AND classid ISNULL
+		AND mapid NOTNULL
+		AND grouping = b'01110'::INT;
+
+-- When we have multiple filters
+CREATE INDEX IF NOT EXISTS leaderboard_bloom ON leaderboard_cube
+	USING bloom (grouping, mapid, classid, formatid, league)
+	WITH (col1=1, col2=1, col3=1, col4=1, col5=1)
+	WHERE playerid NOTNULL;
+
+COMMIT;
+
+ANALYZE VERBOSE leaderboard_cube;

trends/schema.sql
@@ -695,6 +695,7 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS leaderboard_cube AS SELECT
 	formatid,
 	primary_classid AS classid,
 	mapid,
+	grouping(playerid, league, formatid, primary_classid, mapid) AS grouping,
 	sum(log.duration) AS duration,
 	sum((wins > losses)::INT) AS wins,
 	sum((wins = losses)::INT) AS ties,
@@ -712,10 +713,49 @@ GROUP BY CUBE (playerid, league, formatid, classid, mapid)
 ORDER BY mapid, classid, formatid, playerid, league
 WITH NO DATA;
 
-CREATE UNIQUE INDEX IF NOT EXISTS leaderboard_pkey
-	ON leaderboard_cube (mapid, classid, formatid, playerid, league);
+-- To help out the query planner
+CREATE STATISTICS IF NOT EXISTS leaderboard_stats (dependencies, ndistinct, mcv)
+	ON league, formatid, classid, mapid, grouping
+	FROM leaderboard_cube;
 
-CREATE INDEX IF NOT EXISTS leaderboard_classid ON leaderboard_cube (classid, formatid);
+-- When we have no filters (or nothing better)
+CREATE INDEX IF NOT EXISTS leaderboard_grouping ON leaderboard_cube (grouping);
+
+-- When we have a single filter
+CREATE INDEX IF NOT EXISTS leaderboard_league ON leaderboard_cube (league)
+	WHERE playerid NOTNULL
+		AND league NOTNULL
+		AND formatid ISNULL
+		AND classid ISNULL
+		AND mapid ISNULL
+		AND grouping = b'01110'::INT;
+CREATE INDEX IF NOT EXISTS leaderboard_format ON leaderboard_cube (formatid)
+	WHERE playerid NOTNULL
+		AND league ISNULL
+		AND formatid NOTNULL
+		AND classid ISNULL
+		AND mapid ISNULL
+		AND grouping = b'01110'::INT;
+CREATE INDEX IF NOT EXISTS leaderboard_class ON leaderboard_cube (classid)
+	WHERE playerid NOTNULL
+		AND league ISNULL
+		AND formatid ISNULL
+		AND classid NOTNULL
+		AND mapid ISNULL
+		AND grouping = b'01110'::INT;
+CREATE INDEX IF NOT EXISTS leaderboard_map ON leaderboard_cube (mapid)
+	WHERE playerid NOTNULL
+		AND league ISNULL
+		AND formatid ISNULL
+		AND classid ISNULL
+		AND mapid NOTNULL
+		AND grouping = b'01110'::INT;
+
+-- When we have multiple filters
+CREATE INDEX IF NOT EXISTS leaderboard_bloom ON leaderboard_cube
+	USING bloom (grouping, mapid, classid, formatid, league)
+	WITH (col1=1, col2=1, col3=1, col4=1, col5=1)
+	WHERE playerid NOTNULL;
 
 CREATE TABLE IF NOT EXISTS weapon (
 	weaponid SERIAL PRIMARY KEY,

trends/site/root.py
@@ -74,11 +74,16 @@ def leaderboard():
 
     # Since we are using a cube, we need to explicitly select the NULL rows
     cube_clauses = []
-    for (name, column) in (('class', 'classid'), ('format', 'formatid'), ('map', 'mapid')):
+    grouping = 0b00000
+    for (name, column, group) in (
+            ('map',    'mapid',    0b00001),
+            ('class',  'classid',  0b00010),
+            ('format', 'formatid', 0b00100),
+            ('league', 'league',   0b01000),
+    ):
         if not filters[name]:
-            cube_clauses.append("AND {} ISNULL".format(column))
-    if not filters['league']:
-        cube_clauses.append("AND league ISNULL")
+            cube_clauses.append(f"AND {column} ISNULL")
+            grouping |= group
     cube_clauses = '\n'.join(cube_clauses)
 
     order, order_clause = get_order({
@@ -137,7 +142,7 @@ def leaderboard():
                                    sum(dmg) * 1.0 / nullif(sum(dt), 0) AS dr,
                                    sum(hits) * 1.0 / nullif(sum(shots), 0) AS acc
                                FROM leaderboard_cube
-                               WHERE playerid NOTNULL
+                               WHERE playerid NOTNULL AND grouping = %(grouping)s
                                    {}
                                    {}
                                GROUP BY playerid
@@ -147,7 +152,7 @@ def leaderboard():
                            LEFT JOIN player USING (playerid)
                            LEFT JOIN name USING (nameid);"""
                            .format(filter_clauses, cube_clauses, order_clause),
-                        { **filters, 'limit': limit, 'offset': offset })
+                        { **filters, 'grouping': grouping, 'limit': limit, 'offset': offset })
     resp = flask.make_response(flask.render_template("leaderboard.html",
                                leaderboard=leaderboard.fetchall()))
     resp.cache_control.max_age = 3600

test/conftest.py
@@ -3,10 +3,12 @@
 
 from contextlib import contextmanager
 import logging
+import os
 
 import pytest
 from testing.postgresql import Postgresql
 
+import trends
 from trends.sql import db_connect
 from .create import create_test_db
 
@@ -22,6 +24,10 @@ def database(request):
     postgres_args = Postgresql.DEFAULT_SETTINGS['postgres_args']
     postgres_args += " -c full_page_writes=off"
     with Postgresql(postgres_args=postgres_args) as database:
+        with db_connect(database.url()) as c:
+            with open(f"{os.path.dirname(trends.__file__)}/bloom.sql") as bloom:
+                c.cursor().execute(bloom.read())
+
         with caplog_session(request) as caplog:
             with caplog.at_level(logging.ERROR):
                 create_test_db(database.url())

test/create.py
@@ -88,16 +88,15 @@ def create_test_db(url):
     with db_connect(url) as c:
         cur = c.cursor()
         cur.execute("ANALYZE;")
-        # A second time to test partitioning log_json
-        db_init(c)
-        cur.execute("REFRESH MATERIALIZED VIEW leaderboard_cube;")
-        cur.execute("REFRESH MATERIALIZED VIEW map_popularity;")
-
-    with db_connect(url) as c:
         class args:
             since = datetime.fromtimestamp(0)
         trends.importer.link_demos.link_logs(args, c)
         trends.importer.link_matches.link_matches(args, c)
+        cur.execute("ANALYZE;")
+        # A second time to test partitioning log_json
+        db_init(c)
+        cur.execute("REFRESH MATERIALIZED VIEW leaderboard_cube;")
+        cur.execute("REFRESH MATERIALIZED VIEW map_popularity;")
 
 if __name__ == '__main__':
     if len(sys.argv) != 2: