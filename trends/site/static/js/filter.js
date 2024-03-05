Forty-Bot/trends.tf

commit 8ab119f880f05460a96d8b0d8e12dcdb2307e742

site: Fix filtering for players without avatars
If a player doesn't hae an aatar, then avatarhash would be null, causing an
exception. Check for this, and just skip the avatar image in that case.

Fixes: 07c6e92 ("site: Add avatars to player filters")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/static/js/filter.js
@@ -24,11 +24,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function render_option(data, escape) {
 	let div = document.createElement('div');
-	let img = document.createElement('img');
-	img.classList.add('avatar_small');
-	img.setAttribute('src', "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/" +
-	                        `avatars/${data.avatarhash.slice(0, 2)}/${data.avatarhash}.jpg`);
-	div.append(img, " ", data.name);
+	if (data.avatarhash == null) {
+		div.append(data.name);
+	} else {
+		let img = document.createElement('img');
+		img.classList.add('avatar_small');
+		img.setAttribute('src', "https://steamcdn-a.akamaihd.net/steamcommunity/public/"
+					`images/avatars/${data.avatarhash.slice(0, 2)}/` +
+					`${data.avatarhash}.jpg`);
+		div.append(img, " ", data.name);
+	}
 	return div;
 }
 

commit 3d84f7f5c8b93eef74559a0f3fadace01f3b2204

site: Fix filtering by players
I forgot a plus.

Fixes: 8ab119f ("site: Fix filtering for players without avatars")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/static/js/filter.js
@@ -29,7 +29,7 @@ function render_option(data, escape) {
 	} else {
 		let img = document.createElement('img');
 		img.classList.add('avatar_small');
-		img.setAttribute('src', "https://steamcdn-a.akamaihd.net/steamcommunity/public/"
+		img.setAttribute('src', "https://steamcdn-a.akamaihd.net/steamcommunity/public/" +
 					`images/avatars/${data.avatarhash.slice(0, 2)}/` +
 					`${data.avatarhash}.jpg`);
 		div.append(img, " ", data.name);