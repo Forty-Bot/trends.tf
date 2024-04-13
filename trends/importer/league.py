Forty-Bot/trends.tf

commit 481a3205f20deb5a81c8cb76cc6fd00cf6832d30

Support importing ETF2L matches
This adds support for importing ETF2L match data. In the next few commits,
this data will be matched with logs and exposed through the interface. The
schema and whole import process have been designed (with pain) to be ready
for RGL support as well. This has caused the schema to be particularly
contorted, since RGL models its data in a significantly different way from
ETF2L. In many places, fields may be NULL for one league and NOT NULL for
another. This is controlled through a set of functions on the league enum.
I chose this method, since it allows for the regular foreign key
infrastructure, and will hopefully reduce rework (when compared to
open-coding the conditions).

The importer itself is done fairly differently from the logs importer.
Instead of going directly from the API json to importing it, we create an
intermediate representation which is then fed to the importer. This should
hopefully make it easier to paper over differences in the APIs. As usual,
the data is terrible, and the code already has several workarounds.

Hopefully this will make adding RGL support less awful, although I suspect
there will be problems to solve. No schema survives contact with the data.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/importer/league.py
@@ -1,0 +1,234 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+# Copyright (C) 2022 Sean Anderson <seanga2@gmail.com>
+
+import psycopg2.extras
+
+def import_compdiv(c, cd):
+    c.execute(
+        """INSERT INTO competition (league, compid, formatid, name)
+           VALUES (
+               %(league)s, %(compid)s,
+               (SELECT formatid FROM format WHERE format = %(format)s), %(competition)s
+           ) ON CONFLICT DO NOTHING;""", cd)
+
+    if cd['divid'] is None:
+        return
+
+    c.execute("INSERT INTO div_name (division) VALUES (%(division)s) ON CONFLICT DO NOTHING;",
+              cd)
+    c.execute(
+        """INSERT INTO division (league, compid, divid, div_nameid, tier)
+           VALUES (
+               %(league)s, %(compid)s, %(divid)s,
+               (SELECT div_nameid FROM div_name WHERE division = %(division)s), %(tier)s
+           ) ON CONFLICT DO NOTHING;""", cd)
+
+def import_team(c, t):
+    teamid_col = "%(teamid)s"
+    if teamids := t.get('rgl_teamids'):
+        c.execute("SELECT teamid FROM team_comp WHERE rgl_teamid IN %s GROUP BY teamid",
+                  (teamids,))
+        if c.rowcount not in (0, 1):
+            logging.warning("Too many matching teams for linked RGL teamids %s", teamids)
+
+        if row := c.fetchone():
+            t['teamid'] = row[0]
+        else:
+            teamid_col = "DEFAULT"
+
+    c.execute("INSERT INTO team_name (team) VALUES (%(name)s) ON CONFLICT DO NOTHING", t)
+    c.execute(
+        f"""INSERT INTO league_team (league, teamid, team_nameid, avatarhash, fetched)
+            VALUES (
+                %(league)s, {teamid_col},
+                CASE WHEN NOT league_team_per_comp(%(league)s) THEN (
+                    SELECT team_nameid FROM team_name WHERE team = %(name)s
+                ) END,
+                CASE WHEN NOT league_team_per_comp(%(league)s) THEN %(avatarhash)s END,
+                CASE WHEN NOT league_team_per_comp(%(league)s) THEN %(fetched)s END
+            ) ON CONFLICT (league, teamid)
+            DO UPDATE SET
+                team_nameid = EXCLUDED.team_nameid,
+                avatarhash = EXCLUDED.avatarhash,
+                fetched = greatest(EXCLUDED.fetched, league_team.fetched)
+            RETURNING teamid;""", t)
+    if row := c.fetchone():
+        t['teamid'] = row[0]
+
+    t['rgl_teamid'] = t.get('rgl_teamid')
+    c.execute(
+        """INSERT INTO team_comp_backing (
+               league, teamid, compid, team_nameid, rgl_teamid, end_rank, avatarhash, fetched
+           ) VALUES (
+               %(league)s, %(teamid)s, %(compid)s,
+               CASE WHEN league_team_per_COMP(%(league)s) THEN (
+                   SELECT team_nameid FROM team_name WHERE team = %(name)s
+               ) END, %(rgl_teamid)s, %(end_rank)s,
+               CASE WHEN league_team_per_comp(%(league)s) THEN %(avatarhash)s END,
+               CASE WHEN league_team_per_comp(%(league)s) THEN %(fetched)s END
+           ) ON CONFLICT (league, teamid, compid)
+           DO UPDATE SET
+                team_nameid = EXCLUDED.team_nameid,
+                avatarhash = EXCLUDED.avatarhash,
+                end_rank = EXCLUDED.end_rank,
+                fetched = greatest(EXCLUDED.fetched, team_comp_backing.fetched);""", t)
+
+    if 'updates' in t and t['updates']:
+        import_transfers(c, t)
+
+def import_transfers(c, t):
+    # Load new players...
+    c.execute(
+        """CREATE TEMP TABLE new_player (
+               id SERIAL,
+               steamid64 BIGINT,
+               name TEXT,
+               avatarhash TEXT,
+               eu_playerid INT
+           );""")
+    psycopg2.extras.execute_values(c,
+        "INSERT INTO new_player (steamid64, name, avatarhash, eu_playerid) VALUES %s;",
+        (u['player'] for u in t['updates']),
+        "(%(steamid64)s, %(name)s, %(avatarhash)s, %(eu_playerid)s)")
+    # ... and remove duplicates
+    c.execute(
+        """DELETE FROM new_player np1
+           USING new_player np2
+           WHERE np1.steamid64 = np2.steamid64
+               AND np2.id < np1.id""")
+    c.execute("INSERT INTO name (name) SELECT name FROM new_player ON CONFLICT DO NOTHING;")
+    c.execute(
+        """INSERT INTO player (steamid64, nameid, avatarhash, eu_playerid) SELECT
+               steamid64,
+               (SELECT nameid FROM name WHERE name = new_player.name),
+               avatarhash,
+               eu_playerid
+           FROM new_player
+           ON CONFLICT (steamid64)
+           DO UPDATE SET
+               eu_playerid = coalesce(EXCLUDED.eu_playerid, player.eu_playerid);""")
+    c.execute("DROP TABLE new_player;")
+
+    # OK, here's the dance:
+    # We need to determine the actual ranges when a player was rostered. This is effectively
+    # union(intersection(ranges)), but there's no function in postgres to do this. So we are
+    # going to roll our own, inspired by [1]. This is modified from the original to support
+    # removing old ranges, and to support open-ended ranges without clobbering things in
+    # between. That is, if we have something like
+    #   [2,), (,6), [8,)
+    # we want to get
+    #   [2, 6), [8,)
+    # and not
+    #   [,)
+    #
+    # [1] https://dba.stackexchange.com/a/101010/219030
+    c.execute(
+        """CREATE TEMP TABLE old_ranges AS SELECT
+               playerid,
+               rostered
+           FROM team_player
+           WHERE league = %(league)s
+               AND teamid = %(teamid)s
+               AND (compid = %(compid)s OR compid ISNULL);""", t)
+    psycopg2.extras.execute_values(c,
+        """INSERT INTO old_ranges (playerid, rostered) SELECT
+                playerid,
+                rostered::INT8RANGE
+            FROM (VALUES %s) AS ranges(steamid64, rostered)
+            JOIN player USING (steamid64);""",
+        ((u['player']['steamid64'], u['rostered']) for u in t['updates']))
+
+    # This could probably be done as an UPDATE
+    c.execute(
+        """CREATE TEMP TABLE new_ranges AS SELECT
+               playerid,
+               rostered AS old,
+               int8range(
+                   min(lower(rostered)) OVER win,
+                   CASE WHEN min(lower(rostered)) OVER win > max(endtime) OVER win THEN NULL ELSE
+                       max(endtime) OVER win
+                   END
+               ) AS new
+           FROM (SELECT
+                   playerid,
+                   rostered,
+                   endtime,
+                   count(next > endtime OR NULL) OVER WIN AS grp
+               FROM (SELECT
+                       playerid,
+                       rostered,
+                       lead(lower(rostered)) OVER win AS next,
+                       max(upper(rostered)) OVER win AS endtime
+                   FROM old_ranges
+                   WINDOW win AS (
+                       PARTITION BY playerid
+                       ORDER BY least(lower(rostered), upper(rostered))
+                   )
+               ) AS a
+               WINDOW win AS (
+                   PARTITION BY playerid
+                   ORDER BY least(lower(rostered), upper(rostered)) DESC
+               )
+           ) AS b
+           WINDOW win AS (
+               PARTITION BY playerid, grp
+           );""")
+    c.execute("DROP TABLE old_ranges;")
+
+    c.execute(
+        """DELETE FROM team_player
+           USING new_ranges
+           WHERE league = %(league)s
+               AND teamid = %(teamid)s
+               AND (compid = %(compid)s OR compid ISNULL)
+               AND team_player.playerid = new_ranges.playerid
+               AND team_player.rostered = new_ranges.old
+               AND new_ranges.old != new_ranges.new;""", t)
+    c.execute(
+        """INSERT INTO team_player (league, teamid, compid, playerid, rostered)
+           SELECT
+               %(league)s,
+               %(teamid)s,
+               CASE WHEN league_team_per_comp(%(league)s) THEN %(compid)s END,
+               playerid,
+               new
+           FROM new_ranges
+           -- Some joins/leaves happen in the wrong order or are otherwise bogus
+           WHERE new NOTNULL AND NOT isempty(new) AND NOT lower_inf(new)
+           GROUP BY playerid, new
+           ON CONFLICT DO NOTHING;""", t)
+    c.execute("DROP TABLE new_ranges;")
+
+def import_match(c, m):
+    if m['seq'] is not None:
+        c.execute("INSERT INTO round_name (round) VALUES (%(round)s) ON CONFLICT DO NOTHING;", m)
+        col = "div" if m['divid'] else "comp"
+        c.execute(
+            f"""INSERT INTO {col}_round (league, {col}id, round_seq, round_nameid)
+                VALUES (
+                    %(league)s, %({col}id)s, %(seq)s,
+                    (SELECT round_nameid FROM round_name WHERE round = %(round)s)
+                ) ON CONFLICT DO NOTHING;""", m)
+
+    m['winner'] = m.get('winner')
+    c.execute("INSERT INTO map (map) SELECT unnest(%(maps)s::TEXT[]) ON CONFLICT DO NOTHING;", m)
+    c.execute(
+        """INSERT INTO match (
+               league, matchid, compid, divid, teamid1, teamid2, round_seq, scheduled, submitted,
+               mapids, score1, score2, forfeit, fetched
+           ) VALUES (
+               %(league)s, %(matchid)s, %(compid)s, %(divid)s, %(teamid1)s, %(teamid2)s, %(seq)s,
+               %(scheduled)s, %(submitted)s,
+               (SELECT
+                       coalesce(array_agg(mapid), array[]::INT[])
+                   FROM map
+                   WHERE map = any(%(maps)s)
+               ), %(score1)s, %(score2)s, %(forfeit)s, %(fetched)s
+           ) ON CONFLICT (league, matchid)
+           DO UPDATE SET
+               scheduled = EXCLUDED.scheduled,
+               mapids = EXCLUDED.mapids,
+               score1 = EXCLUDED.score1,
+               score2 = EXCLUDED.score2,
+               forfeit = EXCLUDED.forfeit,
+               fetched = greatest(match.fetched, EXCLUDED.fetched);""", m)

salt/backend.sls
@@ -141,6 +141,30 @@
         [Install]
         WantedBy=timers.target
 
+/etc/systemd/system/etf2l_import.service:
+  file.managed:
+    - contents: |
+        [Unit]
+        Description=Import ETF2L matches
+
+        [Service]
+        Type=oneshot
+        EnvironmentFile=/etc/default/trends
+        ExecStart={{ prefix }}/bin/trends_importer -vv etf2l bulk -N postgres:///trends
+        User=daemon
+
+/etc/systemd/system/etf2l_import.timer:
+  file.managed:
+    - contents: |
+        [Unit]
+        Description=Import ETF2L matches every 5 minutes
+
+        [Timer]
+        OnCalendar=*:1/5
+
+        [Install]
+        WantedBy=timers.target
+
 /etc/systemd/system/link.service:
   file.managed:
     - contents: |
@@ -180,6 +204,8 @@ backend_services:
       - /etc/systemd/system/weapon_import.timer
       - /etc/systemd/system/demo_import.service
       - /etc/systemd/system/demo_import.timer
+      - /etc/systemd/system/etf2l_import.service
+      - /etc/systemd/system/etf2l_import.timer
       - /etc/systemd/system/link.service
       - /etc/systemd/system/link.timer
 
@@ -214,6 +240,12 @@ weapon_import.timer:
       - backend_services
 
 demo_import.timer:
+  service.running:
+    - enable: True
+    - require:
+      - backend_services
+
+etf2l_import.timer:
   service.running:
     - enable: True
     - require:

trends/importer/cli.py
@@ -11,6 +11,7 @@ from ..sql import db_connect, db_init
 from ..util import sentry_init
 from .ad import create_ad_parser
 from .demos import create_demos_parser
+from .etf2l import create_etf2l_parser
 from .json import create_json_parser
 from .logs import create_logs_parser
 from .link import create_link_parser
@@ -23,6 +24,7 @@ def create_parser():
     sub = parser.add_subparsers()
     create_ad_parser(sub)
     create_demos_parser(sub)
+    create_etf2l_parser(sub)
     create_json_parser(sub)
     create_link_parser(sub)
     create_logs_parser(sub)

trends/importer/etf2l.py
@@ -1,0 +1,211 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+# Copyright (C) 2022 Sean Anderson <seanga2@gmail.com>
+
+from datetime import datetime
+import re
+import json
+import logging
+import time
+
+from psycopg2.extras import NumericRange
+import sentry_sdk
+
+from .fetch import ETF2LFileFetcher, ETF2LBulkFetcher
+from .league import *
+from ..sql import db_connect
+from ..steamid import SteamID
+from ..util import chunk
+
+def filter_matchids(c, results):
+    with c.cursor() as cur:
+        for results in chunk(results, 100):
+            results = { result['id']: result for result in results }
+            psycopg2.extras.execute_values(cur,
+                """SELECT new.matchid
+                   FROM (VALUES %s) AS new (matchid)
+                   LEFT JOIN match ON (league = 'etf2l' AND match.matchid = new.matchid)
+                   WHERE match.matchid IS NULL""", ((matchid,) for matchid in results.keys()))
+            for row in cur:
+                yield results[row[0]]
+
+UNKNOWN_AVATAR = "https://api.etf2l.org/img/unknown_avatar_full.jpg"
+RE_ETF2L_AVATAR = re.compile(r"^.*([a-z0-9]{13}\.[a-z]{3})$")
+RE_STEAM_AVATAR = re.compile(r"^.*([a-z0-9]{40})(|_medium|_full)\.[a-z]{3}$")
+def parse_avatar(container, regex):
+    url = container['steam']['avatar']
+    if not url or url == UNKNOWN_AVATAR:
+        return
+
+    avatar = regex.match(url)
+    if avatar:
+        return avatar.group(1)
+    else:
+        logging.warning("no avatar hash for %s", url)
+
+eu_type_map = {
+    '1on1': 'other',
+    '1v1': 'other',
+    '2on2': 'ultiduo',
+    '2v2': 'ultiduo',
+    '6on6': 'sixes',
+    '6v6': 'sixes',
+    'National 6v6 Team': 'sixes',
+    'Highlander': 'highlander',
+    'Highlander Open': 'highlander',
+    'National Highlander Team': 'highlander',
+    'Fun Team': 'other',
+    '6v6 Fun Team': 'sixes',
+    'LAN Team': 'other',
+}
+
+def parse_result(result):
+    def parse_team(team):
+        return {
+            'teamid': team['id'],
+            'name': str(team['name']),
+            'avatarhash': parse_avatar(team, RE_ETF2L_AVATAR),
+            'end_rank': None,
+        }
+
+    comp = result['competition']
+    div = result['division']
+    teams = (parse_team(result['clan1']), parse_team(result['clan2']))
+    if teams[0]['teamid'] > teams[1]['teamid']:
+        teams = (teams[1], teams[0])
+        (result['r1'], result['r2']) = (result['r2'], result['r1'])
+
+    # XXX: Dirty hack for dirty data
+    if div.get('id') == 17:
+        comp = {
+            'id': 1,
+            'name': "Season 1",
+            'type': '6on6',
+        }
+
+    return {
+        'league': 'etf2l',
+        'compid': comp['id'],
+        'competition': comp['name'],
+        'format': eu_type_map[comp['type']],
+        'divid': div.get('id'),
+        'division': div['name'],
+        'tier': div['tier'],
+        'teams': teams,
+        'seq': result['week'],
+        'round': result['round'],
+        'matchid': result['id'],
+        'scheduled': result['time'],
+        'submitted': None,
+        'fetched': result['fetched'],
+        'maps': [map for map in result.get('maps', ()) if map != 'variable'] or None,
+        'score1': result['r1'],
+        'score2': result['r2'],
+        'forfeit': not not result['defaultwin'],
+    }
+
+def parse_xfer(xfer):
+    player = xfer['who']
+    ret = {
+        'player': {
+            'steamid64': SteamID(player['steam']['id64']),
+            'eu_playerid': player['id'],
+            'avatarhash': parse_avatar(player, RE_STEAM_AVATAR),
+            'name': player['name'],
+        },
+        'fetched': xfer['fetched']
+    }
+
+    if xfer['type'] == 'joined':
+        ret['rostered'] = NumericRange(lower=xfer['time'])
+    elif xfer['type'] == 'left':
+        ret['rostered'] = NumericRange(upper=xfer['time'])
+    else:
+        raise Error("TODO")
+    return ret
+
+def create_etf2l_parser(sub):
+    etf2l = sub.add_parser("etf2l", help="Import etf2l matches")
+    etf2l.set_defaults(importer=import_etf2l_cli)
+    etf2l_sub = etf2l.add_subparsers()
+    f = etf2l_sub.add_parser("file", help="Import from the local filesystem")
+    f.set_defaults(fetcher=ETF2LFileFetcher)
+    f.add_argument("results", metavar="RESULTS", help="File with match results")
+    f.add_argument("xferdir", metavar="XFERDIR",
+                   help="Directory containing team transfers named 'transfer_TEAMID_PAGE.json'")
+    b = etf2l_sub.add_parser("bulk", help="Bulk import from etf2ls.tf")
+    b.set_defaults(fetcher=ETF2LBulkFetcher)
+    b.add_argument("-s", "--since", type=datetime.fromisoformat,
+                   default=0, metavar="DATE",
+                   help="Only fetch matches created since DATE")
+    b.add_argument("-N", "--new", action='store_true',
+                   help="Only fetch matches created since the newest imported etf2l")
+    b.add_argument("-c", "--count", type=int, default=None,
+                   help="Fetch up to COUNT matches, defaults to unlimited")
+    b.add_argument("-p", "--page", type=int, default=1,
+                   help="Start at a particular page")
+
+def import_etf2l_cli(args, c):
+    with sentry_sdk.start_transaction(op="import", name="etf2l_matches"):
+        if 'new' in args and args.new:
+            with c.cursor() as cur:
+                cur.execute(
+                    """SELECT max(fetched) - 6 * 60 * 60
+                       FROM matches
+                       WHERE league = 'etf2l';""");
+                args.since = cur.fetchone()[0]
+        return import_etf2l(c, args.fetcher(**vars(args)))
+
+
+def import_etf2l(c, fetcher):
+    cur = c.cursor()
+    count = 0
+    for result in filter_matchids(c, fetcher.get_results()):
+        try:
+            res = parse_result(result)
+            for team in res['teams']:
+                team['league'] = 'etf2l'
+                team['compid'] = res['compid']
+                team['divid'] = res['divid']
+
+                cur.execute(
+                    """SELECT coalesce(fetched, 0)
+                       FROM league_team
+                       WHERE league = 'etf2l'
+                           AND teamid = %(teamid)s
+                       UNION ALL
+                       SELECT 0;""", team)
+                row = cur.fetchone()
+                if row[0] <= (res['fetched'] or 0):
+                    team['fetched'] = time.time()
+                    team['updates'] = []
+                    for xfer in fetcher.get_xfers(team['teamid'], since=row[0]):
+                        try:
+                            team['updates'].append(parse_xfer(xfer))
+                        except ValueError:
+                            pass
+                else:
+                    team['fetched'] = row[0]
+                    team['updates'] = ()
+
+            res['teamid1'] = res['teams'][0]['teamid']
+            res['teamid2'] = res['teams'][1]['teamid']
+            # sigh...
+            if res['teamid1'] == res['teamid2']:
+                continue
+
+            with sentry_sdk.start_span(op='db.transaction', description=f"import {result['id']}"):
+                cur.execute("BEGIN;")
+                import_compdiv(cur, res)
+                for team in res['teams']:
+                    import_team(cur, team)
+                import_match(cur, res)
+                cur.execute("COMMIT;")
+        except (IndexError, KeyError, psycopg2.errors.UniqueViolation):
+            logging.exception("Could not parse result %s", result['id'])
+            cur.execute("ROLLBACK;")
+        except psycopg2.Error:
+            logging.error("Could not import result %s", result['id'])
+            raise
+        else:
+            count += 1
+    logging.info("Imported %s matches", count)

trends/importer/fetch.py
@@ -5,6 +5,7 @@ import collections
 import itertools
 import json
 import logging
+import os
 import sqlite3
 import time
 
@@ -430,3 +431,82 @@ class DemoBulkFetcher(DemoListFetcher):
             logging.exception("Could not fetch demo list")
         except (ValueError, KeyError):
             logging.exception("Could not parse demo list")
+
+class ETF2LFileFetcher:
+    def __init__(self, results, xferdir, **kewargs):
+        self.results = results
+        self.xferdir = xferdir
+
+    def get_results(self):
+        with open(self.results) as resultfile:
+            results = json.load(resultfile)
+        for result in results['results']:
+            result['fetched'] = int(os.path.getmtime(self.results))
+            yield result
+
+    def get_xfers(self, teamid, since=0):
+        try:
+            page = max_pages = 1
+            while page <= max_pages:
+                path = f"{self.xferdir}/transfers_{teamid}_{page}.json"
+                with open(path) as xferfile:
+                    xfers = json.load(xferfile)
+                fetched = int(os.path.getmtime(path))
+                max_pages = xfers['page']['total_pages']
+
+                if not xfers['transfers']:
+                    break
+                for xfer in xfers['transfers']:
+                    xfer['fetched'] = fetched
+                    yield xfer
+                page += 1
+        except FileNotFoundError:
+            pass
+
+class ETF2LBulkFetcher:
+    def __init__(self, since=0, count=None, page=1, **kwargs):
+        self.s = create_session()
+        self.since = since
+        self.count = count
+        self.page = page
+
+    def _get_data(self, url, data_key, count=None, since=0, page=1):
+        # Number of datums yielded (up to a maximum of count)
+        yielded = 0
+        # The id we've seen. We assume new demos will all have higher demoids.
+        last_id = None
+
+        try:
+            while True:
+                fetched = int(time.time())
+                resp = self.s.get(f"{url}/{page}.json", params={
+                    'per_page': 100,
+                    'since': since,
+                })
+                resp.raise_for_status()
+
+                resp = resp.json()
+                for datum in resp[data_key] or ():
+                    datum['fetched'] = fetched
+                    yield datum
+                    yielded += 1
+                    if count is not None and yielded >= count:
+                        return
+
+                # No new data this page; give up
+                if page >= resp['page']['total_pages']:
+                    return
+
+                page += 1
+        except (OSError, urllib3.exceptions.HTTPError):
+            logging.exception("Could not fetch from %s", url)
+        except (ValueError, KeyError):
+            logging.exception("Could not parse %s", url)
+
+    def get_results(self):
+        return self._get_data("https://api.etf2l.org/results", 'results', self.count,
+                              self.since, self.page)
+
+    def get_xfers(self, teamid, since=0):
+        return self._get_data(f"https://api.etf2l.org/team/{teamid}/transfers", 'transfers',
+                              since=since)

trends/migrations/etf2l.sql
@@ -1,0 +1,10 @@
+-- SPDX-License-Identifier: AGPL-3.0-only
+-- Copyright (C) 2023 Sean Anderson <seanga2@gmail.com>
+
+ALTER TABLE player ADD eu_playerid INT UNIQUE;
+ALTER TABLE log ADD league LEAGUE;
+ALTER TABLE log ADD matchid INT;
+ALTER TABLE log ADD team1_is_red BOOL;
+ALTER TABLE log ADD FOREIGN KEY (league, matchid) REFERENCES match (league, matchid);
+ALTER TABLE log ADD CHECK (equal(league ISNULL, matchid ISNULL, team1_is_red ISNULL));
+CREATE INDEX IF NOT EXISTS log_match ON log (league, matchid);

commit b2a770f9f8a5d19fc216331042eb99daffdbddd2

importer: league: Fix missing import
The logging package is referenced but not imported.

Fixes: 481a320 ("Support importing ETF2L matches")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/importer/league.py
@@ -1,6 +1,8 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # Copyright (C) 2022 Sean Anderson <seanga2@gmail.com>
 
+import logging
+
 import psycopg2.extras
 
 def import_compdiv(c, cd):