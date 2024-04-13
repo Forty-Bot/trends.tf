Forty-Bot/trends.tf

commit e12e0435c34129a6e70f45ef2237d48b06be1bb9

importer: rgl: Parse pass time seasons
Add a mapping for pass time.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/importer/rgl.py
@@ -38,9 +38,10 @@ rgl_format_map = {
     'HL': 'highlander',
     'Highlander': 'highlander',
     'Newcomer Cup': 'sixes',
+    'PASS Time': 'other',
 }
 
-RE_NAME_FORMAT = re.compile(r"(Sixes|NR Sixes|P7|Prolander|Fresh Meat|HL|Newcomer Cup)")
+RE_NAME_FORMAT = re.compile(r"(Sixes|NR Sixes|P7|Prolander|Fresh Meat|HL|Newcomer Cup|PASS Time)")
 def parse_season(season):
     if season['formatName'] is None:
         m = RE_NAME_FORMAT.match(season['name'])

commit b0740a4ce7f654fba0c8460fb9fadd2635ddb397

importer: rgl: Make season format parsing more robust
We don't always parse RGL seasons correctly. This is due to two reasons:

- Sometimes they add new `formatName`s
- re.match only matches at the beginning of a string

Fix these issues.

Fixes: e12e043 ("importer: rgl: Parse pass time seasons")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/importer/rgl.py
@@ -46,13 +46,13 @@ rgl_format_map = {
 
 RE_NAME_FORMAT = re.compile(r"(Sixes|NR Sixes|P7|Prolander|Fresh Meat|HL|Newcomer Cup|PASS Time)")
 def parse_season(season):
-    if season['formatName'] is None:
-        m = RE_NAME_FORMAT.match(season['name'])
+    try:
+        fmt = rgl_format_map[season['formatName']]
+    except KeyError:
+        m = RE_NAME_FORMAT.search(season['name'])
         if not m:
             raise ValueError(f"Unknown format for {season['name']}")
         fmt = rgl_format_map[m.group(1)]
-    else:
-        fmt = rgl_format_map[season['formatName']]
 
     return {
         'format': fmt,