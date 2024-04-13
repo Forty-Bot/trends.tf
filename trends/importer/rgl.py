Forty-Bot/trends.tf

commit e12e0435c34129a6e70f45ef2237d48b06be1bb9

importer: rgl: Parse pass time seasons
Add a mapping for pass time.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/importer/rgl.py
@@ -38,9 +38,10 @@ rgl_format_map = {
     'HL': 'highlander',
     'Highlander': 'highlander',
     'Newcomer Cup': 'sixes',
+    'PASS Time': 'other',
 }
 
-RE_NAME_FORMAT = re.compile(r"(Sixes|NR Sixes|P7|Prolander|Fresh Meat|HL|Newcomer Cup)")
+RE_NAME_FORMAT = re.compile(r"(Sixes|NR Sixes|P7|Prolander|Fresh Meat|HL|Newcomer Cup|PASS Time)")
 def parse_season(season):
     if season['formatName'] is None:
         m = RE_NAME_FORMAT.match(season['name'])

commit fbeb0622aad38da6923ca0a862ae0aecebc37de4

importer: rgl: Change pass time format to fours
Pass time is a fours format, not other.

Fixes: e12e043 ("importer: rgl: Parse pass time seasons")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/importer/rgl.py
@@ -41,7 +41,7 @@ rgl_format_map = {
     'HL': 'highlander',
     'Highlander': 'highlander',
     'Newcomer Cup': 'sixes',
-    'PASS Time': 'other',
+    'PASS Time': 'fours',
 }
 
 RE_NAME_FORMAT = re.compile(r"(Sixes|NR Sixes|P7|Prolander|Fresh Meat|HL|Newcomer Cup|PASS Time)")

trends/migrations/rgl_pass_fours.sql
@@ -1,0 +1,4 @@
+UPDATE competition
+SET formatid = (SELECT formatid FROM format WHERE format = 'fours')
+WHERE league = 'rgl'
+	AND formatid = (SELECT formatid FROM format WHERE format = 'other');