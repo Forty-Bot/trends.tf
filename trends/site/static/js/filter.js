Forty-Bot/trends.tf

commit b00e66fd3877361804812dacdfd03ffecf115571

site: Add player filter
This adds the frontend to the player filter. We use tom-select since it has
no external dependencies, and seems fairly light. It was also (relatively)
easy to beat to my will.

For the moment, aliases and avatars are not displayed in search results.
However, I would like to add these. In addition, upon submission, the
players' names are lost.

Closes: #25
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/static/js/filter.js
@@ -3,14 +3,16 @@
 
 var maplist = document.getElementById('maps');
 
+function unwrap_json(response) {
+	if (!response.ok) {
+		return {};
+	}
+	return response.json();
+}
+
 function autocomplete_maps() {
 	fetch("/api/v1/maps")
-		.then(response => {
-			if (!response.ok) {
-				return {};
-			}
-			return response.json();
-		})
+		.then(unwrap_json)
 		.then(json => {
 			json.maps.forEach(map => {
 				option = document.createElement("option");
@@ -20,3 +22,21 @@ function autocomplete_maps() {
 		})
 }
 document.getElementById('map_input').addEventListener('focus', autocomplete_maps, {once: true });
+
+document.addEventListener('DOMContentLoaded', () => {
+	new TomSelect(document.getElementById('players_input'), {
+		plugins: ['remove_button'],
+		maxItems: 5,
+		valueField: 'steamid64',
+		labelField: 'name',
+		searchField: ['aliases', 'name'],
+		load: (query, callback) => {
+			fetch(`/api/v1/players?q=${query}`)
+				.then(unwrap_json)
+				.then(json => {
+					callback(json.players);
+				});
+		},
+		shouldLoad: query => query.length > 3,
+	});
+})

trends/site/static/css/style.css
@@ -196,3 +196,12 @@ input:disabled {
 form {
 	padding: 3px;
 }
+
+.ts-wrapper {
+	display: inline;
+}
+
+.ts-control {
+	display: inline !important;
+	padding: 8px !important;
+}

trends/site/templates/base.html
@@ -10,6 +10,10 @@
 	<title>{% block title %}trends.tf{% endblock %}</title>
 	<script type="text/javascript"
 		src="{{ url_for('static', filename="js/dates.js") }}" defer></script>
+	<script type="module"
+	 src="https://cdn.jsdelivr.net/npm/tom-select@2.0.0/dist/js/tom-select.popular.min.js"></script>
+	<link rel="stylesheet" type="text/css"
+	 href="https://cdn.jsdelivr.net/npm/tom-select@2.0.0/dist/css/tom-select.min.css">
 	<script type="text/javascript"
 		src="{{ url_for('static', filename="js/filter.js") }}" defer></script>
 	<link rel="icon" href="{{ url_for('static', filename="img/favicon.ico") }}">

trends/site/templates/macros/filter.html
@@ -16,10 +16,6 @@
 		<input type="text" name="title"
 		       value="{{ (g.filters['title'] or "") | trim("%") }}">
 	</label>
-	<label><abbr title="SteamID64 of a player">Player</abbr>:
-		<input type="text" name="steamid64" pattern="\d+"
-		       value="{{ g.filters['players'] | first or "" }}">
-	</label>
 	{% else %}
 	<label>
 		<abbr title="Games with at least &frac23; time on this class">Class</abbr>:
@@ -43,8 +39,12 @@
 	</label>
 	<label>Map:
 		<input id="map_input" type="text" name="map" list="maps"
-		       value="{{ (g.filters['map'] or "") | trim("%") }}">
+		 value="{{ (g.filters['map'] or "") | trim("%") }}">
 		<datalist id="maps"></datalist>
+	</label>
+	<label>Players:
+		<input id="players_input" type="text" name="steamid64" multiple
+		 value="{{ g.filters['players'] | join(',') }}">
 	</label>
 	{% if date %}
 	<input id="timezone" type="hidden" name="timezone">

trends/site/util.py
@@ -4,6 +4,7 @@
 from collections import namedtuple
 from datetime import datetime, timedelta
 from dateutil import tz
+from itertools import islice
 import sys
 
 import flask
@@ -38,7 +39,13 @@ def get_filter_params():
 
     params['class'] = args.get('class', type=str)
     params['format'] = args.get('format', type=str)
-    params['players'] = args.getlist('steamid64', type=int)
+    if val := args.get('steamid64', type=str):
+        try:
+            params['players'] = [int(player) for player in islice(val.split(','), 5)]
+        except ValueError:
+            params['players'] = []
+    else:
+        params['players'] = []
 
     def set_like_param(name):
         if val := args.get(name, type=str):

commit 6750951e73b1a92483230ab275006bcfdb43a470

site: js: filter: Handle missing ids
Document.getElementById returns null when no matching id is present.
Check for this and avoid trying to use the result.

Fixes: b00e66f ("site: Add player filter")
Fixes: 7a5812d ("Add autocompletion for map names")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/static/js/filter.js
@@ -9,7 +9,7 @@ function unwrap_json(response) {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-	document.getElementById('map_input').addEventListener('focus', () => {
+	document.getElementById('map_input')?.addEventListener('focus', () => {
 		let maplist = document.getElementById('maps');
 
 		fetch("/api/v1/maps").then(unwrap_json).then(json => {
@@ -38,23 +38,26 @@ function render_option(data, escape) {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-	new TomSelect(document.getElementById('players_input'), {
-		plugins: ['remove_button'],
-		maxItems: 5,
-		valueField: 'steamid64',
-		labelField: 'name',
-		searchField: ['aliases', 'name'],
-		load: (query, callback) => {
-			fetch(`/api/v1/players?q=${query}`)
-				.then(unwrap_json)
-				.then(json => {
-					callback(json.players);
-				});
-		},
-		shouldLoad: query => query.length > 3,
-		render: {
-			item: render_option,
-			option: render_option,
-		},
-	});
+	input = document.getElementById('players_input');
+	if (input != null) {
+		new TomSelect(document.getElementById('players_input'), {
+			plugins: ['remove_button'],
+			maxItems: 5,
+			valueField: 'steamid64',
+			labelField: 'name',
+			searchField: ['aliases', 'name'],
+			load: (query, callback) => {
+				fetch(`/api/v1/players?q=${query}`)
+					.then(unwrap_json)
+					.then(json => {
+						callback(json.players);
+					});
+			},
+			shouldLoad: query => query.length > 3,
+			render: {
+				item: render_option,
+				option: render_option,
+			},
+		});
+	}
 })