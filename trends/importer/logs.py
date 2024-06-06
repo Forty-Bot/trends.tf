Forty-Bot/trends.tf

commit 7934a3cd9a0bc9baca7e18ce8862bfc43b27271a

Add support for killstreaks
Add support for parsing killstreaks from logs. These can be useful for
finding clips for frag movies. I didn't add them to the totals page
since as an extrema-based statistic, the only thing we can really show
is max/count. I think those are better shown on the logs page.

Closes: #75
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/importer/logs.py
@@ -357,6 +357,24 @@ def import_log(c, logid, log):
                                  %(dmg)s, %(avg_dmg)s, %(shots)s, %(hits)s
                              );""", weapon)
 
+    for killstreak in log.get('killstreaks', ()):
+        try:
+            steamid = SteamID(killstreak['steamid'])
+        except ValueError:
+            continue
+
+        try:
+            c.execute("SAVEPOINT before_killstreak;")
+            c.execute("""INSERT INTO killstreak (
+                             logid, playerid, time, kills
+                         ) VALUES (
+                            %s, (SELECT playerid FROM player WHERE steamid64 = %s), %s, %s
+                         ) ON CONFLICT DO NOTHING;""",
+                      (logid, steamid, killstreak['time'], killstreak['streak']))
+        except (psycopg2.errors.ForeignKeyViolation, psycopg2.errors.NotNullViolation):
+            logging.warning("%s is only present in killstreak for log %s", steamid, logid)
+            c.execute("ROLLBACK TO SAVEPOINT before_killstreak;")
+
     for (seq, msg) in enumerate(log['chat']):
         try:
             steamid = SteamID(msg['steamid']) if msg['steamid'] != 'Console' else None
@@ -784,6 +802,9 @@ def import_logs(c, fetcher, update_only):
                            PRIMARY KEY ({})
                        );""".format(table[0], table[0], table[1]))
     # This doesn't include foreign keys, so include some which we want to handle in import_log
+    cur.execute("""ALTER TABLE killstreak
+                   ADD FOREIGN KEY (logid, playerid)
+                       REFERENCES player_stats_backing (logid, playerid);""")
     cur.execute("""ALTER TABLE heal_stats
                    ADD FOREIGN KEY (logid, healer)
                        REFERENCES player_stats_backing (logid, playerid),

trends/schema.sql
@@ -608,6 +608,15 @@ CREATE TABLE IF NOT EXISTS player_stats_extra (
 	CHECK ((dmg_real ISNULL) = (dt_real ISNULL))
 );
 
+CREATE TABLE IF NOT EXISTS killstreak (
+	logid INT NOT NULL,
+	playerid INT NOT NULL,
+	time INT NOT NULL,
+	kills INT NOT NULL CHECK (kills > 0),
+	PRIMARY KEY (playerid, logid, time),
+	FOREIGN KEY (logid, playerid) REFERENCES player_stats_backing (logid, playerid)
+);
+
 CREATE TABLE IF NOT EXISTS medic_stats (
 	logid INT NOT NULL,
 	playerid INT NOT NULL,

trends/site/player.py
@@ -85,6 +85,8 @@ log_extra_order_map = {
     'headshots': "headshots",
     'headshots_hit': "headshots_hit",
     'sentries': "sentries",
+    # Not in extra but close enough...
+    'mks': "mks",
 }
 
 # Columns not in player_stats
@@ -175,6 +177,13 @@ def get_logs(c, playerid, filters, extra=False, order_clause="logid DESC", limit
            LEFT JOIN heal_stats_given AS hsg USING (logid, playerid)
            LEFT JOIN heal_stats_received AS hsr USING (logid, playerid)
            {"LEFT JOIN player_stats_extra AS pse USING (logid, playerid)" if extra else ""}
+           {'''LEFT JOIN (SELECT
+                       logid,
+                       playerid,
+                       max(kills) AS mks
+                   FROM killstreak
+                   GROUP BY logid, playerid
+               ) AS ks USING (logid, playerid)''' if extra else ""}
            WHERE ps.playerid = %(playerid)s
            ORDER BY {order_clause} NULLS LAST, logid DESC
            LIMIT %(limit)s OFFSET %(offset)s;""",

cluster.sql
@@ -7,6 +7,7 @@
 
 CLUSTER VERBOSE player_stats_backing USING player_stats_pkey;
 CLUSTER VERBOSE player_stats_extra USING player_stats_extra_pkey;
+CLUSTER VERBOSE killstreak USING killstreak_pkey;
 CLUSTER VERBOSE medic_stats USING medic_stats_pkey;
 CLUSTER VERBOSE class_stats USING class_stats_pkey;
 CLUSTER VERBOSE weapon_stats USING weapon_stats_pkey;

test/create.py
@@ -43,6 +43,7 @@ def create_test_db(url):
                 30099,
                 2297197,
                 2297225,
+                2297265,
                 2344272,
                 2344306,
                 2344331,

test/site_test.py
@@ -200,6 +200,9 @@ linked_demos = {
 }
 
 linked_matches = {
+    2297197: ('rgl', 3009),
+    2297225: ('rgl', 3009),
+    2297265: ('rgl', 3009),
     2344272: ('rgl', 3412),
     2344306: ('rgl', 3412),
     2344331: ('rgl', 3412),
@@ -223,6 +226,7 @@ def test_linked(connection):
     assert { logid: (league, matchid) for logid, league, matchid in cur } == linked_matches
 
 duplicates = {
+    2297265: [2297197],
     2344394: [2344272, 2344306, 2344354],
 }
 

trends/migrations/killstreak.py
@@ -1,0 +1,76 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+# Copyright (C) 2024 Sean Anderson <seanga2@gmail.com>
+
+import logging
+import sys
+
+import psycopg2.extras
+
+from ..importer.cli import init_logging
+from ..sql import db_connect
+from ..steamid import SteamID
+from ..util import chunk
+
+def migrate(database):
+    init_logging(logging.DEBUG)
+    with db_connect(database) as c:
+        with c.cursor() as cur:
+            logging.info("BEGIN")
+            cur.execute("BEGIN");
+            logging.info("CREATE TABLE killstreak")
+            cur.execute("""
+                CREATE TABLE killstreak (
+                    logid INT NOT NULL,
+                    playerid INT NOT NULL,
+                    time INT NOT NULL,
+                    kills INT NOT NULL CHECK (kills > 0),
+                    PRIMARY KEY (playerid, logid, time)
+                );""")
+
+            with c.cursor(name='streaks') as streaks:
+                streaks.execute("""
+                    SELECT
+                       logid,
+                       streak -> 'steamid' AS steamid,
+                       streak -> 'time' AS time,
+                       streak -> 'streak' AS kills
+                    FROM (SELECT
+                           logid,
+                           json_array_elements(data -> 'killstreaks') AS streak
+                        FROM log_json
+                    ) AS killstreak;""")
+                for streaks in chunk(streaks, streaks.itersize):
+                    values = []
+                    for streak in streaks:
+                        try:
+                            values.append((streak[0], SteamID(streak[1]), streak[2], streak[3]))
+                        except ValueError:
+                            continue
+                    logging.info("INSERT killstreak")
+                    psycopg2.extras.execute_values(cur, """
+                        INSERT INTO killstreak (
+                            logid,
+                            playerid,
+                            time,
+                            kills
+                        ) SELECT
+                            logid,
+                            playerid,
+                            time,
+                            killstreak.kills
+                        FROM (VALUES %s) AS killstreak (logid, steamid64, time, kills)
+                        JOIN player USING (steamid64)
+                        JOIN player_stats USING (logid, playerid)
+                        ON CONFLICT DO NOTHING;""",
+                        values, "(%s, %s, %s, %s)")
+            
+            logging.info("ALTER TABLE")
+            cur.execute("""
+                ALTER TABLE killstreak
+                ADD FOREIGN KEY (logid, playerid)
+                REFERENCES player_stats_backing (logid, playerid);""")
+            logging.info("COMMIT")
+            cur.execute("COMMIT;")
+
+if __name__ == "__main__":
+    migrate(sys.argv[1])

trends/site/root.py
@@ -481,6 +481,35 @@ def log(logids):
                       GROUP BY event;""", params)
     events = { event_stats['event']: event_stats['events'] for event_stats in events.fetchall() }
 
+    killstreaks = db.cursor()
+    killstreaks.execute("""SELECT
+                               logid,
+                               title,
+                               array_agg(json_build_object(
+                                   'team', team,
+                                   'steamid64', steamid64,
+                                   'name', name,
+                                   'time', killstreak.time,
+                                   'kills', kills
+                               ) ORDER BY killstreak.time) AS killstreaks
+                           FROM (SELECT
+                                   logid,
+                                   playerid,
+                                   team,
+                                   name,
+                                   time,
+                                   killstreak.kills
+                               FROM killstreak
+                               JOIN player_stats USING (logid, playerid)
+                               JOIN name USING (nameid)
+                               WHERE logid IN %(logids)s
+                           ) AS killstreak
+                           JOIN log USING (logid)
+                           JOIN player USING (playerid)
+                           GROUP BY logid, title
+                           ORDER BY array_position(%(llogids)s, logid);""", params)
+    killstreaks = killstreaks.fetchall()
+
     chats = db.cursor()
     chats.execute("""SELECT
                         logid,
@@ -510,7 +539,8 @@ def log(logids):
 
     return flask.render_template("log.html", logids=logids, logs=logs, matches=matches,
                                  rounds=rounds.fetchall(), players=players, totals=totals,
-                                 medics=medics, events=events, chats=chats)
+                                 medics=medics, events=events, killstreaks=killstreaks,
+                                 chats=chats)
 
 metrics_extension = PrometheusMetrics.for_app_factory(group_by='endpoint', path=None)
 

commit 494cda5baefaa1723f6364e9f0df5fa76cf8dc17

Denormalize mks into player_stats_extra
Store max(kills) in player_stats_extra to eliminate a join on the logs
page. Postgres once again is completely incapable of estimating the
cardinality of joins on composite primary keys.

Fixes: 7934a3c ("Add support for killstreaks")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/importer/logs.py
@@ -734,6 +734,19 @@ def update_acc(cur, bounds=None):
                  .format("WHERE logid BETWEEN %s AND %s" if bounds else ""),
               bounds)
 
+def update_ks(cur):
+    cur.execute("""UPDATE player_stats_extra AS pse SET
+                       mks = new.mks
+                   FROM (SELECT
+                           logid,
+                           playerid,
+                           max(kills) AS mks
+                       FROM killstreak
+                       GROUP BY logid, playerid
+                   ) AS new
+                   WHERE pse.logid = new.logid
+                       AND pse.playerid = new.playerid;""")
+
 def create_logs_parser(sub):
     class LogAction(argparse.Action):
         def __init__(self, option_strings, dest, **kwargs):
@@ -837,6 +850,7 @@ def import_logs(c, fetcher, update_only):
             update_wlt(cur)
             update_player_classes(cur)
             update_acc(cur)
+            update_ks(cur)
             publicize(c, log_tables)
             cur.execute("COMMIT;")
             logging.info("Committed %s imported log(s)...", count)

trends/migrations/mks.sql
@@ -1,0 +1,19 @@
+BEGIN;
+ALTER TABLE player_stats_extra ADD mks INT;
+CREATE TEMP TABLE new AS
+SELECT
+	logid,
+	playerid,
+	max(kills) AS mks
+FROM killstreak
+GROUP BY logid, playerid
+ORDER BY playerid, logid;
+-- Force a merge join
+SET enable_hashjoin = FALSE;
+UPDATE player_stats_extra AS pse SET
+	mks = new.mks
+FROM new
+WHERE pse.logid = new.logid
+	AND pse.playerid = new.playerid;
+SET enable_hashjoin = FALSE;
+COMMIT;

trends/schema.sql
@@ -603,6 +603,7 @@ CREATE TABLE IF NOT EXISTS player_stats_extra (
 	healing INT,
 	cpc INT, -- Capture Point Captures
 	ic INT, -- Intel Captures
+	mks INT, -- Max killstreaks, computed from killstreak
 	PRIMARY KEY (playerid, logid),
 	FOREIGN KEY (logid, playerid) REFERENCES player_stats_backing (logid, playerid),
 	CHECK ((dmg_real ISNULL) = (dt_real ISNULL))

trends/site/player.py
@@ -85,7 +85,6 @@ log_extra_order_map = {
     'headshots': "headshots",
     'headshots_hit': "headshots_hit",
     'sentries': "sentries",
-    # Not in extra but close enough...
     'mks': "mks",
 }
 
@@ -177,13 +176,6 @@ def get_logs(c, playerid, filters, extra=False, order_clause="logid DESC", limit
            LEFT JOIN heal_stats_given AS hsg USING (logid, playerid)
            LEFT JOIN heal_stats_received AS hsr USING (logid, playerid)
            {"LEFT JOIN player_stats_extra AS pse USING (logid, playerid)" if extra else ""}
-           {'''LEFT JOIN (SELECT
-                       logid,
-                       playerid,
-                       max(kills) AS mks
-                   FROM killstreak
-                   GROUP BY logid, playerid
-               ) AS ks USING (logid, playerid)''' if extra else ""}
            WHERE ps.playerid = %(playerid)s
            ORDER BY {order_clause} NULLS LAST, logid DESC
            LIMIT %(limit)s OFFSET %(offset)s;""",