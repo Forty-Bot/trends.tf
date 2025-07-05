Forty-Bot/trends.tf

commit 4c6d6e8be4622167277ea80f6413fe39deae9017

site: Trace cache operations
Trace memcache gets and sets and move the clients into their own file.
This should provide better observability into cache operations.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/cache.py
@@ -1,0 +1,67 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+# Copyright (C) 2020 Sean Anderson <seanga2@gmail.com>
+
+import contextlib
+
+import pylibmc
+import sentry_sdk
+
+class NoopClient:
+    def get(self, key, default=None):
+        return default
+
+    def get_multi(self, keys):
+        return {}
+
+    def set(self, key, value, *args, **kwargs):
+        return False
+
+    def set_multi(self, mapping, *args, **kwargs):
+        return list(mapping.keys())
+
+    def add(self, key, value, *args, **kwargs):
+        return True
+
+    def replace(self, key, value, *args, **kwargs):
+        return False
+
+@contextlib.contextmanager
+def cache_span(op, key):
+    sentry_sdk.add_breadcrumb(type='query', category=op, message=key)
+    with sentry_sdk.start_span(op=op) as span:
+        span.set_data('cache.key', key)
+        yield span
+
+class TracingClient(pylibmc.Client):
+    def get(self, key, default=None):
+        with cache_span('cache.get', key) as span:
+            value = super().get(key, None)
+            if value is None:
+                span.set_data('cache.hit', False)
+                return default
+            else:
+                span.set_data('cache.hit', True)
+                return value
+
+    def get_multi(self, keys):
+        keys = list(keys)
+        with sentry_sdk.start_span('cache.get', keys) as span:
+            values = super().get_multi(keys)
+            span.set_data('cache.hit', bool(values))
+            return values
+
+    def set(self, key, value, *args, **kwargs):
+        with cache_span('cache.set', key):
+            return super().set(key, None, *args, **kwargs)
+
+    def set_multi(self, mapping, *args, **kwargs):
+        with cache_span('cache.set', list(mapping)) as span:
+            return super().set_multi(mapping, *args, **kwargs)
+
+    def add(self, key, value, *args, **kwargs):
+        with cache_span('cache.set', key):
+            return super().add(key, None, *args, **kwargs)
+
+    def replace(self, key, value, *args, **kwargs):
+        with cache_span('cache.set', key):
+            return super().replace(key, None, *args, **kwargs)

trends/site/util.py
@@ -14,8 +14,9 @@ from psycopg2.extras import NumericRange
 import pylibmc
 import werkzeug.exceptions, werkzeug.http
 
-from ..util import clamp
+from ..cache import NoopClient, TracingClient
 from ..sql import db_connect
+from ..util import clamp
 
 def last_modified(since):
     if flask.current_app.debug:
@@ -72,19 +73,12 @@ def view_updated(view, pretty=None):
         if row[0] is not None:
             return last_modified(row[0])
 
-class NoopClient:
-    def get(self, key, default=None):
-        return default
-
-    def set(self, key, value, **kwargs):
-        pass
-
 @global_context('mc_conn')
 def get_mc():
     servers = flask.current_app.config['MEMCACHED_SERVERS']
     if servers:
         try:
-            mc = pylibmc.Client(servers.split(','), binary=True)
+            mc = TracingClient(servers.split(','), binary=True)
             # pylibmc doesn't actually connect until we make a request. Force a connection failure
             # up front so we can log it and use a fallback client
             mc.get('connection_test')

commit cf694ec4a54d9b854d8a4c0e7cc8807d4bb728f8

cache: Fix get_multi span
I forgot to convert this span to use cache_span instead of sentry_sdk's
span. Fix this.

Fixes: 4c6d6e8 ("site: Trace cache operations")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/cache.py
@@ -54,7 +54,7 @@ class TracingClient(pylibmc.Client):
 
     def get_multi(self, keys):
         keys = list(keys)
-        with sentry_sdk.start_span('cache.get', 'get_multi', keys) as span:
+        with cache_span('cache.get', 'get_multi', keys) as span:
             values = super().get_multi(keys)
             span.set_data('cache.hit', bool(values))
             return values