Forty-Bot/trends.tf

commit c48c7186a0960d8b8ac1ab22d0fd44eb0975e118

importer: Purge player overview cache when updating players
Purge the player overview cache whenever the backing data is updated. This
process takes several steps:

1. Update the backing data and add updated rows to cache_purge_player.
2. Commit the transaction. At this point, the new data is visible to the
   frontend, but there may be outstanding stale updates still in progress.
3. Select some players to purge and delete them from the cache. This step
   may be repeated if the final step fails, but this just results in
   excessive purging and not stale data.
4. Delete those players from cache_purge_player.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/cache.py
@@ -33,6 +33,12 @@ class NoopClient:
     def cas(self, key, value, cas, time=0):
         raise pylibmc.NotFound
 
+    def delete(self, key):
+        return False
+
+    def delete_multi(self, keys):
+        return False
+
 @contextlib.contextmanager
 def cache_span(category, op, key):
     sentry_sdk.add_breadcrumb(type='query', category=category, message=key)
@@ -89,7 +95,29 @@ class TracingClient(pylibmc.Client):
         with cache_span('cache.set', 'cas', key):
             return super().cas(key, value, cas, time)
 
+    def delete(self, key):
+        with cache_span('cache.delete', 'delete', key):
+            return super().delete(key)
+
+    # delete_multi just calls delete repeatedly
+
 def mc_connect(servers):
     if servers:
         return TracingClient(servers.split(','), binary=True, behaviors={ 'cas': True })
     return NoopClient()
+
+def purge_players(c, mc):
+    with (sentry_sdk.start_span(op='db.transaction', description="purge"), c.cursor() as cur):
+        while True:
+            cur.execute("BEGIN;")
+            cur.execute("""SELECT steamid64
+                           FROM cache_purge_player
+                           FOR UPDATE SKIP LOCKED
+                           LIMIT 1000;""")
+            players = tuple(row[0] for row in cur)
+            if not len(players):
+                return
+
+            mc.delete_multi(f"overview_{steamid}" for steamid in players)
+            cur.execute("DELETE FROM cache_purge_player WHERE steamid64 IN %s;", (players,))
+            cur.execute("COMMIT;")

test/create.py
@@ -73,7 +73,7 @@ def create_test_db(url, memcached):
                 3384488,
             )
         }
-        trends.importer.logs.import_logs(c, FileFetcher(logs=logfiles), False)
+        trends.importer.logs.import_logs(c, mc, FileFetcher(logs=logfiles), False)
 
     with db_connect(url) as c:
         demofiles = (f"{os.path.dirname(__file__)}/demos/demo_{demoid}.json" for demoid in (
@@ -100,7 +100,7 @@ def create_test_db(url, memcached):
     with db_connect(url) as c:
         fetcher = ETF2LFileFetcher(results=f"{os.path.dirname(__file__)}/etf2l/results.json",
                                    xferdir=f"{os.path.dirname(__file__)}/etf2l/")
-        trends.importer.etf2l.import_etf2l(c, fetcher)
+        trends.importer.etf2l.import_etf2l(c, mc, fetcher)
 
     with db_connect(url) as c:
         dir = f"{os.path.dirname(__file__)}/rgl"

trends/importer/etf2l.py
@@ -11,6 +11,7 @@ import time
 from psycopg2.extras import NumericRange
 import sentry_sdk
 
+from ..cache import purge_players
 from .fetch import ETF2LFileFetcher, ETF2LBulkFetcher, FetchError
 from .league import *
 from ..sql import db_connect
@@ -155,10 +156,9 @@ def import_etf2l_cli(args, c, mc):
                        FROM match
                        WHERE league = 'etf2l';""");
                 args.since = datetime.fromtimestamp(cur.fetchone()[0])
-        return import_etf2l(c, args.fetcher(**vars(args)))
+        return import_etf2l(c, mc, args.fetcher(**vars(args)))
 
-
-def import_etf2l(c, fetcher):
+def import_etf2l(c, mc, fetcher):
     cur = c.cursor()
     count = 0
     for result in filter_matchids(c, fetcher.get_results()):
@@ -202,6 +202,7 @@ def import_etf2l(c, fetcher):
                     import_team(cur, team)
                 import_match(cur, res)
                 cur.execute("COMMIT;")
+            purge_players(c, mc)
         except FetchError:
             continue
         except (IndexError, KeyError, psycopg2.errors.UniqueViolation):

trends/importer/league.py
@@ -174,15 +174,17 @@ def import_transfers(c, t):
                AND np2.id < np1.id""")
     c.execute("INSERT INTO name (name) SELECT name FROM new_player ON CONFLICT DO NOTHING;")
     c.execute(
-        """INSERT INTO player (steamid64, nameid, avatarhash, eu_playerid) SELECT
-               steamid64,
-               (SELECT nameid FROM name WHERE name = new_player.name),
-               avatarhash,
-               eu_playerid
-           FROM new_player
-           ON CONFLICT (steamid64)
-           DO UPDATE SET
-               eu_playerid = coalesce(EXCLUDED.eu_playerid, player.eu_playerid);""")
+        """WITH player AS (INSERT INTO player (steamid64, nameid, avatarhash, eu_playerid) SELECT
+                   steamid64,
+                   (SELECT nameid FROM name WHERE name = new_player.name),
+                   avatarhash,
+                   eu_playerid
+               FROM new_player
+               ON CONFLICT (steamid64)
+               DO UPDATE SET
+                   eu_playerid = coalesce(EXCLUDED.eu_playerid, player.eu_playerid)
+               RETURNING steamid64
+           ) INSERT INTO cache_purge_player SELECT steamid64 FROM player;""")
     c.execute("DROP TABLE new_player;")
 
     # OK, here's the dance:

trends/importer/link_matches.py
@@ -4,6 +4,8 @@
 from datetime import datetime, timedelta
 import logging
 
+from ..cache import purge_players
+
 def create_link_matches_parser(sub):
     link = sub.add_parser("link_matches", help="Link logs and matches")
     link.set_defaults(importer=link_matches)
@@ -90,5 +92,12 @@ def link_matches(args, c, mc):
                            team1_is_red = log_matches.team1_is_red
                        FROM log_matches
                        WHERE log.logid = log_matches.logid;""")
+        cur.execute("""INSERT INTO cache_purge_player (steamid64)
+                       SELECT steamid64
+                       FROM log_matches
+                       JOIN player_stats USING (logid)
+                       JOIN player USING (playerid)
+                       GROUP BY steamid64;""")
         cur.execute("COMMIT;")
         logging.info(f"Linked {count} logs")
+        purge_players(c, mc)

trends/importer/logs.py
@@ -10,6 +10,7 @@ import psycopg2
 import sentry_sdk
 import systemd_watchdog
 
+from ..cache import purge_players
 from .fetch import ListFetcher, BulkFetcher, FileFetcher, ReverseFetcher, CloneLogsFetcher
 from ..steamid import SteamID
 from ..sql import disable_tracing, delete_logs, log_tables, publicize, table_columns
@@ -747,6 +748,13 @@ def update_ks(cur):
                    WHERE pse.logid = new.logid
                        AND pse.playerid = new.playerid;""")
 
+def prepare_purge(cur):
+    cur.execute("""INSERT INTO cache_purge_player (steamid64)
+                   SELECT steamid64
+                   FROM player_stats_backing
+                   JOIN player USING (playerid)
+                   GROUP BY steamid64;""")
+
 def create_logs_parser(sub):
     class LogAction(argparse.Action):
         def __init__(self, option_strings, dest, **kwargs):
@@ -801,9 +809,9 @@ def create_logs_parser(sub):
 
 def import_logs_cli(args, c, mc):
     with sentry_sdk.start_transaction(op="import", name="logs"):
-        return import_logs(c, args.fetcher(**vars(args)), args.update_only)
+        return import_logs(c, mc, args.fetcher(**vars(args)), args.update_only)
 
-def import_logs(c, fetcher, update_only):
+def import_logs(c, mc, fetcher, update_only):
     cur = c.cursor()
     wd = systemd_watchdog.watchdog()
 
@@ -852,8 +860,10 @@ def import_logs(c, fetcher, update_only):
             update_acc(cur)
             update_ks(cur)
             publicize(c, log_tables)
+            prepare_purge(cur)
             cur.execute("COMMIT;")
             logging.info("Committed %s imported log(s)...", count)
+        purge_players(c, mc)
 
     count = 0
     start = datetime.now()

trends/importer/players.py
@@ -9,6 +9,8 @@ import requests, requests.adapters
 import psycopg2, psycopg2.extras
 import urllib3.util
 
+from ..cache import purge_players
+
 def get_steamids_full(c):
     last_steamid = 0
     cur = c.cursor()
@@ -81,13 +83,21 @@ def import_players(args, c, mc):
                                name
                            FROM player_update
                            ON CONFLICT DO NOTHING;""")
-            cur.execute("""UPDATE player
-                           SET
-                              nameid = (SELECT nameid FROM name WHERE name = player_update.name),
-                              avatarhash = player_update.avatarhash
-                           FROM player_update
-                           WHERE player_update.steamid64::BIGINT = player.steamid64;""")
+            cur.execute("""WITH player AS (UPDATE player
+                               SET
+                                  nameid = (SELECT nameid
+                                      FROM name
+                                      WHERE name = player_update.name
+                                  ),
+                                  avatarhash = player_update.avatarhash
+                               FROM player_update
+                               WHERE player_update.steamid64::BIGINT = player.steamid64
+                               RETURNING steamid64
+                           ) INSERT INTO cache_purge_player (steamid64)
+                           SELECT steamid64
+                           FROM player;""")
             cur.execute("COMMIT;")
+            purge_players(c, mc)
             successes += 1
             logging.info("ok")
         except requests.exceptions.HTTPError as e:

trends/schema.sql
@@ -942,3 +942,9 @@ INSERT INTO materialized_view (oid) VALUES
 	('medic_cube'::REGCLASS),
 	('map_popularity'::REGCLASS)
 ON CONFLICT DO NOTHING;
+
+CREATE TABLE IF NOT EXISTS cache_purge_player (
+	steamid64 BIGINT NOT NULL
+);
+
+CREATE INDEX IF NOT EXISTS cache_purge_player_pkey ON cache_purge_player (steamid64);

commit 3ca67acb81314ec6b5344ff75a51516508dcaf3f

cache: Use a sentinel for objects about to be cached
With the existing protocol, stale objects could remain in the cache if they
were being computed while an update was in progress:

importer            wsgi
=================== ===================
                    cache.get()
                    cur.execute(SELECT)
cur.execute(UPDATE)
cache.delete()
                    cache.put()

prevent this by inserting a sentinel object before filling the cache. If
the sentinel is absent after we've generated the object, then the database
has been updated in the meantime. Therefore we only update the cache if the
sentinel remains unchanged.

While we're at it, refactor the caching logic into a context manager to
make it easier to reuse, and move the tests to their own file.

Fixes: c48c718 ("importer: Purge player overview cache when updating players")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/cache.py
@@ -2,6 +2,8 @@
 # Copyright (C) 2020, 25 Sean Anderson <seanga2@gmail.com>
 
 import contextlib
+from functools import wraps
+import logging
 
 import pylibmc
 import sentry_sdk
@@ -99,23 +101,58 @@ class TracingClient(pylibmc.Client):
 
     # delete_multi just calls delete repeatedly
 
+    def flush_all(self):
+        sentry_sdk.add_breadcrumb(type='query', category='cache.clear')
+        with sentry_sdk.start_span(op='cache.clear') as span:
+            return super().flush_all()
+
 def mc_connect(servers):
     if servers:
         return TracingClient(servers.split(','), binary=True, behaviors={ 'cas': True })
     return NoopClient()
 
-def purge_players(c, mc):
+def cache_result(key_template, timeout=120, expire=86400):
+    def decorator(f):
+        @wraps(f)
+        def wrapper(mc, *args, **kwargs):
+            key = key_template.format(*args, **kwargs)
+            val, cas = mc.gets(key)
+            if val is not None:
+                return val
+
+            if cas is None:
+                # Add a dummy value so we can delete it if we have to purge
+                mc.add(key, None, time=timeout)
+
+                val, cas = mc.gets(key)
+                # Did someone else fill the cache in the meantime?
+                if val is not None:
+                    return val
+
+            val = f(mc, *args, **kwargs)
+            try:
+                if cas is not None:
+                    mc.cas(key, val, cas, time=expire)
+            except pylibmc.NotFound:
+                pass
+            return val
+        return wrapper
+    return decorator
+
+def purge(c, mc, col, table, keys):
     with (sentry_sdk.start_span(op='db.transaction', description="purge"), c.cursor() as cur):
         while True:
             cur.execute("BEGIN;")
-            cur.execute("""SELECT steamid64
-                           FROM cache_purge_player
-                           FOR UPDATE SKIP LOCKED
-                           LIMIT 1000;""")
-            players = tuple(row[0] for row in cur)
-            if not len(players):
+            cur.execute(f"SELECT {col} FROM {table} FOR UPDATE SKIP LOCKED LIMIT 1000;")
+            vals = tuple(row[0] for row in cur)
+            if not len(vals):
                 return
 
-            mc.delete_multi(f"overview_{steamid}" for steamid in players)
-            cur.execute("DELETE FROM cache_purge_player WHERE steamid64 IN %s;", (players,))
+            for key in keys:
+                mc.delete_multi(key.format(val) for val in vals)
+            cur.execute(f"DELETE FROM {table} WHERE {col} IN %s;", (vals,))
             cur.execute("COMMIT;")
+            logging.info("Purged %s value(s) from %s", len(vals), table)
+
+def purge_players(c, mc):
+    purge(c, mc, 'steamid64', 'cache_purge_player', ("overview_{}",))

trends/site/player.py
@@ -9,6 +9,7 @@ import pylibmc
 from .util import get_db, get_mc, get_filter_params, get_filter_clauses, get_order, \
                   get_pagination, last_modified
 from ..util import clamp, classes
+from ..cache import cache_result
 
 player = flask.Blueprint('player', __name__)
 
@@ -16,40 +17,23 @@ player = flask.Blueprint('player', __name__)
 def get_player(endpoint, values):
     flask.g.steamid = values['steamid']
 
-@player.before_request
-def get_overview():
+@cache_result("overview_{}")
+def _get_overview(mc, steamid64):
     cur = get_db().cursor()
-    cur.execute("SELECT playerid, eu_playerid, last_active FROM player WHERE steamid64 = %s;",
-                (flask.g.steamid,))
-    for flask.g.playerid, flask.g.etf2lid, last_active in cur:
-        if not last_active:
-            flask.abort(404)
-
-        # FIXME: this is not really accurate...
-        if resp := last_modified(last_active):
-            return resp
-        break
-    else:
-        flask.abort(404)
-
-    mc = get_mc()
-    key = "overview_{}".format(flask.g.steamid)
-    player_overview, cas = mc.gets(key)
-    if player_overview and player_overview['last_active'] == last_active:
-        flask.g.player = player_overview
-        return
-
     cur.execute(
         """SELECT
-               *,
+               playerid,
+               eu_playerid,
+               last_active,
                name,
                avatarhash,
+               overview.*,
                (wins + 0.5 * ties) /
                    (wins + losses + ties) AS winrate,
                (round_wins + 0.5 * round_ties) /
                    (round_wins + round_losses + round_ties) AS round_winrate
-           FROM (
-                SELECT
+           FROM player
+           CROSS JOIN LATERAL (SELECT
                     count(*) AS logs,
                     sum(wins) AS round_wins,
                     sum(losses) AS round_losses,
@@ -58,24 +42,25 @@ def get_overview():
                     sum((wins < losses)::INT) AS losses,
                     sum((wins = losses)::INT) AS ties
                 FROM player_stats
-                WHERE playerid = %(playerid)s
+                WHERE playerid = player.playerid
            ) AS overview
-           JOIN player ON (logs != 0)
            JOIN name USING (nameid)
-           WHERE playerid = %(playerid)s;""", { 'playerid': flask.g.playerid })
+           WHERE logs != 0 AND steamid64 = %s;""", (steamid64,))
 
-    for row in cur:
-        flask.g.player = row
-        if cas is None:
-            mc.add(key, row, time=86400)
-        else:
-            try:
-                mc.cas(key, row, cas, time=86400)
-            except pylibmc.NotFound:
-                pass
-        break
-    else:
+    row = cur.fetchone()
+    if row is None:
         flask.abort(404)
+    return row
+
+@player.before_request
+def get_overview():
+    overview = _get_overview(get_mc(), flask.g.steamid)
+    if resp := last_modified(overview['last_active']):
+        return resp
+
+    flask.g.player = overview
+    flask.g.playerid = overview['playerid']
+    flask.g.etf2lid = overview['eu_playerid']
 
 # The base set of column filters for most queries in this file
 base_filter_columns = frozenset({'league', 'formatid', 'title', 'mapid', 'time', 'logid'})

test/cache_test.py
@@ -1,0 +1,108 @@
+import collections
+
+import pylibmc
+import pytest
+
+from trends.cache import cache_result
+
+class MockClient:
+    def __init__(self, responses, values):
+        self.responses = responses
+        self.values = values
+
+    def _check(self, act_cmd, act_args, value=None):
+        exp_cmd, exp_args, resp = self.responses.pop()
+        assert act_cmd == exp_cmd
+        assert act_args == exp_args
+        self.values.append(value)
+        if isinstance(resp, BaseException):
+            raise resp
+        return resp
+
+    def gets(self, key):
+        return self._check('gets', (key,))
+
+    def add(self, key, value, time=0):
+        return self._check('add', (key, time), value)
+
+    def cas(self, key, value, cas, time=0):
+        return self._check('cas', (key, cas, time), value)
+
+class MockServer:
+    def __init__(self, responses, values):
+        self.responses = responses
+        self.values = values
+
+    def gets(self, key, value, cas):
+        self.responses.appendleft(('gets', (key,), (value, cas)))
+
+    def add(self, key, result, time=0):
+        self.responses.appendleft(('add', (key, time), result))
+
+    def cas(self, key, cas, result, time=0):
+        self.responses.appendleft(('cas', (key, cas, time), result))
+
+@pytest.fixture
+def mock_cache():
+    responses = collections.deque()
+    values = []
+
+    client = MockClient(responses, values)
+    server = MockServer(responses, values)
+    yield client, server
+
+    assert not len(responses)
+
+@cache_result('foo')
+def one(mc):
+    return 1
+
+class E(Exception):
+    pass
+
+@cache_result('foo')
+def error(mc):
+    raise E
+
+def test_hit(mock_cache):
+    client, server = mock_cache
+    server.gets('foo', 1, 0)
+    assert error(client) == 1
+
+def test_miss_fill(mock_cache):
+    client, server = mock_cache
+    server.gets('foo', None, None)
+    server.add('foo', True, time=120)
+    server.gets('foo', None, 0)
+    server.cas('foo', 0, True, time=86400)
+    assert one(client) == 1
+
+def test_miss_error(mock_cache):
+    client, server = mock_cache
+    server.gets('foo', None, None)
+    server.add('foo', True, time=120)
+    server.gets('foo', None, 0)
+    with pytest.raises(E):
+        assert error(client)
+
+def test_late_hit(mock_cache):
+    client, server = mock_cache
+    server.gets('foo', None, None)
+    server.add('foo', False, time=120)
+    server.gets('foo', 2, 0)
+    assert one(client) == 2
+
+def test_no_cas(mock_cache):
+    client, server = mock_cache
+    server.gets('foo', None, None)
+    server.add('foo', True, time=120)
+    server.gets('foo', None, None)
+    assert one(client) == 1
+
+def test_no_dummy(mock_cache):
+    client, server = mock_cache
+    server.gets('foo', None, None)
+    server.add('foo', True, time=120)
+    server.gets('foo', None, 0)
+    server.cas('foo', 0, pylibmc.NotFound, time=86400)
+    assert one(client) == 1

test/site_test.py
@@ -376,90 +376,3 @@ def test_api_logs(client):
         assert "76561198330799279" in players
         assert "76561198046130018" in players
 
-class MockClient:
-    def __init__(self, responses, values):
-        self.responses = responses
-        self.values = values
-
-    def _check(self, act_cmd, act_args, value=None):
-        exp_cmd, exp_args, resp = self.responses.pop()
-        assert act_cmd == exp_cmd
-        assert act_args == exp_args
-        self.values.append(value)
-        if isinstance(resp, BaseException):
-            raise resp
-        return resp
-
-    def gets(self, key):
-        return self._check('gets', (key,))
-
-    def add(self, key, value, time=0):
-        return self._check('add', (key, time), value)
-
-    def cas(self, key, value, cas, time=0):
-        return self._check('cas', (key, cas, time), value)
-
-class MockServer:
-    def __init__(self, responses, values):
-        self.responses = responses
-        self.values = values
-
-    def gets(self, key, value, cas):
-        self.responses.appendleft(('gets', (key,), (value, cas)))
-
-    def add(self, key, result, time=0):
-        self.responses.appendleft(('add', (key, time), result))
-
-    def cas(self, key, cas, result, time=0):
-        self.responses.appendleft(('cas', (key, cas, time), result))
-
-@contextmanager
-def mock_cache(monkeypatch):
-    responses = collections.deque()
-    values = []
-
-    client = MockClient(responses, values)
-    server = MockServer(responses, values)
-    with monkeypatch.context() as ctx:
-        ctx.setattr(trends.site.player, 'get_mc', lambda: client)
-        yield server
-
-    assert not len(responses)
-
-def test_player_cache(client, players, monkeypatch):
-    def check(resp):
-        assert resp.status_code == uncached.status_code
-        assert resp.headers == uncached.headers
-        assert resp.data == uncached.data
-
-    for steamid in players[:10]:
-        key = f"overview_{steamid}"
-        path = f"/player/{steamid}/"
-
-        # Cache miss
-        with mock_cache(monkeypatch) as server:
-            server.gets(key, None, None)
-            server.add(key, True, time=86400)
-            uncached = client.get(path)
-            if uncached.status_code == 404:
-                server.responses.clear()
-                continue
-            value = server.values[1]
-
-        # Cache hit
-        with mock_cache(monkeypatch) as server:
-            server.gets(key, value, 0)
-            check(client.get(path))
-
-        # Stale cache with racing update
-        value['last_active'] -= 1
-        with mock_cache(monkeypatch) as server:
-            server.gets(key, value, 0)
-            server.cas(key, 0, False, time=86400)
-            check(client.get(path))
-
-        # Stale cache with racing delete
-        with mock_cache(monkeypatch) as server:
-            server.gets(key, value, 0)
-            server.cas(key, 0, pylibmc.NotFound, time=86400)
-            check(client.get(path))