97bfb6b743ac9edd2a5f38bde56cb5ece7866214
Sean Anderson <seanga2@gmail.com>
2025-07-27 14:27:14 -0400

    cache: Improve tracing
    Use cache ops only for high-level get/put operations. This provides a
    more-meaningful cache hit rate. The actual cache operations are recorded as
    db ops.
    
    Signed-off-by: Sean Anderson <seanga2@gmail.com>

--- trends/cache.py
+++ trends/cache.py
@@ -48,70 +48,64 @@ class TracingClient(pylibmc.Client):
         self._tracing_enabled = False
 
     @contextlib.contextmanager
-    def _cache_span(self, category, op, key):
+    def _cache_span(self, op, key):
         if self._tracing_enabled:
             yield
             return
 
-        sentry_sdk.add_breadcrumb(type='query', category=category, message=key)
-        with sentry_sdk.start_span(op=category, description=f"{op} {key}") as span:
-            span.set_data('cache.operation', op)
-            span.set_data('cache.key', key)
+        desc = f"{op} {key}"
+        sentry_sdk.add_breadcrumb(category='query', message=desc)
+        with sentry_sdk.start_span(op='db.query', description=desc) as span:
+            span.set_data('db.system', "memcached")
+            span.set_data('db.operation', op)
             yield span
 
     def get(self, key, default=None):
-        with self._cache_span('cache.get', 'get', key) as span:
+        with self._cache_span('get', key) as span:
             value = super().get(key, self)
-            if value is self:
-                span.set_data('cache.hit', False)
-                return default
-            else:
-                span.set_data('cache.hit', True)
-                return value
+            return default if value is self else value
 
     def get_multi(self, keys):
         keys = list(keys)
-        with self._cache_span('cache.get', 'get_multi', keys) as span:
+        with self._cache_span('get_multi', keys) as span:
             values = super().get_multi(keys)
-            span.set_data('cache.hit', bool(values))
             return values
 
     def set(self, key, value, *args, **kwargs):
-        with self._cache_span('cache.set', 'set', key):
+        with self._cache_span('set', key):
             return super().set(key, value, *args, **kwargs)
 
     def set_multi(self, mapping, *args, **kwargs):
-        with self._cache_span('cache.set', 'set_multi', list(mapping)) as span:
+        with self._cache_span('set_multi', ', '.join(mapping.values())) as span:
             return super().set_multi(mapping, *args, **kwargs)
 
     def add(self, key, value, *args, **kwargs):
-        with self._cache_span('cache.set', 'add', key):
+        with self._cache_span('add', key):
             return super().add(key, value, *args, **kwargs)
 
     def replace(self, key, value, *args, **kwargs):
-        with self._cache_span('cache.set', 'replace', key):
+        with self._cache_span('replace', key):
             return super().replace(key, value, *args, **kwargs)
 
     def gets(self, key, default=None):
-        with self._cache_span('cache.get', 'gets', key) as span:
+        with self._cache_span('gets', key) as span:
             value, cas = super().gets(key)
             if value is None and cas is None:
-                span.set_data('cache.hit', False)
                 return None, None
             else:
-                span.set_data('cache.hit', True)
                 return value, cas
 
     def cas(self, key, value, cas, time=0):
-        with self._cache_span('cache.set', 'cas', key):
+        with self._cache_span('cas', key):
             return super().cas(key, value, cas, time)
 
     def delete(self, key):
-        with self._cache_span('cache.delete', 'delete', key):
+        with self._cache_span('delete', key):
             return super().delete(key)
 
     def delete_multi(self, keys):
-        with self._cache_span('cache.delete', 'delete_multi', keys) as span:
+        keys = list(keys)
+        with self._cache_span('delete_multi', ', '.join(keys)) as span:
             self._tracing_disabled = True
             try:
                 return super().delete_multi(keys)
@@ -119,9 +113,11 @@ class TracingClient(pylibmc.Client):
                 self._tracing_disabled = False
 
     def flush_all(self):
-        sentry_sdk.add_breadcrumb(type='query', category='cache.clear')
-        with sentry_sdk.start_span(op='cache.clear') as span:
-            return super().flush_all()
+        sentry_sdk.add_breadcrumb(category='query', message='flush_all')
+        with sentry_sdk.start_span(op='db.query', description='flush_all') as span:
+           span.set_data('db.system', 'memcached')
+           span.set_data('db.operation', 'flush_all')
+           return super().flush_all()
 
 def mc_connect(servers):
     if servers:
@@ -133,23 +129,31 @@ def cache_result(key_template, timeout=120, expire=86400):
         @wraps(f)
         def wrapper(mc, *args, **kwargs):
             key = key_template.format(*args, **kwargs)
-            val, cas = mc.gets(key)
-            if val is not None:
-                return val
-
-            if cas is None:
-                # Add a dummy value so we can delete it if we have to purge
-                mc.add(key, None, time=timeout)
-
+            with sentry_sdk.start_span(op='cache.get', description=key) as span:
+                span.set_data('cache.key', key)
                 val, cas = mc.gets(key)
-                # Did someone else fill the cache in the meantime?
                 if val is not None:
+                    span.set_data('cache.hit', True)
                     return val
+
+                if cas is None:
+                    # Add a dummy value so we can delete it if we have to purge
+                    mc.add(key, None, time=timeout)
+
+                    val, cas = mc.gets(key)
+                    # Did someone else fill the cache in the meantime?
+                    if val is not None:
+                        span.set_data('cache.hit', True)
+                        return val
+
+                span.set_data('cache.hit', False)
 
             val = f(mc, *args, **kwargs)
             try:
                 if cas is not None:
-                    mc.cas(key, val, cas, time=expire)
+                    with sentry_sdk.start_span(op='cache.put', description=key) as span:
+                        span.set_data('cache.key', key)
+                        mc.cas(key, val, cas, time=expire)
             except pylibmc.NotFound:
                 pass
             return val
@@ -165,7 +169,8 @@ def purge(c, mc, col, table, key):
             if not len(vals):
                 return
 
-            mc.delete_multi(key.format(val) for val in vals)
+            with sentry_sdk.start_span(op='cache.remove') as span:
+                mc.delete_multi(key.format(val) for val in vals)
             cur.execute(f"DELETE FROM {table} WHERE {col} IN %s;", (vals,))
             cur.execute("COMMIT;")
             logging.info("Purged %s value(s) from %s", len(vals), table)
