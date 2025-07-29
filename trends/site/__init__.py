Forty-Bot/trends.tf

commit ef15ca9deaab54c02a2fa820be9de0b49ec56f98

Unconditionally enable tracing when debugging
There are limits on tracing in production, but when debugging we can trace
as much as we like.  Adjust sentry's options accordingly.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/util.py
@@ -5,6 +5,7 @@ import enum
 import itertools
 import pkg_resources
 
+import flask
 import sentry_sdk
 from sentry_sdk.integrations.flask import FlaskIntegration
 
@@ -54,10 +55,16 @@ def sentry_init(**kwargs):
     except pkg_resources.DistributionNotFound:
         version = None
 
+    try:
+        debug = flask.current_app.debug
+    except RuntimeError:
+        debug = False
+
     defaults = {
+        'environment': 'development' if debug else 'production' ,
         'release': version,
         'integrations': [FlaskIntegration()],
-        'traces_sample_rate': 0.02,
+        'traces_sample_rate': 1 if debug else 0.02,
         'send_default_pii': True,
     }
 

commit c0c34dcf9304bd98f7035579a784fcf8c9cf3d76

Really enable tracing in debug
Initialize sentry after flask so we can access the application.

Fixes: ef15ca9 ("Unconditionally enable tracing when debugging")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/__init__.py
@@ -4,5 +4,5 @@
 from ..util import sentry_init
 from .wsgi import create_app
 
-sentry_init()
 application = create_app()
+sentry_init(debug=application.debug, environment=application.env)

trends/util.py
@@ -49,19 +49,13 @@ def chunk(iterable, n):
             return
         yield itertools.chain((first,), slice)
 
-def sentry_init(**kwargs):
+def sentry_init(debug=False, **kwargs):
     try:
         version = pkg_resources.require("trends.tf")[0].version
     except pkg_resources.DistributionNotFound:
         version = None
 
-    try:
-        debug = flask.current_app.debug
-    except RuntimeError:
-        debug = False
-
     defaults = {
-        'environment': 'development' if debug else 'production' ,
         'release': version,
         'integrations': [FlaskIntegration()],
         'traces_sample_rate': 1 if debug else 0.02,