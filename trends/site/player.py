Forty-Bot/trends.tf

commit 5e79e3dd0d55879cf6cce5e891867fea0282c7aa

site: Cache player logs
Cache players' logs. The query parameters are encoded into the name as a hash. To invalidate old
keys, the primary overview cache entry has a random "version." This is used as a key for the hash,
ensuring that we use different keys when the overview is purged.

32 bytes of data is probably way overkill for the version. We use base64
instead of base16 to keep the key size down. I considered base85. but it
was around twice as slow. Blake2b is used for the hash since it has a
convenient key parameter, and has a configurable digest size (so we can
get an even number of characters).

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/player.py
@@ -2,12 +2,13 @@
 # Copyright (C) 2020-21 Sean Anderson <seanga2@gmail.com>
 
 from collections import defaultdict
+import secrets
 
 import flask
 import pylibmc
 
 from .util import get_db, get_mc, get_filter_params, get_filter_clauses, get_order, \
-                  get_pagination, last_modified
+                  get_pagination, last_modified, hash_object
 from ..util import clamp, classes
 from ..cache import cache_result
 
@@ -49,12 +50,18 @@ def _get_overview(mc, steamid64):
 
     row = cur.fetchone()
     if row is None:
-        flask.abort(404)
-    return dict(row)
+        return {}
+
+    row = dict(row)
+    row['version'] = secrets.token_bytes(32)
+    return row
 
 @player.before_request
 def get_overview():
     overview = _get_overview(get_mc(), flask.g.steamid)
+    if not overview:
+        flask.abort(404)
+
     if resp := last_modified(overview['last_active']):
         return resp
 
@@ -106,7 +113,9 @@ log_order_map = {
     **log_joined_order_map,
 }
 
-def get_logs(c, playerid, filters, extra=False, order_clause="logid DESC", limit=100, offset=0):
+@cache_result("player_logs_{}_{}")
+def _get_logs(mc, steamid64, digest, playerid, params):
+    filters, extra, order_clause, limit, offset = params
     real_offset = offset
     filter_clauses = get_filter_clauses(filters, 'primary_classid', 'league', 'formatid', 'title',
                                         'mapid', 'time', 'logid', 'duplicate_of')
@@ -126,7 +135,7 @@ def get_logs(c, playerid, filters, extra=False, order_clause="logid DESC", limit
     else:
         extra_cols = ""
 
-    logs = c.cursor()
+    logs = get_db().cursor()
     logs.execute(
         f"""SELECT
                ps.logid,
@@ -178,7 +187,13 @@ def get_logs(c, playerid, filters, extra=False, order_clause="logid DESC", limit
             'offset': offset,
             'real_offset': real_offset
         })
-    return logs
+    return [dict(log) for log in logs]
+
+def get_logs(extra=False, order_clause="logid DESC", limit=100, offset=0):
+    player = flask.g.player
+    params = get_filter_params(), extra, order_clause, limit, offset
+    digest = hash_object(player.get('version', b''), params)
+    return _get_logs(get_mc(), flask.g.steamid, digest, player['playerid'], params)
 
 def get_teams(c, filters, order_clause="rto DESC", limit=10, offset=0):
     inner_clauses = get_filter_clauses(filters, 'league', date_range='rostered')
@@ -369,7 +384,7 @@ def overview(steamid):
                ) AS names
                JOIN name USING (nameid)""", (flask.g.playerid,))
 
-    logs = get_logs(c, flask.g.playerid, filters, limit=25)
+    logs = get_logs(limit=25)
     teams = get_teams(c, filters)
     return flask.render_template("player/overview.html", logs=logs, classes=classes,
                                  formats=formats, aliases=aliases, teams=teams)
@@ -379,9 +394,8 @@ def logs(steamid):
     limit, offset = get_pagination()
     filters = get_filter_params()
     order, order_clause = get_order(log_order_map, 'logid')
-    logs = get_logs(get_db(), flask.g.playerid, filters, extra=True, order_clause=order_clause,
-                    limit=limit, offset=offset)
-    return flask.render_template("player/logs.html", logs=logs.fetchall())
+    logs = get_logs(extra=True, limit=limit, offset=offset)
+    return flask.render_template("player/logs.html", logs=logs)
 
 @player.route('/teams')
 def teams(steamid):

trends/site/util.py
@@ -1,11 +1,14 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # Copyright (C) 2021 Sean Anderson <seanga2@gmail.com>
 
+import base64
 from functools import wraps
 from collections import namedtuple
 from datetime import datetime, timedelta
 from dateutil import tz
+from hashlib import blake2b
 from itertools import islice
+import pickle
 import sys
 
 import flask
@@ -155,6 +158,10 @@ def get_filter_params():
         params['date_range'] = NumericRange(upper=params['date_to_ts'], bounds='(]')
 
     return params
+
+def hash_object(version, obj):
+    digest = blake2b(pickle.dumps(obj, protocol=5), key=version, digest_size=36).digest()
+    return base64.urlsafe_b64encode(digest).decode('ascii')
 
 def get_filter_clauses(params, *valid_columns, **column_map):
     clauses = []

commit 56c2efd4481e6d1476763f8c7f3bab8d80a3af50

site: player: Fix sorting logs
When refactoring I accidentally removed the ability to sort. Whoops.

Fixes: 5e79e3d ("site: Cache player logs")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/player.py
@@ -394,7 +394,7 @@ def logs(steamid):
     limit, offset = get_pagination()
     filters = get_filter_params()
     order, order_clause = get_order(log_order_map, 'logid')
-    logs = get_logs(extra=True, limit=limit, offset=offset)
+    logs = get_logs(extra=True, order_clause=order_clause, limit=limit, offset=offset)
     return flask.render_template("player/logs.html", logs=logs)
 
 @player.route('/teams')