Forty-Bot/trends.tf

commit 3cd71006323e4ce3fd4a86b7a1409b5c9d350ad4

cache: Ignore memcached errors
Just print a warning when we get memcached errors and continue serving the
request. This helps with availability even when the cache temporarily has
problems.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/cache.py
@@ -145,23 +145,25 @@ def cache_result(key_template, timeout=120, expire=86400):
             with sentry_sdk.start_span(op='cache.get', description=key) as span:
                 CACHE_ACCESS.labels(key_template).inc()
                 span.set_data('cache.key', key)
-                val, cas = mc.gets(key)
-                if val is not None:
-                    span.set_data('cache.hit', True)
-                    CACHE_HIT.labels(key_template).inc()
-                    return val
-
-                if cas is None:
-                    # Add a dummy value so we can delete it if we have to purge
-                    mc.add(key, None, time=timeout)
-
+                try:
                     val, cas = mc.gets(key)
-                    # Did someone else fill the cache in the meantime?
                     if val is not None:
                         span.set_data('cache.hit', True)
                         CACHE_HIT.labels(key_template).inc()
                         return val
 
+                    if cas is None:
+                        # Add a dummy value so we can delete it if we have to purge
+                        mc.add(key, None, time=timeout)
+
+                        val, cas = mc.gets(key)
+                        # Did someone else fill the cache in the meantime?
+                        if val is not None:
+                            span.set_data('cache.hit', True)
+                            CACHE_HIT.labels(key_template).inc()
+                            return val
+                except pylibmc.Error:
+                    logging.exception("Could not get %s", key)
                 span.set_data('cache.hit', False)
 
             val = f(mc, *args, **kwargs)
@@ -170,8 +172,8 @@ def cache_result(key_template, timeout=120, expire=86400):
                     with sentry_sdk.start_span(op='cache.put', description=key) as span:
                         span.set_data('cache.key', key)
                         mc.cas(key, val, cas, time=expire)
-            except pylibmc.NotFound:
-                pass
+            except pylibmc.Error:
+                logging.exception("Could not set %s", key)
             return val
         return wrapper
     return decorator

test/cache_test.py
@@ -106,3 +106,17 @@ def test_no_dummy(mock_cache):
     server.gets('foo', None, 0)
     server.cas('foo', 0, pylibmc.NotFound, time=86400)
     assert one(client) == 1
+
+def test_get_error(mock_cache):
+    client, server = mock_cache
+    server.gets('foo', None, None)
+    server.add('foo', pylibmc.Error(), time=120)
+    assert one(client) == 1
+
+def test_set_error(mock_cache):
+    client, server = mock_cache
+    server.gets('foo', None, None)
+    server.add('foo', True, time=120)
+    server.gets('foo', None, 0)
+    server.cas('foo', 0, pylibmc.Error(), time=86400)
+    assert one(client) == 1

trends/site/root.py
@@ -423,7 +423,7 @@ def log(logids):
 
         key = m['league'], m['matchid']
         if key not in matches:
-            m['full_logs'] = set(m['full_logs'])
+            m['full_logs'] = set(m['full_logs'] or ())
             matches[key] = m
         matches[key]['full_logs'].add(logid)
 

commit dcd14a2079a05428831f1b20f0ddeba4b65bd7ed

cache: Fix crash when gets fails
If the initial gets failed then cas would be undefined. Fix this by setting
cas to None if we get an error accessing memcached.

Fixes: 3cd7100 ("cache: Ignore memcached errors")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/cache.py
@@ -165,6 +165,7 @@ def mutable(key_template, timeout=30, expire=86400):
                             return val
                 except pylibmc.Error:
                     logging.exception("Could not get %s", key)
+                    cas = None
                 span.set_data('cache.hit', False)
 
             val = f(mc, *args, **kwargs)

test/cache_test.py
@@ -34,7 +34,8 @@ class MockServer:
         self.values = values
 
     def gets(self, key, value, cas):
-        self.responses.appendleft(('gets', (key,), (value, cas)))
+        result = value if isinstance(value, BaseException) else (value, cas)
+        self.responses.appendleft(('gets', (key,), result))
 
     def add(self, key, result, time=0):
         self.responses.appendleft(('add', (key, time), result))
@@ -108,6 +109,11 @@ def test_no_dummy(mock_cache):
     assert one(client) == 1
 
 def test_get_error(mock_cache):
+    client, server = mock_cache
+    server.gets('foo', pylibmc.Error(), None)
+    assert one(client) == 1
+
+def test_get_add_error(mock_cache):
     client, server = mock_cache
     server.gets('foo', None, None)
     server.add('foo', pylibmc.Error(), time=30)