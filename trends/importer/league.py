Forty-Bot/trends.tf

commit 9f7e4cf4fe9928348ee3f24e5edc45437c61c2b9

site: log: Cache logs and merge the results in python
AI scrapers really love the log endpoint since there are a bunch of ways to
add/remove logs. To improve performance, add some caching. This is done
per-log so that removing a log (or adding another) will mostly hit the
cache. Unfortunately, this means we have to do the aggregations in python.
While far more verbose than SQL, this ends up only being around 150 lines
more, mostly because the SQL can also be simplified.

To aid in development, the data passed to the template is exactly the same
as before this commit. The next commit will refactor things to simplify the
template (now that we have some more flexibility on our data structure).

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/importer/league.py
@@ -369,3 +369,8 @@ def import_match(c, m):
                    fetched = greatest(fetched, %(fetched)s)
                 WHERE league = %(league)s
                     AND matchid = %(matchid)s;""", m)
+        cur.execute(
+            """INSERT INTO cache_purge_log (logid)
+               SELECT logid
+               FROM log
+               WHERE league = %(league)s AND matchid = %(matchid)s;""", m)

trends/cache.py
@@ -156,7 +156,7 @@ def cache_result(key_template, timeout=120, expire=86400):
         return wrapper
     return decorator
 
-def purge(c, mc, col, table, keys):
+def purge(c, mc, col, table, key):
     with (sentry_sdk.start_span(op='db.transaction', description="purge"), c.cursor() as cur):
         while True:
             cur.execute("BEGIN;")
@@ -165,11 +165,13 @@ def purge(c, mc, col, table, keys):
             if not len(vals):
                 return
 
-            for key in keys:
-                mc.delete_multi(key.format(val) for val in vals)
+            mc.delete_multi(key.format(val) for val in vals)
             cur.execute(f"DELETE FROM {table} WHERE {col} IN %s;", (vals,))
             cur.execute("COMMIT;")
             logging.info("Purged %s value(s) from %s", len(vals), table)
 
+def purge_logs(c, mc):
+    purge(c, mc, 'logid', 'cache_purge_log', "log_{}")
+
 def purge_players(c, mc):
-    purge(c, mc, 'steamid64', 'cache_purge_player', ("overview_{}",))
+    purge(c, mc, 'steamid64', 'cache_purge_player', "overview_{}")

trends/importer/etf2l.py
@@ -11,7 +11,7 @@ import time
 from psycopg2.extras import NumericRange
 import sentry_sdk
 
-from ..cache import purge_players
+from ..cache import purge_logs, purge_players
 from .fetch import ETF2LFileFetcher, ETF2LBulkFetcher, FetchError
 from .league import *
 from ..sql import db_connect
@@ -202,6 +202,7 @@ def import_etf2l(c, mc, fetcher, filter=filter_matchids):
                     import_team(cur, team)
                 import_match(cur, res)
                 cur.execute("COMMIT;")
+            purge_logs(c, mc)
             purge_players(c, mc)
         except FetchError:
             continue

trends/importer/link_demos.py
@@ -46,5 +46,6 @@ def link_logs(args, c, mc):
                            updated = greatest(extract(EPOCH FROM now())::BIGINT, log.updated + 1)
                        FROM linked
                        WHERE log.logid = linked.logid;""")
+        cur.execute("INSERT INTO cache_purge_log (logid) SELECT logid FROM linked;")
         cur.execute("COMMIT;")
         logging.info(f"Linked {count} logs")

trends/importer/link_matches.py
@@ -4,7 +4,7 @@
 from datetime import datetime, timedelta
 import logging
 
-from ..cache import purge_players
+from ..cache import purge_logs, purge_players
 
 def create_link_matches_parser(sub):
     link = sub.add_parser("link_matches", help="Link logs and matches")
@@ -92,6 +92,7 @@ def link_matches(args, c, mc):
                            team1_is_red = log_matches.team1_is_red
                        FROM log_matches
                        WHERE log.logid = log_matches.logid;""")
+        cur.execute("INSERT INTO cache_purge_log (logid) SELECT logid FROM log_matches;")
         cur.execute("""INSERT INTO cache_purge_player (steamid64)
                        SELECT steamid64
                        FROM log_matches
@@ -100,4 +101,5 @@ def link_matches(args, c, mc):
                        GROUP BY steamid64;""")
         cur.execute("COMMIT;")
         logging.info(f"Linked {count} logs")
+        purge_logs(c, mc)
         purge_players(c, mc)

trends/importer/logs.py
@@ -10,7 +10,7 @@ import psycopg2
 import sentry_sdk
 import systemd_watchdog
 
-from ..cache import purge_players
+from ..cache import purge_logs, purge_players
 from .fetch import ListFetcher, BulkFetcher, FileFetcher, ReverseFetcher, CloneLogsFetcher
 from ..steamid import SteamID
 from ..sql import disable_tracing, delete_logs, log_tables, publicize, table_columns
@@ -749,6 +749,7 @@ def update_ks(cur):
                        AND pse.playerid = new.playerid;""")
 
 def prepare_purge(cur):
+    cur.execute("INSERT INTO cache_purge_log (logid) SELECT logid FROM log;")
     cur.execute("""INSERT INTO cache_purge_player (steamid64)
                    SELECT steamid64
                    FROM player_stats_backing
@@ -863,6 +864,7 @@ def import_logs(c, mc, fetcher, update_only):
             publicize(c, log_tables)
             cur.execute("COMMIT;")
             logging.info("Committed %s imported log(s)...", count)
+        purge_logs(c, mc)
         purge_players(c, mc)
 
     count = 0

trends/importer/players.py
@@ -97,6 +97,7 @@ def import_players(args, c, mc):
                            SELECT steamid64
                            FROM player;""")
             cur.execute("COMMIT;")
+            # Technically we should purge logs as well, but I don't think it matters too much
             purge_players(c, mc)
             successes += 1
             logging.info("ok")

trends/schema.sql
@@ -948,3 +948,9 @@ CREATE TABLE IF NOT EXISTS cache_purge_player (
 );
 
 CREATE INDEX IF NOT EXISTS cache_purge_player_pkey ON cache_purge_player (steamid64);
+
+CREATE TABLE IF NOT EXISTS cache_purge_log (
+	logid INT NOT NULL
+);
+
+CREATE INDEX IF NOT EXISTS cache_purge_log_pkey ON cache_purge_log (logid);

commit 83ff2e6a75fb599070f6788b3e91d721f1e5a14b

importer: league: Fix purging matches
The cursor is called `c` and not `cur`.

Right now we avoid purging when inserting since there is nothing to
invalidate. But just purge anyway to be safe.

Fixes: 9f7e4cf ("site: log: Cache logs and merge the results in python")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/importer/league.py
@@ -369,6 +369,6 @@ def import_match(c, m):
                    fetched = greatest(fetched, %(fetched)s)
                 WHERE league = %(league)s
                     AND matchid = %(matchid)s;""", m)
-        cur.execute(
-            """INSERT INTO cache_purge_match (league, matchid)
-               VALUES (%(league)s, %(matchid)s);""", m)
+    c.execute(
+        """INSERT INTO cache_purge_match (league, matchid)
+           VALUES (%(league)s, %(matchid)s);""", m)