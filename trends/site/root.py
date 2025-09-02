Forty-Bot/trends.tf

commit 667bbab52a9254cb4d0f1dfdca9ce3fb7f4dc4b7

site: log: Only get each match once
Even through we usually hit the cache, repeatedly fetching the same matches
distorts the cache hit rate and is slower than just checking if we already
have it.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/root.py
@@ -418,12 +418,11 @@ def log(logids):
     matches = {}
     logid_set = set(logids)
     for logid, log in logs.items():
-        if not (m := get_match(mc, log['summary']['league'], log['summary']['matchid'])):
-            continue
-
-        key = m['league'], m['matchid']
+        key = log['summary']['league'], log['summary']['matchid']
         if key not in matches:
-            m['full_logs'] = set(m['full_logs'] or ())
+            if not (m := get_match(mc, *key)):
+                continue
+            m['full_logs'] = set(m['full_logs'])
             matches[key] = m
         matches[key]['full_logs'].add(logid)
 

commit 23e04ab18adc7569e7e31bb4b0ca695735308f55

site: log: Fix rendering matches where all logs are duplicates
There are around 10 matches where all of the linked logs are duplicates.
From what I can tell this usually happens when there is something that
causes the earlier log(s) not to match (such as rounds from the warm-up
being included). Then someone uploads an edited log that matches with the
match.

I'm nopt really sure what to do with these in general, as there are not
very many of them. For now, just ensure we don't crash if full_logs is
None.

Fixes: 667bbab ("site: log: Only get each match once")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/root.py
@@ -429,7 +429,7 @@ def log(logids):
         if key not in matches:
             if not (m := get_match(mc, *key)):
                 continue
-            m['full_logs'] = set(m['full_logs'])
+            m['full_logs'] = set(m['full_logs'] or ())
             matches[key] = m
         matches[key]['full_logs'].add(logid)
 

test/create.py
@@ -58,6 +58,8 @@ def create_test_db(url, memcached):
         2401045,
         2408458,
         2408491,
+        2418123,
+        2418147,
         2426167,
         2426192,
         2500876,
@@ -89,6 +91,7 @@ def create_test_db(url, memcached):
         318447,
         322265,
         322285,
+        327276,
         331371,
         375937,
         585088,
@@ -126,6 +129,7 @@ def create_test_db(url, memcached):
          3099,
          3412,
          4664,
+         4971,
         26935,
     )
 

test/site_test.py
@@ -203,6 +203,7 @@ linked_demos = {
     2401045: 318447,
     2408458: 322265,
     2408491: 322285,
+    2418123: 327276,
     2426192: 331371,
     2506954: 375937,
     2844704: 585088,
@@ -229,6 +230,7 @@ linked_matches = {
     2392557: ('rgl', 4664),
     2408458: ('etf2l', 77326),
     2408491: ('etf2l', 77326),
+    2418147: ('rgl', 4971),
     2426167: ('etf2l', 77318),
     2426192: ('etf2l', 77318),
     3251446: ('etf2l', 84064),
@@ -248,6 +250,7 @@ def test_linked(connection):
 duplicates = {
     2297265: [2297197],
     2344394: [2344272, 2344306, 2344354],
+    2418147: [2418123],
 }
 
 def test_dupes(connection):