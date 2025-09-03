Forty-Bot/trends.tf

commit c48c7186a0960d8b8ac1ab22d0fd44eb0975e118

importer: Purge player overview cache when updating players
Purge the player overview cache whenever the backing data is updated. This
process takes several steps:

1. Update the backing data and add updated rows to cache_purge_player.
2. Commit the transaction. At this point, the new data is visible to the
   frontend, but there may be outstanding stale updates still in progress.
3. Select some players to purge and delete them from the cache. This step
   may be repeated if the final step fails, but this just results in
   excessive purging and not stale data.
4. Delete those players from cache_purge_player.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/importer/players.py
@@ -9,6 +9,8 @@ import requests, requests.adapters
 import psycopg2, psycopg2.extras
 import urllib3.util
 
+from ..cache import purge_players
+
 def get_steamids_full(c):
     last_steamid = 0
     cur = c.cursor()
@@ -81,13 +83,21 @@ def import_players(args, c, mc):
                                name
                            FROM player_update
                            ON CONFLICT DO NOTHING;""")
-            cur.execute("""UPDATE player
-                           SET
-                              nameid = (SELECT nameid FROM name WHERE name = player_update.name),
-                              avatarhash = player_update.avatarhash
-                           FROM player_update
-                           WHERE player_update.steamid64::BIGINT = player.steamid64;""")
+            cur.execute("""WITH player AS (UPDATE player
+                               SET
+                                  nameid = (SELECT nameid
+                                      FROM name
+                                      WHERE name = player_update.name
+                                  ),
+                                  avatarhash = player_update.avatarhash
+                               FROM player_update
+                               WHERE player_update.steamid64::BIGINT = player.steamid64
+                               RETURNING steamid64
+                           ) INSERT INTO cache_purge_player (steamid64)
+                           SELECT steamid64
+                           FROM player;""")
             cur.execute("COMMIT;")
+            purge_players(c, mc)
             successes += 1
             logging.info("ok")
         except requests.exceptions.HTTPError as e:

test/create.py
@@ -73,7 +73,7 @@ def create_test_db(url, memcached):
                 3384488,
             )
         }
-        trends.importer.logs.import_logs(c, FileFetcher(logs=logfiles), False)
+        trends.importer.logs.import_logs(c, mc, FileFetcher(logs=logfiles), False)
 
     with db_connect(url) as c:
         demofiles = (f"{os.path.dirname(__file__)}/demos/demo_{demoid}.json" for demoid in (
@@ -100,7 +100,7 @@ def create_test_db(url, memcached):
     with db_connect(url) as c:
         fetcher = ETF2LFileFetcher(results=f"{os.path.dirname(__file__)}/etf2l/results.json",
                                    xferdir=f"{os.path.dirname(__file__)}/etf2l/")
-        trends.importer.etf2l.import_etf2l(c, fetcher)
+        trends.importer.etf2l.import_etf2l(c, mc, fetcher)
 
     with db_connect(url) as c:
         dir = f"{os.path.dirname(__file__)}/rgl"

trends/cache.py
@@ -33,6 +33,12 @@ class NoopClient:
     def cas(self, key, value, cas, time=0):
         raise pylibmc.NotFound
 
+    def delete(self, key):
+        return False
+
+    def delete_multi(self, keys):
+        return False
+
 @contextlib.contextmanager
 def cache_span(category, op, key):
     sentry_sdk.add_breadcrumb(type='query', category=category, message=key)
@@ -89,7 +95,29 @@ class TracingClient(pylibmc.Client):
         with cache_span('cache.set', 'cas', key):
             return super().cas(key, value, cas, time)
 
+    def delete(self, key):
+        with cache_span('cache.delete', 'delete', key):
+            return super().delete(key)
+
+    # delete_multi just calls delete repeatedly
+
 def mc_connect(servers):
     if servers:
         return TracingClient(servers.split(','), binary=True, behaviors={ 'cas': True })
     return NoopClient()
+
+def purge_players(c, mc):
+    with (sentry_sdk.start_span(op='db.transaction', description="purge"), c.cursor() as cur):
+        while True:
+            cur.execute("BEGIN;")
+            cur.execute("""SELECT steamid64
+                           FROM cache_purge_player
+                           FOR UPDATE SKIP LOCKED
+                           LIMIT 1000;""")
+            players = tuple(row[0] for row in cur)
+            if not len(players):
+                return
+
+            mc.delete_multi(f"overview_{steamid}" for steamid in players)
+            cur.execute("DELETE FROM cache_purge_player WHERE steamid64 IN %s;", (players,))
+            cur.execute("COMMIT;")

trends/importer/etf2l.py
@@ -11,6 +11,7 @@ import time
 from psycopg2.extras import NumericRange
 import sentry_sdk
 
+from ..cache import purge_players
 from .fetch import ETF2LFileFetcher, ETF2LBulkFetcher, FetchError
 from .league import *
 from ..sql import db_connect
@@ -155,10 +156,9 @@ def import_etf2l_cli(args, c, mc):
                        FROM match
                        WHERE league = 'etf2l';""");
                 args.since = datetime.fromtimestamp(cur.fetchone()[0])
-        return import_etf2l(c, args.fetcher(**vars(args)))
+        return import_etf2l(c, mc, args.fetcher(**vars(args)))
 
-
-def import_etf2l(c, fetcher):
+def import_etf2l(c, mc, fetcher):
     cur = c.cursor()
     count = 0
     for result in filter_matchids(c, fetcher.get_results()):
@@ -202,6 +202,7 @@ def import_etf2l(c, fetcher):
                     import_team(cur, team)
                 import_match(cur, res)
                 cur.execute("COMMIT;")
+            purge_players(c, mc)
         except FetchError:
             continue
         except (IndexError, KeyError, psycopg2.errors.UniqueViolation):

trends/importer/league.py
@@ -174,15 +174,17 @@ def import_transfers(c, t):
                AND np2.id < np1.id""")
     c.execute("INSERT INTO name (name) SELECT name FROM new_player ON CONFLICT DO NOTHING;")
     c.execute(
-        """INSERT INTO player (steamid64, nameid, avatarhash, eu_playerid) SELECT
-               steamid64,
-               (SELECT nameid FROM name WHERE name = new_player.name),
-               avatarhash,
-               eu_playerid
-           FROM new_player
-           ON CONFLICT (steamid64)
-           DO UPDATE SET
-               eu_playerid = coalesce(EXCLUDED.eu_playerid, player.eu_playerid);""")
+        """WITH player AS (INSERT INTO player (steamid64, nameid, avatarhash, eu_playerid) SELECT
+                   steamid64,
+                   (SELECT nameid FROM name WHERE name = new_player.name),
+                   avatarhash,
+                   eu_playerid
+               FROM new_player
+               ON CONFLICT (steamid64)
+               DO UPDATE SET
+                   eu_playerid = coalesce(EXCLUDED.eu_playerid, player.eu_playerid)
+               RETURNING steamid64
+           ) INSERT INTO cache_purge_player SELECT steamid64 FROM player;""")
     c.execute("DROP TABLE new_player;")
 
     # OK, here's the dance:

trends/importer/link_matches.py
@@ -4,6 +4,8 @@
 from datetime import datetime, timedelta
 import logging
 
+from ..cache import purge_players
+
 def create_link_matches_parser(sub):
     link = sub.add_parser("link_matches", help="Link logs and matches")
     link.set_defaults(importer=link_matches)
@@ -90,5 +92,12 @@ def link_matches(args, c, mc):
                            team1_is_red = log_matches.team1_is_red
                        FROM log_matches
                        WHERE log.logid = log_matches.logid;""")
+        cur.execute("""INSERT INTO cache_purge_player (steamid64)
+                       SELECT steamid64
+                       FROM log_matches
+                       JOIN player_stats USING (logid)
+                       JOIN player USING (playerid)
+                       GROUP BY steamid64;""")
         cur.execute("COMMIT;")
         logging.info(f"Linked {count} logs")
+        purge_players(c, mc)

trends/importer/logs.py
@@ -10,6 +10,7 @@ import psycopg2
 import sentry_sdk
 import systemd_watchdog
 
+from ..cache import purge_players
 from .fetch import ListFetcher, BulkFetcher, FileFetcher, ReverseFetcher, CloneLogsFetcher
 from ..steamid import SteamID
 from ..sql import disable_tracing, delete_logs, log_tables, publicize, table_columns
@@ -747,6 +748,13 @@ def update_ks(cur):
                    WHERE pse.logid = new.logid
                        AND pse.playerid = new.playerid;""")
 
+def prepare_purge(cur):
+    cur.execute("""INSERT INTO cache_purge_player (steamid64)
+                   SELECT steamid64
+                   FROM player_stats_backing
+                   JOIN player USING (playerid)
+                   GROUP BY steamid64;""")
+
 def create_logs_parser(sub):
     class LogAction(argparse.Action):
         def __init__(self, option_strings, dest, **kwargs):
@@ -801,9 +809,9 @@ def create_logs_parser(sub):
 
 def import_logs_cli(args, c, mc):
     with sentry_sdk.start_transaction(op="import", name="logs"):
-        return import_logs(c, args.fetcher(**vars(args)), args.update_only)
+        return import_logs(c, mc, args.fetcher(**vars(args)), args.update_only)
 
-def import_logs(c, fetcher, update_only):
+def import_logs(c, mc, fetcher, update_only):
     cur = c.cursor()
     wd = systemd_watchdog.watchdog()
 
@@ -852,8 +860,10 @@ def import_logs(c, fetcher, update_only):
             update_acc(cur)
             update_ks(cur)
             publicize(c, log_tables)
+            prepare_purge(cur)
             cur.execute("COMMIT;")
             logging.info("Committed %s imported log(s)...", count)
+        purge_players(c, mc)
 
     count = 0
     start = datetime.now()

trends/schema.sql
@@ -942,3 +942,9 @@ INSERT INTO materialized_view (oid) VALUES
 	('medic_cube'::REGCLASS),
 	('map_popularity'::REGCLASS)
 ON CONFLICT DO NOTHING;
+
+CREATE TABLE IF NOT EXISTS cache_purge_player (
+	steamid64 BIGINT NOT NULL
+);
+
+CREATE INDEX IF NOT EXISTS cache_purge_player_pkey ON cache_purge_player (steamid64);

commit 2898dfa694dcfac05bb35f2bc2e15f5d63761c21

importer: player: Fix ambiguous column
We need to be a bit more specific.

Fixes: c48c718 ("importer: Purge player overview cache when updating players")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/importer/players.py
@@ -92,7 +92,7 @@ def import_players(args, c, mc):
                                   avatarhash = player_update.avatarhash
                                FROM player_update
                                WHERE player_update.steamid64::BIGINT = player.steamid64
-                               RETURNING steamid64
+                               RETURNING player.steamid64
                            ) INSERT INTO cache_purge_player (steamid64)
                            SELECT steamid64
                            FROM player;""")