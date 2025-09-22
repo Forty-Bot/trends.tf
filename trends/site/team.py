Forty-Bot/trends.tf

commit 2ab934eb4e15e2cb1029b0ebb2cdc338b5c078a0

site: Cache comps and teams
Cache competition and teams summary information. Add the purging
infrastructure for these objects as well.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/team.py
@@ -3,9 +3,10 @@
 
 import flask
 
+from .. import cache
 from .common import get_players
-from .util import get_db, get_filter_params, get_filter_clauses, get_order, get_pagination, \
-                  last_modified
+from .util import get_db, get_filter_params, get_filter_clauses, get_mc, get_order, \
+                  get_pagination, last_modified
 
 team = flask.Blueprint('team', __name__)
 
@@ -13,8 +14,8 @@ team = flask.Blueprint('team', __name__)
 def get_teamid(endpoint, values):
     flask.g.teamid = values['teamid']
 
-@team.before_request
-def get_comp():
+@cache.mutable("team_{}_{}")
+def _get_team(mc, league, teamid):
     db = get_db()
     cur = db.cursor()
     cur.execute(
@@ -32,20 +33,11 @@ def get_comp():
            FROM team_comp_backing
            WHERE league = %(league)s AND rgl_teamid = %(teamid)s
            UNION ALL
-           SELECT NULL, NULL, NULL, NULL, NULL;""",
-        { 'league': flask.g.league, 'teamid': flask.g.teamid })
+           SELECT NULL, NULL, NULL, NULL, NULL;""", { 'league': league, 'teamid': teamid })
 
-    flask.g.team = cur.fetchone()
-    if flask.g.team['teamid'] is None:
-        flask.abort(404)
-    elif flask.g.team['teamid'] != flask.g.teamid:
-        args = flask.request.args | flask.request.view_args
-        args['teamid'] = flask.g.team['teamid']
-        return flask.redirect(flask.url_for(flask.request.endpoint, **args), 301)
-
-    # FIXME: not quite accurate when we display logs
-    if resp := last_modified(flask.g.team['fetched']):
-        return resp
+    team = cur.fetchone()
+    if team['teamid'] is None:
+        return None, None, None
 
     cur.execute(
         """SELECT
@@ -60,8 +52,21 @@ def get_comp():
                    total(rounds_lost) AS rounds_lost
                FROM match_wlt
                WHERE league = %s AND teamid = %s
-           ) AS wlt;""", (flask.g.league, flask.g.teamid))
-    flask.g.team_wlt = cur.fetchone()
+           ) AS wlt;""", (league, teamid))
+    return team, cur.fetchone(), cache.version(mc)
+
+@team.before_request
+def get_team():
+    flask.g.team, flask.g.team_wlt, etag = _get_team(get_mc(), flask.g.league, flask.g.teamid)
+    if flask.g.team['teamid'] is None:
+        flask.abort(404)
+    if flask.g.team['teamid'] != flask.g.teamid:
+        args = flask.request.args | flask.request.view_args
+        args['teamid'] = flask.g.team['teamid']
+        return flask.redirect(flask.url_for(flask.request.endpoint, **args), 301)
+
+    if resp := last_modified(None, etag):
+        return resp
 
 def get_matches(league, teamid, filters, order_clause="scheduled DESC", limit=100, offset=0):
     filter_clauses = get_filter_clauses(filters, comp='competition.name', time='scheduled')

trends/cache.py
@@ -216,7 +216,8 @@ for prefix in ('logs', 'players'):
     key = f"{prefix}_version"
     vars()[key] = immutable(key, expire=0)(version)
 
-matches_version = immutable("matches_{}_version", expire=0)(version)
+for prefix in ('comps', 'teams', 'matches'):
+    vars()[f"{prefix}_version"] = immutable(f"{prefix}_{{}}_version", expire=0)(version)
 
 def purge(c, mc, col, table, prefix, cond="TRUE"):
     with (sentry_sdk.start_span(op='db.transaction', description="purge"), c.cursor() as cur):
@@ -249,6 +250,14 @@ def purge_logs(c, mc):
     _update_version(mc, 'logs')
     mc.delete("index")
     purge(c, mc, 'logid', 'cache_purge_log', "log_")
+
+def purge_comps(c, mc, league):
+    _update_version(mc, f"comps_{league}")
+    purge(c, mc, 'compid', 'cache_purge_comp', f"comp_{league}_", f"league = '{league}'")
+
+def purge_teams(c, mc, league):
+    _update_version(mc, f"teams_{league}")
+    purge(c, mc, 'teamid', 'cache_purge_team', f"team_{league}_", f"league = '{league}'")
 
 def purge_matches(c, mc, league):
     _update_version(mc, f"matches_{league}")

trends/importer/etf2l.py
@@ -11,7 +11,7 @@ import time
 from psycopg2.extras import NumericRange
 import sentry_sdk
 
-from ..cache import purge_matches, purge_players
+from ..cache import purge_comps, purge_matches, purge_players, purge_teams
 from .fetch import ETF2LFileFetcher, ETF2LBulkFetcher, FetchError
 from .league import *
 from ..sql import db_connect
@@ -202,6 +202,8 @@ def import_etf2l(c, mc, fetcher, filter=filter_matchids):
                     import_team(cur, team)
                 import_match(cur, res)
                 cur.execute("COMMIT;")
+            purge_comps(c, mc, 'etf2l')
+            purge_teams(c, mc, 'etf2l')
             purge_matches(c, mc, 'etf2l')
             purge_players(c, mc)
         except FetchError:

trends/importer/league.py
@@ -37,6 +37,8 @@ def import_compdiv(c, cd):
                (SELECT div_nameid FROM div_name WHERE division = %(division)s), %(tier)s
            ) ON CONFLICT DO NOTHING;""", cd)
 
+    c.execute("INSERT INTO cache_purge_comp (league, compid) VALUES (%(league)s, %(compid)s);", cd)
+
 def combine_teams(c, league, teamids):
     """Combine teams
     Combine the teams specified into one team. This may occasionally be necessary when RGL teams are
@@ -151,6 +153,8 @@ def import_team(c, t):
 
     if 'updates' in t and t['updates']:
         import_transfers(c, t)
+
+    c.execute("INSERT INTO cache_purge_team (league, teamid) VALUES (%(league)s, %(teamid)s);", t)
 
 def import_transfers(c, t):
     # Load new players...
@@ -372,3 +376,6 @@ def import_match(c, m):
     c.execute(
         """INSERT INTO cache_purge_match (league, matchid)
            VALUES (%(league)s, %(matchid)s);""", m)
+    c.execute("INSERT INTO cache_purge_comp (league, compid) VALUES (%(league)s, %(compid)s);", m)
+    c.execute("""INSERT INTO cache_purge_team (league, teamid)
+                 VALUES (%(league)s, %(teamid1)s), (%(league)s, %(teamid2)s);""", m)

trends/importer/link_matches.py
@@ -4,7 +4,7 @@
 from datetime import datetime, timedelta
 import logging
 
-from ..cache import purge_logs, purge_matches, purge_players
+from ..cache import purge_comps, purge_logs, purge_matches, purge_players, purge_teams
 from ..util import League
 
 def create_link_matches_parser(sub):
@@ -94,6 +94,21 @@ def link_matches(args, c, mc):
                        FROM log_matches
                        WHERE log.logid = log_matches.logid;""")
         cur.execute("INSERT INTO cache_purge_log (logid) SELECT logid FROM log_matches;")
+        cur.execute("""INSERT INTO cache_purge_comp (league, compid)
+                       SELECT league, compid
+                       FROM log_matches
+                       JOIN match USING (league, matchid)
+                       GROUP BY league, compid;""")
+        cur.execute("""INSERT INTO cache_purge_team (league, teamid)
+                       SELECT league, teamid1
+                       FROM log_matches
+                       JOIN match USING (league, matchid)
+                       GROUP BY league, teamid1
+                       UNION ALL
+                       SELECT league, teamid2
+                       FROM log_matches
+                       JOIN match USING (league, matchid)
+                       GROUP BY league, teamid2;""")
         cur.execute("""INSERT INTO cache_purge_match (league, matchid)
                        SELECT league, matchid
                        FROM log_matches;""")

trends/importer/rgl.py
@@ -12,7 +12,7 @@ import time
 from psycopg2.extras import NumericRange
 import sentry_sdk
 
-from ..cache import purge_matches, purge_players
+from ..cache import purge_comps, purge_matches, purge_players, purge_teams
 from .fetch import FetchError, RGLBulkFetcher, RGLFileFetcher
 from .league import *
 from ..sql import db_connect
@@ -239,6 +239,8 @@ def import_rgl(c, mc, fetcher, filter=filter_matchids):
                 res['score1'] = res['teams'][0]['score']
                 res['score2'] = res['teams'][1]['score']
                 import_match(cur, res)
+                purge_comps(c, mc, 'rgl')
+                purge_teams(c, mc, 'rgl')
                 purge_matches(c, mc, 'rgl')
                 purge_players(c, mc)
                 cur.execute("COMMIT;")

trends/schema.sql
@@ -955,6 +955,20 @@ CREATE TABLE IF NOT EXISTS cache_purge_log (
 
 CREATE INDEX IF NOT EXISTS cache_purge_log_pkey ON cache_purge_log (logid);
 
+CREATE TABLE IF NOT EXISTS cache_purge_comp (
+	league LEAGUE NOT NULL,
+	compid INT NOT NULL
+);
+
+CREATE INDEX IF NOT EXISTS cache_purge_comp_pkey ON cache_purge_comp (league, compid);
+
+CREATE TABLE IF NOT EXISTS cache_purge_team (
+	league LEAGUE NOT NULL,
+	teamid INT NOT NULL
+);
+
+CREATE INDEX IF NOT EXISTS cache_purge_team_pkey ON cache_purge_team (league, teamid);
+
 CREATE TABLE IF NOT EXISTS cache_purge_match (
 	league LEAGUE NOT NULL,
 	matchid INT NOT NULL

trends/site/comp.py
@@ -3,8 +3,10 @@
 
 import flask
 
+from .. import cache
 from .common import get_matches, get_players
-from .util import get_db, get_filter_params, get_filter_clauses, get_order, get_pagination
+from .util import get_db, get_filter_params, get_filter_clauses, get_mc, get_order, \
+                  get_pagination, last_modified \
 
 comp = flask.Blueprint('comp', __name__)
 
@@ -12,28 +14,36 @@ comp = flask.Blueprint('comp', __name__)
 def get_compid(endpoint, values):
     flask.g.compid = values['compid']
 
-@comp.before_request
-def get_comp():
+@cache.mutable("comp_{}_{}")
+def _get_comp(mc, league, compid):
     db = get_db()
-    comp = db.cursor()
-    comp.execute(
+    cur = db.cursor()
+    cur.execute(
         """SELECT
                name,
                format
            FROM competition
            JOIN format USING (formatid)
-           WHERE league = %s AND compid = %s;""", (flask.g.league, flask.g.compid))
-    flask.g.comp = comp.fetchone()
-    if flask.g.comp is None:
-        flask.abort(404)
+           WHERE league = %s AND compid = %s;""", (league, compid))
+    comp = cur.fetchone()
+    if comp is None:
+        return None, None, None
 
-    divs = db.cursor()
-    divs.execute(
+    cur.execute(
         """SELECT json_object_agg(divid, division ORDER BY tier ASC, division DESC)
            FROM division
            JOIN div_name USING (div_nameid)
-           WHERE league=%s AND compid=%s;""", (flask.g.league, flask.g.compid))
-    flask.g.divs = divs.fetchone()[0] or {}
+           WHERE league=%s AND compid=%s;""", (league, compid))
+    return comp, cur.fetchone()[0] or {}, cache.version(mc)
+
+@comp.before_request
+def get_comp():
+    flask.g.comp, flask.g.divs, etag = _get_comp(get_mc(), flask.g.league, flask.g.compid)
+    if flask.g.comp is None:
+        flask.abort(404)
+
+    if resp := last_modified(None, etag):
+        return resp
 
 @comp.route('/')
 def overview(league, compid):

commit e26408ae56109797f6123321ededeae3cb35c744

site: team: Fix 404 for missing teams
If a team is missing then the cache will contain None, None, None. Check for this properly.

Fixes: 2ab934e ("site: Cache comps and teams")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/site/team.py
@@ -58,7 +58,7 @@ def _get_team(mc, league, teamid):
 @team.before_request
 def get_team():
     flask.g.team, flask.g.team_wlt, etag = _get_team(get_mc(), flask.g.league, flask.g.teamid)
-    if flask.g.team['teamid'] is None:
+    if flask.g.team is None:
         flask.abort(404)
     if flask.g.team['teamid'] != flask.g.teamid:
         args = flask.request.args | flask.request.view_args