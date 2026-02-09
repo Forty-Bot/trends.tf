Forty-Bot/trends.tf

commit dc3a261a8aa46211d337bb176bde71bf9b59b197

importer: Provide memcached client to importers
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/importer/weapons.py
@@ -199,7 +199,7 @@ def create_weapons_parser(sub):
                                 "master/tf/scripts/items/items_game.txt",
                         help="Location to fetch items_game.txt from")
 
-def import_remote(args, c):
+def import_remote(args, c, mc):
     s = requests.Session()
     resp = s.head(args.url)
     resp.raise_for_status()

salt/backend.sls
@@ -14,7 +14,8 @@
         [Service]
         Type=notify
         EnvironmentFile=/etc/default/trends
-        ExecStart={{ prefix }}/bin/trends_importer -vv logs bulk -c 200 postgres:///trends
+        ExecStart={{ prefix }}/bin/trends_importer -vv logs bulk -c 200 postgres:///trends \
+                  127.0.0.1:11211
         User=daemon
         WatchdogSec=60
 
@@ -39,7 +40,8 @@
         [Service]
         Type=simple
         EnvironmentFile=/etc/default/trends
-        ExecStart={{ prefix }}/bin/trends_importer players -k ${STEAMKEY} -w 1 random postgres:///trends
+        ExecStart={{ prefix }}/bin/trends_importer players -k ${STEAMKEY} -w 1 random \
+                  postgres:///trends 127.0.0.1:11211
         User=daemon
         Restart=on-failure
 
@@ -54,7 +56,7 @@
 
         [Service]
         Type=oneshot
-        ExecStart={{ prefix }}/bin/trends_importer -v refresh postgres:///trends
+        ExecStart={{ prefix }}/bin/trends_importer -v refresh postgres:///trends 127.0.0.1:11211
         User=daemon
 
 /etc/systemd/system/view_refresh.timer:
@@ -78,7 +80,8 @@
         [Service]
         Type=oneshot
         EnvironmentFile=/etc/default/trends
-        ExecStart={{ prefix }}/bin/trends_importer -vv weapons remote postgres:///trends
+        ExecStart={{ prefix }}/bin/trends_importer -vv weapons remote postgres:///trends \
+                  127.0.0.1:11211
         User=daemon
 
 /etc/systemd/system/weapon_import.timer:
@@ -102,7 +105,8 @@
         [Service]
         Type=oneshot
         EnvironmentFile=/etc/default/trends
-        ExecStart={{ prefix }}/bin/trends_importer -vv demos bulk -N postgres:///trends
+        ExecStart={{ prefix }}/bin/trends_importer -vv demos bulk -N postgres:///trends \
+                  127.0.0.1:11211
         User=daemon
 
 /etc/systemd/system/demo_import.timer:
@@ -126,7 +130,8 @@
         [Service]
         Type=oneshot
         EnvironmentFile=/etc/default/trends
-        ExecStart={{ prefix }}/bin/trends_importer -vv etf2l bulk -N postgres:///trends
+        ExecStart={{ prefix }}/bin/trends_importer -vv etf2l bulk -N postgres:///trends \
+                  127.0.0.1:11211
         User=daemon
 
 /etc/systemd/system/etf2l_import.timer:
@@ -150,7 +155,8 @@
         [Service]
         Type=oneshot
         EnvironmentFile=/etc/default/trends
-        ExecStart={{ prefix }}/bin/trends_importer -vv rgl bulk -N postgres:///trends
+        ExecStart={{ prefix }}/bin/trends_importer -vv rgl bulk -N postgres:///trends \
+                  127.0.0.1:11211
         User=daemon
 
 /etc/systemd/system/rgl_import.timer:
@@ -174,7 +180,8 @@
         [Service]
         Type=oneshot
         EnvironmentFile=/etc/default/trends
-        ExecStart={{ prefix }}/bin/trends_importer -vv link_demos postgres:///trends
+        ExecStart={{ prefix }}/bin/trends_importer -vv link_demos postgres:///trends \
+                  127.0.0.1:11211
         User=daemon
 
 /etc/systemd/system/link.timer:
@@ -198,7 +205,8 @@
         [Service]
         Type=oneshot
         EnvironmentFile=/etc/default/trends
-        ExecStart={{ prefix }}/bin/trends_importer -vv link_matches postgres:///trends
+        ExecStart={{ prefix }}/bin/trends_importer -vv link_matches postgres:///trends \
+                  127.0.0.1:11211
         User=daemon
 
 /etc/systemd/system/link_matches.timer:

test/conftest.py
@@ -30,7 +30,7 @@ def database(request):
 
         with caplog_session(request) as caplog:
             with caplog.at_level(logging.ERROR):
-                create_test_db(database.url())
+                create_test_db(database.url(), None)
             if caplog.records:
                 pytest.fail("Error creating test database")
 

test/create.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import os
 import sys
 
+from trends.cache import mc_connect
 import trends.importer.demos
 import trends.importer.logs
 import trends.importer.etf2l
@@ -30,10 +31,11 @@ class RGLSecondFetcher(RGLFileFetcher):
     def get_matchids(self):
         yield 4664
 
-def create_test_db(url):
+def create_test_db(url, memcached):
     # We use separate connections for importing because we use temporary tables which will alias
     # other queries.
     with db_connect(url) as c:
+        mc = mc_connect(memcached)
         cur = c.cursor()
         db_schema(cur)
         db_init(c)
@@ -110,16 +112,16 @@ def create_test_db(url):
         cur.execute("ANALYZE;")
         class args:
             since = datetime.fromtimestamp(0)
-        trends.importer.link_demos.link_logs(args, c)
-        trends.importer.link_matches.link_matches(args, c)
+        trends.importer.link_demos.link_logs(args, c, mc)
+        trends.importer.link_matches.link_matches(args, c, mc)
         cur.execute("ANALYZE;")
         # A second time to test partitioning log_json
         db_init(c)
-        trends.importer.refresh.refresh(None, c)
+        trends.importer.refresh.refresh(None, c, mc)
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print(f"Usage: {sys.argv[0]} <database url>", file=sys.stderr)
+    if len(sys.argv) < 2 or len(sys.argv) > 3:
+        print(f"Usage: {sys.argv[0]} <database url> [<memcached urls>]", file=sys.stderr)
         sys.exit(1)
 
-    create_test_db(sys.argv[1])
+    create_test_db(sys.argv[1], sys.argv[2] if len(sys.argv) == 3 else "")

trends/cache.py
@@ -88,3 +88,8 @@ class TracingClient(pylibmc.Client):
     def cas(self, key, value, cas, time=0):
         with cache_span('cache.set', 'cas', key):
             return super().cas(key, value, cas, time)
+
+def mc_connect(servers):
+    if servers:
+        return TracingClient(servers.split(','), binary=True, behaviors={ 'cas': True })
+    return NoopClient()

trends/importer/cli.py
@@ -7,6 +7,7 @@ import sys
 
 import psycopg2
 
+from ..cache import mc_connect
 from ..sql import db_connect, db_init
 from ..util import sentry_init
 from .demos import create_demos_parser
@@ -33,6 +34,8 @@ def create_parser():
     create_weapons_parser(sub)
     parser.add_argument("database", default="postgresql:///trends", metavar="DATABASE",
                         help="Database URL to connect to")
+    parser.add_argument("memcached", default="127.0.0.1:11211", metavar="MEMCACHED",
+                        help="A comma-separated list of memcached servers to connect to")
     parser.add_argument("-v", "--verbose", action='count', default=0, dest='verbosity',
                         help=("Print additional debug information. May be specified multiple "
                               "times for increased verbosity."))
@@ -65,4 +68,7 @@ def main():
         with c.cursor() as cur:
             cur.execute("ROLLBACK;")
 
-    args.importer(args, c)
+    mc = mc_connect(args.memcached)
+    mc.get("connection_test")
+
+    args.importer(args, c, mc)

trends/importer/demos.py
@@ -94,7 +94,7 @@ def create_demos_parser(sub):
     l.add_argument("-i", "--id", action='append', type=int, metavar="DEMOID",
                    dest='demoids', help="Fetch demo DEMOID")
 
-def import_demos_cli(args, c):
+def import_demos_cli(args, c, mc):
     with sentry_sdk.start_transaction(op="import", name="demos"):
         if 'new' in args and args.new:
             with c.cursor() as cur:

trends/importer/etf2l.py
@@ -146,7 +146,7 @@ def create_etf2l_parser(sub):
     b.add_argument("-p", "--page", type=int, default=1,
                    help="Start at a particular page")
 
-def import_etf2l_cli(args, c):
+def import_etf2l_cli(args, c, mc):
     with sentry_sdk.start_transaction(op="import", name="etf2l_matches"):
         if 'new' in args and args.new:
             with c.cursor() as cur:

commit 8611d20e932abf99f594f49775abc46eead969d5

importer: weapons: Fix local importer
This importer is missing a memcached argument.

Fixes: dc3a261 ("importer: Provide memcached client to importers")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

trends/importer/weapons.py
@@ -217,5 +217,5 @@ def import_remote(args, c, mc):
         import_weapons(c, resp.text)
         etag.write(resp.headers['etag'])
 
-def import_local(args, c):
+def import_local(args, c, mc):
     import_weapons(c, args.items.read())